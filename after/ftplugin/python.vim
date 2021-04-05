5a25dd4400ca4590ef1d2562700b2c8022f6fda2
razak17 <moabdulrazak17@gmail.com>
2021-04-05 22:00:49 +0000

    change python code run function back to old

--- after/ftplugin/python.vim
+++ after/ftplugin/python.vim
@@ -3,4 +3,4 @@ setlocal shiftwidth=4
 setlocal softtabstop=4
 setlocal tabstop=4
 
-noremap <F10> :lua require 'internal.quickrun'.RunPython()<CR>
+noremap <F10> :call RunPython()<CR>
--- lua/internal/quickrun.lua
+++ lua/internal/quickrun.lua
@@ -1,20 +1,16 @@
 local config = {}
 local api = vim.api
 
-function config.RunPython()
-    api.nvim_command('exec "! python %"')
-end
-
 function config.RunTS()
-    api.nvim_command('exec "! ts-node %"')
+  api.nvim_command('exec "! ts-node %"')
 end
 
 function config.RunJS()
-    api.nvim_command('exec "! node %"')
+  api.nvim_command('exec "! node %"')
 end
 
 function config.RunEX()
-    api.nvim_command('exec "! elixir %"')
+  api.nvim_command('exec "! elixir %"')
 end
 
 return config
