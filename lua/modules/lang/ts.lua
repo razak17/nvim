eb88a341acc6f18371cbddf056baad965074f827
razak17 <moabdulrazak17@gmail.com>
2021-04-05 23:52:27 +0000

    fix treesitters parsers code repitition

--- lua/modules/lang/ts.lua
+++ lua/modules/lang/ts.lua
@@ -2,58 +2,73 @@ local api = vim.api
 local M = {}
 
 local fts = {
-    "c", "cpp", "css", "erlang", "graphql", "go", "haskell", "html",
-    "javascript", "jsdoc", "julia", "json", "lua", "python", "rust", "sh",
-    "toml", "tsx", "typescript", "yaml"
-}
-
-local parsers = {
-    "bash", "c", "cpp", "css", "erlang", "graphql", "go", "haskell", "html",
-    "javascript", "jsdoc", "julia", "json", "lua", "python", "rust", "toml",
-    "tsx", "typescript", "yaml"
+  "c",
+  "cpp",
+  "css",
+  "erlang",
+  "graphql",
+  "go",
+  "haskell",
+  "html",
+  "javascript",
+  "jsdoc",
+  "julia",
+  "json",
+  "lua",
+  "python",
+  "rust",
+  "sh",
+  "toml",
+  "tsx",
+  "typescript",
+  "yaml"
 }
 
 local synoff = function()
-    local filetypes = vim.fn.join(fts, ",")
-    vim.cmd("au FileType " .. filetypes .. " set syn=off")
-    vim.cmd("au FileType " .. filetypes ..
-                " lua require'modules.lang.utils'.matchit()")
+  local filetypes = vim.fn.join(fts, ",")
+  vim.cmd("au FileType " .. filetypes .. " set syn=off")
+  vim.cmd("au FileType " .. filetypes ..
+              " lua require'modules.lang.utils'.matchit()")
+end
+
+local parsers = function()
+  table.remove(fts, 16)
+  table.insert(fts, 'bash')
 end
 
 function M.setup()
-    synoff()
-    require'nvim-treesitter.configs'.setup {
-        ensure_installed = parsers,
-        highlight = {enable = true},
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<leader>Tn",
-                scope_incremental = "<leader>Tm",
-                -- init_selection = "gnn",
-                node_incremental = "grn",
-                -- scope_incremental = "grc",
-                node_decremental = "grm"
-            }
-        },
-        indent = {enable = true},
-        textobjects = {
-            select = {
-                enable = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner"
-                }
-            }
+  synoff()
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = parsers(),
+    highlight = {enable = true},
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<leader>Tn",
+        scope_incremental = "<leader>Tm",
+        -- init_selection = "gnn",
+        node_incremental = "grn",
+        -- scope_incremental = "grc",
+        node_decremental = "grm"
+      }
+    },
+    indent = {enable = true},
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner"
         }
+      }
     }
+  }
 
-    vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+  vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
-    api.nvim_set_keymap('n', 'R', ':write | edit | TSBufEnable highlight<CR>',
-                        {});
+  api.nvim_set_keymap('n', 'R', ':write | edit | TSBufEnable highlight<CR>', {});
 end
 
 return M
