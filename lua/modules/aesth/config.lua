93d84151298e6ee46b979bfd7abeafd745be886f
razak17 <moabdulrazak17@gmail.com>
2021-04-24 15:43:48 +0000

    (upd): move git_signs to own directory.

--- lua/modules/aesth/config.lua
+++ lua/modules/aesth/config.lua
@@ -13,40 +13,7 @@ function config.dashboard()
 end
 
 function config.gitsigns()
-  require('gitsigns').setup {
-    signs = {
-      add = {hl = 'GitGutterAdd', text = '▋'},
-      change = {hl = 'GitGutterChange', text = '▋'},
-      delete = {hl = 'GitGutterDelete', text = '▋'},
-      topdelete = {hl = 'GitGutterDeleteChange', text = '▔'},
-      changedelete = {hl = 'GitGutterChange', text = '▎'}
-    },
-    keymaps = {
-      -- Default keymap options
-      noremap = true,
-      buffer = true,
-
-      ['n ]g'] = {
-        expr = true,
-        "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"
-      },
-      ['n [g'] = {
-        expr = true,
-        "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"
-      },
-
-      ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-      ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-      ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-      ['n <leader>he'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-      ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-      ['n <leader>ht'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-
-      -- Text objects
-      ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-      ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
-    }
-  }
+  require('modules.aesth.git_signs')
 end
 
 function config.nvim_tree()
---
+++ lua/modules/aesth/git_signs/init.lua
@@ -0,0 +1,34 @@
+require('gitsigns').setup {
+  signs = {
+    add = {hl = 'GitGutterAdd', text = '▋'},
+    change = {hl = 'GitGutterChange', text = '▋'},
+    delete = {hl = 'GitGutterDelete', text = '▋'},
+    topdelete = {hl = 'GitGutterDeleteChange', text = '▔'},
+    changedelete = {hl = 'GitGutterChange', text = '▎'}
+  },
+  keymaps = {
+    -- Default keymap options
+    noremap = true,
+    buffer = true,
+
+    ['n ]g'] = {
+      expr = true,
+      "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"
+    },
+    ['n [g'] = {
+      expr = true,
+      "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"
+    },
+
+    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <leader>he'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+    ['n <leader>ht'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
+
+    -- Text objects
+    ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+    ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+  }
+}
