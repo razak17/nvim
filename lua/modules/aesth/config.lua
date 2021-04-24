2bfd62e8f55652d558fbbd555a9888efa2ddcf49
razak17 <moabdulrazak17@gmail.com>
2021-04-24 15:45:48 +0000

    (upd): move nvim_tree to own directory.

--- lua/modules/aesth/config.lua
+++ lua/modules/aesth/config.lua
@@ -17,30 +17,7 @@ function config.gitsigns()
 end
 
 function config.nvim_tree()
-  vim.g.nvim_tree_side = 'right'
-  vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
-  vim.g.nvim_tree_quit_on_open = 0
-  vim.g.nvim_tree_hide_dotfiles = 0
-  vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_indent_markers = 1
-  vim.g.nvim_tree_indent_markers = 1
-  vim.g.nvim_tree_bindings = {
-    ["l"] = ":lua require'nvim-tree'.on_keypress('edit')<CR>",
-    ["h"] = ":lua require'nvim-tree'.on_keypress('close_node')<CR>",
-    ["s"] = ":lua require'nvim-tree'.on_keypress('vsplit')<CR>",
-    ["i"] = ":lua require'nvim-tree'.on_keypress('split')<CR>"
-  }
-  vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-      unstaged = "✗",
-      staged = "✓",
-      unmerged = "",
-      renamed = "➜",
-      untracked = "★"
-    }
-  }
+  require('modules.aesth.nvim_tree')
 end
 
 function config.bg()
