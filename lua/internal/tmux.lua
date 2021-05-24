dfd22ef5fd05cad045d3155b8a9113047d507fcf
razak17 <moabdulrazak17@gmail.com>
2021-05-24 01:22:07 +0000

    (upd): add the tmux file.

---
+++ lua/internal/tmux.lua
@@ -0,0 +1,25 @@
+local G = require 'core.global'
+local M = {}
+
+local fn = vim.fn
+local fmt = string.format
+
+-- Ref: https://github.com/akinsho/dotfiles/blob/main/.config/nvim/lua/as/tmux.lua
+function M.on_enter()
+  local session = fn.fnamemodify(vim.loop.cwd(), ":t") or "Neovim"
+  local window_title = session
+
+  local fname = fn.expand("%:t")
+  if G.is_empty(fname) then
+    return
+  end
+  window_title = fmt("%s", session)
+  fn.jobstart(fmt("tmux rename-window '%s'", window_title))
+end
+
+function M.on_leave()
+  fn.jobstart("tmux set-window-option automatic-rename on")
+end
+
+return M
+
