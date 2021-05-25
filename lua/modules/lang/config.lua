4bb5c155e2bfba2b13c860082f5299a7d2be08da
razak17 <moabdulrazak17@gmail.com>
2021-05-25 22:12:07 +0000

    (fix): aesth module check complete.

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -1,68 +1,7 @@
 local config = {}
 
 function config.nvim_treesitter()
-  -- require('modules.lang.ts')
-  local fts = {
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "graphql",
-    "jsdoc",
-    "json",
-    "yaml",
-    "go",
-    "c",
-    "cpp",
-    "rust",
-    "python",
-    "bash",
-    "lua"
-  }
-
-  local get_filetypes = function()
-    local parsers = require("nvim-treesitter.parsers")
-    local configs = parsers.get_parser_configs()
-    return vim.tbl_map(function(ft)
-      return configs[ft].filetype or ft
-    end, parsers.available_parsers())
-  end
-
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = fts,
-    highlight = {enable = true}
-    -- autotag = {enable = true},
-    -- autopairs = {enable = true},
-    -- rainbow = {
-    --   enable = true,
-    --   extended_mode = true,
-    --   disable = {"lua", "json"},
-    --   colors = {
-    --     "royalblue3",
-    --     "darkorange3",
-    --     "seagreen3",
-    --     "firebrick",
-    --     "darkorchid3"
-    --   }
-    -- },
-    -- matchup = {enable = true, disable = {"c", "python"}},
-  }
-
-  local fold =
-      "set foldmethod=expr foldtext=v:lua.folds() foldexpr=nvim_treesitter#foldexpr()"
-  -- -- TODO
-  -- -- local highlight = ":edit | TSBufEnable highlight<CR>"
-
-  local targets = get_filetypes()
-  local filetypes = table.concat(targets, ",")
-
-  -- -- vim.cmd [[edit | TSBufEnable highlight]]
-  vim.cmd(string.format("autocmd FileType %s %s", filetypes, fold))
-  -- -- vim.cmd(string.format("autocmd FileType %s %s", filetypes, highlight))
-
-  vim.api.nvim_set_keymap('n', 'R', ':write | edit | TSBufEnable highlight<CR>',
-                          {});
+  require('modules.lang.ts')
 end
 
 function config.nvim_lsp()
--- lua/modules/lang/ts/init.lua
+++ lua/modules/lang/ts/init.lua
@@ -1,20 +1,20 @@
 local fts = {
-  "html",
-  "css",
-  "javascript",
-  "typescript",
-  "tsx",
-  "graphql",
-  "jsdoc",
-  "json",
-  "yaml",
-  "go",
-  "c",
-  "cpp",
-  "lua",
-  "rust",
-  "python",
-  "bash"
+  -- "html",
+  -- "css",
+  -- "javascript",
+  -- "typescript",
+  -- "tsx",
+  -- "graphql",
+  -- "jsdoc",
+  -- "json",
+  -- "yaml",
+  -- "go",
+  -- "c",
+  -- "cpp",
+  -- "rust",
+  -- "python",
+  -- "bash",
+  "lua"
 }
 
 local get_filetypes = function()
@@ -28,37 +28,29 @@ end
 vim.cmd [[packadd nvim-treesitter]]
 require'nvim-treesitter.configs'.setup {
   highlight = {enable = true},
-  autotag = {enable = true},
-  autopairs = {enable = true},
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    disable = {"lua", "json"},
-    colors = {
-      "royalblue3",
-      "darkorange3",
-      "seagreen3",
-      "firebrick",
-      "darkorchid3"
-    }
-  },
-  matchup = {enable = true, disable = {"c", "python"}},
+  -- autotag = {enable = true},
+  -- autopairs = {enable = true},
+  -- rainbow = {
+  --   enable = true,
+  --   extended_mode = true,
+  --   disable = {"lua", "json"},
+  --   colors = {
+  --     "royalblue3",
+  --     "darkorange3",
+  --     "seagreen3",
+  --     "firebrick",
+  --     "darkorchid3"
+  --   }
+  -- },
+  -- matchup = {enable = true, disable = {"c", "python"}},
   ensure_installed = fts
 }
 
 local fold =
     "set foldmethod=expr foldtext=v:lua.folds() foldexpr=nvim_treesitter#foldexpr()"
-local syn_off = "set syntax=off"
--- TODO
--- local highlight = ":edit | TSBufEnable highlight<CR>"
-
 local targets = get_filetypes()
 local filetypes = table.concat(targets, ",")
-
--- vim.cmd [[edit | TSBufEnable highlight]]
-vim.cmd(string.format("autocmd FileType %s %s", filetypes, syn_off))
 vim.cmd(string.format("autocmd FileType %s %s", filetypes, fold))
--- vim.cmd(string.format("autocmd FileType %s %s", filetypes, highlight))
 
 vim.api.nvim_set_keymap('n', 'R', ':write | edit | TSBufEnable highlight<CR>',
                         {});
