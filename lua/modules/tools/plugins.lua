f512c203534873b6d0ec5b45efb24c8f67e6dec0
razak17 <moabdulrazak17@gmail.com>
2021-06-06 01:23:56 +0000

    (upd): add .lua-format.

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -3,28 +3,20 @@ local conf = require('modules.tools.config')
 
 tools['mbbill/undotree'] = {cmd = "UndotreeToggle"}
 
-tools['voldikss/vim-floaterm'] = {event = "VimEnter", config = conf.floaterm}
-
+-- tools['tpope/vim-fugitive'] = {event = 'VimEnter'}
+--
 tools['tweekmonster/startuptime.vim'] = {cmd = "StartupTime"}
 
--- tools['tpope/vim-fugitive'] = {event = 'VimEnter'}
+tools['voldikss/vim-floaterm'] = {event = "VimEnter", config = conf.floaterm}
 
 tools['MattesGroeger/vim-bookmarks'] = {
   event = {'BufReadPre', 'BufNewFile'},
   config = conf.bookmarks
 }
 
--- tools['glacambre/firenvim'] = {
---   run = function()
---     vim.fn['firenvim#install'](0)
---   end
--- }
-
 tools['iamcco/markdown-preview.nvim'] = {
   ft = 'markdown',
-  config = function()
-    vim.g.mkdp_auto_start = 0
-  end
+  config = function() vim.g.mkdp_auto_start = 0 end
 }
 
 tools['brooth/far.vim'] = {
