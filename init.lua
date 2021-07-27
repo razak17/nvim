56e38a2563634f500065257f25b61abc09df47fd
razak17 <moabdulrazak17@gmail.com>
2021-07-27 12:31:07 +0000

    (upd): add files.

---
+++ init.lua
@@ -0,0 +1,10 @@
+local fn, cmd = vim.fn, vim.cmd
+
+if fn.filereadable(fn.fnamemodify("~/.vimrc.local", ":p")) > 0 then
+  cmd [[source ~/.vimrc.local]]
+end
+
+-- Load Modules:
+require 'globals'
+require 'defaults'
+require 'core'
