f2acf7bf0fb0b5ca82031a145292018afbd6d857
razak17 <moabdulrazak17@gmail.com>
2021-07-27 22:01:23 +0000

    updates

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
--- Load Modules:
-require 'globals'
-require 'defaults'
-require 'core'
+local fn, cmd = vim.fn, vim.cmd
+
+if fn.filereadable(fn.fnamemodify("~/.vimrc.local", ":p")) > 0 then
+  cmd [[source ~/.vimrc.local]]
+end
+
+-- Load Modules
+require "globals"
+require "defaults"
+require "core"
