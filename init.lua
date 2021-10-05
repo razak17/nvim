0cf975fef905292587d934b02bf6497c4ab459ac
razak17 <moabdulrazak17@gmail.com>
2021-10-05 02:09:42 +0000

    updates

--- init.lua
+++ init.lua
@@ -1,10 +1,31 @@
 local fn, cmd = vim.fn, vim.cmd
 
+vim.cmd [[syntax off]]
+vim.cmd [[filetype off]]
+
+-- Load base config
 if fn.filereadable(fn.fnamemodify("~/.vimrc.local", ":p")) > 0 then
   cmd [[source ~/.vimrc.local]]
 end
 
+-- Bootstrap
+local home_dir = vim.loop.os_homedir()
+
+vim.opt.rtp:append(home_dir .. "/.local/share/rvim/site")
+
+vim.opt.rtp:remove(home_dir .. "/.config/nvim")
+vim.opt.rtp:remove(home_dir .. "/.config/nvim/after")
+vim.opt.rtp:append(home_dir .. "/.config/rvim")
+vim.opt.rtp:append(home_dir .. "/.config/rvim/after")
+
+vim.opt.rtp:remove(home_dir .. "/.local/share/nvim/site")
+vim.opt.rtp:remove(home_dir .. "/.local/share/nvim/site/after")
+vim.opt.rtp:append(home_dir .. "/.local/share/rvim/site")
+vim.opt.rtp:append(home_dir .. "/.local/share/rvim/site/after")
+
+vim.cmd [[let &packpath = &runtimepath]]
+
 -- Load Modules
-require "globals"
-require "defaults"
+-- require "globals"
+-- require "defaults"
 require "core"
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,6 +1,6 @@
 local function load_core()
-  require "core.globals"
-  require "core.config"
+  require "globals"
+  require "defaults"
   require "core.opts"
   -- require "core.highlights"
 end
