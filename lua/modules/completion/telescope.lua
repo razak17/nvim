a26d1899f0232fdc96c0ef57eb813211d8b17def
razak17 <moabdulrazak17@gmail.com>
2021-10-28 20:53:22 +0000

    chore(telescope): refactor telescope config.

--- lua/modules/completion/telescope.lua
+++ lua/modules/completion/telescope.lua
@@ -1,60 +1,87 @@
 return function()
-  rvim.telescope = {
-    prompt_prefix = " ❯ ",
-    layout_config = { height = 0.9, width = 0.9 },
-    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-  }
-
-  local status_ok, actions = rvim.safe_require "telescope.actions"
-  if not status_ok then
+  local telescope_ok, telescope = pcall(require, "telescope")
+  if not telescope_ok then
     return
   end
 
-  require("telescope").setup {
-    defaults = {
-      prompt_prefix = rvim.telescope.prompt_prefix,
-      selection_caret = " ",
-      sorting_strategy = "ascending",
-      file_ignore_patterns = {
-        "yarn.lock",
-        "target/*",
-        "node_modules/*",
-        "dist/*",
-        ".git/*",
-        "venv/*",
-        ".venv/*",
-        "__pycache__/*",
-      },
-      layout_strategy = "horizontal",
-      layout_config = rvim.telescope.layout_config,
-      borderchars = rvim.telescope.borderchars,
-      file_sorter = require("telescope.sorters").get_fzy_sorter,
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      mappings = {
-        i = {
-          ["<c-c>"] = function()
-            vim.cmd "stopinsert!"
-          end,
-          ["<esc>"] = actions.close,
-          ["<c-s>"] = actions.select_horizontal,
-          ["<C-e>"] = actions.send_to_qflist,
+  local previewers = require "telescope.previewers"
+  local sorters = require "telescope.sorters"
+  local actions = require "telescope.actions"
+
+  rvim.telescope = {
+    setup = {
+      defaults = {
+        prompt_prefix = " ❯ ",
+        selection_caret = " ",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+        file_browser = { hidden = true },
+        layout_config = {
+          height = 0.9,
+          width = 0.9,
+          preview_cutoff = 120,
+          horizontal = { mirror = false },
+          vertical = { mirror = false },
         },
-      },
-      file_browser = { hidden = true },
-      extensions = {
-        fzy_native = {
-          override_generic_sorter = false,
-          override_file_sorter = true,
+        file_ignore_patterns = {
+          "yarn.lock",
+          "target/*",
+          "node_modules/*",
+          "dist/*",
+          ".git/*",
+          "venv/*",
+          ".venv/*",
+          "__pycache__/*",
         },
-        media_files = {
-          filetypes = { "png", "webp", "jpg", "jpeg" },
-          find_cmd = "rg",
+        mappings = {
+          i = {
+            ["<c-c>"] = function()
+              vim.cmd "stopinsert!"
+            end,
+            ["<C-n>"] = actions.move_selection_next,
+            ["<C-b>"] = actions.move_selection_previous,
+            ["<esc>"] = actions.close,
+            ["<C-'>"] = actions.cycle_history_next,
+            ["<C-k>"] = actions.cycle_history_prev,
+            ["<C-e>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            ["<c-s>"] = actions.select_horizontal,
+            ["<CR>"] = actions.select_default + actions.center,
+          },
+          n = {
+            ["<C-n>"] = actions.move_selection_next,
+            ["<C-b>"] = actions.move_selection_previous,
+            ["<C-e>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          },
         },
+        pickers = {
+          find_files = {
+            find_command = { "fd", "--type=file", "--hidden", "--smart-case" },
+          },
+          live_grep = {
+            --@usage don't include the filename in the search results
+            only_sort_text = true,
+          },
+        },
+        extensions = {
+          fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+          },
+          media_files = {
+            filetypes = { "png", "webp", "jpg", "jpeg" },
+            find_cmd = "rg",
+          },
+        },
+        file_sorter = sorters.get_fzy_sorter,
+        file_previewer = previewers.vim_buffer_cat.new,
+        grep_previewer = previewers.vim_buffer_vimgrep.new,
+        qflist_previewer = previewers.vim_buffer_qflist.new,
       },
     },
   }
+
+  telescope.setup(rvim.telescope.setup)
 
   if rvim.plugin_loaded "telescope-fzy-native.nvim" then
     vim.cmd [[packadd telescope-fzy-native.nvim]]
@@ -79,14 +106,12 @@ return function()
   local extensions = {
     "grep_string_prompt",
     "bg_selector",
+    -- TODO: Fix not a git directory error for the two below.
     "nvim_files",
     "dotfiles",
   }
 
-  local function load_extensions(exps)
-    for _, ext in ipairs(exps) do
-      require("telescope").load_extension(ext)
-    end
+  for _, ext in ipairs(extensions) do
+    require("telescope").load_extension(ext)
   end
-  load_extensions(extensions)
 end
