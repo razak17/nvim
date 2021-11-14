e1278fca33970a9c51552b09f75bf2386f329fcb
razak17 <moabdulrazak17@gmail.com>
2021-11-14 17:01:48 +0000

    patch(lsp): disable showing active providers on statusline (for now).

--- lua/modules/aesth/statusline.lua
+++ lua/modules/aesth/statusline.lua
@@ -172,38 +172,40 @@ return function()
   local function get_attached_provider_name(msg)
     msg = msg or "LSP Inactive"
 
-    local buf_clients = vim.lsp.buf_get_clients()
-    if next(buf_clients) == nil then
-      -- TODO: clean up this if statement
-      if type(msg) == "boolean" or #msg == 0 then
-        return "LS Inactive"
-      end
-      return msg
-    end
+    -- local buf_clients = vim.lsp.buf_get_clients()
+    -- if next(buf_clients) == nil then
+    --   -- TODO: clean up this if statement
+    --   if type(msg) == "boolean" or #msg == 0 then
+    --     return "LS Inactive"
+    --   end
+    --   return msg
+    -- end
 
-    local buf_ft = vim.bo.filetype
-    local buf_client_names = {}
-    for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" then
-        table.insert(buf_client_names, client.name)
-      end
-    end
+    -- local buf_ft = vim.bo.filetype
+    -- local buf_client_names = {}
+    -- for _, client in pairs(buf_clients) do
+    --   if client.name ~= "null-ls" then
+    --     table.insert(buf_client_names, client.name)
+    --   end
+    -- end
 
-    -- add formatter
-    local formatters = require "lsp.null-ls.formatters"
-    local supported_formatters = formatters.list_registered_providers(buf_ft)
-    vim.list_extend(buf_client_names, supported_formatters)
+    -- -- add formatter
+    -- local formatters = require "lsp.null-ls.formatters"
+    -- local supported_formatters = formatters.list_registered_providers(buf_ft)
+    -- vim.list_extend(buf_client_names, supported_formatters)
 
-    -- add linter
-    local linters = require "lsp.null-ls.linters"
-    local supported_linters = linters.list_registered_providers(buf_ft)
-    vim.list_extend(buf_client_names, supported_linters)
+    -- -- add linter
+    -- local linters = require "lsp.null-ls.linters"
+    -- local supported_linters = linters.list_registered_providers(buf_ft)
+    -- vim.list_extend(buf_client_names, supported_linters)
 
-    -- local null_ls = require "lsp.null-ls"
-    -- local null_ls_providers = null_ls.list_supported_provider_names(vim.bo.filetype)
-    -- vim.list_extend(buf_client_names, null_ls_providers)
+    -- -- local null_ls = require "lsp.null-ls"
+    -- -- local null_ls_providers = null_ls.list_supported_provider_names(vim.bo.filetype)
+    -- -- vim.list_extend(buf_client_names, null_ls_providers)
 
-    return table.concat(buf_client_names, " • ")
+    -- return table.concat(buf_client_names, " • ")
+
+    return msg
   end
 
   -- Right side
