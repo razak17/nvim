811dc50177e28941578b39e446257bc66e684c9a
razak17 <moabdulrazak17@gmail.com>
2021-11-18 22:49:40 +0000

    feat(telescope): custom finder for nvim files changelog.

--- lua/telescope/_extensions/nvim_files.lua
+++ lua/telescope/_extensions/nvim_files.lua
@@ -1,16 +1,14 @@
 local _, builtin = pcall(require, "telescope.builtin")
+local _, finders = pcall(require, "telescope.finders")
+local _, pickers = pcall(require, "telescope.pickers")
+local _, sorters = pcall(require, "telescope.sorters")
 local _, themes = pcall(require, "telescope.themes")
+local _, actions = pcall(require, "telescope.actions")
+local _, previewers = pcall(require, "telescope.previewers")
+local _, make_entry = pcall(require, "telescope.make_entry")
 
 local nvim_config = {}
 local cwd_git = "~/.dots/rvim"
-
--- nvim_config.git_files = function()
---   builtin.git_files { prompt_title = "Git files for nvim config", cwd = cwd_git }
--- end
-
--- nvim_config.files = function()
---   builtin.find_files { prompt_title = "VimRC", cwd = get_config_dir() }
--- end
 
 nvim_config.files = function(opts)
   opts = opts or {}
@@ -52,6 +50,42 @@ nvim_config.grep_files = function(opts)
   }
   opts = vim.tbl_deep_extend("force", theme_opts, opts)
   builtin.live_grep(opts)
+end
+
+nvim_config.view_changelog = function()
+  local opts = { cwd = get_config_dir() }
+  opts.entry_maker = make_entry.gen_from_git_commits(opts)
+
+  pickers.new(opts, {
+    prompt_title = "LunarVim changelog",
+
+    finder = finders.new_oneshot_job(
+      vim.tbl_flatten {
+        "git",
+        "log",
+        "--pretty=oneline",
+        "--abbrev-commit",
+      },
+      opts
+    ),
+    previewer = {
+      previewers.git_commit_diff_to_parent.new(opts),
+      previewers.git_commit_diff_to_head.new(opts),
+      previewers.git_commit_diff_as_was.new(opts),
+      previewers.git_commit_message.new(opts),
+    },
+
+    --TODO: consider opening a diff view when pressing enter
+    attach_mappings = function(_, map)
+      map("i", "<enter>", actions._close)
+      map("n", "<enter>", actions._close)
+      map("i", "<esc>", actions._close)
+      map("n", "<esc>", actions._close)
+      map("n", "q", actions._close)
+      return true
+    end,
+    sorter = sorters.generic_sorter,
+  }):find()
 end
 
 nvim_config.commits = function()
