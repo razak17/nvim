c2b51ff30c9c057f559e2542773563a89ce5cf43
razak17 <moabdulrazak17@gmail.com>
2021-11-18 22:48:17 +0000

    feat(telescope): custom finder for nvim files.

--- lua/telescope/_extensions/nvim_files.lua
+++ lua/telescope/_extensions/nvim_files.lua
@@ -1,13 +1,36 @@
-local builtin = require "telescope.builtin"
+local _, builtin = pcall(require, "telescope.builtin")
+local _, themes = pcall(require, "telescope.themes")
+
 local nvim_config = {}
 local cwd_git = "~/.dots/rvim"
 
-nvim_config.git_files = function()
-  builtin.git_files { prompt_title = "Git files for nvim config", cwd = cwd_git }
-end
+-- nvim_config.git_files = function()
+--   builtin.git_files { prompt_title = "Git files for nvim config", cwd = cwd_git }
+-- end
 
-nvim_config.files = function()
-  builtin.find_files { prompt_title = "VimRC", cwd = get_config_dir() }
+-- nvim_config.files = function()
+--   builtin.find_files { prompt_title = "VimRC", cwd = get_config_dir() }
+-- end
+
+nvim_config.files = function(opts)
+  opts = opts or {}
+  local theme_opts = themes.get_ivy {
+    sorting_strategy = "ascending",
+    layout_strategy = "bottom_pane",
+    prompt_prefix = " ❯ ",
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+    layout_config = {
+      height = 0.9,
+      width = 0.9,
+      preview_cutoff = 120,
+      horizontal = { mirror = false },
+      vertical = { mirror = false },
+    },
+    prompt_title = "~ Rvim files ~",
+    cwd = get_config_dir(),
+  }
+  opts = vim.tbl_deep_extend("force", theme_opts, opts)
+  builtin.find_files(opts)
 end
 
 nvim_config.commits = function()
