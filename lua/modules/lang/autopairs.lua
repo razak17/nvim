ad048a959f26bb8b794be98d99e94b4c99c29d65
razak17 <moabdulrazak17@gmail.com>
2021-12-09 20:06:46 +0000

    fix(autopairs): add missing configuration entries

--- lua/modules/lang/autopairs.lua
+++ lua/modules/lang/autopairs.lua
@@ -5,18 +5,44 @@ return function()
       on_config_done = nil,
       ---@usage auto insert after select function or method item
       map_complete = true,
-      ---@usage  -- modifies the function or method delimiter by filetypes
+      ---@usage  modifies the function or method delimiter by filetypes
       map_char = {
         all = "(",
         tex = "{",
       },
+      ---@usage check bracket in same line
       enable_check_bracket_line = false,
       ---@usage check treesitter
       check_ts = true,
       ts_config = {
-        lua = { "string" },
-        javascript = { "template_string" },
         java = false,
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+      },
+      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+      enable_moveright = true,
+      ---@usage disable when recording or executing a macro
+      disable_in_macro = false,
+      ---@usage add bracket pairs after quote
+      enable_afterquote = true,
+      ---@usage map the <BS> key
+      map_bs = true,
+      ---@usage map <c-w> to delete a pair if possible
+      map_c_w = false,
+      ---@usage disable when insert after visual block mode
+      disable_in_visualblock = false,
+      ---@usage  change default fast_wrap
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0, -- Offset from pattern match
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
       },
     },
   }
@@ -28,6 +54,15 @@ return function()
     check_ts = rvim.autopairs.setup.check_ts,
     enable_check_bracket_line = rvim.autopairs.setup.enable_check_bracket_line,
     ts_config = rvim.autopairs.setup.ts_config,
+    disable_filetype = rvim.autopairs.setup.disable_filetype,
+    disable_in_macro = rvim.autopairs.setup.disable_in_macro,
+    ignored_next_char = rvim.autopairs.setup.ignored_next_char,
+    enable_moveright = rvim.autopairs.setup.enable_moveright,
+    enable_afterquote = rvim.autopairs.setup.enable_afterquote,
+    map_c_w = rvim.autopairs.setup.map_c_w,
+    map_bs = rvim.autopairs.setup.map_bs,
+    disable_in_visualblock = rvim.autopairs.setup.disable_in_visualblock,
+    fast_wrap = rvim.autopairs.setup.fast_wrap,
   }
 
   autopairs.add_rule(Rule("$$", "$$", "tex"))
