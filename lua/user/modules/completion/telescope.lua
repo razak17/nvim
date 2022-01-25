3c67e299babd4f30fb653f509e6758f2e231e8db
razak17 <moabdulrazak17@gmail.com>
2022-01-25 16:05:45 +0000

    refactor(telescope): b remaps back to p

--- lua/user/modules/completion/telescope.lua
+++ lua/user/modules/completion/telescope.lua
@@ -71,18 +71,18 @@ return function()
               vim.cmd "stopinsert!"
             end,
             ["<C-n>"] = actions.move_selection_next,
-            ["<C-b>"] = actions.move_selection_previous,
+            ["<C-p>"] = actions.move_selection_previous,
             ["<esc>"] = actions.close,
             ["<C-'>"] = actions.cycle_history_next,
             ["<C-k>"] = actions.cycle_history_prev,
-            ["<C-e>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
             ["<c-s>"] = actions.select_horizontal,
             ["<CR>"] = actions.select_default + actions.center,
           },
           n = {
             ["<C-n>"] = actions.move_selection_next,
-            ["<C-b>"] = actions.move_selection_previous,
-            ["<C-e>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            ["<C-p>"] = actions.move_selection_previous,
+            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
           },
         },
         extensions = {
