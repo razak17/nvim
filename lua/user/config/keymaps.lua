d32a49b68d3b2643d4fc0815b5c851d7b21ad74f
razak17 <moabdulrazak17@gmail.com>
2022-01-25 16:00:08 +0000

    refactor(ajk): remove all other j to n remaps

--- lua/user/config/keymaps.lua
+++ lua/user/config/keymaps.lua
@@ -170,7 +170,7 @@ nnoremap["<C-c>"] = "<Esc>"
 
 -- Better window movement
 nnoremap["<C-h>"] = "<C-w>h"
-nnoremap["<C-n>"] = "<C-w>j"
+nnoremap["<C-j>"] = "<C-w>j"
 nnoremap["<C-k>"] = "<C-w>k"
 nnoremap["<C-l>"] = "<C-w>l"
 
@@ -242,7 +242,7 @@ cnoremap["<S-Tab>"] = {
 }
 
 -- Use alt + hjkl to resize windows
-nnoremap["<M-n>"] = ":resize -2<CR>"
+nnoremap["<M-j>"] = ":resize -2<CR>"
 nnoremap["<M-k>"] = ":resize +2<CR>"
 nnoremap["<M-h>"] = ":vertical resize +2<CR>"
 nnoremap["<M-l>"] = ":vertical resize -2<CR>"
