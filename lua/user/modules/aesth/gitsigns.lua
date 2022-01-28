0df496951b22b6e05b859708412476a6e3aba9e9
razak17 <moabdulrazak17@gmail.com>
2022-01-28 15:14:27 +0000

    fix(gitsigns): watch_index is deprecated

--- lua/user/modules/aesth/gitsigns.lua
+++ lua/user/modules/aesth/gitsigns.lua
@@ -37,7 +37,10 @@ return function()
         ["o ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
         ["x ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
       },
-      watch_index = { interval = 1000 },
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
       sign_priority = 6,
       update_debounce = 200,
       status_formatter = nil, -- Use default
