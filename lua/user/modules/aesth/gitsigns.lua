7c1a3d77a467d841422f90b3544bf189e18aa6c6
razak17 <moabdulrazak17@gmail.com>
2022-01-28 11:50:12 +0000

    fix(gitsigns): config.keymaps is deprecated

--- lua/user/modules/aesth/gitsigns.lua
+++ lua/user/modules/aesth/gitsigns.lua
@@ -13,35 +13,44 @@ return function()
         topdelete = { hl = "GitGutterDeleteChange", text = "▔" },
         changedelete = { hl = "GitGutterChange", text = "▎" },
       },
-      keymaps = {
-        -- Default keymap options
-        noremap = true,
-        buffer = true,
-
-        ["n ]g"] = {
-          expr = true,
-          "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
-        },
-        ["n [g"] = {
-          expr = true,
-          "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
-        },
-        ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ["n <leader>he"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-        ["n <leader>ht"] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-
-        -- Text objects
-        ["o ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-        ["x ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-      },
       watch_index = { interval = 1000 },
       sign_priority = 6,
       update_debounce = 200,
       status_formatter = nil, -- Use default
       use_decoration_api = false,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+        map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+
+        -- Actions
+        map({ "n", "v" }, "<leader>hs", gs.stage_hunk)
+        map({ "n", "v" }, "<leader>hr", gs.reset_hunk)
+        map("n", "<leader>hS", gs.stage_buffer)
+        map("n", "<leader>hu", gs.undo_stage_hunk)
+        map("n", "<leader>hR", gs.reset_buffer)
+        map("n", "<leader>hp", gs.preview_hunk)
+        map("n", "<leader>hb", function()
+          gs.blame_line { full = true }
+        end)
+        map("n", "<leader>tb", gs.toggle_current_line_blame)
+        map("n", "<leader>hd", gs.diffthis)
+        map("n", "<leader>hD", function()
+          gs.diffthis "~"
+        end)
+        map("n", "<leader>td", gs.toggle_deleted)
+
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end,
     },
   }
 
