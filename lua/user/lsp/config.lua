4cd39ae728464c45830bbbf3532c2cbe65186f11
razak17 <moabdulrazak17@gmail.com>
2022-01-29 14:07:37 +0000

    chore(lsp): remove hardcoded kind_items in config

--- lua/user/lsp/config.lua
+++ lua/user/lsp/config.lua
@@ -1,33 +1,4 @@
 return {
-  completion = {
-    item_kind = {
-      "   (Text) ",
-      "   (Method)",
-      " ƒ  (Function)",
-      "   (Constructor)",
-      " ﴲ  (Field)",
-      "   (Variable)",
-      "   (Class)",
-      " ﰮ  (Interface)",
-      "   (Module)",
-      " 襁 (Property)",
-      "   (Unit)",
-      "   (Value)",
-      " 了 (Enum)",
-      "   (Keyword)",
-      "   (Snippet)",
-      "   (Color)",
-      "   (File)",
-      "   (Reference)",
-      "   (Folder)",
-      "   (EnumMember)",
-      "   (Constant)",
-      " ﳤ  (Struct)",
-      " 鬒 (Event)",
-      "   (Operator)",
-      "   (TypeParameter)",
-    },
-  },
   diagnostics = {
     signs = {
       active = true,
