f85c623ca94ed206a8e344c2b977280f20f050e3
razak17 <moabdulrazak17@gmail.com>
2022-02-02 21:06:14 +0000

    chore(buffer): remove no longer needed autoloads

--- plugin/buffers.lua
+++ plugin/buffers.lua
@@ -1,33 +1,3 @@
-function _G.DelThisBuffer()
-  vim.api.nvim_exec(
-    [[
-      update
-      "bprevious | split | bnext | bdelete
-      split | bdelete
-    ]],
-    false
-  )
-end
-
-function _G.DelToLeft()
-  vim.api.nvim_exec(
-    [[
-      silent execute 'bdelete' join(range(1, bufnr() - 1))
-    ]],
-    false
-  )
-end
-
-function _G.DelAllBuffers()
-  vim.api.nvim_exec(
-    [[
-      wall
-      silent execute 'bdelete ' . join(utils#buf_filt(0))
-    ]],
-    false
-  )
-end
-
 function _G.DelAllExceptCurrent()
   vim.api.nvim_exec(
     [[
