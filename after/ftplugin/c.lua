e99b2186d238b7c18d3c3fdd1d5ac462aa171589
razak17 <moabdulrazak17@gmail.com>
2022-02-12 18:05:28 +0000

    feat(abbrev): add simple abbreviation for c

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,3 +1,20 @@
 require("user.lsp.manager").setup "clangd"
 
 vim.cmd [[setlocal nospell]]
+vim.cmd [[iabbrev cc /*<CR><CR>/<Up>]]
+vim.cmd [[iabbrev #i #include]]
+vim.cmd [[iabbrev #d #define]]
+
+vim.cmd [[iabbrev start #include <stdio.h>
+                            \<CR>
+                            \#include <stdlib.h>
+                            \<CR>
+                            \#include <stdbool.h>
+                            \<CR>
+                            \<CR>
+                            \int main() {
+                            \<CR>
+                            \  printf("hello\n");
+                            \<CR>
+                            \  return 0;
+                            \<CR>]]
