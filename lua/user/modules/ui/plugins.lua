effd5b700e0402e99c57bbf52dc3eb3873692d88
razak17 <moabdulrazak17@gmail.com>
2022-02-23 18:27:43 +0000

    refactor(lsp): replace figdet with nvim-notify

--- lua/user/modules/ui/plugins.lua
+++ lua/user/modules/ui/plugins.lua
@@ -44,9 +44,33 @@ ui["lewis6991/gitsigns.nvim"] = {
   disable = not rvim.plugin.git_signs.active,
 }
 
-ui["j-hui/fidget.nvim"] = {
-  config = utils.load_conf("ui", "fidget"),
-  disable = not rvim.plugin.fidget.active,
+ui["rcarriga/nvim-notify"] = {
+  cond = utils.not_headless, -- TODO: causes blocking output in headless mode
+  config = function()
+    local notify = require "notify"
+    notify.setup {
+      stages = "fade_in_slide_out", -- fade
+      timeout = 3000,
+    }
+    ---Send a notification
+    --@param msg of the notification to show to the user
+    --@param level Optional log level
+    --@param opts Dictionary with optional options (timeout, etc)
+    vim.notify = function(msg, level, opts)
+      local l = vim.log.levels
+      opts = opts or {}
+      level = level or l.INFO
+      local levels = {
+        [l.DEBUG] = "Debug",
+        [l.INFO] = "Information",
+        [l.WARN] = "Warning",
+        [l.ERROR] = "Error",
+      }
+      opts.title = opts.title or type(level) == "string" and level or levels[level]
+      notify(msg, level, opts)
+    end
+  end,
+  disable = not rvim.plugin.nvim_notify.active,
 }
 
 return ui
