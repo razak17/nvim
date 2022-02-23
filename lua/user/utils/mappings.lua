7b01e09a4aa2741798bc66f7d78890090d2f0c3e
razak17 <moabdulrazak17@gmail.com>
2022-02-23 17:21:22 +0000

    refactor(mappings): simplify mappings wrapper

--- lua/user/utils/mappings.lua
+++ lua/user/utils/mappings.lua
@@ -10,17 +10,6 @@
 -- But frankly I like to be in control of my mappings and I honestly think that very soon this
 -- won't be needed rvim mappings are likely to become simpler once the native api is improved
 
-local fmt = string.format
-
----check if a mapping already exists
----@param lhs string
----@param mode string
----@return boolean
-function rvim.has_map(lhs, mode)
-  mode = mode or "n"
-  return vim.fn.maparg(lhs, mode) ~= ""
-end
-
 ---create a mapping function factory
 ---@param mode string
 ---@param o table
@@ -33,11 +22,8 @@ local function make_mapper(mode, o)
   ---@param rhs string|function
   ---@param opts table
   return function(lhs, rhs, opts)
-    assert(lhs ~= mode, fmt("The lhs should not be the same rvim mode for %s", lhs))
-    assert(type(rhs) == "string" or type(rhs) == "function", '"rhs" should be a function or string')
     -- If the label is all that was passed in, set the opts automagically
     opts = type(opts) == "string" and { label = opts } or opts and vim.deepcopy(opts) or {}
-
     if opts.label then
       local ok, wk = rvim.safe_require("which-key", { silent = true })
       if ok then
@@ -45,14 +31,13 @@ local function make_mapper(mode, o)
       end
       opts.label = nil
     end
-
     opts = vim.tbl_extend("keep", opts, parent_opts)
     vim.keymap.set(mode, lhs, rhs, opts)
   end
 end
 
-local map_opts = { noremap = false, silent = true }
-local noremap_opts = { noremap = true, silent = true }
+local map_opts = { remap = true, silent = true }
+local noremap_opts = { silent = true }
 
 -- A recursive commandline mapping
 rvim.nmap = make_mapper("n", map_opts)
@@ -85,18 +70,3 @@ rvim.tnoremap = make_mapper("t", noremap_opts)
 rvim.snoremap = make_mapper("s", noremap_opts)
 -- A non recursive commandline mapping
 rvim.cnoremap = make_mapper("c", { noremap = true, silent = false })
-
----Factory function to create multi mode map functions
----e.g. `rvim.map({"n", "s"}, lhs, rhs, opts)`
----@param target string
----@return fun(modes: string[], lhs: string, rhs: string, opts: table)
-local function multimap(target)
-  return function(modes, lhs, rhs, opts)
-    for _, m in ipairs(modes) do
-      rvim[m .. target](lhs, rhs, opts)
-    end
-  end
-end
-
-rvim.map = multimap "map"
-rvim.noremap = multimap "noremap"
