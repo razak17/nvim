c09103c78750b36646d9a64ba688b786e2f8dc41
razak17 <moabdulrazak17@gmail.com>
2022-02-23 18:36:07 +0000

    feat(lsp): add show message handler using notify

--- lua/user/core/opts.lua
+++ lua/user/core/opts.lua
@@ -200,7 +200,6 @@ M.load_commands = function()
     title = true,
     titlelen = 70,
     magic = true,
-    termguicolors = true,
     noerrorbells = true,
     t_Co = 256,
     shell = "/bin/zsh",
