4a5258f667ad106de1e96ed48ff110eb6ee651cd
razak17 <moabdulrazak17@gmail.com>
2022-02-24 19:59:50 +0000

    feat(filetype): add filetype.lua

---
+++ filetype.lua
@@ -0,0 +1,16 @@
+if not vim.filetype then
+  return
+end
+
+vim.g.do_filetype_lua = 1
+
+vim.filetype.add {
+  filename = {
+    [".gitignore"] = "conf",
+    ["Podfile"] = "ruby",
+  },
+  pattern = {
+    ["*.gradle"] = "groovy",
+    ["*.env.*"] = "env",
+  },
+}
