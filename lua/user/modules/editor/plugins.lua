4294e6932564af4f57e0ce71b4bac4b5e0911e90
razak17 <moabdulrazak17@gmail.com>
2022-02-28 23:17:02 +0000

    refactor(plugins): update dial config

--- lua/user/modules/editor/plugins.lua
+++ lua/user/modules/editor/plugins.lua
@@ -34,15 +34,41 @@ editor["moll/vim-bbye"] = {
 editor["monaqa/dial.nvim"] = {
   event = { "BufWinEnter" },
   config = function()
-    local nmap = rvim.nmap
-    local vmap = rvim.nmap
+    local dial = require "dial.map"
+    local augend = require "dial.augend"
+    local map = vim.keymap.set
+    map("n", "<C-a>", dial.inc_normal(), { remap = false })
+    map("n", "<C-x>", dial.dec_normal(), { remap = false })
+    map("v", "<C-a>", dial.inc_visual(), { remap = false })
+    map("v", "<C-x>", dial.dec_visual(), { remap = false })
+    map("v", "g<C-a>", dial.inc_gvisual(), { remap = false })
+    map("v", "g<C-x>", dial.dec_gvisual(), { remap = false })
 
-    nmap("<C-a>", "<Plug>(dial-increment)")
-    nmap("<C-x>", "<Plug>(dial-decrement)")
-    vmap("<C-a>", "<Plug>(dial-increment)")
-    vmap("<C-x>", "<Plug>(dial-decrement)")
-    vmap("g<C-a>", "<Plug>(dial-increment-additional)")
-    vmap("g<C-x>", "<Plug>(dial-decrement-additional)")
+    require("dial.config").augends:register_group {
+      -- default augends used when no group name is specified
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.bool,
+        augend.constant.new {
+          elements = { "&&", "||" },
+          word = false,
+          cyclic = true,
+        },
+      },
+      dep_files = {
+        augend.semver.alias.semver,
+      },
+    }
+
+    vim.api.nvim_create_autocmd {
+      pattern = { "yaml", "toml" },
+      event = "FileType",
+      callback = function()
+        map("n", "<C-a>", require("dial.map").inc_normal "dep_files", { remap = true })
+      end,
+    }
   end,
   disable = not rvim.plugin.dial.active,
 }
