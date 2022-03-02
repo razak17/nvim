345bca4685f8c85fa54d515fe81f4ed2d3b80de3
razak17 <moabdulrazak17@gmail.com>
2022-03-02 19:49:38 +0000

    fix(indentline): use setup config

--- lua/user/modules/ui/indent_blankline.lua
+++ lua/user/modules/ui/indent_blankline.lua
@@ -1,39 +1,49 @@
 return function()
-  vim.g.indent_blankline_char = "│"
-  vim.g.indent_blankline_show_first_indent_level = true
-  vim.g.indent_blankline_filetype_exclude = {
-    "dashboard",
-    "log",
-    "gitcommit",
-    "packer",
-    "markdown",
-    "json",
-    "txt",
-    "outline",
-    "help",
-    "NvimTree",
-    "git",
-    "TelescopePrompt",
-    "undotree",
-    "dap-repl",
-    "", -- for all buffers without a file type
-  }
-  vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-  vim.g.indent_blankline_show_trailing_blankline_indent = false
-  vim.g.indent_blankline_show_current_context = true
-  vim.g.show_current_context_start_on_current_line = false
-  vim.g.indent_blankline_context_patterns = {
-    "class",
-    "function",
-    "method",
-    "block",
-    "list_literal",
-    "selector",
-    "^if",
-    "^table",
-    "if_statement",
-    "while",
-    "for",
+  require("indent_blankline").setup {
+    char = "│", -- ┆ ┊ 
+    show_foldtext = false,
+    show_current_context = true,
+    show_current_context_start = true,
+    show_current_context_start_on_current_line = false,
+    show_first_indent_level = true,
+    filetype_exclude = {
+      "dap-repl",
+      "startify",
+      "dashboard",
+      "log",
+      "fugitive",
+      "gitcommit",
+      "packer",
+      "vimwiki",
+      "markdown",
+      "json",
+      "txt",
+      "vista",
+      "help",
+      "NvimTree",
+      "git",
+      "TelescopePrompt",
+      "undotree",
+      "flutterToolsOutline",
+      "norg",
+      "org",
+      "orgagenda",
+      "", -- for all buffers without a file type
+    },
+    buftype_exclude = { "terminal", "nofile" },
+    context_patterns = {
+      "class",
+      "function",
+      "method",
+      "block",
+      "list_literal",
+      "selector",
+      "^if",
+      "^table",
+      "if_statement",
+      "while",
+      "for",
+    },
   }
   -- because lazy load indent-blankline so need read this autocmd
   vim.cmd "autocmd CursorMoved * IndentBlanklineRefresh"
