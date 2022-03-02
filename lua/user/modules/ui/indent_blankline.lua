e2b0c2ea0ea5d882b1dac762e3151d2575416c2a
razak17 <moabdulrazak17@gmail.com>
2022-03-02 19:45:34 +0000

    fix(indentline): disable context start

--- lua/user/modules/ui/indent_blankline.lua
+++ lua/user/modules/ui/indent_blankline.lua
@@ -21,6 +21,7 @@ return function()
   vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
   vim.g.indent_blankline_show_trailing_blankline_indent = false
   vim.g.indent_blankline_show_current_context = true
+  vim.g.show_current_context_start_on_current_line = false
   vim.g.indent_blankline_context_patterns = {
     "class",
     "function",
