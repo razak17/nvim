84876ef067938e3b5bfd3156c666949a16a5baa2
razak17 <moabdulrazak17@gmail.com>
2022-03-08 21:31:54 +0000

    chore(bufferline): dog food tab mode

--- lua/user/modules/ui/bufferline.lua
+++ lua/user/modules/ui/bufferline.lua
@@ -80,6 +80,7 @@ return function()
   rvim.bufferline = {
     setup = {
       options = {
+        mode = "tabs",
         view = "default",
         close_icon = "",
         buffer_close_icon = "",
