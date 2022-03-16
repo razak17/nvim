e0806b4b16a87ad7eb8f5e3e62d5a2e8ec375583
razak17 <moabdulrazak17@gmail.com>
2022-03-16 16:10:49 +0000

    refactor(ftdetect): use filetype.lua instead

--- filetype.lua
+++ filetype.lua
@@ -5,11 +5,26 @@ end
 vim.g.do_filetype_lua = 1
 
 vim.filetype.add {
+  extension = {
+    lock = "yaml",
+  },
   filename = {
     [".gitignore"] = "conf",
+    ["Makefile.toml"] = "cargo-make",
     ["Podfile"] = "ruby",
+    [".vimrc.local"] = "vim",
   },
   pattern = {
+    ["*.graphql"] = "graphql",
+    ["*.graphqls"] = "graphql",
+    ["*.gql"] = "graphql",
+    ["haproxy*.c*"] = "haproxy",
+    ["*.hsc"] = "haskell",
+    ["*.bpk"] = "haskell",
+    ["*.hsig"] = "haskell",
+    ["*.tmux.conf"] = "tmux",
+    ["*.svelte"] = "svelte",
+    ["*.sol"] = "solidity",
     ["*.gradle"] = "groovy",
     ["*.env.*"] = "env",
   },
