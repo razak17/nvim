b735056e8dcca03ff4d417542b8290029d113840
razak17 <moabdulrazak17@gmail.com>
2022-03-19 19:11:18 +0000

    feat(hightlights): add win separator

--- lua/zephyr/overrides.lua
+++ lua/zephyr/overrides.lua
@@ -10,6 +10,8 @@ local function general_overrides()
   local darker_bg = util.alter_color(normal_bg, -10)
   local msg_area_bg = rvim.transparent_window and "NONE" or darker_bg
   util.all {
+    { "VertSplit", { background = "NONE", foreground = util.get_hl("NonText", "fg") } },
+    { "WinSeparator", { background = "NONE", foreground = util.get_hl("NonText", "fg") } },
     { "MsgArea", { background = msg_area_bg } },
     { "mkdLineBreak", { link = "NONE" } },
     -----------------------------------------------------------------------------//
@@ -82,6 +84,7 @@ local function set_sidebar_highlight()
     { "PanelHeading", { background = bg_color, bold = true } },
     { "PanelVertSplit", { foreground = split_color, background = bg_color } },
     { "PanelVertSplitAlt", { foreground = bg_color, background = bg_color } },
+    { "PanelWinSeparator", { foreground = split_color, background = bg_color } },
     { "PanelStNC", { background = st_color, cterm = { italic = true } } },
     { "PanelSt", { background = st_color } },
   }
