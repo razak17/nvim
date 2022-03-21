f992459dbb1799f5cb940418154b1451388d8017
razak17 <moabdulrazak17@gmail.com>
2022-03-21 22:04:40 +0000

    fix(nvim-tree): tree taking half the window on open

--- lua/user/modules/ui/nvimtree.lua
+++ lua/user/modules/ui/nvimtree.lua
@@ -78,7 +78,7 @@ return function()
         },
         open_file = {
           quit_on_open = false,
-          resize_window = false,
+          resize_window = true,
           window_picker = {
             enable = true,
             chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
