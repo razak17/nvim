67eb8d03c133c8de899bc20f9ade52e905f06273
razak17 <moabdulrazak17@gmail.com>
2022-03-28 17:39:12 +0000

    feat(lspconfig): override borders

---
+++ ftplugin/lspinfo.lua
@@ -0,0 +1,3 @@
+-- Override lspconfig's default border (or lack there of to use my own)
+-- @reference: https://github.com/neovim/nvim-lspconfig/issues/1717
+vim.api.nvim_win_set_config(0, { border = rvim.style.border.line })
