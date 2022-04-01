e3df6bac5dbaa464c16df94d4f235dd5da0912da
razak17 <moabdulrazak17@gmail.com>
2022-04-01 00:42:08 +0000

    patch(luasnip): load both user and friendly snippets
    calling the vscode lazy_load function twice (for now)

--- lua/user/modules/completion/init.lua
+++ lua/user/modules/completion/init.lua
@@ -19,66 +19,60 @@ completion["folke/which-key.nvim"] = {
 -- nvim-cmp
 completion["hrsh7th/nvim-cmp"] = {
   config = load_conf("completion", "cmp"),
-  requires = {
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-  },
   disable = not rvim.plugins.completion.cmp.active,
 }
 
+completion["L3MON4D3/LuaSnip"] = {
+  config = load_conf("completion", "luasnip"),
+  disable = not rvim.plugins.completion.luasnip.active,
+}
+
+completion["rafamadriz/friendly-snippets"] = {
+  disable = not rvim.plugins.completion.friendly_snippets.active,
+}
+
 completion["hrsh7th/cmp-nvim-lsp"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-nvim-lua"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-nvim-lsp-document-symbol"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["saadparwaiz1/cmp_luasnip"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-buffer"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-path"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-cmdline"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["f3fora/cmp-spell"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["hrsh7th/cmp-emoji"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["octaltree/cmp-look"] = {
-  after = "nvim-cmp",
   disable = not rvim.plugins.completion.cmp.active,
 }
 
 completion["petertriho/cmp-git"] = {
   opt = true,
-  -- after = "nvim-cmp",
   config = function()
     require("cmp_git").setup {
       filetypes = { "gitcommit", "NeogitCommitMessage" },
@@ -110,15 +104,6 @@ completion["github/copilot.vim"] = {
     require("zephyr.util").plugin("copilot", { "CopilotSuggestion", { link = "Comment" } })
   end,
   disable = not rvim.plugins.completion.copilot.active,
-}
-
-completion["L3MON4D3/LuaSnip"] = {
-  config = load_conf("completion", "luasnip"),
-  disable = not rvim.plugins.completion.luasnip.active,
-}
-
-completion["rafamadriz/friendly-snippets"] = {
-  disable = not rvim.plugins.completion.friendly_snippets.active,
 }
 
 return completion
