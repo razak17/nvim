1fb0aa3c5b1e105082665eeb0b47107a36c00787
razak17 <moabdulrazak17@gmail.com>
2022-04-06 04:27:54 +0000

    chore(plugins): add config files

---
+++ lua/user/modules/editor/marks.lua
@@ -0,0 +1,20 @@
+return function()
+  require("zephyr.util").plugin("marks", { MarkSignHL = { foreground = "Red" } })
+  require("which-key").register({
+    m = {
+      name = "marks",
+      b = { "<Cmd>MarksListBuf<CR>", "list buffer" },
+      g = { "<Cmd>MarksQFListGlobal<CR>", "list global" },
+      ["0"] = { "<Cmd>BookmarksQFList 0<CR>", "list bookmark" },
+    },
+  }, {
+    prefix = "<leader>",
+  })
+  require("marks").setup {
+    builtin_marks = { "." },
+    bookmark_0 = {
+      sign = "⚑",
+      virt_text = "bookmarks",
+    },
+  }
+end
---
+++ lua/user/modules/editor/vim-surround-funk.lua
@@ -0,0 +1,24 @@
+return function()
+  vim.g.surround_funk_create_mappings = 0
+  local map = vim.keymap.set
+  -- operator pending mode: grip surround
+  map({ "n", "v" }, "gs", "<Plug>(GripSurroundObject)")
+  map({ "n", "v" }, "gS", "<Plug>(GripSurroundObjectNoPaste)")
+  map({ "o", "x" }, "sF", "<Plug>(SelectWholeFUNCTION)")
+  require("which-key").register {
+    ["<leader>r"] = {
+      name = "+dsf: delete",
+      s = {
+        F = { "<Plug>(DeleteSurroundingFunction)", "delete surrounding function" },
+        f = { "<Plug>(DeleteSurroundingFUNCTION)", "delete surrounding outer function" },
+      },
+    },
+    ["<leader>C"] = {
+      name = "+dsf: change",
+      s = {
+        F = { "<Plug>(ChangeSurroundingFunction)", "change surrounding function" },
+        f = { "<Plug>(ChangeSurroundingFUNCTION)", "change outer surrounding function" },
+      },
+    },
+  }
+end
