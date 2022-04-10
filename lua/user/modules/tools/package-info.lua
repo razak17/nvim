2cce5b4ad4fa1862ecbe63bbb36c814cb9e18118
razak17 <moabdulrazak17@gmail.com>
2022-04-10 01:59:40 +0000

    chore(package-info.nvim): add config file

---
+++ lua/user/modules/tools/package-info.lua
@@ -0,0 +1,64 @@
+return function()
+  require("package-info").setup {
+    colors = {
+      up_to_date = "#3C4048", -- Text color for up to date package virtual text
+      outdated = "#d19a66", -- Text color for outdated package virtual text
+    },
+    icons = {
+      enable = true, -- Whether to display icons
+      style = {
+        up_to_date = "|  ", -- Icon for up to date packages
+        outdated = "|  ", -- Icon for outdated packages
+      },
+    },
+  }
+
+  -- Show package versions
+  rvim.nnoremap(
+    "<leader>ns",
+    ":lua require('package-info').show()<CR>",
+    { label = "package-info: show" }
+  )
+
+  -- Hide package versions
+  rvim.nnoremap(
+    "<leader>nc",
+    ":lua require('package-info').hide()<CR>",
+    { label = "package-info: hide" }
+  )
+
+  -- Update package on line
+  rvim.nnoremap(
+    "<leader>nu",
+    ":lua require('package-info').update()<CR>",
+    { label = "package-info: update" }
+  )
+
+  -- Delete package on line
+  rvim.nnoremap(
+    "<leader>nd",
+    ":lua require('package-info').delete()<CR>",
+    { label = "package-info: delete" }
+  )
+
+  -- Install a new package
+  rvim.nnoremap(
+    "<leader>ni",
+    ":lua require('package-info').install()<CR>",
+    { label = "package-info: install" }
+  )
+
+  -- Reinstall dependencies
+  rvim.nnoremap(
+    "<leader>nr",
+    ":lua require('package-info').reinstall()<CR>",
+    { label = "package-info: reinstall" }
+  )
+
+  -- Install a different package version
+  rvim.nnoremap(
+    "<leader>np",
+    ":lua require('package-info').change_version()<CR>",
+    { label = "package-info: change version" }
+  )
+end
