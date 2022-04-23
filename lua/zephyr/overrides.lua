a67f474c7e5271793b836f99c3f3c0db8ce00b10
Razak <moabdulrazak17@gmail.com>
2022-04-23 15:24:14 +0000

    feat(highlights): add custom TSNamespace highlight

--- lua/zephyr/overrides.lua
+++ lua/zephyr/overrides.lua
@@ -47,6 +47,7 @@ local function general_overrides()
     -----------------------------------------------------------------------------//
     -- Treesitter
     -----------------------------------------------------------------------------//
+    TSNamespace = { link = 'TypeBuiltin' },
     TSKeywordReturn = { italic = true, foreground = keyword_fg },
     -- TSError = { undercurl = true, sp = error_line, foreground = "NONE" } ,
     TSParameter = { italic = true, bold = true, foreground = "NONE" },
