6709af438c0029effb9cf0b65241df31f94c28cd
Razak <moabdulrazak17@gmail.com>
2022-05-03 20:39:57 +0000

    fix(lsp): format on focus lost

--- lua/user/lsp/formatting.lua
+++ lua/user/lsp/formatting.lua
@@ -2,30 +2,27 @@ local M = {}
 local Log = require "user.core.log"
 local fmt = string.format
 
+local format_on_save = rvim.util.format_on_save
+local format_on_focus_lost = rvim.util.format_on_focus_lost
+
 local function get_format(defaults)
-  -- accept a basic boolean `rvim.util.format_on_save=true`
-  if type(defaults) ~= "table" then
+  if type(defaults) == "table" then
     return defaults
   end
 
-  return {
-    pattern = rvim.util.format_on_save.pattern or defaults.pattern,
-    timeout = rvim.util.format_on_save.timeout or defaults.timeout,
-  }
+  if defaults == true then
+    return {
+      pattern = rvim.util.format_on_save.pattern,
+      timeout = rvim.util.format_on_save.timeout,
+    }
+  end
 end
 
-local get_format_on_save_opts = function(defaults)
-  return get_format(defaults)
-end
-
-local get_format_on_focus_lost_opts = function(defaults)
-  return get_format(defaults)
-end
-
-local function enable_format(name, opts)
-  rvim.augroup("FormatOnSave", {
+local function enable_format(name, defaults, event)
+  local opts = get_format(defaults)
+  rvim.augroup(name, {
     {
-      event = { "BufWritePre" },
+      event = { event },
       pattern = { opts.pattern },
       command = function()
         require("user.utils.lsp").format { timeout_ms = opts.timeout, filter = opts.filter }
@@ -35,29 +32,9 @@ local function enable_format(name, opts)
   Log:debug(fmt("enabled %s", name))
 end
 
-function M.enable_format_on_save()
-  local defaults = rvim.util.format_on_save
-  local opts = get_format_on_save_opts(defaults)
-  enable_format("FormatOnSave", opts)
-end
-
-function M.enable_format_on_focus_lost()
-  local defaults = rvim.util.format_on_focus_lost
-  local opts = get_format_on_focus_lost_opts(defaults)
-  enable_format("FormatOnFocusLost", opts)
-end
-
 local function disable_format(name)
   rvim.disable_augroup(name)
   Log:debug(fmt("disabled %s", name))
-end
-
-function M.disable_format_on_save()
-  disable_format "FormatOnSave"
-end
-
-function M.disable_format_on_focus_lost()
-  disable_format "FormatOnFocusLost"
 end
 
 ---@param cond table
@@ -70,46 +47,25 @@ local function configure_format(cond, name, event)
       Log:debug(fmt("reloading %s configuration", name))
     end
     if name == "FormatOnSave" then
-      M.enable_format_on_save()
+      enable_format("FormatOnSave", format_on_save, event)
     elseif name == "FormatOnFocusLost" then
-      M.enable_format_on_focus_lost()
+      enable_format("FormatOnFocusLost", format_on_focus_lost, event)
     end
   else
-    if name == "FormatOnSave" then
-      M.disable_format_on_save()
-    elseif name == "FormatOnFocusLost" then
-      M.disable_format_on_focus_lost()
-    end
+    disable_format(name)
   end
 end
 
 function M.configure_format_on_save()
   local name = "FormatOnSave"
-  local cond = rvim.util.format_on_save
-  configure_format(cond, name, "BufWritePre")
+  local event = "BufWritePre"
+  configure_format(format_on_save, name, event)
 end
 
 function M.configure_format_on_focus_lost()
-  -- local name = "FormatOnFocusLost"
-  -- local cond = rvim.util.format_on_focus_lost
-  -- configure_format(cond, name, "FocusLost")
-  if rvim.util.format_on_focus_lost then
-    if vim.fn.exists "#FormatOnFocusLost#FocusLost" == 1 then
-      rvim.disable_augroup "FormatOnFocusLost"
-      Log:debug "reloading FormatOnFocusLost configuration"
-    end
-    M.enable_format_on_focus_lost()
-  else
-    M.disable_format_on_focus_lost()
-  end
-end
-
-function M.toggle_format_on_save()
-  if vim.fn.exists "#format_on_save#BufWritePre" == 0 then
-    M.enable_format_on_save()
-  else
-    M.disable_format_on_save()
-  end
+  local name = "FormatOnFocusLost"
+  local event = "FocusLost"
+  configure_format(format_on_focus_lost, name, event)
 end
 
 return M
