968e4d15c509c0a1bb8ff2f5b78524f47e866735
Razak <moabdulrazak17@gmail.com>
2022-05-25 00:28:19 +0000

    feat(dressing): center ui.select for treesitter prompts

--- lua/user/modules/ui/init.lua
+++ lua/user/modules/ui/init.lua
@@ -66,29 +66,6 @@ ui["rcarriga/nvim-notify"] = {
 ui["stevearc/dressing.nvim"] = {
   event = "BufWinEnter",
   config = function()
-    require("zephyr.util").plugin("dressing", { FloatTitle = { inherit = "Visual", bold = true } })
-    require("dressing").setup({
-      input = {
-        winblend = 2,
-        insert_only = false,
-        border = rvim.style.border.line,
-      },
-      select = {
-        telescope = require("telescope.themes").get_cursor({
-          layout_config = {
-            -- NOTE: the limit is half the max lines because this is the cursor theme so
-            -- unless the cursor is at the top or bottom it realistically most often will
-            -- only have half the screen available
-            height = function(self, _, max_lines)
-              local results = #self.finder.results
-              local PADDING = 4 -- this represents the size of the telescope window
-              local LIMIT = math.floor(max_lines / 2)
-              return (results <= (LIMIT - PADDING) and results + PADDING or LIMIT)
-            end,
-          },
-        }),
-      },
-    })
   end,
   disable = not rvim.plugins.ui.dressing.active,
 }
