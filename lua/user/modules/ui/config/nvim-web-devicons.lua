03c124ea456cfa8f1475a23463b27422a0c61ca2
Razak <moabdulrazak17@gmail.com>
2022-06-04 15:53:05 +0000

    fix(nvim-web-devicons): wrap config in a function

--- lua/user/modules/ui/config/nvim-web-devicons.lua
+++ lua/user/modules/ui/config/nvim-web-devicons.lua
@@ -1,51 +1,53 @@
-local status_ok, nvim_web_devicons = rvim.safe_require("nvim-web-devicons")
-if not status_ok then
-  return
+return function()
+  local status_ok, nvim_web_devicons = rvim.safe_require("nvim-web-devicons")
+  if not status_ok then
+    return
+  end
+
+  local P = rvim.palette
+
+  nvim_web_devicons.set_icon({
+    sh = {
+      ico = "",
+      color = P.chartreuse,
+      cterm_color = "59",
+      name = "Sh",
+    },
+    [".gitattributes"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "59",
+      name = "GitAttributes",
+    },
+    [".gitconfig"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "59",
+      name = "GitConfig",
+    },
+    [".gitignore"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "59",
+      name = "GitIgnore",
+    },
+    [".gitlab-ci.yml"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "166",
+      name = "GitlabCI",
+    },
+    [".gitmodules"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "59",
+      name = "GitModules",
+    },
+    ["diff"] = {
+      icon = "",
+      color = P.dark_red,
+      cterm_color = "59",
+      name = "Diff",
+    },
+  })
 end
-
-local P = rvim.palette
-
-nvim_web_devicons.set_icon({
-  sh = {
-    icon = "",
-    color = P.chartreuse,
-    cterm_color = "59",
-    name = "Sh",
-  },
-  [".gitattributes"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "59",
-    name = "GitAttributes",
-  },
-  [".gitconfig"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "59",
-    name = "GitConfig",
-  },
-  [".gitignore"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "59",
-    name = "GitIgnore",
-  },
-  [".gitlab-ci.yml"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "166",
-    name = "GitlabCI",
-  },
-  [".gitmodules"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "59",
-    name = "GitModules",
-  },
-  ["diff"] = {
-    icon = "",
-    color = P.dark_red,
-    cterm_color = "59",
-    name = "Diff",
-  },
-})
