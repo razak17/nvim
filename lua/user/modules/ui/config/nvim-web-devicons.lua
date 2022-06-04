d34f21418b06ba83c7391d3a63d3ad04dde9a7fb
Razak <moabdulrazak17@gmail.com>
2022-06-04 15:26:54 +0000

    chore(plugins): add config file nvim-web-devicons

---
+++ lua/user/modules/ui/config/nvim-web-devicons.lua
@@ -0,0 +1,51 @@
+local status_ok, nvim_web_devicons = rvim.safe_require("nvim-web-devicons")
+if not status_ok then
+  return
+end
+
+local P = rvim.palette
+
+nvim_web_devicons.set_icon({
+  sh = {
+    icon = "",
+    color = P.chartreuse,
+    cterm_color = "59",
+    name = "Sh",
+  },
+  [".gitattributes"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "59",
+    name = "GitAttributes",
+  },
+  [".gitconfig"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "59",
+    name = "GitConfig",
+  },
+  [".gitignore"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "59",
+    name = "GitIgnore",
+  },
+  [".gitlab-ci.yml"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "166",
+    name = "GitlabCI",
+  },
+  [".gitmodules"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "59",
+    name = "GitModules",
+  },
+  ["diff"] = {
+    icon = "",
+    color = P.dark_red,
+    cterm_color = "59",
+    name = "Diff",
+  },
+})
