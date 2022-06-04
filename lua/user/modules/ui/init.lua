42b4c784a858654baa0b4e8959555feed800fd4b
Razak <moabdulrazak17@gmail.com>
2022-06-04 15:56:09 +0000

    chore(illuminate): add keymaps

--- lua/user/modules/ui/init.lua
+++ lua/user/modules/ui/init.lua
@@ -142,6 +142,8 @@ ui["EdenEast/nightfox.nvim"] = {
 ui["RRethy/vim-illuminate"] = {
   config = function()
     vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree", "dashboard", "neo-tree" }
+    rvim.nnoremap("<a-n>", ':lua require"illuminate".next_reference{wrap=true}<cr>')
+    rvim.nnoremap("<a-p>", ':lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>')
   end,
   disable = not rvim.plugins.ui.illuminate.active,
 }
