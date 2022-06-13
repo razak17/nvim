c2403bdc04d02e10917a0980fdbca014fd78b247
Razak <moabdulrazak17@gmail.com>
2022-06-13 01:14:38 +0000

    refactor(telescope): make ivy theme re-usable

--- lua/user/modules/tools/config/telescope/init.lua
+++ lua/user/modules/tools/config/telescope/init.lua
@@ -4,6 +4,8 @@ return function()
     return
   end
 
+  rvim.telescope = {}
+
   local previewers = require("telescope.previewers")
   local sorters = require("telescope.sorters")
   local actions = require("telescope.actions")
@@ -47,207 +49,209 @@ return function()
 
   ---@param opts table
   ---@return table
-  function rvim.telescope_dropdown(opts)
+  function rvim.telescope.telescope_dropdown(opts)
     return themes.get_dropdown(get_border(opts))
   end
 
-  local dropdown = rvim.telescope_dropdown
+  function rvim.telescope.ivy(opts)
+    return require("telescope.themes").get_ivy(vim.tbl_deep_extend("keep", opts or {}, {
+      borderchars = {
+        preview = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" },
+      },
+    }))
+  end
 
-  rvim.telescope = {
-    setup = {
-      defaults = {
-        prompt_prefix = " " .. icons.misc.chevron_right_alt .. " ",
-        selection_caret = " " .. icons.misc.double_chevron_right .. " ", --  ,
-        cycle_layout_list = { "flex", "horizontal", "vertical", "bottom_pane", "center" },
-        sorting_strategy = "ascending",
-        layout_strategy = "horizontal",
-        set_env = { ["TERM"] = vim.env.TERM },
-        borderchars = border.telescope.prompt,
-        file_browser = { hidden = true },
-        color_devicons = true,
-        dynamic_preview_title = true,
-        layout_config = {
-          height = 0.9,
-          width = 0.9,
-          preview_cutoff = 120,
-          horizontal = {
-            width_padding = 0.04,
-            height_padding = 0.1,
-            preview_width = 0.6,
-          },
-          vertical = {
-            width_padding = 0.05,
-            height_padding = 0.1,
-            preview_height = 0.5,
+  local dropdown = rvim.telescope.telescope_dropdown
+
+  rvim.telescope.setup = {
+    defaults = {
+      prompt_prefix = " " .. icons.misc.chevron_right_alt .. " ",
+      selection_caret = " " .. icons.misc.double_chevron_right .. " ", --  ,
+      cycle_layout_list = { "flex", "horizontal", "vertical", "bottom_pane", "center" },
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      set_env = { ["TERM"] = vim.env.TERM },
+      borderchars = border.telescope.prompt,
+      file_browser = { hidden = true },
+      color_devicons = true,
+      dynamic_preview_title = true,
+      layout_config = {
+        height = 0.9,
+        width = 0.9,
+        preview_cutoff = 120,
+        horizontal = {
+          width_padding = 0.04,
+          height_padding = 0.1,
+          preview_width = 0.6,
+        },
+        vertical = {
+          width_padding = 0.05,
+          height_padding = 0.1,
+          preview_height = 0.5,
+        },
+      },
+      winblend = 5,
+      history = {
+        path = rvim.get_cache_dir() .. "/telescope/history.sqlite3",
+      },
+      file_ignore_patterns = {
+        "%.jpg",
+        "%.jpeg",
+        "%.png",
+        "%.otf",
+        "%.ttf",
+        "%.DS_Store",
+        "%.lock",
+        "^.git/",
+        "^node_modules/",
+        "^site-packages/",
+      },
+      path_display = { "smart", "absolute", "truncate" },
+      file_sorter = sorters.get_fzy_sorter,
+      file_previewer = previewers.vim_buffer_cat.new,
+      grep_previewer = previewers.vim_buffer_vimgrep.new,
+      qflist_previewer = previewers.vim_buffer_qflist.new,
+      mappings = {
+        i = {
+          ["<C-w>"] = actions.send_selected_to_qflist,
+          ["<c-c>"] = function()
+            vim.cmd("stopinsert!")
+          end,
+          ["<esc>"] = actions.close,
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<c-s>"] = actions.select_horizontal,
+          ["<CR>"] = actions.select_default,
+          ["<c-e>"] = layout_actions.toggle_preview,
+          ["<c-l>"] = layout_actions.cycle_layout_next,
+          ["<C-A>"] = telescope_custom_actions.multi_selection_open,
+          ["<Tab>"] = actions.toggle_selection,
+        },
+        n = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<C-A>"] = telescope_custom_actions.multi_selection_open,
+        },
+      },
+      extensions = {
+        media_files = {
+          -- filetypes whitelist
+          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+          filetypes = { "png", "webp", "jpg", "jpeg" },
+          find_cmd = "rg", -- find command (defaults to `fd`)
+        },
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+        frecency = {
+          db_root = join_paths(rvim.get_cache_dir(), "telescope"),
+          show_scores = true,
+          show_unindexed = true,
+          ignore_patterns = { "*.git/*", "*/tmp/*" },
+          disable_devicons = false,
+          workspaces = {
+            ["conf"] = "/home/razak/.config",
+            ["rvim"] = "/home/razak/.config/rvim",
+            ["data"] = "/home/razak/.local/share",
+            ["project"] = "/home/razak/personal/workspace/coding",
+            ["notes"] = "/home/razak/notes/src",
           },
         },
-        winblend = 5,
-        history = {
-          path = rvim.get_cache_dir() .. "/telescope/history.sqlite3",
-        },
-        file_ignore_patterns = {
-          "%.jpg",
-          "%.jpeg",
-          "%.png",
-          "%.otf",
-          "%.ttf",
-          "%.DS_Store",
-          "%.lock",
-          "^.git/",
-          "^node_modules/",
-          "^site-packages/",
-        },
-        path_display = { "smart", "absolute", "truncate" },
-        file_sorter = sorters.get_fzy_sorter,
-        file_previewer = previewers.vim_buffer_cat.new,
-        grep_previewer = previewers.vim_buffer_vimgrep.new,
-        qflist_previewer = previewers.vim_buffer_qflist.new,
-        mappings = {
-          i = {
-            ["<C-w>"] = actions.send_selected_to_qflist,
-            ["<c-c>"] = function()
-              vim.cmd("stopinsert!")
-            end,
-            ["<esc>"] = actions.close,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-            ["<c-s>"] = actions.select_horizontal,
-            ["<CR>"] = actions.select_default,
-            ["<c-e>"] = layout_actions.toggle_preview,
-            ["<c-l>"] = layout_actions.cycle_layout_next,
-            ["<C-A>"] = telescope_custom_actions.multi_selection_open,
-            ["<Tab>"] = actions.toggle_selection,
-          },
-          n = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-            ["<C-A>"] = telescope_custom_actions.multi_selection_open,
-          },
-        },
-        extensions = {
-          media_files = {
-            -- filetypes whitelist
-            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-            filetypes = { "png", "webp", "jpg", "jpeg" },
-            find_cmd = "rg", -- find command (defaults to `fd`)
-          },
-          fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-          },
-          frecency = {
-            db_root = join_paths(rvim.get_cache_dir(), "telescope"),
-            show_scores = true,
-            show_unindexed = true,
-            ignore_patterns = { "*.git/*", "*/tmp/*" },
-            disable_devicons = false,
-            workspaces = {
-              ["conf"] = "/home/razak/.config",
-              ["rvim"] = "/home/razak/.config/rvim",
-              ["data"] = "/home/razak/.local/share",
-              ["project"] = "/home/razak/personal/workspace/coding",
-              ["notes"] = "/home/razak/notes/src",
-            },
-          },
-          ["ui-select"] = {
-            themes.get_cursor(get_border({
-              layout_config = {
-                cursor = {
-                  width = 25,
-                },
-              },
-            })),
-          },
-          file_browser = {
-            theme = "ivy",
-            mappings = {
-              ["i"] = {
-                i = {
-                  ["<C-h>"] = require("telescope").extensions.file_browser.actions.goto_home_dir,
-                },
-              },
-              ["n"] = {
-                n = {
-                  ["<C-h>"] = require("telescope").extensions.file_browser.actions.goto_home_dir,
-                  f = false,
-                },
-              },
-            },
-          },
-          howdoi = require("telescope.themes").get_ivy({
-            borderchars = {
-              preview = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" },
-            },
-          }),
-        },
-        pickers = {
-          buffers = dropdown({
-            sort_mru = true,
-            sort_lastused = true,
-            show_all_buffers = true,
-            ignore_current_buffer = true,
-            previewer = false,
-            theme = "dropdown",
-            mappings = {
-              i = { ["<c-x>"] = "delete_buffer" },
-              n = { ["<c-x>"] = "delete_buffer" },
-            },
-          }),
-          find_files = {
-            hidden = true,
-          },
-          keymaps = dropdown({
+        ["ui-select"] = {
+          themes.get_cursor(get_border({
             layout_config = {
-              height = 18,
-              width = 0.5,
-            },
-          }),
-          live_grep = {
-            --@usage don't include the filename in the search results
-            only_sort_text = true,
-            -- NOTE: previewing html seems to cause some stalling/blocking whilst live grepping
-            -- so filter out html.
-            file_ignore_patterns = {
-              ".git/",
-              "%.html",
-              "dotbot/.*",
-              "zsh/plugins/.*",
-            },
-            max_results = 2000,
-            on_input_filter_cb = function(prompt)
-              -- AND operator for live_grep like how fzf handles spaces with wildcards in rg
-              return { prompt = prompt:gsub("%s", ".*") }
-            end,
-          },
-          oldfiles = dropdown(),
-          current_buffer_fuzzy_find = dropdown({
-            previewer = false,
-            shorten_path = false,
-          }),
-          colorscheme = {
-            enable_preview = true,
-          },
-          git_branches = dropdown(),
-          git_bcommits = {
-            layout_config = {
-              horizontal = {
-                preview_width = 0.55,
+              cursor = {
+                width = 25,
               },
             },
-          },
-          git_commits = {
-            layout_config = {
-              horizontal = {
-                preview_width = 0.55,
-              },
-            },
-          },
-          reloader = dropdown(),
+          })),
         },
+        file_browser = {
+          theme = "ivy",
+          mappings = {
+            ["i"] = {
+              i = {
+                ["<C-h>"] = require("telescope").extensions.file_browser.actions.goto_home_dir,
+              },
+            },
+            ["n"] = {
+              n = {
+                ["<C-h>"] = require("telescope").extensions.file_browser.actions.goto_home_dir,
+                f = false,
+              },
+            },
+          },
+        },
+        howdoi = rvim.telescope.ivy(),
+      },
+      pickers = {
+        buffers = dropdown({
+          sort_mru = true,
+          sort_lastused = true,
+          show_all_buffers = true,
+          ignore_current_buffer = true,
+          previewer = false,
+          theme = "dropdown",
+          mappings = {
+            i = { ["<c-x>"] = "delete_buffer" },
+            n = { ["<c-x>"] = "delete_buffer" },
+          },
+        }),
+        find_files = {
+          hidden = true,
+        },
+        keymaps = dropdown({
+          layout_config = {
+            height = 18,
+            width = 0.5,
+          },
+        }),
+        live_grep = {
+          --@usage don't include the filename in the search results
+          only_sort_text = true,
+          -- NOTE: previewing html seems to cause some stalling/blocking whilst live grepping
+          -- so filter out html.
+          file_ignore_patterns = {
+            ".git/",
+            "%.html",
+            "dotbot/.*",
+            "zsh/plugins/.*",
+          },
+          max_results = 2000,
+          on_input_filter_cb = function(prompt)
+            -- AND operator for live_grep like how fzf handles spaces with wildcards in rg
+            return { prompt = prompt:gsub("%s", ".*") }
+          end,
+        },
+        oldfiles = dropdown(),
+        current_buffer_fuzzy_find = dropdown({
+          previewer = false,
+          shorten_path = false,
+        }),
+        colorscheme = {
+          enable_preview = true,
+        },
+        git_branches = dropdown(),
+        git_bcommits = {
+          layout_config = {
+            horizontal = {
+              preview_width = 0.55,
+            },
+          },
+        },
+        git_commits = {
+          layout_config = {
+            horizontal = {
+              preview_width = 0.55,
+            },
+          },
+        },
+        reloader = dropdown(),
       },
     },
   }
