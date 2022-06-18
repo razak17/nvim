02ee936764209ee679ec9dbebc3d10c9115b792e
Razak <moabdulrazak17@gmail.com>
2022-06-18 16:08:50 +0000

    feat(dial.nvim): add case switching

--- lua/user/modules/editor/config/dial.lua
+++ lua/user/modules/editor/config/dial.lua
@@ -1,6 +1,6 @@
 return function()
-  local dial = require "dial.map"
-  local augend = require "dial.augend"
+  local dial = require("dial.map")
+  local augend = require("dial.augend")
   local map = vim.keymap.set
   map("n", "<C-a>", dial.inc_normal(), { remap = false })
   map("n", "<C-x>", dial.dec_normal(), { remap = false })
@@ -9,30 +9,34 @@ return function()
   map("v", "g<C-a>", dial.inc_gvisual(), { remap = false })
   map("v", "g<C-x>", dial.dec_gvisual(), { remap = false })
 
-  require("dial.config").augends:register_group {
+  require("dial.config").augends:register_group({
     -- default augends used when no group name is specified
     default = {
       augend.integer.alias.decimal,
       augend.integer.alias.hex,
       augend.date.alias["%Y/%m/%d"],
       augend.constant.alias.bool,
-      augend.constant.new {
+      augend.constant.new({
         elements = { "&&", "||" },
         word = false,
         cyclic = true,
-      },
+      }),
+      augend.case.new({
+        types = { "camelCase", "snake_case", "PascalCase", "SCREAMING_SNAKE_CASE" },
+        cyclic = true,
+      }),
     },
     dep_files = {
       augend.semver.alias.semver,
     },
-  }
+  })
 
   rvim.augroup("DialMaps", {
     {
       event = "FileType",
       pattern = { "yaml", "toml" },
       command = function()
-        map("n", "<C-a>", require("dial.map").inc_normal "dep_files", { remap = true })
+        map("n", "<C-a>", require("dial.map").inc_normal("dep_files"), { remap = true })
       end,
     },
   })
