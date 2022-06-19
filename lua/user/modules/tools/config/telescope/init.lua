9022cc6dd1203634cccf8269ec0bffe66a18a3ad
Razak <moabdulrazak17@gmail.com>
2022-06-19 14:47:59 +0000

    chore(telescope): change selection caret

--- lua/user/modules/tools/config/telescope/init.lua
+++ lua/user/modules/tools/config/telescope/init.lua
@@ -66,7 +66,7 @@ return function()
   rvim.telescope.setup = {
     defaults = {
       prompt_prefix = " " .. icons.misc.chevron_right_alt .. " ",
-      selection_caret = " " .. icons.misc.double_chevron_right .. " ", --  ,
+      selection_caret = " " .. icons.misc.chevron_right .. " ", --  ,
       cycle_layout_list = { "flex", "horizontal", "vertical", "bottom_pane", "center" },
       sorting_strategy = "ascending",
       layout_strategy = "horizontal",
