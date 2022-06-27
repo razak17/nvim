c367caca7a99da9b000d0b295f243612fdeb13d0
Razak <moabdulrazak17@gmail.com>
2022-06-27 16:30:01 +0000

    chore(lsp): update on_init

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,5 +1,7 @@
-local M = {}
 local Log = require("user.core.log")
+local fn = vim.fn
+
+local M = {}
 
 local function setup_capabilities()
   local snippet = {
@@ -39,18 +41,31 @@ function M.global_capabilities()
   return capabilities
 end
 
-function M.global_on_init(client, bufnr)
-  if rvim.lsp.on_init_callback then
-    rvim.lsp.on_init_callback(client, bufnr)
-    Log:debug("Called lsp.on_init_callback")
+function rvim.lsp.on_init(client)
+  local path = client.workspace_folders[1].name
+  local config_path = path .. '/.vim/settings.json'
+  if fn.filereadable(config_path) == 0 then
+    return true
+  end
+  local ok, json = pcall(fn.readfile, config_path)
+  if not ok then
     return
   end
+  local overrides = vim.json.decode(table.concat(json, '\n'))
+  for name, config in pairs(overrides) do
+    if name == client.name then
+      local original = client.config
+      client.config = vim.tbl_deep_extend('force', original, config)
+      client.notify('workspace/didChangeConfiguration')
+    end
+  end
+  return true
 end
 
 function M.get_global_opts()
   return {
     on_attach = rvim.lsp.on_attach,
-    on_init = M.global_on_init,
+    on_init = rvim.lsp.on_init,
     capabilities = M.global_capabilities(),
   }
 end
