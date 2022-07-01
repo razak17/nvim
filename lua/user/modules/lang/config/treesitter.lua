b3ecaf039e406e4e8fb2eb390ced6cb0c98e6b7a
Razak <moabdulrazak17@gmail.com>
2022-07-01 01:59:57 +0000

    feat(treesitter): improve selection mappings

--- lua/user/modules/lang/config/treesitter.lua
+++ lua/user/modules/lang/config/treesitter.lua
@@ -53,9 +53,8 @@ return function()
         enable = true,
         keymaps = {
           init_selection = "<CR>",
-          scope_incremental = "<CR>",
           node_incremental = "<TAB>",
-          node_decremental = "<S-TAB>",
+          node_decremental = "<C-CR>",
         },
       },
       textobjects = {
