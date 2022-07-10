4adbc6d755ee8b50e6b109bdbe6fe99fbc9a9424
Razak <moabdulrazak17@gmail.com>
2022-07-10 09:50:18 +0000

    docs(highlights): update type annotations

--- lua/user/utils/highlights.lua
+++ lua/user/utils/highlights.lua
@@ -89,6 +89,10 @@ local function get(group_name)
   return {}
 end
 
+---@class HLAttrs
+---@field from string
+---@field attr 'foreground' | 'fg' | 'background' | 'bg'
+
 ---This helper takes a table of highlights and converts any highlights
 ---specified as `highlight_prop = { from = 'group'}` into the underlying colour
 ---by querying the highlight property of the from group so it can be used when specifying highlights
@@ -98,7 +102,7 @@ end
 ---  M.set_hl({ MatchParen = {foreground = {from = 'ErrorMsg'}}})
 ---```
 ---This will take the foreground colour from ErrorMsg and set it to the foreground of MatchParen.
----@param opts table<string, string|boolean|table<string,string>>
+---@param opts table<string, string|boolean|HLAttrs>
 local function convert_hl_to_val(opts)
   for name, value in pairs(opts) do
     if type(value) == 'table' and value.from then
