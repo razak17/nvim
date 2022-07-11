2560f36249e52aa054a7b10d12a15be47f7d9279
Razak <moabdulrazak17@gmail.com>
2022-07-11 19:22:37 +0000

    chore(highlights): keep dark_green for FIXMEs

--- lua/user/highlights.lua
+++ lua/user/highlights.lua
@@ -60,7 +60,7 @@ local function general_overrides()
     TSParameter = { italic = true, bold = true, foreground = 'NONE' },
     -- highlight FIXME comments
     commentTSWarning = { background = P.teal, foreground = 'fg', bold = true },
-    commentTSDanger = { background = P.error_red, foreground = P.base0, bold = true },
+    commentTSDanger = { background = P.dark_green, foreground = P.base0, bold = true },
     commentTSNote = { background = P.blue, foreground = P.base0, bold = true },
     CommentTasksTodo = { link = 'commentTSWarning' },
     CommentTasksFixme = { link = 'commentTSDanger' },
