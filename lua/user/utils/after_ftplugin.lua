1a028367bbf8ad20b7cec5da3a47b3b4b507a0fa
Razak <moabdulrazak17@gmail.com>
2022-07-13 15:01:39 +0000

    refactor(ftplugin/lua): add lambda surround

--- lua/user/utils/after_ftplugin.lua
+++ lua/user/utils/after_ftplugin.lua
@@ -4,54 +4,6 @@ local nnoremap = rvim.nnoremap
 local fn = vim.fn
 local fmt = string.format
 
-local function find(word, ...)
-  for _, str in ipairs({ ... }) do
-    local match_start, match_end = string.find(word, str)
-    if match_start then
-      return str, match_start, match_end
-    end
-  end
-end
-
---- Stolen from nlua.nvim this function attempts to open
---- vim help docs if an api or vim.fn function otherwise it
---- shows the lsp hover doc
---- @param word string
---- @param callback function
-local function keyword(word, callback)
-  local original_iskeyword = vim.bo.iskeyword
-
-  vim.bo.iskeyword = vim.bo.iskeyword .. ',.'
-  word = word or fn.expand('<cword>')
-
-  vim.bo.iskeyword = original_iskeyword
-
-  -- TODO: This is a sub par work around, since I usually rename `vim.api` -> `api` or similar
-  -- consider maybe using treesitter in the future
-  local api_match = find(word, 'api', 'vim.api')
-  local fn_match = find(word, 'fn', 'vim.fn')
-  if api_match then
-    local _, finish = string.find(word, api_match .. '.')
-    local api_function = string.sub(word, finish + 1)
-
-    vim.cmd(string.format('help %s', api_function))
-    return
-  elseif fn_match then
-    local _, finish = string.find(word, fn_match .. '.')
-    if not finish then
-      return
-    end
-    local api_function = string.sub(word, finish + 1) .. '()'
-
-    vim.cmd(string.format('help %s', api_function))
-    return
-  elseif callback then
-    callback()
-  else
-    vim.lsp.buf.hover()
-  end
-end
-
 function M.go()
   vim.opt_local.spell = true
   vim.opt_local.expandtab = false
@@ -149,9 +101,74 @@ function M.lua()
     return
   end
 
-  -- This allows tpope's vim.surround to surround a text object with a function or conditional
-  vim.b[fmt('surround_%s', fn.char2nr('F'))] = 'function \1function: \1() \r end'
-  vim.b[fmt('surround_%s', fn.char2nr('i'))] = 'if \1if: \1 then \r end'
+  local function find(word, ...)
+    for _, str in ipairs({ ... }) do
+      local match_start, match_end = string.find(word, str)
+      if match_start then
+        return str, match_start, match_end
+      end
+    end
+  end
+
+  --- Stolen from nlua.nvim this function attempts to open
+  --- vim help docs if an api or vim.fn function otherwise it
+  --- shows the lsp hover doc
+  --- @param word string
+  --- @param callback function
+  local function keyword(word, callback)
+    local original_iskeyword = vim.bo.iskeyword
+
+    vim.bo.iskeyword = vim.bo.iskeyword .. ',.'
+    word = word or fn.expand('<cword>')
+
+    vim.bo.iskeyword = original_iskeyword
+
+    -- TODO: This is a sub par work around, since I usually rename `vim.api` -> `api` or similar
+    -- consider maybe using treesitter in the future
+    local api_match = find(word, 'api', 'vim.api')
+    local fn_match = find(word, 'fn', 'vim.fn')
+    if api_match then
+      local _, finish = string.find(word, api_match .. '.')
+      local api_function = string.sub(word, finish + 1)
+
+      vim.cmd(string.format('help %s', api_function))
+      return
+    elseif fn_match then
+      local _, finish = string.find(word, fn_match .. '.')
+      if not finish then
+        return
+      end
+      local api_function = string.sub(word, finish + 1) .. '()'
+
+      vim.cmd(string.format('help %s', api_function))
+      return
+    elseif callback then
+      callback()
+    else
+      vim.lsp.buf.hover()
+    end
+  end
+
+  rvim.ftplugin_conf('nvim-surround', function(surround)
+    local utils = require('nvim-surround.utils')
+    surround.buffer_setup({
+      delimiters = {
+        pairs = {
+          l = { 'function () ', ' end' },
+          F = function()
+            return {
+              fmt('local function %s() ', utils.get_input('Enter a function name: ')),
+              ' end',
+            }
+          end,
+          i = {
+            'if ',
+            { ' then', 'end' },
+          },
+        },
+      },
+    })
+  end)
 
   nnoremap('gK', keyword, { buffer = 0, desc = 'goto keyword' })
   nnoremap('<leader>so', function()
