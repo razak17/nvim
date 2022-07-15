583d7cea6e58db0979cf37e98c4a6ef14fd4f518
Razak <moabdulrazak17@gmail.com>
2022-07-15 23:37:03 +0000

    chore(lsp): remove unused utils

--- lua/user/utils/lsp.lua
+++ lua/user/utils/lsp.lua
@@ -7,15 +7,6 @@ function M.is_client_active(name)
   return tbl.find_first(clients, function(client) return client.name == name end)
 end
 
----Get all supported filetypes by nvim-lsp-installer
----@return table supported filestypes as a list of strings
-function M.get_all_supported_filetypes()
-  local status_ok, lsp_installer_filetypes =
-    pcall(require, 'nvim-lsp-installer._generated.filetype_map')
-  if not status_ok then return {} end
-  return vim.tbl_keys(lsp_installer_filetypes or {})
-end
-
 ---Get supported filetypes per server
 ---@param server_name string can be any server supported by nvim-lsp-installer
 ---@return table supported filestypes as a list of strings
