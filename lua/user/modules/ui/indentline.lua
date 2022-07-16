c771872cba4ac1e987f2a3cf2921fad3d880a95e
Razak <moabdulrazak17@gmail.com>
2022-07-16 19:51:53 +0000

    chore(indentline): disable current context underline

--- lua/user/modules/ui/indentline.lua
+++ lua/user/modules/ui/indentline.lua
@@ -5,7 +5,7 @@ return function()
     context_char = '▎',
     char_priority = 12,
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
     show_current_context_start_on_current_line = false,
     show_first_indent_level = true,
     filetype_exclude = {
