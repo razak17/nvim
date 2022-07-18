999a80b7771def428b96e121d432540a548036ee
Razak <moabdulrazak17@gmail.com>
2022-07-18 23:51:21 +0000

    fix(lsp): disable single file mode for denols
    don't want to have in running in react or other javascript projects

--- lua/user/lsp/providers/denols.lua
+++ lua/user/lsp/providers/denols.lua
@@ -1,8 +1,6 @@
 local opts = {
-  root_dir = function(fname)
-    local util = require('lspconfig/util')
-    return util.root_pattern('deno.json')(fname) or util.root_pattern('deno.jsonc')(fname)
-  end,
+  root_dir = require('lspconfig').util.root_pattern('deno.json', 'deno.jsonc'),
+  single_file_support = false,
 }
 
 return opts
