39e4674c48e8e001b3e004d899710dc39a90ce15
Razak <moabdulrazak17@gmail.com>
2022-07-19 20:14:59 +0000

    feat(hop): add t/T mappings

--- lua/user/modules/tools/hop.lua
+++ lua/user/modules/tools/hop.lua
@@ -9,49 +9,51 @@ return function()
     -- TODO: Multi window mode is currently not working in nvim 0.8
     hop.hint_char1({ multi_windows = false })
   end)
-
   -- NOTE: override F/f using hop motions
   map(
-    { 'x', 'n' },
+    { 'x', 'n', 'o' },
     'F',
     function()
       hop.hint_char1({
         direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
-        current_line_only = false,
+        current_line_only = true,
         inclusive_jump = false,
       })
     end
   )
+
+  map(
+    { 'x', 'n', 'o' },
+    'f',
+    function()
+      hop.hint_char1({
+        direction = require('hop.hint').HintDirection.AFTER_CURSOR,
+        current_line_only = true,
+        inclusive_jump = false,
+      })
+    end
+  )
+
   map(
     { 'x', 'n' },
-    'f',
+    't',
     function()
       hop.hint_char1({
         direction = require('hop.hint').HintDirection.AFTER_CURSOR,
-        current_line_only = false,
-        inclusive_jump = false,
+        current_line_only = true,
+        hint_offset = -1,
       })
     end
   )
 
-  rvim.onoremap(
-    'F',
+  map(
+    { 'x', 'n' },
+    'T',
     function()
       hop.hint_char1({
         direction = require('hop.hint').HintDirection.BEFORE_CURSOR,
-        current_line_only = false,
-        inclusive_jump = false,
-      })
-    end
-  )
-
-  rvim.onoremap(
-    'f',
-    function()
-      hop.hint_char1({
-        direction = require('hop.hint').HintDirection.AFTER_CURSOR,
-        current_line_only = false,
-        inclusive_jump = false,
+        current_line_only = true,
+        hint_offset = 1,
       })
     end
   )
