d057dba47362ee8e2a40950bd89fa123824f4b8e
Razak <moabdulrazak17@gmail.com>
2022-07-23 10:15:43 +0000

    chore(neo-tree): simplify source selector config

--- lua/user/modules/ui/neo-tree.lua
+++ lua/user/modules/ui/neo-tree.lua
@@ -23,23 +23,15 @@ return function()
     ['<leader>e'] = { '<Cmd>Neotree toggle reveal<CR>', 'toggle tree' },
   })
   require('neo-tree').setup({
-    -- source_selector = {
-    --   winbar = true, -- toggle to show selector on winbar
-    --   statusline = false, -- toggle to show selector on statusline
-    --   tabs_layout = 'start',
-    --   tab_labels = { -- falls back to source_name if nil
-    --     filesystem = ' Files',
-    --     buffers = ' Buffers',
-    --     git_status = ' Git',
-    --   },
-    --   tabs_min_width = 11,
-    --   separator = ' ',
-    --   highlight_tab = 'NeoTreeTab',
-    --   highlight_tab_active = 'NeoTreeActiveTab',
-    --   highlight_separator = 'NeoTreeSeparator',
-    --   highlight_separator_active = 'NeoTreeSeparator',
-    --   highlight_background = 'NeoTreeTabBackground',
-    -- },
+    source_selector = {
+      -- winbar = true,
+      separator = ' ',
+      highlight_tab = 'NeoTreeTab',
+      highlight_tab_active = 'NeoTreeActiveTab',
+      highlight_separator = 'NeoTreeSeparator',
+      highlight_separator_active = 'NeoTreeSeparator',
+      highlight_background = 'NeoTreeTabBackground',
+    },
     enable_git_status = true,
     git_status_async = true,
     event_handlers = {
