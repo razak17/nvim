46a02382a6ca751020cbb7abb468f7f25552c3d6
Razak <moabdulrazak17@gmail.com>
2022-07-26 17:26:59 +0000

    chore(which-key): remove excess config options

--- lua/user/modules/tools/which_key.lua
+++ lua/user/modules/tools/which_key.lua
@@ -4,33 +4,13 @@ return function()
 
   which_key.setup({
     plugins = {
-      marks = true, -- shows a list of your marks on ' and `
-      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-      -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-      -- No actual key bindings are created
-      presets = {
-        operators = false, -- adds help for operators like d, y, ...
-        motions = false, -- adds help for motions
-        text_objects = false, -- help for text objects triggered after entering an operator
-        windows = true, -- default bindings on <c-w>
-        nav = true, -- misc bindings to work with windows
-        z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
-      },
-      spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
+      spelling = { enabled = true },
     },
-    icons = {
-      breadcrumb = icons.misc.double_chevron_right, -- symbol used in the command line area that shows your active key combo
-      group = '+', -- symbol prepended to a group
-    },
-    window = {
-      border = rvim.style.border.current,
-    },
-    layout = {
-      align = 'center',
-    },
+    icons = { breadcrumb = icons.misc.double_chevron_right },
+    window = { border = rvim.style.border.current },
+    layout = { align = 'center' },
     hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
-    show_help = true, -- show help message on the command line when the popup is visible
+    show_help = true,
   })
 
   which_key.register({
