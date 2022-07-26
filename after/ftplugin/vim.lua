317fb1ee2968f0cca5f127a2c2fc296a08084e01
Razak <moabdulrazak17@gmail.com>
2022-07-26 16:01:36 +0000

    chore: remove colorcolumn option

--- after/ftplugin/vim.lua
+++ after/ftplugin/vim.lua
@@ -1,4 +1,3 @@
-vim.opt_local.colorcolumn = 120
 vim.cmd([[setlocal iskeyword+=:,#]])
 vim.opt_local.foldmethod = 'marker'
 rvim.nnoremap('so', ":source % <bar> :lua vim.notify('Sourced ' .. vim.fn.expand('%'))<CR>")
