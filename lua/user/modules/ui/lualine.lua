82d926154fc39749f7003bf2544203275afb9be2
Razak <moabdulrazak17@gmail.com>
2022-07-27 00:23:19 +0000

    chore(lualine): vim.lsp.buf_get_clients is deprecated

--- lua/user/modules/ui/lualine.lua
+++ lua/user/modules/ui/lualine.lua
@@ -147,7 +147,7 @@ return function()
     -- Lsp server name .
     function(msg)
       msg = msg or 'LS Inactive'
-      local buf_clients = vim.lsp.buf_get_clients()
+      local buf_clients = vim.lsp.get_active_clients()
       if next(buf_clients) == nil then
         -- TODO: clean up this if statement
         if type(msg) == 'boolean' or #msg == 0 then return 'LS Inactive' end
