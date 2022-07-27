de6037c2966130bc59e689676d5926dc9729999a
Razak <moabdulrazak17@gmail.com>
2022-07-27 17:46:23 +0000

    chore(vim-illuminate): ignore packer

--- lua/user/modules/ui/plugins.lua
+++ lua/user/modules/ui/plugins.lua
@@ -105,6 +105,7 @@ package({
       'lspinfo',
       'lsp-installer',
       'harpoon',
+      'packer',
     }
     rvim.nnoremap('<a-n>', ':lua require"illuminate".next_reference{wrap=true}<cr>')
     rvim.nnoremap('<a-p>', ':lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>')
