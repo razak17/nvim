b4ffe3a19c9c93261565b65adc9bde6391495440
Razak <moabdulrazak17@gmail.com>
2022-07-27 02:21:31 +0000

    feat(plugin): trial crates.nvim

--- lua/user/modules/lang/plugins.lua
+++ lua/user/modules/lang/plugins.lua
@@ -128,6 +128,28 @@ package({
   end,
 })
 
+package({
+  'Saecki/crates.nvim',
+  config = function()
+    require('crates').setup({
+      popup = {
+        -- autofocus = true,
+        style = 'minimal',
+        border = 'rounded',
+        show_version_date = false,
+        show_dependency_version = true,
+        max_height = 30,
+        min_width = 20,
+        padding = 1,
+      },
+      null_ls = {
+        enabled = true,
+        name = 'crates.nvim',
+      },
+    })
+  end,
+})
+
 -- Treesitter
 package({
   'nvim-treesitter/nvim-treesitter',
