4f46ddca394e2cf3946c18e533b71e3b25abc6e7
Razak <moabdulrazak17@gmail.com>
2022-07-27 18:15:54 +0000

    refactor(null_ls): use alias

--- lua/user/modules/lang/null-ls.lua
+++ lua/user/modules/lang/null-ls.lua
@@ -1,26 +1,20 @@
 return function()
   local null_ls = require('null-ls')
+  local diagnostics = null_ls.builtins.diagnostics
+  local formatting = null_ls.builtins.formatting
   null_ls.setup({
     debounce = 150,
     sources = {
-      null_ls.builtins.diagnostics.zsh,
-      null_ls.builtins.diagnostics.flake8,
-      null_ls.builtins.diagnostics.eslint_d:with({
-        filetypes = {
-          'javascript',
-          'javascriptreact',
-          'typescript',
-          'typescriptreact',
-          'vue',
-        },
+      diagnostics.zsh,
+      diagnostics.flake8,
+      diagnostics.eslint_d:with({
+        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
       }),
-      null_ls.builtins.diagnostics.golangci_lint,
-      null_ls.builtins.diagnostics.shellcheck.with({
-        extra_args = { '--severity', 'warning' },
-      }),
+      diagnostics.golangci_lint,
+      diagnostics.shellcheck.with({ extra_args = { '--severity', 'warning' } }),
       -- formatters
-      null_ls.builtins.formatting.black.with({ extra_args = { '--fast' } }),
-      null_ls.builtins.formatting.eslint_d.with({
+      formatting.black.with({ extra_args = { '--fast' } }),
+      formatting.eslint_d.with({
         extra_args = { '--fix' },
         filetypes = {
           'vue',
@@ -32,20 +26,12 @@ return function()
           'typescript',
         },
       }),
-      null_ls.builtins.formatting.prettier_d_slim.with({
-        filetypes = {
-          'html',
-          'yaml',
-          'graphql',
-          'markdown',
-          'css',
-        },
+      formatting.prettier_d_slim.with({
+        filetypes = { 'html', 'yaml', 'graphql', 'markdown', 'css' },
       }),
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.formatting.shfmt,
-      null_ls.builtins.formatting.stylua.with({
-        condition = function() return rvim.executable('stylua') end,
-      }),
+      formatting.isort,
+      formatting.shfmt,
+      formatting.stylua.with({ condition = function() return rvim.executable('stylua') end }),
     },
   })
   rvim.augroup('NullLsConfig', {
