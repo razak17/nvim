48d6c9dd6ddc16212741dc2bbe82f5458d2d0f60
Razak <moabdulrazak17@gmail.com>
2022-07-27 18:00:39 +0000

    refactor(winbar): handle global opts better

--- lua/user/config/defaults.lua
+++ lua/user/config/defaults.lua
@@ -1,5 +1,3 @@
-rvim.keys = { leader = 'space', localleader = ',' }
-
 rvim.paths = {
   snippets = join_paths(rvim.get_config_dir(), 'snippets', 'textmate'),
   packer_compiled = join_paths(rvim.get_runtime_dir(), 'site', 'lua', '_compiled_rolling.lua'),
@@ -9,21 +7,19 @@ rvim.paths = {
   mason = join_paths(rvim.get_runtime_dir(), 'mason'),
 }
 
-rvim.plugins = { SANE = true, packer = { active = true } }
-
 rvim.ui = {
   line_wrap_cursor_movement = false,
   transparent_window = false,
   defer = false,
-  winbar_opts = {
+  winbar = {
     enable = false,
     use_filename = true,
-    use_ft_icon = false,
+    use_icon = false,
   },
 }
 
-rvim.lang = { format_on_save = true }
-
-rvim.util = { disabled_providers = { 'python', 'ruby', 'perl' }, save_on_focus_lost = true }
-
 rvim.colorscheme = 'zephyr'
+rvim.lang = { format_on_save = true }
+rvim.keys = { leader = 'space', localleader = ',' }
+rvim.plugins = { SANE = true, packer = { active = true } }
+rvim.util = { disabled_providers = { 'python', 'ruby', 'perl' }, save_on_focus_lost = true }
