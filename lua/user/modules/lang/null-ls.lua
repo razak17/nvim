9c0d3e05aafce0423b7a84f379f2c78dc42494b3
Razak <moabdulrazak17@gmail.com>
2022-07-30 11:11:10 +0000

    chore(null-ls): switch to prettierd from prettier_d_slim
    hasn't been updated in a while

--- lua/user/modules/lang/null-ls.lua
+++ lua/user/modules/lang/null-ls.lua
@@ -14,27 +14,11 @@ return function()
       -- linters
       diagnostics.zsh,
       diagnostics.flake8,
-      diagnostics.eslint_d:with({
-        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
-      }),
+      diagnostics.eslint_d,
       diagnostics.shellcheck.with({ extra_args = { '--severity', 'warning' } }),
       -- formatters
       formatting.black.with({ extra_args = { '--fast' } }),
-      formatting.prettier_d_slim.with({
-        filetypes = {
-          'html',
-          'yaml',
-          'graphql',
-          'markdown',
-          'css',
-          'json',
-          'jsonc',
-          'javascript',
-          'javascriptreact',
-          'typescriptreact',
-          'typescript',
-        },
-      }),
+      formatting.prettierd,
       formatting.isort,
       formatting.shfmt,
       formatting.stylua.with({ condition = function() return rvim.executable('stylua') end }),
