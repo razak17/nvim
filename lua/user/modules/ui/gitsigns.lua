84f8bbb3c9a08f006fb4ce249de2a97b515ceabe
Razak <moabdulrazak17@gmail.com>
2022-08-14 08:23:13 +0000

    chore(gitsigns): simplify config

--- lua/user/modules/ui/gitsigns.lua
+++ lua/user/modules/ui/gitsigns.lua
@@ -1,61 +1,58 @@
 return function()
   if not rvim.plugin_installed('gitsigns.nvim') then return end
+
+  local gitsigns = require('gitsigns')
   local cwd = vim.fn.getcwd()
 
-  rvim.gitsigns = {
-    setup = {
-      signs = {
-        add = { hl = 'GitGutterAdd', text = '▋' },
-        change = { hl = 'GitGutterChange', text = '▋' },
-        delete = { hl = 'GitGutterDelete', text = '▋' },
-        topdelete = { hl = 'GitGutterDeleteChange', text = '▔' },
-        changedelete = { hl = 'GitGutterChange', text = '▎' },
-      },
-      _threaded_diff = true, -- NOTE: experimental but I'm curious
-      word_diff = false,
-      current_line_blame = not cwd:match('personal') and not cwd:match('dotfiles'),
-      numhl = false,
-      preview_config = {
-        border = rvim.style.border.current,
-      },
-      on_attach = function()
-        local gs = package.loaded.gitsigns
-
-        local function qf_list_modified() gs.setqflist('all') end
-
-        require('which-key').register({
-          ['<leader>h'] = {
-            name = 'Gitsigns',
-            d = { gs.toggle_deleted, 'show deleted lines' },
-            j = { gs.next_hunk, 'Next Hunk' },
-            k = { gs.prev_hunk, 'Prev Hunk' },
-            p = { gs.preview_hunk, 'preview hunk' },
-            r = { gs.reset_hunk, 'reset current hunk' },
-            s = { gs.stage_hunk, 'stage current hunk' },
-            u = { gs.undo_stage_hunk, 'undo stage' },
-            w = { gs.toggle_word_diff, 'toggle word diff' },
-          },
-          ['<leader>gb'] = { gs.blame_line, 'gitsigns: blame' },
-          ['<leader>gL'] = { qf_list_modified, 'gitsigns: list modified in quickfix' },
-          ['<leader>gr'] = {
-            name = '+reset',
-            e = { gs.reset_buffer, 'gitsigns: reset entire buffer' },
-          },
-          ['<leader>gw'] = { gs.stage_buffer, 'gitsigns: stage entire buffer' },
-        })
-      end,
-      watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-      },
-      sign_priority = 6,
-      update_debounce = 200,
-      status_formatter = nil, -- Use default
+  gitsigns.setup({
+    signs = {
+      add = { hl = 'GitGutterAdd', text = '▋' },
+      change = { hl = 'GitGutterChange', text = '▋' },
+      delete = { hl = 'GitGutterDelete', text = '▋' },
+      topdelete = { hl = 'GitGutterDeleteChange', text = '▔' },
+      changedelete = { hl = 'GitGutterChange', text = '▎' },
     },
-  }
+    _threaded_diff = true, -- NOTE: experimental but I'm curious
+    word_diff = false,
+    current_line_blame = not cwd:match('personal') and not cwd:match('dotfiles'),
+    numhl = false,
+    preview_config = {
+      border = rvim.style.border.current,
+    },
+    on_attach = function()
+      local gs = package.loaded.gitsigns
 
-  local gitsigns = require("gitsigns")
-  gitsigns.setup(rvim.gitsigns.setup)
+      local function qf_list_modified() gs.setqflist('all') end
+
+      require('which-key').register({
+        ['<leader>h'] = {
+          name = 'Gitsigns',
+          d = { gs.toggle_deleted, 'show deleted lines' },
+          j = { gs.next_hunk, 'Next Hunk' },
+          k = { gs.prev_hunk, 'Prev Hunk' },
+          p = { gs.preview_hunk, 'preview hunk' },
+          r = { gs.reset_hunk, 'reset current hunk' },
+          s = { gs.stage_hunk, 'stage current hunk' },
+          u = { gs.undo_stage_hunk, 'undo stage' },
+          w = { gs.toggle_word_diff, 'toggle word diff' },
+        },
+        ['<leader>gb'] = { gs.blame_line, 'gitsigns: blame' },
+        ['<leader>gL'] = { qf_list_modified, 'gitsigns: list modified in quickfix' },
+        ['<leader>gr'] = {
+          name = '+reset',
+          e = { gs.reset_buffer, 'gitsigns: reset entire buffer' },
+        },
+        ['<leader>gw'] = { gs.stage_buffer, 'gitsigns: stage entire buffer' },
+      })
+    end,
+    watch_gitdir = {
+      interval = 1000,
+      follow_files = true,
+    },
+    sign_priority = 6,
+    update_debounce = 200,
+    status_formatter = nil, -- Use default
+  })
 
   rvim.vnoremap(
     '<leader>hs',
