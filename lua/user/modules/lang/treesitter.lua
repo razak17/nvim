bbf1c3a9bda42d66ddc031a3a8066552cb8dac28
Razak <moabdulrazak17@gmail.com>
2022-08-17 22:02:22 +0000

    refactor(treesitter): simplify config
    remove rvim.treesitter

--- lua/user/modules/lang/treesitter.lua
+++ lua/user/modules/lang/treesitter.lua
@@ -1,97 +1,9 @@
 if not rvim.plugin_installed('nvim-treesitter') then return end
 
-rvim.treesitter = rvim.treesitter or {
-  install_attempted = {},
-}
-
 return function()
-  rvim.treesitter = {
-    setup = {
-      auto_install = true,
-      highlight = { enabled = true },
-      ensure_installed = {
-        'lua',
-        'dart',
-        'rust',
-        'typescript',
-        'javascript',
-        'comment',
-        'markdown',
-        'markdown_inline',
-        'prisma',
-        'graphql',
-        'go',
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<TAB>',
-          node_decremental = '<C-CR>',
-        },
-      },
-      textobjects = {
-        lookahead = true,
-        select = {
-          enable = true,
-          include_surrounding_whitespace = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['aC'] = '@conditional.outer',
-            ['iC'] = '@conditional.inner',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['[w'] = { '@parameter.inner' },
-          },
-          swap_previous = {
-            [']w'] = { '@parameter.inner' },
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = { '@function.outer', '@class.outer' },
-          },
-          goto_previous_start = {
-            ['[m'] = { '@function.outer', '@class.outer' },
-          },
-        },
-        lsp_interop = {
-          enable = true,
-          border = rvim.style.border.current,
-          peek_definition_code = {
-            ['<leader>lu'] = '@function.outer',
-            ['<leader>lC'] = '@class.outer',
-          },
-        },
-      },
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = 1000,
-        disable = { 'lua', 'json', 'c', 'cpp', 'html' },
-        colors = {
-          'royalblue3',
-          'darkorange3',
-          'seagreen3',
-          'firebrick',
-          'darkorchid3',
-        },
-      },
-    },
-  }
-
   ---Get all filetypes for which we have a treesitter parser installed
   ---@return string[]
-  function rvim.treesitter.get_filetypes()
-    vim.cmd([[packadd nvim-treesitter]])
+  local function get_filetypes()
     local parsers = require('nvim-treesitter.parsers')
     local configs = parsers.get_parser_configs()
     return vim.tbl_map(
@@ -104,20 +16,92 @@ return function()
   if not status_ok then return end
 
   treesitter_configs.setup({
-    highlight = { enable = rvim.treesitter.setup.highlight.enabled },
-    incremental_selection = rvim.treesitter.setup.incremental_selection,
-    textobjects = rvim.treesitter.setup.textobjects,
+    highlight = { enabled = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<C-CR>',
+      },
+    },
+    textobjects = {
+      lookahead = true,
+      select = {
+        enable = true,
+        include_surrounding_whitespace = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['aC'] = '@conditional.outer',
+          ['iC'] = '@conditional.inner',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['[w'] = { '@parameter.inner' },
+        },
+        swap_previous = {
+          [']w'] = { '@parameter.inner' },
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = { '@function.outer', '@class.outer' },
+        },
+        goto_previous_start = {
+          ['[m'] = { '@function.outer', '@class.outer' },
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        border = rvim.style.border.current,
+        peek_definition_code = {
+          ['<leader>lu'] = '@function.outer',
+          ['<leader>lC'] = '@class.outer',
+        },
+      },
+    },
     indent = { enable = { 'javascriptreact' } },
     matchup = { enable = true, disable = { 'c', 'python' } },
     autopairs = { enable = true },
-    rainbow = rvim.treesitter.setup.rainbow,
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = 1000,
+      disable = { 'lua', 'json', 'c', 'cpp', 'html' },
+      colors = {
+        'royalblue3',
+        'darkorange3',
+        'seagreen3',
+        'firebrick',
+        'darkorchid3',
+      },
+    },
     query_linter = {
       enable = true,
       use_virtual_text = true,
       lint_events = { 'BufWrite', 'CursorHold' },
     },
     playground = { persist_queries = true },
-    ensure_installed = rvim.treesitter.setup.ensure_installed,
+    ensure_installed = {
+      'lua',
+      'dart',
+      'rust',
+      'typescript',
+      'javascript',
+      'comment',
+      'markdown',
+      'markdown_inline',
+      'prisma',
+      'graphql',
+      'go',
+    },
   })
 
   -- Only apply folding to supported files:
@@ -125,7 +109,7 @@ return function()
     rvim.augroup('TreesitterFolds', {
       {
         event = { 'FileType' },
-        pattern = rvim.treesitter.get_filetypes(),
+        pattern = get_filetypes(),
         command = function() vim.cmd('setlocal foldexpr=nvim_treesitter#foldexpr()') end,
       },
     })
