8112837484d68f5b7c9dfaf36d0ef9b67888d66d
Razak <moabdulrazak17@gmail.com>
2022-09-02 17:19:31 +0000

    refactor(cmp): remove use of rvim global

--- lua/user/modules/editor/cmp.lua
+++ lua/user/modules/editor/cmp.lua
@@ -1,15 +1,13 @@
 return function()
   if not rvim.plugin_installed('nvim-cmp') then return end
 
-  local status_cmp_ok, cmp = rvim.safe_require('cmp')
-  if not status_cmp_ok then return end
+  local cmp = require('cmp')
+  local h = require('user.utils.highlights')
 
   local fn, api = vim.fn, vim.api
-
   local t = rvim.replace_termcodes
   local border = rvim.style.border.current
   local lsp_hls = rvim.lsp.kind_highlights
-  local util = require('user.utils.highlights')
   local ellipsis = rvim.style.icons.misc.ellipsis
   local luasnip = require('luasnip')
 
@@ -34,7 +32,7 @@ return function()
     }
   )
 
-  util.plugin('Cmp', kind_hls)
+  h.plugin('Cmp', kind_hls)
 
   local cmp_window = {
     border = border,
@@ -77,43 +75,61 @@ return function()
     fallback()
   end
 
-  rvim.cmp = {
-    setup = {
-      experimental = { ghost_text = false },
-      preselect = cmp.PreselectMode.None,
-      window = {
-        completion = cmp.config.window.bordered(cmp_window),
-        documentation = cmp.config.window.bordered(cmp_window),
+  cmp.setup({
+    experimental = { ghost_text = false },
+    preselect = cmp.PreselectMode.None,
+    window = {
+      completion = cmp.config.window.bordered(cmp_window),
+      documentation = cmp.config.window.bordered(cmp_window),
+    },
+    snippet = {
+      expand = function(args) require('luasnip').lsp_expand(args.body) end,
+    },
+    mapping = {
+      ['<c-h>'] = cmp.mapping(
+        function() api.nvim_feedkeys(fn['copilot#Accept'](t('<Tab>')), 'n', true) end
+      ),
+      ['<C-k>'] = cmp.mapping.select_prev_item(),
+      ['<C-j>'] = cmp.mapping.select_next_item(),
+      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<Tab>'] = cmp.mapping(tab, { 'i', 's', 'c' }),
+      ['<S-Tab>'] = cmp.mapping(shift_tab, { 'i', 's', 'c' }),
+      ['<C-q>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<C-space>'] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- If nothing is selected don't complete
+    },
+    formatting = {
+      deprecated = true,
+      fields = { 'kind', 'abbr', 'menu' },
+      source_names = {
+        luasnip = '(SN)',
+        nvim_lsp = '(LSP)',
+        path = '(Path)',
+        buffer = '(Buf)',
+        dictionary = '(Dict)',
+        spell = '(SP)',
+        cmdline = '(Cmd)',
+        git = '(Git)',
+        calc = '(Calc)',
+        emoji = '(E)',
+        cmdline_history = '(Hist)',
+        rg = '(Rg)',
       },
-      snippet = {
-        expand = function(args) require('luasnip').lsp_expand(args.body) end,
-      },
-      mapping = {
-        ['<c-h>'] = cmp.mapping(
-          function() api.nvim_feedkeys(fn['copilot#Accept'](t('<Tab>')), 'n', true) end
-        ),
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<Tab>'] = cmp.mapping(tab, { 'i', 's', 'c' }),
-        ['<S-Tab>'] = cmp.mapping(shift_tab, { 'i', 's', 'c' }),
-        ['<C-q>'] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ['<C-space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- If nothing is selected don't complete
-      },
-      formatting = {
-        deprecated = true,
-        fields = { 'kind', 'abbr', 'menu' },
-        source_names = {
+      format = function(entry, vim_item)
+        local MAX = math.floor(vim.o.columns * 0.5)
+        local codicons = rvim.style.codicons
+        if #vim_item.abbr >= MAX then vim_item.abbr = vim_item.abbr:sub(1, MAX) .. ellipsis end
+        vim_item.kind = codicons.kind[vim_item.kind]
+        if entry.source.name == 'emoji' then vim_item.kind = codicons.misc.smiley end
+        vim_item.menu = ({
           luasnip = '(SN)',
           nvim_lsp = '(LSP)',
           path = '(Path)',
           buffer = '(Buf)',
-          dictionary = '(Dict)',
           spell = '(SP)',
           cmdline = '(Cmd)',
           git = '(Git)',
@@ -121,55 +137,36 @@ return function()
           emoji = '(E)',
           cmdline_history = '(Hist)',
           rg = '(Rg)',
-        },
-        format = function(entry, vim_item)
-          local MAX = math.floor(vim.o.columns * 0.5)
-          local codicons = rvim.style.codicons
-          if #vim_item.abbr >= MAX then vim_item.abbr = vim_item.abbr:sub(1, MAX) .. ellipsis end
-          vim_item.kind = codicons.kind[vim_item.kind]
-          if entry.source.name == 'emoji' then vim_item.kind = codicons.misc.smiley end
-          vim_item.menu = rvim.cmp.setup.formatting.source_names[entry.source.name]
-          return vim_item
-        end,
-      },
-      sources = {
-        { name = 'luasnip' },
-        { name = 'nvim_lsp' },
-        { name = 'path' },
-        {
-          name = 'buffer',
-          keyword_length = 2,
-          options = {
-            get_bufnrs = function()
-              local bufs = {}
-              for _, win in ipairs(api.nvim_list_wins()) do
-                bufs[api.nvim_win_get_buf(win)] = true
-              end
-              return vim.tbl_keys(bufs)
-            end,
-          },
-        },
-        {
-          name = 'dictionary',
-          keyword_length = 5,
-        },
-        { name = 'spell' },
-        { name = 'git' },
-        { name = 'calc' },
-        { name = 'emoji' },
-        { name = 'nvim_lsp_document_symbol' },
-        { name = 'cmdline_history', priority = 10, max_item_count = 5 },
-      },
+          dictionary = '(Dict)',
+        })[entry.source.name]
+        return vim_item
+      end,
+    },
+    sources = cmp.config.sources({
+      { name = 'luasnip' },
+      { name = 'nvim_lsp' },
+      { name = 'path' },
       {
         name = 'rg',
         keyword_length = 4,
         max_item_count = 10,
         option = { additional_arguments = '--max-depth 8' },
       },
-    },
-  }
-
-  require('cmp').setup(rvim.cmp.setup)
+      {
+        name = 'dictionary',
+        keyword_length = 5,
+      },
+    }, {
+      {
+        name = 'buffer',
+        keyword_length = 2,
+        options = {
+          get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+        },
+      },
+      { name = 'spell' },
+    }),
+  })
 
   local search_sources = {
     sources = cmp.config.sources({
