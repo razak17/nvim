894ead757438d1eb65ded4a1d8de87e942eede33
Razak <moabdulrazak17@gmail.com>
2022-09-03 15:06:54 +0000

    chore(after/html): use opt_local for vim.bo commands
    in order to use keywords like append and remove

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -3,9 +3,9 @@ vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 -- Fix quirkiness in indentation
-vim.bo.indentkeys:remove('*<Return>')
+vim.opt_local.indentkeys:remove('*<Return>')
 -- setlocal indentkeys-=*<Return>
 -- Make lines longer, and don't break them automatically
 vim.cmd([[setlocal tw=120 linebreak textwidth=0]])
 vim.wo.wrap = false
-vim.bo.matchpairs:append('<:>')
+vim.opt_local.matchpairs:append('<:>')
