64668086f433c81c6c5c0d57b7f9de8855a38d60
Razak <moabdulrazak17@gmail.com>
2022-10-13 00:40:55 +0000

    chore(plugin): disable neogit

--- lua/user/modules/tools/plugins.lua
+++ lua/user/modules/tools/plugins.lua
@@ -171,29 +171,6 @@ use({
 })
 
 use({
-  'TimUntersberger/neogit',
-  requires = 'plenary.nvim',
-  config = function()
-    local neogit = require('neogit')
-    neogit.setup({
-      disable_signs = false,
-      disable_hint = true,
-      disable_commit_confirmation = true,
-      disable_builtin_notifications = true,
-      disable_insert_on_commit = false,
-      signs = {
-        section = { '', '' }, -- "", ""
-        item = { '▸', '▾' },
-        hunk = { '樂', '' },
-      },
-      integrations = {
-        diffview = true,
-      },
-    })
-  end,
-})
-
-use({
   'iamcco/markdown-preview.nvim',
   run = function() vim.fn['mkdp#util#install']() end,
   ft = { 'markdown' },
@@ -528,4 +505,28 @@ use({
     )
     rvim.nnoremap('<leader>dC', '<Cmd>DeleteDebugPrints<CR>', 'debugprint: clear all')
   end,
+})
+
+use({
+  'TimUntersberger/neogit',
+  requires = 'plenary.nvim',
+  config = function()
+    local neogit = require('neogit')
+    neogit.setup({
+      disable_signs = false,
+      disable_hint = true,
+      disable_commit_confirmation = true,
+      disable_builtin_notifications = true,
+      disable_insert_on_commit = false,
+      signs = {
+        section = { '', '' }, -- "", ""
+        item = { '▸', '▾' },
+        hunk = { '樂', '' },
+      },
+      integrations = {
+        diffview = true,
+      },
+    })
+  end,
+  disable = true,
 })
