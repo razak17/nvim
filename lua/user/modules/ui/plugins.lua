a3de70d71da7e78e4cf3ccada04a42f38813ca0e
Razak <moabdulrazak17@gmail.com>
2022-11-06 14:11:18 +0000

    feat(plugin): bring back window-picker

--- lua/user/modules/ui/plugins.lua
+++ lua/user/modules/ui/plugins.lua
@@ -48,6 +48,23 @@ use({
   requires = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
+    {
+      's1n7ax/nvim-window-picker',
+      tag = 'v1.*',
+      config = function()
+        require('window-picker').setup({
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            bo = {
+              filetype = { 'neo-tree-popup', 'quickfix', 'incline' },
+              buftype = { 'terminal', 'quickfix', 'nofile' },
+            },
+          },
+          other_win_hl_color = require('user.utils.highlights').get('Visual', 'bg'),
+        })
+      end,
+    },
   },
 })
 
