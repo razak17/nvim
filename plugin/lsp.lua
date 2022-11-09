8b7e14514cc023be601505f31082b45085d8a063
Razak <moabdulrazak17@gmail.com>
2022-11-09 00:42:58 +0000

    fix(lsp): fix format_on_save exclusion augroup

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -109,14 +109,14 @@ local function setup_autocommands(client, bufnr)
   end)
 
   augroup(FEATURES.FORMATTING, function(provider)
-    if rvim.find_string(format_exclusions.format_on_save, vim.bo.ft) then return end
     return {
       {
         event = 'BufWritePre',
         buffer = bufnr,
         desc = 'LSP: Format on save',
         command = function(args)
-          if not vim.g.formatting_disabled and not vim.b.formatting_disabled then
+          local excluded = rvim.find_string(format_exclusions.format_on_save, vim.bo.ft)
+          if not excluded and not vim.g.formatting_disabled and not vim.b.formatting_disabled then
             local clients = clients_by_capability(args.buf, provider)
             format({ bufnr = args.buf, async = #clients == 1 })
           end
