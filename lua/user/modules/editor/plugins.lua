4e3e758cd5e388c0cbb3c2b1bf5cf653c00fa298
Razak <moabdulrazak17@gmail.com>
2022-12-08 17:36:52 +0000

    refactor(comment): simplify pre_hook function

--- lua/user/modules/editor/plugins.lua
+++ lua/user/modules/editor/plugins.lua
@@ -85,9 +85,6 @@ use({
     require('Comment').setup({
       pre_hook = function(ctx)
         local U = require('Comment.utils')
-        -- Determine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
-        -- Determine the location where to calculate commentstring from
         local location = nil
         if ctx.ctype == U.ctype.blockwise then
           location = require('ts_context_commentstring.utils').get_cursor_location()
@@ -95,7 +92,7 @@ use({
           location = require('ts_context_commentstring.utils').get_visual_start_location()
         end
         return require('ts_context_commentstring.internal').calculate_commentstring({
-          key = type,
+          key = ctx.ctype == U.ctype.linewise and '__default' or '__multiline',
           location = location,
         })
       end,
