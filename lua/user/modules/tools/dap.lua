e6dabfe23c833c5259e32be4e54680fc106b1412
Razak <moabdulrazak17@gmail.com>
2022-12-20 10:34:52 +0000

    feat(dap): add multiple debug scenarios for delve

--- lua/user/modules/tools/dap.lua
+++ lua/user/modules/tools/dap.lua
@@ -65,6 +65,27 @@ return function()
         request = 'launch', -- Whether to "launch" or "attach" to program
         program = '${file}', -- The buffer you are focused on when running nvim-dap
       },
+      {
+        type = 'go',
+        name = 'Debug test (go.mod)',
+        request = 'launch',
+        mode = 'test',
+        program = './${relativeFileDirname}',
+      },
+      {
+        type = 'go',
+        name = 'Attach (Pick Process)',
+        mode = 'local',
+        request = 'attach',
+        processId = require('dap.utils').pick_process,
+      },
+      {
+        type = 'go',
+        name = 'Attach (127.0.0.1:9080)',
+        mode = 'remote',
+        request = 'attach',
+        port = '9080',
+      },
     },
   }
   -- nodejs
