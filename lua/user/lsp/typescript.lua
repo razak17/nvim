42f5030d250dd51e3f047b63cec2e0d5ffc233e9
Razak <moabdulrazak17@gmail.com>
2022-12-27 02:19:22 +0000

    chore(typescript): remove opts
    already used by default

--- lua/user/lsp/typescript.lua
+++ lua/user/lsp/typescript.lua
@@ -1,8 +1,4 @@
 local ok, which_key = rvim.safe_require('which-key')
 if ok then which_key.register({ ['<localleader>'] = { r = { name = 'Rust Tools' } } }) end
 
-require('typescript').setup({
-  disable_commands = false,
-  debug = false,
-  go_to_source_definition = { fallback = true },
-})
+require('typescript').setup()
