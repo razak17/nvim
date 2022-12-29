8de5099825786027995ac72b9c19f22e17f5ccdd
Razak <moabdulrazak17@gmail.com>
2022-12-29 21:45:22 +0000

    fix(dap): go configuration overriding previous configurations

--- lua/user/modules/lang/dap.lua
+++ lua/user/modules/lang/dap.lua
@@ -14,9 +14,7 @@ local M = {
       config = function()
         require('dapui').setup({
           windows = { indent = 2 },
-          floating = {
-            border = rvim.style.border.current,
-          },
+          floating = { border = rvim.style.border.current },
         })
         local dapui = require('dapui')
         local dap = require('dap')
@@ -151,35 +149,33 @@ function M.config()
       args = { 'dap', '-l', '127.0.0.1:${port}' },
     },
   }
-  dap.configurations = {
-    go = {
-      {
-        type = 'go', -- Which adapter to use
-        name = 'Debug', -- Human readable name
-        request = 'launch', -- Whether to "launch" or "attach" to program
-        program = '${file}', -- The buffer you are focused on when running nvim-dap
-      },
-      {
-        type = 'go',
-        name = 'Debug test (go.mod)',
-        request = 'launch',
-        mode = 'test',
-        program = './${relativeFileDirname}',
-      },
-      {
-        type = 'go',
-        name = 'Attach (Pick Process)',
-        mode = 'local',
-        request = 'attach',
-        processId = require('dap.utils').pick_process,
-      },
-      {
-        type = 'go',
-        name = 'Attach (127.0.0.1:9080)',
-        mode = 'remote',
-        request = 'attach',
-        port = '9080',
-      },
+  dap.configurations.go = {
+    {
+      type = 'go', -- Which adapter to use
+      name = 'Debug', -- Human readable name
+      request = 'launch', -- Whether to "launch" or "attach" to program
+      program = '${file}', -- The buffer you are focused on when running nvim-dap
+    },
+    {
+      type = 'go',
+      name = 'Debug test (go.mod)',
+      request = 'launch',
+      mode = 'test',
+      program = './${relativeFileDirname}',
+    },
+    {
+      type = 'go',
+      name = 'Attach (Pick Process)',
+      mode = 'local',
+      request = 'attach',
+      processId = require('dap.utils').pick_process,
+    },
+    {
+      type = 'go',
+      name = 'Attach (127.0.0.1:9080)',
+      mode = 'remote',
+      request = 'attach',
+      port = '9080',
     },
   }
   -- nodejs
