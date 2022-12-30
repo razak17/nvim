fed3d055252b48186061fd4e5ae85880a1ca76bc
Razak <moabdulrazak17@gmail.com>
2022-12-30 17:13:08 +0000

    chore(copilot): move cmp event to cmp config
    don't want it to trigger cmp on start

--- lua/user/modules/editor/cmp.lua
+++ lua/user/modules/editor/cmp.lua
@@ -190,6 +190,9 @@ function M.config()
     }),
   })
 
+  cmp.event:on('menu_opened', function() vim.b.copilot_suggestion_hidden = true end)
+  cmp.event:on('menu_closed', function() vim.b.copilot_suggestion_hidden = false end)
+
   cmp.setup.cmdline({ '/', '?' }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
