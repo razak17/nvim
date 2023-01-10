af3a8ed8a1a63edad2395bf2279b3b7e10938041
Razak <moabdulrazak17@gmail.com>
2023-01-10 20:44:05 +0000

    feat(after/gitcommit): add config

---
+++ after/ftplugin/gitcommit.lua
@@ -0,0 +1,16 @@
+vim.opt_local.spell = true
+
+rvim.ftplugin_conf(
+  'cmp',
+  function(cmp)
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+        { name = 'luasnip' },
+        { name = 'dictionary' },
+        { name = 'spell' },
+        { name = 'emoji' },
+        { name = 'buffer' },
+      }),
+    })
+  end
+)
