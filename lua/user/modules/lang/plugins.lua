c67e7f315a09ab52cf4ef4b8e866bfe999e1b05f
Razak <moabdulrazak17@gmail.com>
2023-01-10 18:46:19 +0000

    chore(plugins): switch to mini.pairs from nvim-autopairs

--- lua/user/modules/lang/plugins.lua
+++ lua/user/modules/lang/plugins.lua
@@ -107,6 +107,12 @@ return {
   },
 
   {
+    'echasnovski/mini.pairs',
+    event = 'VeryLazy',
+    config = function(_, opts) require('mini.pairs').setup(opts) end,
+  },
+
+  {
     'folke/paint.nvim',
     enabled = false,
     event = 'BufReadPre',
