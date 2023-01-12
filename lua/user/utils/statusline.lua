6fab53164b567358262efebe98a5c16c1d1bb5a3
Razak <moabdulrazak17@gmail.com>
2023-01-12 23:20:16 +0000

    refactor(statusline): move lualine stuff

--- lua/user/utils/statusline.lua
+++ lua/user/utils/statusline.lua
@@ -12,49 +12,6 @@ local constants = {
   CLICK_END = '%X',
 }
 
-M.conditions = {
-  buffer_not_empty = function() return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 end,
-  hide_in_width = function() return vim.fn.winwidth(0) > 99 end,
-  check_git_workspace = function()
-    local filepath = vim.fn.expand('%:p:h')
-    local gitdir = vim.fn.finddir('.git', filepath .. ';')
-    return gitdir and #gitdir > 0 and #gitdir < #filepath
-  end,
-}
-
-function M.diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed,
-    }
-  end
-end
-
-function M.env_cleanup(venv)
-  if string.find(venv, '/') then
-    local final_venv = venv
-    for w in venv:gmatch('([^/]+)') do
-      final_venv = w
-    end
-    venv = final_venv
-  end
-  return venv
-end
-
-function M.python_env()
-  if vim.bo.filetype == 'python' then
-    local venv = vim.env.CONDA_DEFAULT_ENV
-    if venv then return string.format('(%s)', M.env_cleanup(venv)) end
-    venv = vim.env.VIRTUAL_ENV
-    if venv then return string.format('(%s)', M.env_cleanup(venv)) end
-    return ''
-  end
-  return ''
-end
-
 local function sum_lengths(list)
   return rvim.fold(function(acc, item) return acc + (item.length or 0) end, list, 0)
 end
@@ -222,6 +179,52 @@ function M.component_raw(item, opts)
     winid = win_id,
   })
   return { component = item, length = data.width, priority = priority }
+end
+
+----------------------------------------------------------------------------------------------------
+-- Lualine Stuff
+----------------------------------------------------------------------------------------------------
+M.conditions = {
+  buffer_not_empty = function() return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 end,
+  hide_in_width = function() return vim.fn.winwidth(0) > 99 end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end,
+}
+
+function M.diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
+function M.env_cleanup(venv)
+  if string.find(venv, '/') then
+    local final_venv = venv
+    for w in venv:gmatch('([^/]+)') do
+      final_venv = w
+    end
+    venv = final_venv
+  end
+  return venv
+end
+
+function M.python_env()
+  if vim.bo.filetype == 'python' then
+    local venv = vim.env.CONDA_DEFAULT_ENV
+    if venv then return string.format('(%s)', M.env_cleanup(venv)) end
+    venv = vim.env.VIRTUAL_ENV
+    if venv then return string.format('(%s)', M.env_cleanup(venv)) end
+    return ''
+  end
+  return ''
 end
 
 return M
