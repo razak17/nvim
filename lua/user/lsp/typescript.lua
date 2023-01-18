49c84dc421944285c21d337659d662258ab5ddc7
Razak <moabdulrazak17@gmail.com>
2023-01-18 03:42:49 +0000

    chore(typescript): improve performance in large files

--- lua/user/lsp/typescript.lua
+++ lua/user/lsp/typescript.lua
@@ -1,4 +1,14 @@
 local ok, which_key = rvim.safe_require('which-key')
 if ok then which_key.register({ ['<localleader>'] = { r = { name = 'Rust Tools' } } }) end
 
-require('typescript').setup({})
+require('typescript').setup({
+  server = {
+    -- NOTE: Apparently setting this to false improves performance
+    -- https://github.com/sublimelsp/LSP-typescript/issues/129#issuecomment-1281643371
+    initializationOptions = {
+      preferences = {
+        includeCompletionsForModuleExports = false,
+      },
+    },
+  },
+})
