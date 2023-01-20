64982ba0b4d556b9be0f6e20d2ac7ccabbe613ca
Razak <moabdulrazak17@gmail.com>
2023-01-20 11:26:36 +0000

    chore(which-key): silence uninstalled error when called in rust ftplugin

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,5 +1,5 @@
 vim.bo.textwidth = 120
 vim.opt_local.spell = true
 
-local ok, which_key = rvim.safe_require('which-key')
+local ok, which_key = rvim.safe_require('which-key', { silent = true })
 if ok then which_key.register({ ['<localleader>'] = { r = { name = 'Rust Tools' } } }) end
