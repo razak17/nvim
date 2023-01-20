eda95f33bebb5c03867161191872b55f3486bee9
Razak <moabdulrazak17@gmail.com>
2023-01-20 17:57:07 +0000

    feat(conceal): add lua queries

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -20,10 +20,11 @@
 (("comment_start"    @comment) (#set! conceal ""))
 
 ;; Function names
-((function_call name: (identifier) @TSNote (#eq? @TSNote "require")) (#set! conceal ""))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "print"  )) (#set! conceal ""))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "pairs"  )) (#set! conceal "P"))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "ipairs" )) (#set! conceal "I"))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "join_paths")) (#set! conceal "J"))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "require"   )) (#set! conceal ""))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "print"     )) (#set! conceal ""))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "pairs"     )) (#set! conceal "P"))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "ipairs"    )) (#set! conceal "I"))
 
 ;; table.
 ((dot_index_expression table: (identifier) @keyword  (#eq? @keyword  "math" )) (#set! conceal ""))
@@ -41,6 +42,7 @@
 (((dot_index_expression) @field (#eq? @field "vim.env"     )) (#set! conceal "$"))
 (((dot_index_expression) @field (#eq? @field "vim.o"       )) (#set! conceal "O"))
 (((dot_index_expression) @field (#eq? @field "vim.bo"      )) (#set! conceal "B"))
+(((dot_index_expression) @field (#eq? @field "vim.b"       )) (#set! conceal "b"))
 (((dot_index_expression) @field (#eq? @field "vim.wo"      )) (#set! conceal "W"))
 
 (((dot_index_expression) @field (#eq? @field "vim.keymap.set")) (#set! conceal ""))
