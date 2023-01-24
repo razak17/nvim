f0e19b8efaeef244379e97064a90260dfdbba64e
Razak <moabdulrazak17@gmail.com>
2023-01-24 16:44:39 +0000

    chore(styles): rename file

--- lua/user/bootstrap.lua
+++ lua/user/bootstrap.lua
@@ -20,5 +20,5 @@ vim.cmd([[let &packpath = &runtimepath]])
 ----------------------------------------------------------------------------------------------------
 R('user.config.defaults')
 R('user.config.settings')
-R('user.config.style')
+R('user.config.styles')
 R('user.config.lsp')
