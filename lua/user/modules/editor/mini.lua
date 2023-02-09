1ce11c65e758c5447cd10d6f3eea38241b9f1448
Razak <moabdulrazak17@gmail.com>
2023-02-09 03:54:17 +0000

    feat(surround): update mappings

--- lua/user/modules/editor/mini.lua
+++ lua/user/modules/editor/mini.lua
@@ -23,17 +23,17 @@ return {
 
   {
     'echasnovski/mini.surround',
-    keys = { 'gz' },
+    event = 'VeryLazy',
     config = function()
       require('mini.surround').setup({
         mappings = {
-          add = 'gza', -- Add surrounding in Normal and Visual modes
-          delete = 'gzd', -- Delete surrounding
-          find = 'gzf', -- Find surrounding (to the right)
-          find_left = 'gzF', -- Find surrounding (to the left)
-          highlight = 'gzh', -- Highlight surrounding
-          replace = 'gzr', -- Replace surrounding
-          update_n_lines = 'gzn', -- Update `n_lines`
+          add = 'ys', -- Add surrounding in Normal and Visual modes
+          delete = 'ds', -- Delete surrounding
+          find = 'yf', -- Find surrounding (to the right)
+          find_left = 'yF', -- Find surrounding (to the left)
+          highlight = 'yh', -- Highlight surrounding
+          replace = 'yr', -- Replace surrounding
+          update_n_lines = 'yn', -- Update `n_lines`
         },
       })
     end,
