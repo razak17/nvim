45eb5ac53bb9f5f81f9e37a9ee487b4b6f06b346
Razak <moabdulrazak17@gmail.com>
2023-02-09 20:57:17 +0000

    chore(plugins): remove unused plugins

--- lua/user/modules/lang/plugins.lua
+++ lua/user/modules/lang/plugins.lua
@@ -236,46 +236,4 @@ return {
     end,
     ft = { 'typescript', 'typescriptreact' },
   },
-
-  {
-    'folke/paint.nvim',
-    enabled = false,
-    event = 'BufReadPre',
-    config = function()
-      require('paint').setup({
-        highlights = {
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%-%s*(@%w+)',
-            hl = 'Constant',
-          },
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%[%[(@%w+)',
-            hl = 'Constant',
-          },
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%-%s*@field%s+(%S+)',
-            hl = '@field',
-          },
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%-%s*@class%s+(%S+)',
-            hl = '@variable.builtin',
-          },
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%-%s*@alias%s+(%S+)',
-            hl = '@keyword',
-          },
-          {
-            filter = { filetype = 'lua' },
-            pattern = '%s*%-%-%-%s*@param%s+(%S+)',
-            hl = '@parameter',
-          },
-        },
-      })
-    end,
-  },
 }
