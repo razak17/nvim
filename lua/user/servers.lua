04c0ce9307f4cdae6652699ab4ee0b788789a4f2
Razak <moabdulrazak17@gmail.com>
2023-02-14 21:49:05 +0000

    fix(servers): add snippetSupport and resolveSupport capabilities

--- lua/user/servers.lua
+++ lua/user/servers.lua
@@ -7,9 +7,17 @@ local M = {}
 
 local function global_capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.colorProvider = { dynamicRegistration = true }
   capabilities.textDocument.completion.completionItem.documentationFormat =
     { 'markdown', 'plaintext' }
+  capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {
+      'documentation',
+      'detail',
+      'additionalTextEdits',
+    },
+  }
   capabilities.textDocument.codeAction = {
     dynamicRegistration = false,
     codeActionLiteralSupport = {
