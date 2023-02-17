6a98a6bed6e12283cae28e9325516585ca7c8ba8
Razak <moabdulrazak17@gmail.com>
2023-02-17 00:33:36 +0000

    chore(ftplugin/html): use opt_local for settings

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,11 +1,9 @@
--- Set indent width to two spaces
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
--- Fix quirkiness in indentation
-vim.opt_local.indentkeys:remove('*<Return>')
--- setlocal indentkeys-=*<Return>
--- Make lines longer, and don't break them automatically
-vim.cmd([[setlocal tw=120 linebreak textwidth=0]])
-vim.wo.wrap = false
-vim.opt_local.matchpairs:append('<:>')
+local opt = vim.opt_local
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.matchpairs:append('<:>')
+opt.indentkeys:remove('*<Return>') -- Fix quirkiness in indentation
+
+vim.cmd([[setlocal tw=120 linebreak textwidth=0]]) -- Make lines longer, and don't break them automatically
