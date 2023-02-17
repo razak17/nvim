297f8947e93e5c735d8a63d9ec2f07c26c27a92e
Razak <moabdulrazak17@gmail.com>
2023-02-17 00:34:19 +0000

    chore(ftplugin/qf): add file

---
+++ after/ftplugin/qf.lua
@@ -0,0 +1,25 @@
+local opt = vim.opt_local
+local cmd = vim.cmd
+
+cmd.wincmd('J') -- force quickfix to open beneath all other splits
+
+opt.wrap = false
+opt.number = false -- HACK: this is forcefully set by nvim-bqf intentionally to override my settings
+opt.signcolumn = 'yes'
+opt.buflisted = false -- quickfix buffers should not pop up when doing :bn or :bp
+
+rvim.adjust_split_height(3, 10)
+opt.winfixheight = true
+----------------------------------------------------------------------------------
+-- Helper functions
+----------------------------------------------------------------------------------
+rvim.nnoremap('dd', rvim.qf.delete, { desc = 'delete current quickfix entry', buffer = 0 })
+rvim.vnoremap('d', rvim.qf.delete, { desc = 'delete selected quickfix entry', buffer = 0 })
+----------------------------------------------------------------------------------
+-- Mappings
+----------------------------------------------------------------------------------
+rvim.nnoremap('H', ':colder<CR>', { buffer = 0 })
+rvim.nnoremap('L', ':cnewer<CR>', { buffer = 0 })
+
+-- Resources and inspiration
+-- 2. https://github.com/romainl/vim-qf/blob/2e385e6d157314cb7d0385f8da0e1594a06873c5/autoload/qf.vim#L22
