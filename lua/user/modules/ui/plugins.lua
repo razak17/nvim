fd9558ecfaa662d5e15d6ad226fcf73a601b11fe
Razak <moabdulrazak17@gmail.com>
2023-02-25 01:29:26 +0000

    feat(headlines): tone down the distracting colours

--- lua/user/modules/ui/plugins.lua
+++ lua/user/modules/ui/plugins.lua
@@ -80,18 +80,32 @@ return {
     'lukas-reineke/headlines.nvim',
     ft = { 'org', 'norg', 'markdown', 'yaml' },
     config = function()
-      require('headlines').setup({
-        markdown = {
-          headline_highlights = { 'Headline1', 'Headline2', 'Headline3' },
+      hl.plugin('Headlines', {
+        theme = {
+          ['*'] = {
+            { Dash = { background = '#0b60a1', bold = true } },
+          },
+          ['horizon'] = {
+            {
+              Headline = {
+                bold = true,
+                italic = true,
+                background = { from = 'Normal', alter = 20 },
+              },
+            },
+            {
+              Headline1 = {
+                inherit = 'Headline',
+                fg = { from = 'Type' },
+              },
+            },
+          },
         },
+      })
+      require('headlines').setup({
+        markdown = { headline_highlights = { 'Headline1' } },
         org = { headline_highlights = false },
         norg = { codeblock_highlight = false },
-      })
-      hl.plugin('Headlines', {
-        { Headline1 = { background = '#003c30', foreground = 'White' } },
-        { Headline2 = { background = '#00441b', foreground = 'White' } },
-        { Headline3 = { background = '#084081', foreground = 'White' } },
-        { Dash = { background = '#0b60a1', bold = true } },
       })
     end,
   },
