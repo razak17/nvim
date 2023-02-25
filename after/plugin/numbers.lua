93862c68e3c29368d217e2648218ec54e6d98669
Razak <moabdulrazak17@gmail.com>
2023-02-25 01:25:54 +0000

    refactor(numbers): simplify logic

--- after/plugin/numbers.lua
+++ after/plugin/numbers.lua
@@ -1,10 +1,9 @@
 if not rvim then return end
 local ui = rvim.ui
+
 -- Inspiration
 -- 1. vim-relativity
 -- 2. numbers.vim - https://github.com/myusuf3/numbers.vim/blob/master/plugin/numbers.vim
-
-if not rvim then return end
 
 local api = vim.api
 local M = {}
@@ -26,59 +25,42 @@ local function is_blocked()
   local win_type = vim.fn.win_gettype()
 
   if not api.nvim_buf_is_valid(0) and not api.nvim_buf_is_loaded(0) then return true end
-
   if win_type == 'command' or vim.wo.diff or vim.wo.previewwindow then return true end
 
-  local ft_settings = ui.decorations.filetypes[vim.bo.ft]
-  local bt_settings = ui.decorations.buftypes[vim.bo.buftype]
-  if (ft_settings and not ft_settings.number) or (bt_settings and not bt_settings.number) then
-    return true
-  end
-  return false
+  local ft_settings = ui.decorations.get(vim.bo.ft, 'number', 'ft')
+  local bt_settings = ui.decorations.get(vim.bo.bt, 'number', 'bt')
+  return ft_settings == false or bt_settings == false
 end
 
 local function enable_relative_number()
-  if is_ignored() or not is_enabled then return end
-  if is_blocked() then
-    vim.wo.number = false
-    vim.wo.relativenumber = false
-    return
-  end
-  vim.wo.number = true
-  vim.wo.relativenumber = true
+  if not is_enabled then return end
+  if is_ignored() then return end
+  local enabled = not is_blocked()
+  vim.wo.number, vim.wo.relativenumber = enabled, enabled
 end
 
 local function disable_relative_number()
   if is_ignored() then return end
-  if is_blocked() then
-    vim.wo.number = false
-    vim.wo.relativenumber = false
-    return
-  end
-  vim.wo.number = true
-  vim.wo.relativenumber = false
+  vim.wo.number, vim.wo.relativenumber = not is_blocked(), false
 end
 
 rvim.command('ToggleRelativeNumber', function()
   is_enabled = not is_enabled
   if is_enabled then
     enable_relative_number()
-    return
+  else
+    disable_relative_number()
   end
-  disable_relative_number()
 end)
-map('n', '<leader>or', '<cmd>ToggleRelativeNumber<CR>', { desc = 'toggle relativenumber' })
 
 rvim.augroup('ToggleRelativeLineNumbers', {
   {
     event = { 'BufEnter', 'FileType', 'FocusGained', 'InsertLeave' },
-    pattern = { '*' },
-    command = function() enable_relative_number() end,
+    command = enable_relative_number,
   },
   {
     event = { 'FocusLost', 'BufLeave', 'InsertEnter', 'TermOpen' },
-    pattern = { '*' },
-    command = function() disable_relative_number() end,
+    command = disable_relative_number,
   },
 })
 
