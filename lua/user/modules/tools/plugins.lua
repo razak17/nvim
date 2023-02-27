4228aaa3b0fe933d085e52ac55b6c2a77acded3c
Razak <moabdulrazak17@gmail.com>
2023-02-27 08:01:27 +0000

    refactor(plugins): remove jaq and sniprun modules

--- lua/user/modules/tools/plugins.lua
+++ lua/user/modules/tools/plugins.lua
@@ -197,6 +197,77 @@ return {
   },
 
   {
+    'is0n/jaq-nvim',
+    cmd = 'Jaq',
+    keys = {
+      { '<leader>rr', ':silent only | Jaq<CR>', desc = 'jaq: run' },
+    },
+    config = function()
+      require('jaq-nvim').setup({
+        cmds = {
+          default = 'term',
+          external = {
+            typescript = 'ts-node %',
+            javascript = 'node %',
+            python = 'python %',
+            rust = 'cargo run',
+            cpp = 'g++ % -o $fileBase && ./$fileBase',
+            go = 'go run %',
+          },
+        },
+        behavior = { startinsert = true },
+        terminal = { position = 'vert', size = 60 },
+      })
+      rvim.augroup('JaqConfig', {
+        {
+          event = { 'Filetype' },
+          pattern = { 'Jaq' },
+          command = function() vim.api.nvim_win_set_config(0, { border = rvim.ui.current.border }) end,
+        },
+      })
+    end,
+  },
+
+  {
+    'michaelb/sniprun',
+    cmd = { 'SnipRun', 'SnipInfo', 'SnipReset', 'SnipClose', 'SnipLive' },
+    build = 'bash ./install.sh',
+    keys = {
+      { '<localleader>sr', '<cmd>SnipRun<CR>', desc = 'sniprun: run' },
+      { '<localleader>sr', '<cmd>SnipRun<CR>', desc = 'sniprun: run', mode = 'v' },
+      { '<localleader>so', '<cmd>SnipRunOperator<CR>', desc = 'sniprun: run operator' },
+      { '<localleader>sc', '<cmd>SnipClose<CR>', desc = 'sniprun: close' },
+      { '<localleader>sl', '<cmd>SnipLive<CR>', desc = 'sniprun: live mode' },
+      { '<localleader>sx', '<cmd>SnipReset<CR>', desc = 'sniprun: reset' },
+    },
+    config = function()
+      local hl = rvim.highlight
+      require('sniprun').setup({
+        live_mode_toggle = 'enable',
+        snipruncolors = {
+          SniprunVirtualTextOk = {
+            bg = hl.get('FloatTitle', 'bg'),
+            fg = hl.get('FloatTitle', 'fg'),
+            ctermbg = 'LightBlue',
+            cterfg = 'Black',
+          },
+          SniprunFloatingWinOk = { fg = hl.get('CursorLine', 'fg'), ctermfg = 'LightBlue' },
+          SniprunVirtualTextErr = {
+            bg = hl.get('DiagnosticVirtualTextError', 'bg'),
+            fg = hl.get('DiagnosticVirtualTextError', 'fg'),
+            ctermbg = 'DarkRed',
+            cterfg = 'Black',
+          },
+          SniprunFloatingWinErr = {
+            fg = hl.get('DiagnosticVirtualTextError', 'fg'),
+            ctermfg = 'DarkRed',
+          },
+        },
+      })
+    end,
+  },
+
+  {
     'razak17/lab.nvim',
     event = 'InsertEnter',
     build = 'cd js && npm ci',
