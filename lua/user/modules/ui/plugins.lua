6ab41c202061c210340dbe6f7ff92a027acd9819
Razak <moabdulrazak17@gmail.com>
2023-02-28 08:14:30 +0000

    refactor(dressing): move out of own file

--- lua/user/modules/ui/plugins.lua
+++ lua/user/modules/ui/plugins.lua
@@ -62,6 +62,44 @@ return {
   },
 
   {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    config = function()
+      -- NOTE: the limit is half the max lines because this is the cursor theme so
+      -- unless the cursor is at the top or bottom it realistically most often will
+      -- only have half the screen available
+      local function get_height(self, _, max_lines)
+        local results = #self.finder.results
+        local PADDING = 4 -- this represents the size of the telescope window
+        local LIMIT = math.floor(max_lines / 2)
+        return (results <= (LIMIT - PADDING) and results + PADDING or LIMIT)
+      end
+
+      require('dressing').setup({
+        input = {
+          insert_only = false,
+          border = rvim.ui.current.border,
+          win_options = { winblend = 2 },
+        },
+        select = {
+          get_config = function()
+            return {
+              backend = 'telescope',
+              telescope = require('telescope.themes').get_cursor({
+                layout_config = { height = get_height },
+                borderchars = rvim.ui.border.ui_select,
+              }),
+            }
+          end,
+          telescope = require('telescope.themes').get_dropdown({
+            layout_config = { height = get_height },
+          }),
+        },
+      })
+    end,
+  },
+
+  {
     'j-hui/fidget.nvim',
     event = 'BufReadPre',
     config = function()
