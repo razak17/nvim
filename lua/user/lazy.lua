7273f9e2391adc4c1056f486facd1f5512afe822
Razak <moabdulrazak17@gmail.com>
2023-03-01 13:32:47 +0000

    refactor(lazy): simplify loading plugins

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -4,32 +4,20 @@ local Lazy = {}
 Lazy.__index = Lazy
 
 function Lazy:load_plugins()
+  local modules_dir = join_paths(rvim.get_user_dir(), 'modules')
+
   self.repos = {}
 
-  local function get_plugins_list()
-    local list = {}
-    local modules_dir = join_paths(rvim.get_user_dir(), 'modules')
-    local tmp = vim.split(fn.globpath(modules_dir, '*/*.lua'), '\n')
-    for _, f in ipairs(tmp) do
-      list[#list + 1] = string.match(f, 'lua/(.+).lua$')
-    end
-    return list
+  local list = {}
+  local tmp = vim.split(fn.globpath(modules_dir, '*/*.lua'), '\n')
+  for _, f in ipairs(tmp) do
+    list[#list + 1] = string.match(f, 'lua/(.+).lua$')
   end
-
-  local plugins = get_plugins_list()
 
   if not rvim.plugins.SANE then return end
 
-  for _, m in ipairs(plugins) do
-    local suffix = string.match(m, '.*/(.*)')
-    local module = require(m)
-    if suffix ~= 'plugins' then
-      self.repos[#self.repos + 1] = module
-    else
-      for _, repo in ipairs(module) do
-        self.repos[#self.repos + 1] = repo
-      end
-    end
+  for _, m in ipairs(list) do
+    self.repos[#self.repos + 1] = require(m)
   end
 end
 
@@ -47,7 +35,6 @@ function Lazy:bootstrap()
   end
   vim.opt.rtp:prepend(lazy_path)
   self:load_plugins()
-  local lazy = require('lazy')
   local opts = {
     root = join_paths(rvim.get_pack_dir(), 'lazy'),
     defaults = { lazy = true },
@@ -57,7 +44,6 @@ function Lazy:bootstrap()
       path = join_paths(vim.env.HOME, 'personal', 'workspace', 'coding', 'plugins'),
       patterns = { 'razak17' },
     },
-    install = { colorscheme = { 'zephyr', 'habamax' } },
     ui = { border = rvim.ui.current.border },
     checker = {
       enabled = true,
@@ -76,7 +62,7 @@ function Lazy:bootstrap()
     },
     readme = { root = join_paths(rvim.get_cache_dir(), 'lazy', 'readme') },
   }
-  lazy.setup(self.repos, opts)
+  require('lazy').setup(self.repos, opts)
   map('n', '<localleader>L', '<cmd>Lazy<CR>', { desc = 'lazygit: toggle ui' })
 end
 
