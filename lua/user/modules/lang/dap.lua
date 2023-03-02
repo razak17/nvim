927a7dbcc9d078d32dcd8546d828fc07040b70bb
Razak <moabdulrazak17@gmail.com>
2023-03-02 14:43:46 +0000

    chore(dap-vscode-js): use in js/ts files

--- lua/user/modules/lang/dap.lua
+++ lua/user/modules/lang/dap.lua
@@ -45,106 +45,112 @@ local function log_breakpoint()
 end
 
 return {
-  'mfussenegger/nvim-dap',
-  keys = {
-    { '<localleader>db', toggle_breakpoint, desc = 'dap: toggle breakpoint' },
-    { '<localleader>dB', set_breakpoint, desc = 'dap: set conditional breakpoint' },
-    { '<localleader>dc', continue, desc = 'dap: continue or start debugging' },
-    { '<localleader>dC', clear_breakpoints, desc = 'dap: clear breakpoint' },
-    { '<localleader>de', step_out, desc = 'dap: step out' },
-    { '<localleader>dh', step_back, desc = 'dap: step back' },
-    { '<localleader>di', step_into, desc = 'dap: step into' },
-    { '<localleader>dl', run_last, desc = 'dap REPL: run last' },
-    { '<localleader>dL', log_breakpoint, desc = 'dap: log breakpoint' },
-    { '<localleader>do', step_over, desc = 'dap: step over' },
-    { '<localleader>dr', restart, desc = 'dap: restart' },
-    { '<localleader>dw', hover, desc = 'dap: hover' },
-    { '<localleader>dx', terminate, desc = 'dap: terminate' },
-    { '<localleader>dt', repl_toggle, desc = 'dap REPL: toggle' },
-    { '<localleader>da', attach, desc = 'dap(node): attach' },
-    { '<localleader>dA', attach_to_remote, desc = 'dap(node): attach to remote' },
-    {
-      '<localleader>du',
-      function() require('dapui').toggle({ reset = true }) end,
-      desc = 'dap-ui: toggle',
+  {
+    'mfussenegger/nvim-dap',
+    keys = {
+      { '<localleader>db', toggle_breakpoint, desc = 'dap: toggle breakpoint' },
+      { '<localleader>dB', set_breakpoint, desc = 'dap: set conditional breakpoint' },
+      { '<localleader>dc', continue, desc = 'dap: continue or start debugging' },
+      { '<localleader>dC', clear_breakpoints, desc = 'dap: clear breakpoint' },
+      { '<localleader>de', step_out, desc = 'dap: step out' },
+      { '<localleader>dh', step_back, desc = 'dap: step back' },
+      { '<localleader>di', step_into, desc = 'dap: step into' },
+      { '<localleader>dl', run_last, desc = 'dap REPL: run last' },
+      { '<localleader>dL', log_breakpoint, desc = 'dap: log breakpoint' },
+      { '<localleader>do', step_over, desc = 'dap: step over' },
+      { '<localleader>dr', restart, desc = 'dap: restart' },
+      { '<localleader>dw', hover, desc = 'dap: hover' },
+      { '<localleader>dx', terminate, desc = 'dap: terminate' },
+      { '<localleader>dt', repl_toggle, desc = 'dap REPL: toggle' },
+      { '<localleader>da', attach, desc = 'dap(node): attach' },
+      { '<localleader>dA', attach_to_remote, desc = 'dap(node): attach to remote' },
+      {
+        '<localleader>du',
+        function() require('dapui').toggle({ reset = true }) end,
+        desc = 'dap-ui: toggle',
+      },
     },
-  },
-  config = function()
-    local fn, icons = vim.fn, rvim.ui.icons
-    local dap = require('dap')
+    config = function()
+      local fn, icons = vim.fn, rvim.ui.icons
+      local dap = require('dap')
 
-    -- DON'T automatically stop at exceptions
-    dap.defaults.fallback.exception_breakpoints = {}
+      -- DON'T automatically stop at exceptions
+      dap.defaults.fallback.exception_breakpoints = {}
 
-    fn.sign_define('DapBreakpoint', {
-      text = icons.misc.bug_alt,
-      texthl = 'DapBreakpoint',
-      linehl = '',
-      numhl = '',
-    })
+      fn.sign_define('DapBreakpoint', {
+        text = icons.misc.bug_alt,
+        texthl = 'DapBreakpoint',
+        linehl = '',
+        numhl = '',
+      })
 
-    fn.sign_define('DapBreakpointRejected', {
-      text = icons.misc.bug_alt,
-      texthl = 'DapBreakpointRejected',
-      linehl = '',
-      numhl = '',
-    })
+      fn.sign_define('DapBreakpointRejected', {
+        text = icons.misc.bug_alt,
+        texthl = 'DapBreakpointRejected',
+        linehl = '',
+        numhl = '',
+      })
 
-    fn.sign_define('DapStopped', {
-      text = icons.misc.dap_hollow,
-      texthl = 'DapStopped',
-      linehl = '',
-      numhl = '',
-    })
+      fn.sign_define('DapStopped', {
+        text = icons.misc.dap_hollow,
+        texthl = 'DapStopped',
+        linehl = '',
+        numhl = '',
+      })
 
-    -- python
-    require('user.dap.python')
-    -- go
-    require('user.dap.go')
-    -- typescript
-    require('user.dap.typescript')
-    -- Lua
-    require('user.dap.lua')
-    -- codelldb
-    require('user.dap.rust')
-  end,
-  dependencies = {
-    {
-      'mxsdev/nvim-dap-vscode-js',
-      opts = {
-        node_path = 'node',
-        debugger_path = rvim.get_runtime_dir() .. '/site/pack/lazy/vscode-js-debug',
-        log_file_path = rvim.get_cache_dir() .. '/dap_vscode_js.log',
-        adapters = {
-          'pwa-node',
-          'pwa-chrome',
-          'pwa-msedge',
-          'node-terminal',
-          'pwa-extensionHost',
+      -- python
+      require('user.dap.python')
+      -- go
+      require('user.dap.go')
+      -- typescript
+      require('user.dap.typescript')
+      -- Lua
+      require('user.dap.lua')
+      -- codelldb
+      require('user.dap.rust')
+    end,
+    dependencies = {
+      {
+        'rcarriga/nvim-dap-ui',
+        config = function()
+          local dap = require('dap')
+          local dapui = require('dapui')
+
+          dapui.setup({ windows = { indent = 2 }, floating = { border = rvim.ui.current.border } })
+
+          dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
+          dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
+          dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
+        end,
+      },
+      {
+        'theHamsta/nvim-dap-virtual-text',
+        opts = {
+          enabled = true,
+          enabled_commands = true,
+          highlight_changed_variables = true,
+          all_frames = true,
         },
       },
     },
-    { 'microsoft/vscode-js-debug', build = 'npm install --legacy-peer-deps && npm run compile' },
-    {
-      'rcarriga/nvim-dap-ui',
-      config = function()
-        local dap = require('dap')
-        local dapui = require('dapui')
-
-        dapui.setup({ windows = { indent = 2 }, floating = { border = rvim.ui.current.border } })
-
-        dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
-        dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
-        dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
-      end,
-    },
-    {
-      'theHamsta/nvim-dap-virtual-text',
-      opts = {
-        enabled = true,
-        enabled_commands = true,
-        highlight_changed_variables = true,
-        all_frames = true,
+  },
+  {
+    'microsoft/vscode-js-debug',
+    build = 'npm install --legacy-peer-deps && npm run compile',
+  },
+  {
+    'mxsdev/nvim-dap-vscode-js',
+    ft = { 'typescriptreact', 'typescript', 'javascript', 'javascriptreact' },
+    opts = {
+      node_path = 'node',
+      debugger_path = rvim.get_runtime_dir() .. '/site/pack/lazy/vscode-js-debug',
+      log_file_path = rvim.get_cache_dir() .. '/dap_vscode_js.log',
+      adapters = {
+        'pwa-node',
+        'pwa-chrome',
+        'pwa-msedge',
+        'node-terminal',
+        'pwa-extensionHost',
       },
     },
   },
