9b24b47a59c359f0d14425385c2778b3babbfdc2
Razak <moabdulrazak17@gmail.com>
2023-03-03 17:56:41 +0000

    fix(treesitter): disable syntax entirely
    as this was causing typescript react to trigger syntax highlighting

--- lua/user/modules/lang/treesitter.lua
+++ lua/user/modules/lang/treesitter.lua
@@ -15,7 +15,10 @@ return {
 
       treesitter_configs.setup({
         auto_install = true,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
         context_commentstring = {
           enable = true,
           enable_autocmd = false,
