e5438e4f81210226ec42cdaa561b96373771b3d4
Razak <moabdulrazak17@gmail.com>
2023-03-05 19:04:01 +0000

    chore(lsp): simplify server config

--- lua/user/servers.lua
+++ lua/user/servers.lua
@@ -17,6 +17,7 @@ local servers = {
   sqls = {},
   svelte = {},
   eslint = {},
+  yamlls = {},
   tsserver = {
     on_attach = function(client)
       client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
@@ -30,31 +31,23 @@ local servers = {
   vimls = {},
   prosemd_lsp = {
     root_dir = function(fname) return require('lspconfig/util').root_pattern('README.md')(fname) end,
-    single_file_support = false,
   },
   rust_analyzer = {},
   denols = {
     root_dir = function(fname)
       return require('lspconfig/util').root_pattern('deno.json', 'deno.jsonc')(fname)
     end,
-    single_file_support = false,
   },
   emmet_ls = {
     filetypes = { 'astro', 'html', 'css', 'sass', 'scss', 'typescriptreact', 'javascriptreact' },
     root_dir = function(fname) return require('lspconfig/util').root_pattern('package.json')(fname) end,
-    single_file_support = false,
   },
   --- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
   gopls = {
     settings = {
       gopls = {
         gofumpt = true,
-        codelenses = {
-          generate = true,
-          gc_details = false,
-          test = true,
-          tidy = true,
-        },
+        codelenses = { generate = true, gc_details = false, test = true, tidy = true },
         hints = {
           assignVariableTypes = true,
           compositeLiteralFields = true,
@@ -63,10 +56,7 @@ local servers = {
           parameterNames = true,
           rangeVariableTypes = true,
         },
-        analyses = {
-          unusedparams = true,
-          loopclosure = true,
-        },
+        analyses = { unusedparams = true, loopclosure = true },
         usePlaceholders = true,
         completeUnimported = true,
         staticcheck = true,
@@ -82,14 +72,6 @@ local servers = {
       -- @see: https://github.com/nvim-telescope/telescope.nvim/issues/964
       client.server_capabilities.workspaceSymbolProvider = false
     end,
-    root_dir = function(fname)
-      return require('lspconfig/util').root_pattern(
-        '.graphqlrc*',
-        '.graphql.config.*',
-        'graphql.config.*'
-      )(fname)
-    end,
-    single_file_support = false,
   },
   jsonls = {
     init_options = { provideFormatter = false },
@@ -100,7 +82,6 @@ local servers = {
       },
     },
     root_dir = function(fname) return require('lspconfig/util').root_pattern('package.json')(fname) end,
-    single_file_support = false,
   },
   pyright = {
     python = {
@@ -138,68 +119,23 @@ local servers = {
   end,
   tailwindcss = {
     -- cmd = { '/home/razak/.bun/bin/tailwindcss-language-server', '--stdio' },
-    -- cmd = { '/home/razak/.local/share/nvim/mason/bin/tailwindcss-language-server', '--stdio' },
-    -- cmd = { 'tailwindcss-language-server', '--stdio' },
     root_dir = function(fname)
-      return require('lspconfig/util').root_pattern(
-        'tailwind.config.js',
-        'tailwind.config.cjs',
-        'tailwind.js',
-        'tailwind.cjs'
-      )(fname)
+      return require('lspconfig/util').root_pattern('tailwind.config.js', 'tailwind.config.cjs')(
+        fname
+      )
     end,
-    single_file_support = false,
   },
   vuels = {
     setup = {
       root_dir = function(fname)
-        return require('lspconfig/util').root_pattern('package.json', 'vue.config.js')(fname)
+        return require('lspconfig/util').root_pattern('vue.config.js', 'package.json')(fname)
       end,
       init_options = {
         config = {
           vetur = {
-            completion = {
-              autoImport = true,
-              tagCasing = 'kebab',
-              useScaffoldSnippets = true,
-            },
             useWorkspaceDependencies = true,
-            validation = {
-              script = true,
-              style = true,
-              template = true,
-            },
-          },
-        },
-      },
-    },
-  },
-  yamlls = {
-    settings = {
-      yaml = {
-        hover = true,
-        completion = true,
-        validate = true,
-        customTags = {
-          '!reference sequence', -- necessary for gitlab-ci.yaml files
-        },
-        schemaStore = {
-          enable = true,
-          url = 'https://www.schemastore.org/api/json/catalog.json',
-        },
-        schemas = {
-          kubernetes = {
-            'daemon.{yml,yaml}',
-            'manager.{yml,yaml}',
-            'restapi.{yml,yaml}',
-            'role.{yml,yaml}',
-            'role_binding.{yml,yaml}',
-            '*onfigma*.{yml,yaml}',
-            '*ngres*.{yml,yaml}',
-            '*ecre*.{yml,yaml}',
-            '*eployment*.{yml,yaml}',
-            '*ervic*.{yml,yaml}',
-            'kubectl-edit*.yaml',
+            completion = { autoImport = true, tagCasing = 'kebab', useScaffoldSnippets = true },
+            validation = { script = true, style = true, template = true },
           },
         },
       },
