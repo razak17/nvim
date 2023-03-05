912771850a4f7b8a11bca0cefe875df47633d8a2
Razak <moabdulrazak17@gmail.com>
2023-03-05 16:47:43 +0000

    chore(ui): reduce truncation of error notifications

--- lua/user/modules/ui/notify.lua
+++ lua/user/modules/ui/notify.lua
@@ -15,7 +15,7 @@ return {
     local notify = require('notify')
 
     notify.setup({
-      max_width = function() return math.floor(vim.o.columns * 0.4) end,
+      max_width = function() return math.floor(vim.o.columns * 0.6) end,
       max_height = function() return math.floor(vim.o.lines * 0.8) end,
       background_colour = 'NormalFloat',
       on_open = function(win)
