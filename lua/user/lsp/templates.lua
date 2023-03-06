01010f97fe86c64e657d324ecb7e721523dc9f5b
Razak <moabdulrazak17@gmail.com>
2023-03-06 08:51:19 +0000

    chore(lsp): simplify generating templates

--- lua/user/lsp/templates.lua
+++ lua/user/lsp/templates.lua
@@ -2,54 +2,26 @@ local M = {}
 
 local uv, fmt = vim.loop, string.format
 local ftplugin_dir = rvim.lsp.templates_dir
-
+-- stylua: ignore
 local configured_filetypes = {
-  'astro',
-  'lua',
-  'javascript',
-  'javascriptreact',
-  'typescript',
-  'typescriptreact',
-  'python',
-  'go',
-  'rust',
-  'yaml',
-  'vue',
-  'vim',
-  'json',
-  'jsonc',
-  'html',
-  'css',
-  'sh',
-  'zsh',
-  'markdown',
-  'graphql',
-  'sql',
-  'prisma',
-  'svelte',
+  'astro', 'lua', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'python', 'go', 'rust', 'yaml',
+  'vue', 'vim', 'json', 'jsonc', 'html', 'css', 'sh', 'zsh', 'markdown', 'graphql', 'sql', 'prisma', 'svelte',
 }
-
----Write data to a file
----@param path string can be full or relative to `cwd`
----@param txt string|table text to be written, uses `vim.inspect` internally for tables
----@param flag string used to determine access mode, common flags: "w" for `overwrite` or "a" for `append`
-local function write_file(path, txt, flag)
-  local data = type(txt) == 'string' and txt or vim.inspect(txt)
-  uv.fs_open(path, flag, 438, function(open_err, fd)
-    assert(not open_err, open_err)
-    uv.fs_write(fd, data, -1, function(write_err)
-      assert(not write_err, write_err)
-      uv.fs_close(fd, function(close_err) assert(not close_err, close_err) end)
-    end)
-  end)
-end
 
 ---Write command to file
 ---@param filename string the filetype for praticular server
 ---@param server_name string can be any server supported by nvim-lsp-installer
 local function write_manager(filename, server_name)
   local cmd = fmt([[require("user.lsp.manager").setup(%q)]], server_name)
-  write_file(filename, fmt('%s\n', cmd), 'a')
+  local text = fmt('%s\n', cmd)
+  local data = type(text) == 'string' and text or vim.inspect(text)
+  uv.fs_open(filename, 'a', 438, function(open_err, fd)
+    assert(not open_err, open_err)
+    uv.fs_write(fd, data, -1, function(write_err)
+      assert(not write_err, write_err)
+      uv.fs_close(fd, function(close_err) assert(not close_err, close_err) end)
+    end)
+  end)
 end
 
 ---Get supported filetypes per server
@@ -63,7 +35,10 @@ local function get_supported_filetypes(server_name)
   local config = require('user.servers')(server_name)
   -- override filetypes from lspconfig
   if config and config.filetypes then filetypes = config.filetypes end
-  return filetypes
+  return vim.tbl_filter(
+    function(ft) return vim.tbl_contains(configured_filetypes, ft) end,
+    filetypes or {}
+  )
 end
 
 ---Get supported servers per filetype
@@ -83,21 +58,11 @@ function M.remove_template_files()
   end
 end
 
----Get filetypes for particular server
----@param server_name string can be any server supported by nvim-lsp-installer
----@return string[] list of filtetypes for server_name
-local function getFileTypes(server_name)
-  return vim.tbl_filter(
-    function(ft) return vim.tbl_contains(configured_filetypes, ft) end,
-    get_supported_filetypes(server_name) or {}
-  )
-end
-
 ---Generates an ftplugin file based on the server_name in the selected directory
 ---@param server_name string name of a valid language server, e.g. pyright, gopls, tsserver, etc.
 ---@param dir string the full path to the desired directory
 local function generate_ftplugin(server_name, dir)
-  local filetypes = getFileTypes(server_name)
+  local filetypes = get_supported_filetypes(server_name)
   if not filetypes then return end
   for _, filetype in ipairs(filetypes) do
     filetype = filetype:match('%.([^.]*)$') or filetype
@@ -110,6 +75,7 @@ end
 ---The files are generated to a runtimepath: "$RVIM_RUNTIME_DIR/site/after/ftplugin/template.lua"
 function M.generate_templates(servers_names)
   servers_names = servers_names or get_supported_servers()
+---Remove Templates
   M.remove_template_files()
   if not rvim.is_directory(ftplugin_dir) then vim.fn.mkdir(ftplugin_dir, 'p') end
   for _, server in ipairs(servers_names) do
