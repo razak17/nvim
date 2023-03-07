cf7b9d76435a58edbe49fe347064d38e7010f84f
Razak <moabdulrazak17@gmail.com>
2023-03-07 21:32:45 +0000

    refactor(conceal): move to after/plugin

--- after/plugin/autocommands.lua
+++ after/plugin/autocommands.lua
@@ -1,6 +1,6 @@
 if not rvim or not rvim.plugins.enable then return end
 
-local fn, api, fmt = vim.fn, vim.api, string.format
+local fn, api = vim.fn, vim.api
 
 local smart_close_filetypes = {
   'help',
@@ -232,35 +232,4 @@ rvim.augroup('Utilities', {
     vim.opt_local.formatoptions:remove('r')
     vim.opt_local.formatoptions:remove('o')
   end,
-})
-
-local conceal_html_class = function(bufnr)
-  local namespace = vim.api.nvim_create_namespace('HTMLClassConceal')
-  local language_tree = vim.treesitter.get_parser(bufnr, 'html')
-  local syntax_tree = language_tree:parse()
-  local root = syntax_tree[1]:root()
-
-  local query = vim.treesitter.parse_query(
-    'html',
-    [[
-    ((attribute
-        (attribute_name) @att_name (#eq? @att_name "class")
-        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
-    ]]
-  )
-
-  for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_()) do
-    local start_row, start_col, end_row, end_col = captures[2]:range()
-    vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
-      end_line = end_row,
-      end_col = end_col,
-      conceal = metadata[2].conceal,
-    })
-  end
-end
-
-rvim.augroup('HTMLClassConceal', {
-  event = { 'BufEnter', 'BufWritePost', 'TextChanged', 'InsertLeave' },
-  pattern = { '*.html', '*.svelte', '*.astro' },
-  command = function() conceal_html_class(vim.api.nvim_get_current_buf()) end,
 })
---
+++ after/plugin/conceal.lua
@@ -0,0 +1,32 @@
+local api = vim.api
+
+local conceal_html_class = function(bufnr)
+  local namespace = api.nvim_create_namespace('HTMLClassConceal')
+  local language_tree = vim.treesitter.get_parser(bufnr, 'html')
+  local syntax_tree = language_tree:parse()
+  local root = syntax_tree[1]:root()
+
+  local query = vim.treesitter.parse_query(
+    'html',
+    [[
+    ((attribute
+        (attribute_name) @att_name (#eq? @att_name "class")
+        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
+    ]]
+  )
+
+  for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_()) do
+    local start_row, start_col, end_row, end_col = captures[2]:range()
+    api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
+      end_line = end_row,
+      end_col = end_col,
+      conceal = metadata[2].conceal,
+    })
+  end
+end
+
+rvim.augroup('HTMLClassConceal', {
+  event = { 'BufEnter', 'BufWritePost', 'TextChanged', 'InsertLeave' },
+  pattern = { '*.html', '*.svelte', '*.astro' },
+  command = function() conceal_html_class(api.nvim_get_current_buf()) end,
+})
