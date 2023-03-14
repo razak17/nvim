1cabaf6abd11296501526e29ad26a965d8c14056
Razak <moabdulrazak17@gmail.com>
2023-03-14 19:12:06 +0000

    fix(flatten): ensure new files open in current window

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,4 +1,4 @@
-local api, fn, ui, fmt = vim.api, vim.fn, rvim.ui, string.format
+local api, fn, cmd, fmt, ui = vim.api, vim.fn, vim.cmd, string.format, rvim.ui
 local hl, border = rvim.highlight, ui.current.border
 
 return {
@@ -326,14 +326,22 @@ return {
   {
     'willothy/flatten.nvim',
     lazy = false,
-    opts = {
+    priority = 1001,
+    config = {
+      window = { open = 'current' },
       callbacks = {
-        pre_open = function() require('toggleterm').toggle() end,
-        post_open = function(_, winnr)
-          require('toggleterm').toggle()
-          api.nvim_set_current_win(winnr)
-        end,
         block_end = function() require('toggleterm').toggle() end,
+        pre_open = function() require('toggleterm').toggle() end,
+        post_open = function(bufnr, winnr)
+          local term = require('toggleterm.terminal').get_last_focused()
+          if term and term:is_float() then
+            term:close()
+            cmd.buffer(bufnr)
+          else
+            require('toggleterm').toggle()
+            api.nvim_set_current_win(winnr)
+          end
+        end,
       },
     },
   },
