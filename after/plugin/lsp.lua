f11d0c0d4e17481bf3a0bfd438017aff80aa4d59
Razak <moabdulrazak17@gmail.com>
2023-03-22 22:42:55 +0000

    refactor(lsp)!: remove smart diagnostic qf command

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,12 +47,6 @@ local function clients_by_capability(bufnr, capability)
   )
 end
 
----@param buf integer
----@return boolean
-local function is_buffer_valid(buf)
-  return buf and api.nvim_buf_is_loaded(buf) and api.nvim_buf_is_valid(buf)
-end
-
 --- Create augroups for each LSP feature and track which capabilities each client
 --- registers in a buffer local table
 ---@param bufnr integer
@@ -151,9 +145,7 @@ local function setup_autocommands(client, bufnr)
       event = { 'BufEnter', 'CursorHold', 'InsertLeave' },
       desc = 'LSP: Code Lens',
       buffer = bufnr,
-      command = function(args)
-        if is_buffer_valid(args.buf) then lsp.codelens.refresh() end
-      end,
+      command = function() pcall(lsp.codelens.refresh) end,
     }
   end)
   vim.b[bufnr].lsp_events = events
@@ -415,43 +407,9 @@ rvim.augroup('LspSetupCommands', {
 local command = rvim.command
 
 command('LspFormat', function() format({ bufnr = 0, async = false }) end)
+command('LspDiagnostics', function() diagnostic.setqflist({ open = true }) end)
 
--- A helper function to auto-update the quickfix list when new diagnostics come
--- in and close it once everything is resolved. This functionality only runs whilst
--- the list is open.
--- similar functionality is provided by: https://github.com/onsails/diaglist.nvim
-do
-  ---@type integer?
-  local id
-  local TITLE = 'DIAGNOSTICS'
-  -- A helper function to auto-update the quickfix list when new diagnostics come
-  -- in and close it once everything is resolved. This functionality only runs whilst
-  -- the list is open.
-  -- similar functionality is provided by: https://github.com/onsails/diaglist.nvim
-  local function smart_quickfix_diagnostics()
-    if not is_buffer_valid(api.nvim_get_current_buf()) then return end
-
-    diagnostic.setqflist({ open = false, title = TITLE })
-    rvim.list.toggle.qf()
-
-    if not rvim.list.is_open() and id then
-      api.nvim_del_autocmd(id)
-      id = nil
-    end
-
-    if not id then
-      id = api.nvim_create_autocmd('DiagnosticChanged', {
-        callback = function()
-          -- skip QF lists that we did not populate
-          if not rvim.list.is_open() or fn.getqflist({ title = 0 }).title ~= TITLE then return end
-          diagnostic.setqflist({ open = false, title = TITLE })
-          if #fn.getqflist() == 0 then rvim.list.toggle.qf() end
-        end,
-      })
-    end
-  end
-  command('LspDiagnostics', smart_quickfix_diagnostics)
-end
+map('n', '<leader>ll', '<Cmd>LspDiagnostics<CR>', { desc = 'toggle quickfix diagnostics' })
 
 ----------------------------------------------------------------------------------------------------
 -- Signs
