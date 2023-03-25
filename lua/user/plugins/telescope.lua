53f2751394c9b934cb21444bb6b38c2cc67ceeef
Razak <moabdulrazak17@gmail.com>
2023-03-25 22:24:56 +0000

    chore(telescope): remove WebSearch command
    I rarely use it

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -82,47 +82,6 @@ local function b(picker, opts)
   return function() require('telescope.builtin')[picker](opts) end
 end
 
-local function cmd(alias, command)
-  return vim.api.nvim_create_user_command(alias, command, { nargs = 0 })
-end
-
-cmd('WebSearch', function()
-  local search = vim.fn.input('Search String: ')
-  local pickers = require('telescope.pickers')
-  local actions = require('telescope.actions')
-  local finders = require('telescope.finders')
-
-  local searcher = function(opts)
-    opts = opts or {}
-    pickers
-      .new(opts, {
-        prompt_title = 'Web Search',
-        finder = finders.new_table({
-          results = {
-            { 'Github', ('https://github.com/search?q=' .. search:gsub(' ', '+')) },
-            { 'DuckDuckGo', ('https://html.duckduckgo.com/html?q=' .. search:gsub(' ', '+')) },
-            { 'Google Search', ('https://www.google.com/search?q=' .. search:gsub(' ', '+')) },
-            { 'Youtube', ('https://youtube.com/results?search_query=' .. search:gsub(' ', '+')) },
-          },
-          entry_maker = function(entry)
-            return { value = entry, display = entry[1], ordinal = entry[1] }
-          end,
-        }),
-        sorter = require('telescope.config').values.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = require('telescope.actions.state').get_selected_entry()
-            rvim.open(selection['value'][2])
-          end)
-          return true
-        end,
-      })
-      :find()
-  end
-  searcher(rvim.telescope.minimal_ui())
-end)
-
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
@@ -140,7 +99,6 @@ return {
     { '<leader>fp', projects, desc = 'projects' },
     { '<leader>fr', b('resume'), desc = 'resume last picker' },
     { '<leader>fs', live_grep, desc = 'find string' },
-    { '<leader>fS', '<cmd>WebSearch<CR>', desc = 'web search' },
     { '<leader>fu', undo, desc = 'undo' },
     { '<leader>fw', b('grep_string'), desc = 'find word' },
     { '<leader>fva', b('autocommands'), desc = 'autocommands' },
