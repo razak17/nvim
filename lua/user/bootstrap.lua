9cb676a0b947d4fd91379ac571829cd4a5708e87
Razak <moabdulrazak17@gmail.com>
2023-03-28 07:16:06 +0000

    chore(bootstrap): update projects directory

--- lua/user/bootstrap.lua
+++ lua/user/bootstrap.lua
@@ -3,7 +3,7 @@ if not rvim then return end
 local g, fn, env, rtp = vim.g, vim.fn, vim.env, vim.opt.rtp
 
 g.dotfiles = env.DOTFILES or fn.expand('~/.dots')
-g.projects_dir = env.DEV_HOME or fn.expand('~/projects')
+g.projects_dir = env.DEV_HOME or fn.expand('~/personal/workspace/coding')
 ----------------------------------------------------------------------------------------------------
 -- Ensure all autocommands are cleared
 vim.api.nvim_create_augroup('vimrc', {})
