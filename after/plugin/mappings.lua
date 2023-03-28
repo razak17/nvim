f7a69967d9b39aa8a340dc7f823a066c65ffa4d7
Razak <moabdulrazak17@gmail.com>
2023-03-28 12:08:23 +0000

    chore(mapping): add quit mapping

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -178,6 +178,7 @@ cnoremap('::', "<C-r>=fnameescape(expand('%:p:h'))<cr>/")
 -- even if the file isn't changed so that things like hot reload work
 nnoremap('<C-s>', '<cmd>silent! write<CR>')
 nnoremap('<leader>x', ':q<CR>', { desc = 'quit' })
+nnoremap('<leader>q', ':q<CR>', { desc = 'quit' })
 ----------------------------------------------------------------------------------------------------
 -- ?ie | entire object
 ----------------------------------------------------------------------------------------------------
