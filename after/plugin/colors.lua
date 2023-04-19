5c5eb64607392c3d0234602b78a27338bd4c0e76
Razak <moabdulrazak17@gmail.com>
2023-04-19 23:43:02 +0000

    chore(colors): update colors for lsp references

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -50,7 +50,7 @@ local function general_overrides()
     ------------------------------------------------------------------------------------------------
     { LspCodeLens = { inherit = 'Comment', bold = true, italic = false } },
     { LspCodeLensSeparator = { bold = false, italic = false } },
-    { LspReferenceText = { bg = 'NONE', underline = true, sp = { from = 'Whitespace', attr = 'fg' } } },
+    { LspReferenceText = { bg = { from = 'Whitespace',attr = 'fg' }, underline = true, sp = { from = 'Whitespace', attr = 'fg' } } },
     { LspReferenceRead = { link = 'LspReferenceText' } },
     { LspReferenceWrite = { inherit = 'LspReferenceText', bold = true, underline = true } },
     { LspSignatureActiveParameter = { link = 'Visual' } },
