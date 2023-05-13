47ee00036139a73e8168157f5c9aebc2e45b39b8
Razak <moabdulrazak17@gmail.com>
2023-05-13 05:56:41 +0000

    chore(statusline): minimal statusline for mason filetype

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -108,159 +108,161 @@ end
 local function multi_selection_open(prompt_bufnr) multiopen(prompt_bufnr, 'edit') end
 
 return {
-  'nvim-telescope/telescope.nvim',
-  keys = {
-    { '<c-p>', find_files, desc = 'find files' },
-    { '<leader>f?', b('help_tags'), desc = 'help tags' },
-    { '<leader>fa', b('builtin', { include_extensions = true }), desc = 'builtins' },
-    { '<leader>fb', b('current_buffer_fuzzy_find'), desc = 'find in current buffer' },
-    { '<leader>fc', nvim_config, desc = 'nvim config' },
-    { '<leader>ff', project_files, desc = 'project files' },
-    { '<leader>fh', frecency, desc = 'Most (f)recently used files' },
-    { '<leader>fL', luasnips, desc = 'luasnip: available snippets' },
-    { '<leader>fn', notifications, desc = 'notify: notifications' },
-    { '<leader>fo', b('buffers'), desc = 'buffers' },
-    { '<leader>fp', projects, desc = 'projects' },
-    { '<leader>fr', b('resume'), desc = 'resume last picker' },
-    { '<leader>fs', live_grep, desc = 'find string' },
-    { '<leader>fu', undo, desc = 'undo' },
-    { '<leader>fw', b('grep_string'), desc = 'find word' },
-    { '<leader>fva', b('autocommands'), desc = 'autocommands' },
-    { '<leader>fvh', b('highlights'), desc = 'highlights' },
-    { '<leader>fvk', b('keymaps'), desc = 'autocommands' },
-    { '<leader>fvo', b('vim_options'), desc = 'vim options' },
-    { '<leader>fvo', b('registers'), desc = 'registers' },
-    -- Git
-    { '<leader>gs', b('git_status'), desc = 'git status' },
-    { '<leader>fgb', b('git_branches'), desc = 'git branches' },
-    { '<leader>fgB', delta_git_bcommits, desc = 'buffer commits' },
-    { '<leader>fgc', delta_git_commits, desc = 'commits' },
-    -- LSP
-    { '<leader>ld', b('lsp_document_symbols'), desc = 'telescope: document symbols' },
-    { '<leader>ls', b('lsp_dynamic_workspace_symbols'), desc = 'telescope: workspace symbols' },
-    { '<leader>le', b('diagnostics', { bufnr = 0 }), desc = 'telescope: document diagnostics' },
-    { '<leader>lw', b('diagnostics'), desc = 'telescope: workspace diagnostics' },
-  },
-  config = function()
-    local previewers = require('telescope.previewers')
-    local sorters = require('telescope.sorters')
-    local actions = require('telescope.actions')
-    local layout_actions = require('telescope.actions.layout')
-    local themes = require('telescope.themes')
+  {
+    'nvim-telescope/telescope.nvim',
+    keys = {
+      { '<c-p>', find_files, desc = 'find files' },
+      { '<leader>f?', b('help_tags'), desc = 'help tags' },
+      { '<leader>fa', b('builtin', { include_extensions = true }), desc = 'builtins' },
+      { '<leader>fb', b('current_buffer_fuzzy_find'), desc = 'find in current buffer' },
+      { '<leader>fc', nvim_config, desc = 'nvim config' },
+      { '<leader>ff', project_files, desc = 'project files' },
+      { '<leader>fh', frecency, desc = 'Most (f)recently used files' },
+      { '<leader>fL', luasnips, desc = 'luasnip: available snippets' },
+      { '<leader>fn', notifications, desc = 'notify: notifications' },
+      { '<leader>fo', b('buffers'), desc = 'buffers' },
+      { '<leader>fp', projects, desc = 'projects' },
+      { '<leader>fr', b('resume'), desc = 'resume last picker' },
+      { '<leader>fs', live_grep, desc = 'find string' },
+      { '<leader>fu', undo, desc = 'undo' },
+      { '<leader>fw', b('grep_string'), desc = 'find word' },
+      { '<leader>fva', b('autocommands'), desc = 'autocommands' },
+      { '<leader>fvh', b('highlights'), desc = 'highlights' },
+      { '<leader>fvk', b('keymaps'), desc = 'autocommands' },
+      { '<leader>fvo', b('vim_options'), desc = 'vim options' },
+      { '<leader>fvo', b('registers'), desc = 'registers' },
+      -- Git
+      { '<leader>gs', b('git_status'), desc = 'git status' },
+      { '<leader>fgb', b('git_branches'), desc = 'git branches' },
+      { '<leader>fgB', delta_git_bcommits, desc = 'buffer commits' },
+      { '<leader>fgc', delta_git_commits, desc = 'commits' },
+      -- LSP
+      { '<leader>ld', b('lsp_document_symbols'), desc = 'telescope: document symbols' },
+      { '<leader>ls', b('lsp_dynamic_workspace_symbols'), desc = 'telescope: workspace symbols' },
+      { '<leader>le', b('diagnostics', { bufnr = 0 }), desc = 'telescope: document diagnostics' },
+      { '<leader>lw', b('diagnostics'), desc = 'telescope: workspace diagnostics' },
+    },
+    config = function()
+      local previewers = require('telescope.previewers')
+      local sorters = require('telescope.sorters')
+      local actions = require('telescope.actions')
+      local layout_actions = require('telescope.actions.layout')
+      local themes = require('telescope.themes')
 
-    require('telescope').setup({
-      defaults = {
-        prompt_prefix = fmt(' %s  ', ui.codicons.misc.search_alt),
-        selection_caret = fmt(' %s ', ui.codicons.misc.pick),
-        cycle_layout_list = { 'flex', 'horizontal', 'vertical', 'bottom_pane', 'center' },
-        sorting_strategy = 'ascending',
-        layout_strategy = 'flex',
-        set_env = { ['TERM'] = vim.env.TERM },
-        borderchars = border.common,
-        file_browser = { hidden = true },
-        color_devicons = true,
-        dynamic_preview_title = true,
-        layout_config = { horizontal = { preview_width = 0.55 } },
-        winblend = 0,
-        history = {
-          path = join_paths(data, 'databases', 'telescope_history.sqlite3'),
-        },
+      require('telescope').setup({
+        defaults = {
+          prompt_prefix = fmt(' %s  ', ui.codicons.misc.search_alt),
+          selection_caret = fmt(' %s ', ui.codicons.misc.pick),
+          cycle_layout_list = { 'flex', 'horizontal', 'vertical', 'bottom_pane', 'center' },
+          sorting_strategy = 'ascending',
+          layout_strategy = 'flex',
+          set_env = { ['TERM'] = vim.env.TERM },
+          borderchars = border.common,
+          file_browser = { hidden = true },
+          color_devicons = true,
+          dynamic_preview_title = true,
+          layout_config = { horizontal = { preview_width = 0.55 } },
+          winblend = 0,
+          history = {
+            path = join_paths(data, 'databases', 'telescope_history.sqlite3'),
+          },
         -- stylua: ignore
         file_ignore_patterns = {
           '%.jpg', '%.jpeg', '%.png', '%.otf', '%.ttf', '%.DS_Store', '^.git/', 'node%_modules/.*',
           '^dist/', '^build/',  '^site-packages/', '%.yarn/.*',
         },
-        path_display = { 'truncate' },
-        file_sorter = sorters.get_fzy_sorter,
-        file_previewer = previewers.vim_buffer_cat.new,
-        grep_previewer = previewers.vim_buffer_vimgrep.new,
-        qflist_previewer = previewers.vim_buffer_qflist.new,
-        mappings = {
-          i = {
-            ['<C-w>'] = actions.send_selected_to_qflist,
-            ['<c-c>'] = function() vim.cmd.stopinsert() end,
-            ['<esc>'] = actions.close,
-            ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
-            ['<c-s>'] = actions.select_horizontal,
-            ['<c-e>'] = layout_actions.toggle_preview,
-            ['<c-l>'] = layout_actions.cycle_layout_next,
-            ['<C-a>'] = multi_selection_open,
-            ['<c-r>'] = actions.to_fuzzy_refine,
-            ['<Tab>'] = actions.toggle_selection,
-            ['<CR>'] = stopinsert(actions.select_default),
-          },
-          n = {
-            ['<C-j>'] = actions.move_selection_next,
-            ['<C-k>'] = actions.move_selection_previous,
-            ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
-            ['<C-a>'] = multi_selection_open,
-          },
-        },
-      },
-      pickers = {
-        registers = cursor(),
-        reloader = dropdown(),
-        oldfiles = dropdown(),
-        git_branches = dropdown(),
-        find_files = { hidden = true },
-        colorscheme = { enable_preview = true },
-        keymaps = dropdown({ layout_config = { height = 18, width = 0.5 } }),
-        git_commits = { layout_config = { horizontal = { preview_width = 0.55 } } },
-        git_bcommits = { layout_config = { horizontal = { preview_width = 0.55 } } },
-        current_buffer_fuzzy_find = dropdown({ previewer = false, shorten_path = false }),
-        diagnostics = themes.get_ivy({ wrap_results = true, borderchars = { preview = border.ivy } }),
-        buffers = dropdown({
-          sort_mru = true,
-          sort_lastused = true,
-          show_all_buffers = true,
-          ignore_current_buffer = true,
-          previewer = false,
-          mappings = { i = { ['<c-x>'] = 'delete_buffer' }, n = { ['<c-x>'] = 'delete_buffer' } },
-        }),
-        live_grep = themes.get_ivy({
-          borderchars = { preview = border.ivy },
-          file_ignore_patterns = { '.git/', '%.svg', '%.lock', '%-lock.yaml', 'node_modules', '%-lock.json' },
-          max_results = 2000,
-          additional_args = { '--trim' },
-        }),
-      },
-      extensions = {
-        persisted = dropdown(),
-        frecency = {
-          db_root = join_paths(data, 'databases'),
-          default_workspace = 'CWD',
-          show_unindexed = false, -- Show all files or only those that have been indexed
-          ignore_patterns = { '*.git/*', '*/tmp/*', '*node_modules/*', '*vendor/*' },
-          workspaces = { conf = vim.env.DOTFILES, project = vim.g.projects_dir },
-        },
-        undo = {
+          path_display = { 'truncate' },
+          file_sorter = sorters.get_fzy_sorter,
+          file_previewer = previewers.vim_buffer_cat.new,
+          grep_previewer = previewers.vim_buffer_vimgrep.new,
+          qflist_previewer = previewers.vim_buffer_qflist.new,
           mappings = {
             i = {
-              ['<C-a>'] = require('telescope-undo.actions').yank_additions,
-              ['<C-d>'] = require('telescope-undo.actions').yank_deletions,
-              ['<C-u>'] = require('telescope-undo.actions').restore,
+              ['<C-w>'] = actions.send_selected_to_qflist,
+              ['<c-c>'] = function() vim.cmd.stopinsert() end,
+              ['<esc>'] = actions.close,
+              ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
+              ['<c-s>'] = actions.select_horizontal,
+              ['<c-e>'] = layout_actions.toggle_preview,
+              ['<c-l>'] = layout_actions.cycle_layout_next,
+              ['<C-a>'] = multi_selection_open,
+              ['<c-r>'] = actions.to_fuzzy_refine,
+              ['<Tab>'] = actions.toggle_selection,
+              ['<CR>'] = stopinsert(actions.select_default),
+            },
+            n = {
+              ['<C-j>'] = actions.move_selection_next,
+              ['<C-k>'] = actions.move_selection_previous,
+              ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
+              ['<C-a>'] = multi_selection_open,
             },
           },
         },
-        menufacture = {
-          mappings = { main_menu = { [{ 'i', 'n' }] = '<C-;>' } },
+        pickers = {
+          registers = cursor(),
+          reloader = dropdown(),
+          oldfiles = dropdown(),
+          git_branches = dropdown(),
+          find_files = { hidden = true },
+          colorscheme = { enable_preview = true },
+          keymaps = dropdown({ layout_config = { height = 18, width = 0.5 } }),
+          git_commits = { layout_config = { horizontal = { preview_width = 0.55 } } },
+          git_bcommits = { layout_config = { horizontal = { preview_width = 0.55 } } },
+          current_buffer_fuzzy_find = dropdown({ previewer = false, shorten_path = false }),
+          diagnostics = themes.get_ivy({ wrap_results = true, borderchars = { preview = border.ivy } }),
+          buffers = dropdown({
+            sort_mru = true,
+            sort_lastused = true,
+            show_all_buffers = true,
+            ignore_current_buffer = true,
+            previewer = false,
+            mappings = { i = { ['<c-x>'] = 'delete_buffer' }, n = { ['<c-x>'] = 'delete_buffer' } },
+          }),
+          live_grep = themes.get_ivy({
+            borderchars = { preview = border.ivy },
+            file_ignore_patterns = { '.git/', '%.svg', '%.lock', '%-lock.yaml', 'node_modules', '%-lock.json' },
+            max_results = 2000,
+            additional_args = { '--trim' },
+          }),
         },
-      },
-    })
+        extensions = {
+          persisted = dropdown(),
+          frecency = {
+            db_root = join_paths(data, 'databases'),
+            default_workspace = 'CWD',
+            show_unindexed = false, -- Show all files or only those that have been indexed
+            ignore_patterns = { '*.git/*', '*/tmp/*', '*node_modules/*', '*vendor/*' },
+            workspaces = { conf = vim.env.DOTFILES, project = vim.g.projects_dir },
+          },
+          undo = {
+            mappings = {
+              i = {
+                ['<C-a>'] = require('telescope-undo.actions').yank_additions,
+                ['<C-d>'] = require('telescope-undo.actions').yank_deletions,
+                ['<C-u>'] = require('telescope-undo.actions').restore,
+              },
+            },
+          },
+          menufacture = {
+            mappings = { main_menu = { [{ 'i', 'n' }] = '<C-;>' } },
+          },
+        },
+      })
 
-    require('telescope').load_extension('zf-native')
-    require('telescope').load_extension('frecency')
-    require('telescope').load_extension('undo')
-    require('telescope').load_extension('menufacture')
-    require('telescope').load_extension('persisted')
-    require('telescope').load_extension('notify')
+      require('telescope').load_extension('zf-native')
+      require('telescope').load_extension('frecency')
+      require('telescope').load_extension('undo')
+      require('telescope').load_extension('menufacture')
+      require('telescope').load_extension('persisted')
+      require('telescope').load_extension('notify')
 
-    vim.api.nvim_exec_autocmds('User', { pattern = 'TelescopeConfigComplete', modeline = false })
-  end,
-  dependencies = {
-    'natecraddock/telescope-zf-native.nvim',
-    'nvim-telescope/telescope-frecency.nvim',
-    'debugloop/telescope-undo.nvim',
-    'molecule-man/telescope-menufacture',
+      vim.api.nvim_exec_autocmds('User', { pattern = 'TelescopeConfigComplete', modeline = false })
+    end,
+    dependencies = {
+      'natecraddock/telescope-zf-native.nvim',
+      'nvim-telescope/telescope-frecency.nvim',
+      'debugloop/telescope-undo.nvim',
+      'molecule-man/telescope-menufacture',
+    },
   },
 }
