6a1abed3653820e06f2d175182545872fb9d1e3c
Razak <moabdulrazak17@gmail.com>
2023-05-15 14:26:06 +0000

    chore(statusline): fix padding for spell and ts icons

--- lua/user/plugins/statusline.lua
+++ lua/user/plugins/statusline.lua
@@ -261,14 +261,14 @@ return {
 
     ins_right({
       function() return icons.misc.spell_check end,
-      padding = { left = 1, right = 0 },
+      padding = { left = 1, right = 1 },
       color = { fg = colors.blue, gui = 'bold' },
       cond = function() return vim.wo.spell and conditions.hide_in_width() end,
     })
 
     ins_right({
       ts_active,
-      padding = { left = 1, right = 0 },
+      padding = { left = 0, right = 0 },
       color = { fg = colors.darker_green, gui = 'bold' },
       cond = conditions.hide_in_width,
     })
