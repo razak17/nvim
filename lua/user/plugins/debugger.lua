a4f7d057a6df465d0d7f0673d6f2e4a082b40fc0
Razak <moabdulrazak17@gmail.com>
2023-05-15 01:41:28 +0000

    chore(dapui): use default layout

--- lua/user/plugins/debugger.lua
+++ lua/user/plugins/debugger.lua
@@ -69,19 +69,6 @@ return {
         opts = {
           windows = { indent = 2 },
           floating = { border = rvim.ui.current.border },
-          layouts = {
-            {
-              elements = {
-                { id = 'scopes', size = 0.25 },
-                { id = 'breakpoints', size = 0.25 },
-                { id = 'stacks', size = 0.25 },
-                { id = 'watches', size = 0.25 },
-              },
-              position = 'left',
-              size = 20,
-            },
-            { elements = { { id = 'repl', size = 0.9 } }, position = 'bottom', size = 10 },
-          },
         },
       },
     },
