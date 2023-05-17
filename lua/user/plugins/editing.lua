2ce652a8c079c4bdc654bb996ec5b578ea12a129
Razak <moabdulrazak17@gmail.com>
2023-05-17 12:31:56 +0000

    chore(comment): switch back to comment.nvim
    mini.comment not working when commentstring changes

--- lua/user/plugins/editing.lua
+++ lua/user/plugins/editing.lua
@@ -188,6 +188,7 @@ return {
   },
   {
     'numToStr/Comment.nvim',
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
     keys = { 'gcc', { 'gc', mode = { 'x', 'n', 'o' } } },
     opts = function(_, opts)
       local ok, integration = pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
