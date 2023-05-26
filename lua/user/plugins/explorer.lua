3b75282b9666d9fa8dccba6aad85a718ac2500c7
Razak <moabdulrazak17@gmail.com>
2023-05-26 01:24:04 +0000

    feat(neo-tree): find and grep text with telescope

--- lua/user/plugins/explorer.lua
+++ lua/user/plugins/explorer.lua
@@ -27,6 +27,26 @@ end
 
 local function will_rename_handler(data) make_rename_params({ old_name = data.source, new_name = data.destination }) end
 
+local function get_telescope_opts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require('telescope.actions')
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require('telescope.actions.state')
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if filename == nil then filename = selection[1] end
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require('neo-tree.sources.filesystem').navigate(state, state.path, filename)
+      end)
+      return true
+    end,
+  }
+end
+
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
@@ -96,7 +116,23 @@ return {
             mappings = {
               ['/'] = 'noop',
               ['g/'] = 'fuzzy_finder',
+              ['gf'] = 'telescope_find',
+              ['gw'] = 'telescope_grep',
             },
+          },
+          commands = {
+            telescope_find = function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              if vim.fn.isdirectory(path) == 0 then return end
+              require('telescope.builtin').find_files(get_telescope_opts(state, path))
+            end,
+            telescope_grep = function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              if vim.fn.isdirectory(path) == 0 then return end
+              require('telescope.builtin').live_grep(get_telescope_opts(state, path))
+            end,
           },
         },
         default_component_configs = {
