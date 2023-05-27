55d6b48d66865ca42bff5c885cbf6f961e62cb14
Razak <moabdulrazak17@gmail.com>
2023-05-27 14:04:50 +0000

    feat(plugins): add smart-splits
    (scope): title

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -34,6 +34,23 @@ return {
       should_autosave = function() return vim.bo.filetype ~= 'alpha' end,
     },
   },
+  {
+    'mrjones2014/smart-splits.nvim',
+    config = true,
+    build = './kitty/install-kittens.bash',
+    keys = {
+      -- moving between splits
+      { '<C-h>', function() require('smart-splits').move_cursor_left() end },
+      { '<C-j>', function() require('smart-splits').move_cursor_down() end },
+      { '<C-k>', function() require('smart-splits').move_cursor_up() end },
+      { '<C-l>', function() require('smart-splits').move_cursor_right() end },
+      -- swapping buffers between windows
+      { '<leader><leader>h', function() require('smart-splits').swap_buf_left() end, desc = { 'swap left' } },
+      { '<leader><leader>j', function() require('smart-splits').swap_buf_down() end, { desc = 'swap down' } },
+      { '<leader><leader>k', function() require('smart-splits').swap_buf_up() end, { desc = 'swap up' } },
+      { '<leader><leader>l', function() require('smart-splits').swap_buf_right() end, { desc = 'swap right' } },
+    },
+  },
   -- }}}
   ----------------------------------------------------------------------------------------------------
   -- LSP,Completion & Debugger {{{1
