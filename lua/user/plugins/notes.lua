f570b7ce7e807eee56d4717127437468c7f46f82
Razak <moabdulrazak17@gmail.com>
2023-05-30 21:45:59 +0000

    chore(headlines): use upstream

--- lua/user/plugins/notes.lua
+++ lua/user/plugins/notes.lua
@@ -43,7 +43,7 @@ return {
     },
   },
   {
-    'razak17/headlines.nvim',
+    'lukas-reineke/headlines.nvim',
     ft = { 'org', 'norg', 'markdown', 'yaml' },
     config = function()
       highlight.plugin('Headlines', {
