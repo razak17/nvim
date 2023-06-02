107e23696c46c360a7317cc71807c7d69b52fa94
Razak <moabdulrazak17@gmail.com>
2023-06-02 19:30:12 +0000

    feat(plugins): add nvim-spider

--- lua/user/plugins/motions.lua
+++ lua/user/plugins/motions.lua
@@ -75,4 +75,14 @@ return {
       },
     },
   },
+  {
+    'chrisgrieser/nvim-spider',
+    event = 'VeryLazy',
+    keys = {
+      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'x', 'n', 'o' } },
+      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'x', 'n', 'o' } },
+      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'x', 'n', 'o' } },
+      { 'ge', "<cmd>lua require('spider').motion('ge')<CR>", mode = { 'x', 'n', 'o' } },
+    },
+  },
 }
