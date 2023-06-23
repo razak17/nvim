615e946a3213edef3f38122c8df9cc38816f84aa
razak17 <moabdulrazak17@gmail.com>
2023-06-23 21:31:05 +0000

    refactor(plugins)!: trial flash over flit
    just testing things out.

--- lua/user/plugins/search.lua
+++ lua/user/plugins/search.lua
@@ -10,12 +10,14 @@ end
 return {
   {
     'ggandor/flit.nvim',
+    enabled = false,
     keys = { 'f' },
     dependencies = { 'ggandor/leap.nvim' },
     opts = { labeled_modes = 'nvo', multiline = false },
   },
   {
     'ggandor/leap.nvim',
+    enabled = false,
     keys = { { 's', leap_keys, mode = 'n' } },
     opts = { equivalence_classes = { ' \t\r\n', '([{', ')]}', '`"\'' } },
     config = function(_, opts)
@@ -26,5 +28,26 @@ return {
       })
       require('leap').setup(opts)
     end,
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      search = {
+        mode = 'fuzzy',
+      },
+    },
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function() require('flash').jump() end,
+      },
+      {
+        'S',
+        mode = { 'o', 'x' },
+        function() require('flash').treesitter() end,
+      },
+    },
   },
 }
