29650312a76fceddacdc19d985927d80239c8601
razak17 <moabdulrazak17@gmail.com>
2023-06-25 15:22:10 +0000

    chore(queries): replace more outdated icons

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,15 +1,15 @@
 ;; extends
 ;; source: https://github.com/ziontee113/yt-tutorials/tree/treesitter_conceal
 
-(("return"   @keyword) (#set! conceal ""))
+(("return"   @keyword) (#set! conceal "󰜴"))
 
 (("local"    @keyword) (#set! conceal "~"))
 
 (("if"       @keyword) (#set! conceal "?"))
 (("else"     @keyword) (#set! conceal "!"))
 (("elseif"   @keyword) (#set! conceal "¿"))
-(("function" @keyword) (#set! conceal "ﬦ"))
-(("function" @keyword) (#set! conceal ""))
+(("function" @keyword) (#set! conceal "󰘧"))
+(("function" @keyword) (#set! conceal "󰊕"))
 (("for"      @keyword) (#set! conceal ""))
 
 (("and"      @keyword) (#set! conceal "▼"))
@@ -20,7 +20,7 @@
 (("comment_start"    @comment) (#set! conceal ""))
 
 ;; Function names
-((function_call name: (identifier) @TSNote (#eq? @TSNote "require"   )) (#set! conceal ""))
+((function_call name: (identifier) @TSNote (#eq? @TSNote "require"   )) (#set! conceal "󰇚"))
 ((function_call name: (identifier) @TSNote (#eq? @TSNote "print"     )) (#set! conceal ""))
 ((function_call name: (identifier) @TSNote (#eq? @TSNote "pairs"     )) (#set! conceal "P"))
 ((function_call name: (identifier) @TSNote (#eq? @TSNote "ipairs"    )) (#set! conceal "I"))
@@ -29,14 +29,14 @@
 ((dot_index_expression table: (identifier) @keyword  (#eq? @keyword  "math" )) (#set! conceal ""))
 
 ;; break_statement
-(((break_statement) @keyword) (#set! conceal "ﰈ"))
+(((break_statement) @keyword) (#set! conceal "󰑟"))
 
 ;; vim.*
 (((dot_index_expression) @field (#eq? @field "vim.cmd"     )) (#set! conceal ""))
 (((dot_index_expression) @field (#eq? @field "vim.api"     )) (#set! conceal ""))
 (((dot_index_expression) @field (#eq? @field "vim.fn"      )) (#set! conceal "#"))
 (((dot_index_expression) @field (#eq? @field "vim.g"       )) (#set! conceal "G"))
-(((dot_index_expression) @field (#eq? @field "vim.schedule")) (#set! conceal ""))
+(((dot_index_expression) @field (#eq? @field "vim.schedule")) (#set! conceal "󱑔"))
 (((dot_index_expression) @field (#eq? @field "vim.opt"     )) (#set! conceal "S"))
 (((dot_index_expression) @field (#eq? @field "vim.env"     )) (#set! conceal "$"))
 (((dot_index_expression) @field (#eq? @field "vim.o"       )) (#set! conceal "O"))
