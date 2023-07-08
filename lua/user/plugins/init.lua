b709b67bb3de45f160bab566f40e6474a0b97053
razak17 <moabdulrazak17@gmail.com>
2023-07-08 21:07:26 +0000

    feat(plugins): add nvim-autopairs
    replaces mini.pairs, just more feature rich

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -256,9 +256,25 @@ return {
     },
   },
   {
-    'echasnovski/mini.pairs',
+    'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = function() require('mini.pairs').setup() end,
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      local autopairs = require('nvim-autopairs')
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+      autopairs.setup({
+        close_triple_quotes = true,
+        disable_filetype = { 'neo-tree-popup' },
+        check_ts = true,
+        fast_wrap = { map = '<c-e>' },
+        ts_config = {
+          lua = { 'string' },
+          dart = { 'string' },
+          javascript = { 'template_string' },
+        },
+      })
+    end,
   },
   {
     'karb94/neoscroll.nvim',
