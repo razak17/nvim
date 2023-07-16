9860098293d8aece648c0bea483f0f7d43782eac
razak17 <moabdulrazak17@gmail.com>
2023-07-16 21:26:53 +0000

    feat(plugins): add obsidian.nvim
    disable neorg

--- lua/user/plugins/notes.lua
+++ lua/user/plugins/notes.lua
@@ -1,11 +1,46 @@
 local fmt, fn = string.format, vim.fn
-local highlight = rvim.highlight
-local function sync(path) return fmt('%s/notes/%s', fn.expand(vim.env.HOME), path) end
+local function sync(path) return fmt('%s/%s', fn.expand(vim.env.HOME), path) end
 
 return {
   {
+    'epwalsh/obsidian.nvim',
+    event = { 'BufReadPre ' .. sync('Obsidian/**.md') },
+    keys = {
+      { '<localleader>ob', '<Cmd>ObsidianBacklinks<CR>', desc = 'obsidian: buffer backlinks' },
+      { '<localleader>od', '<Cmd>ObsidianToday<CR>', desc = 'obsidian: open daily note' },
+      { '<localleader>on', ':ObsidianNew ', desc = 'obsidian: new note' },
+      { '<localleader>oy', '<Cmd>ObsidianYesterday<CR>', desc = 'obsidian: previous daily note' },
+      { '<localleader>oo', ':ObsidianOpen ', desc = 'obsidian: open in app' },
+      { '<localleader>os', '<Cmd>ObsidianSearch<CR>', desc = 'obsidian: search' },
+      { '<localleader>ot', '<Cmd>ObsidianTemplate<CR>', desc = 'obsidian: insert template' },
+    },
+    opts = {
+      dir = '~/Obsidian',
+      notes_subdir = 'Zettelkasten',
+      daily_notes = { folder = 'Daily Notes' },
+      templates = { subdir = 'Templates' },
+      finder = 'fzf-lua',
+    },
+    config = function(_, opts)
+      require('obsidian').setup(opts)
+      vim.keymap.set('n', 'gf', function()
+        if require('obsidian').util.cursor_on_markdown_link() then
+          return '<cmd>ObsidianFollowLink<CR>'
+        else
+          return 'gf'
+        end
+      end, { noremap = false, expr = true })
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+      'nvim-telescope/telescope.nvim',
+      'ibhagwan/fzf-lua',
+    },
+  },
+  {
     'nvim-neorg/neorg',
-    enabled = rvim.treesitter.enable,
+    enabled = false,
     ft = 'norg',
     keys = {
       { '<localleader>nx', '<cmd>Neorg return<CR>', 'neorg: return' },
@@ -33,9 +68,9 @@ return {
         ['core.dirman'] = {
           config = {
             workspaces = {
-              notes = sync('neorg'),
-              tasks = sync('neorg/neovim'),
-              work = sync('neorg/work'),
+              notes = sync('notes/neorg'),
+              tasks = sync('notes/neorg/neovim'),
+              work = sync('notes/neorg/work'),
             },
             default_workspace = 'notes',
           },
