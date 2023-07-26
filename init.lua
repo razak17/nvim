426c976118501c9b2fa9f526c42d5eb8a4f2e20f
razak17 <moabdulrazak17@gmail.com>
2023-07-26 22:14:39 +0000

    refactor(config): introduce user config

--- init.lua
+++ init.lua
@@ -15,42 +15,9 @@ if vim.loader then vim.loader.enable() end
 g.dotfiles = env.DOTFILES or fn.expand('~/.dots')
 g.projects_dir = env.DEV_HOME or fn.expand('~/personal/workspace/coding')
 ----------------------------------------------------------------------------------------------------
--- Global namespace
-----------------------------------------------------------------------------------------------------
-local namespace = {
-  ai = { enable = env.RVIM_AI_ENABLED == '1' },
-  debug = { enable = false },
-  autosave = { enable = true },
-  lsp = {
-    enable = env.RVIM_LSP_ENABLED == '1' and env.RVIM_PLUGINS_MINIMAL == '0',
-    disabled = {
-      filetypes = {},
-      directories = { vim.fn.stdpath('data') },
-      servers = { 'emmet_ls' },
-    },
-    format_on_save = { enable = true },
-    signs = { enable = false },
-    hover_diagnostics = { enable = true },
-  },
-  mappings = {},
-  minimal = env.RVIM_LSP_ENABLED == '0' or env.RVIM_PLUGINS_ENABLED == '0' or env.RVIM_NONE == '1',
-  none = env.RVIM_NONE == '1',
-  plugins = {
-    enable = env.RVIM_PLUGINS_ENABLED == '1',
-    disabled = {},
-    minimal = env.RVIM_PLUGINS_MINIMAL == '1',
-  },
-  treesitter = { enable = true },
-  ui = {
-    statuscolumn = { enable = true },
-  },
-}
-
-_G.rvim = rvim or namespace
-_G.map = vim.keymap.set
-----------------------------------------------------------------------------------------------------
 -- Load Modules
 ----------------------------------------------------------------------------------------------------
--- Order matters here as globals needs to be instantiated first etc.
+-- Order matters here as config and globals need to be instantiated first etc.
+require('user.config')
 require('user.globals')
 require('user.bootstrap')
