4acca2e0c5aac0c0ac7d23aa82ac1b3a3ce66310
razak17 <moabdulrazak17@gmail.com>
2023-08-08 21:32:44 +0000

    chore(ufo): remove custom fold handler
    it doesn't work with inlay hints

--- lua/rm/plugins/ui.lua
+++ lua/rm/plugins/ui.lua
@@ -2,7 +2,6 @@ local api = vim.api
 local ui, highlight = rvim.ui, rvim.highlight
 local codicons = ui.codicons
 local border = ui.current.border
-local strwidth = vim.api.nvim_strwidth
 local separators, decorations = ui.icons.separators, ui.decorations
 
 return {
@@ -390,45 +389,6 @@ return {
         enable_get_fold_virt_text = true,
         close_fold_kinds = { 'imports', 'comment' },
         provider_selector = function(_, ft) return ft_map[ft] or { 'treesitter', 'indent' } end,
-        fold_virt_text_handler = function(virt_text, _, end_lnum, width, truncate, ctx)
-          local result, cur_width, padding = {}, 0, ''
-          local suffix_width = strwidth(ctx.text)
-          local target_width = width - suffix_width
-
-          for _, chunk in ipairs(virt_text) do
-            local chunk_text = chunk[1]
-            local chunk_width = strwidth(chunk_text)
-            if target_width > cur_width + chunk_width then
-              table.insert(result, chunk)
-            else
-              chunk_text = truncate(chunk_text, target_width - cur_width)
-              local hl_group = chunk[2]
-              table.insert(result, { chunk_text, hl_group })
-              chunk_width = strwidth(chunk_text)
-              if cur_width + chunk_width < target_width then
-                padding = padding .. (' '):rep(target_width - cur_width - chunk_width)
-              end
-              break
-            end
-            cur_width = cur_width + chunk_width
-          end
-
-          if ft_map[vim.bo[ctx.bufnr].ft] == 'lsp' then
-            table.insert(result, { ' ⋯ ', 'UfoFoldedEllipsis' })
-            return result
-          end
-
-          local end_text = ctx.get_fold_virt_text(end_lnum)
-          -- reformat the end text to trim excess whitespace from
-          -- indentation usually the first item is indentation
-          if end_text[1] and end_text[1][1] then
-            end_text[1][1] = end_text[1][1]:gsub('[%s\t]+', '')
-          end
-
-          vim.list_extend(result, { { ' ⋯ ', 'UfoFoldedEllipsis' }, unpack(end_text) })
-          table.insert(result, { padding, '' })
-          return result
-        end,
       })
     end,
     dependencies = { 'kevinhwang91/promise-async' },
