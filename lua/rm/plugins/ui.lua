62956105ad6424ec868cfc5176a363cc9e500c18
razak17 <moabdulrazak17@gmail.com>
2023-08-12 23:31:57 +0000

    feat(hlslesn): add lens override

--- lua/rm/plugins/ui.lua
+++ lua/rm/plugins/ui.lua
@@ -1,6 +1,6 @@
 local api = vim.api
 local ui, highlight = rvim.ui, rvim.highlight
-local codicons = ui.codicons
+local icons, codicons = ui.icons, ui.codicons
 local border = ui.current.border
 local separators, decorations = ui.icons.separators, ui.decorations
 
@@ -124,13 +124,43 @@ return {
       { '*', "*<Cmd>lua require('hlslens').start()<CR>" },
       { '#', "#<Cmd>lua require('hlslens').start()<CR>" },
     },
-    opts = {},
-    config = function(_, opts)
+    config = function()
       highlight.plugin('nvim-hlslens', {
         { HlSearchNear = { fg = { from = 'Search' }, bg = 'NONE' } },
         { HlSearchLens = { fg = { from = 'Search' }, bg = 'NONE' } },
       })
-      require('hlslens').setup(opts)
+      require('hlslens').setup({
+        nearest_float_when = false,
+        override_lens = function(render, posList, nearest, idx, relIdx)
+          local sfw = vim.v.searchforward == 1
+          local indicator, text, chunks
+          local absRelIdx = math.abs(relIdx)
+          if absRelIdx > 1 then
+            indicator = ('%d%s'):format(
+              absRelIdx,
+              sfw ~= (relIdx > 1) and icons.misc.up or icons.misc.down
+            )
+          elseif absRelIdx == 1 then
+            indicator = sfw ~= (relIdx == 1) and icons.misc.up or icons.misc.down
+          else
+            indicator = icons.misc.dot
+          end
+          local lnum, col = unpack(posList[idx])
+          if nearest then
+            local cnt = #posList
+            if indicator ~= '' then
+              text = ('[%s %d/%d]'):format(indicator, idx, cnt)
+            else
+              text = ('[%d/%d]'):format(idx, cnt)
+            end
+            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLensNear' } }
+          else
+            text = ('[%s %d]'):format(indicator, idx)
+            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLens' } }
+          end
+          render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+        end,
+      })
     end,
   },
   {
