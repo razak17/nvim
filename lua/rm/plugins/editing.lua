f143dc4473601a83e4b5d1bf5ab724fdfa0a2764
razak17 <moabdulrazak17@gmail.com>
2023-08-12 23:45:51 +0000

    chore(neogen): add more mappings

--- lua/rm/plugins/editing.lua
+++ lua/rm/plugins/editing.lua
@@ -96,9 +96,29 @@ return {
     'danymat/neogen',
     keys = {
       {
-        '<localleader>lc',
+        '<localleader>nd',
         function() require('neogen').generate() end,
         desc = 'neogen: generate doc',
+      },
+      {
+        '<localleader>nf',
+        function() require('neogen').generate({ type = 'file' }) end,
+        desc = 'neogen: file doc',
+      },
+      {
+        '<localleader>nc',
+        function() require('neogen').generate({ type = 'class' }) end,
+        desc = 'neogen: class doc',
+      },
+      {
+        '<localleader>nf',
+        function() require('neogen').generate({ type = 'func' }) end,
+        desc = 'neogen: func doc',
+      },
+      {
+        '<localleader>nt',
+        function() require('neogen').generate({ type = 'type' }) end,
+        desc = 'neogen: type doc',
       },
     },
     opts = { snippet_engine = 'luasnip' },
