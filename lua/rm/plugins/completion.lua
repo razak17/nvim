cde79ab9b5d3e002889e523b917e30296cca6bf6
razak17 <moabdulrazak17@gmail.com>
2023-08-18 00:23:27 +0000

    chore(completion): update sorting strategy

--- lua/rm/plugins/completion.lua
+++ lua/rm/plugins/completion.lua
@@ -82,13 +82,25 @@ return {
         sorting = {
           comparators = {
             require('copilot_cmp.comparators').prioritize,
+            cmp.config.compare.locality,
             cmp.config.compare.offset,
+            cmp.config.compare.recently_used,
             cmp.config.compare.exact,
-            cmp.config.compare.score,
+            cmp.config.compare.order,
+            cmp.config.compare.length,
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find('^_+')
+              local _, entry2_under = entry2.completion_item.label:find('^_+')
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
           },
         },
         matching = {
