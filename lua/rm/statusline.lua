1d91a001403d363408e57a2b8b5eb736c079ac10
razak17 <moabdulrazak17@gmail.com>
2023-08-22 17:26:29 +0000

    chore(lazy): update plugins

--- lua/rm/statusline.lua
+++ lua/rm/statusline.lua
@@ -467,7 +467,7 @@ return {
   },
   word_count = {
     condition = function() return vim.bo.filetype == 'markdown' end,
-    provider = function() return tostring(vim.fn.wordcount().words) .. ' words' end,
+    provider = function() return ' ' .. tostring(vim.fn.wordcount().words) .. ' words' end,
     hl = { fg = fg, bg = bg },
   },
   lsp_clients = {
