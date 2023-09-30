1bd9c2a949b0f43cba2f4e8813bad38a03734c61
razak17 <moabdulrazak17@gmail.com>
2023-09-30 15:39:41 +0000

    feat(plugins): add format-ts-errors

--- lua/rm/plugins/init.lua
+++ lua/rm/plugins/init.lua
@@ -998,7 +998,11 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     cond = rvim.lsp.enable
       and not rvim.find_string(rvim.plugins.disabled, 'typescript-tools.nvim'),
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'neovim/nvim-lspconfig',
+      'davidosomething/format-ts-errors.nvim',
+    },
     opts = {
       settings = {
         tsserver_file_preferences = {
@@ -1011,6 +1015,31 @@ return {
           includeInlayPropertyDeclarationTypeHints = true,
           includeInlayEnumMemberValueHints = true,
         },
+      },
+      handlers = {
+        ['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
+          if result.diagnostics == nil then return end
+
+          -- ignore some tsserver diagnostics
+          local idx = 1
+          while idx <= #result.diagnostics do
+            local entry = result.diagnostics[idx]
+
+            local formatter = require('format-ts-errors')[entry.code]
+            entry.message = formatter and formatter(entry.message)
+              or entry.message
+
+            -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            if entry.code == 80001 then
+              -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+              table.remove(result.diagnostics, idx)
+            else
+              idx = idx + 1
+            end
+          end
+
+          vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+        end,
       },
     },
   },
