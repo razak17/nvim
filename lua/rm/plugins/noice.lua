fe2cbb9c642068e782cd3e192f0de8a2b70f391a
razak17 <moabdulrazak17@gmail.com>
2023-10-10 22:55:15 +0000

    chore(noice)!: disable stylize markdown overrides

--- lua/rm/plugins/noice.lua
+++ lua/rm/plugins/noice.lua
@@ -49,8 +49,8 @@ return {
         throttle = 1000 / 800,
       },
       override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = false,
+        ['vim.lsp.util.stylize_markdown'] = false,
         ['cmp.entry.get_documentation'] = true,
       },
     },
