9b268e7dc1e03083f3074767cd966ce0a1f825f9
razak17 <moabdulrazak17@gmail.com>
2023-10-19 20:54:26 +0000

    feat(plugins): add symbol-usage.nvim

--- lua/rm/plugins/core.lua
+++ lua/rm/plugins/core.lua
@@ -423,6 +423,108 @@ return {
     end,
     dependencies = { 'neovim/nvim-lspconfig' },
   },
+  {
+    'Wansmer/symbol-usage.nvim',
+    cond = rvim.lsp.enable,
+    event = 'LspAttach',
+    config = function()
+      highlight.plugin('symbol-usage', {
+        theme = {
+          ['onedark'] = {
+            {
+              SymbolUsageRounding = {
+                italic = true,
+                fg = { from = 'CursorLine', attr = 'bg' },
+              },
+            },
+            {
+              SymbolUsageContent = {
+                italic = true,
+                bg = { from = 'CursorLine' },
+                fg = { from = 'Comment' },
+              },
+            },
+            {
+              SymbolUsageRef = {
+                italic = true,
+                bg = { from = 'CursorLine' },
+                fg = { from = 'Function' },
+              },
+            },
+            {
+              SymbolUsageDef = {
+                italic = true,
+                bg = { from = 'CursorLine' },
+                fg = { from = 'Type' },
+              },
+            },
+            {
+              SymbolUsageImpl = {
+                italic = true,
+                bg = { from = 'CursorLine' },
+                fg = { from = '@keyword' },
+              },
+            },
+            {
+              SymbolUsageContent = {
+                bold = false,
+                italic = true,
+                bg = { from = 'CursorLine' },
+                fg = { from = 'Comment' },
+              },
+            },
+          },
+        },
+      })
+
+      local function text_format(symbol)
+        local res = {}
+
+        local round_start = { '', 'SymbolUsageRounding' }
+        local round_end = { '', 'SymbolUsageRounding' }
+
+        if symbol.references then
+          local usage = symbol.references <= 1 and 'usage' or 'usages'
+          local num = symbol.references == 0 and 'no' or symbol.references
+          table.insert(res, round_start)
+          table.insert(res, { '󰌹 ', 'SymbolUsageRef' })
+          table.insert(
+            res,
+            { ('%s %s'):format(num, usage), 'SymbolUsageContent' }
+          )
+          table.insert(res, round_end)
+        end
+
+        if symbol.definition then
+          if #res > 0 then table.insert(res, { ' ', 'NonText' }) end
+          table.insert(res, round_start)
+          table.insert(res, { '󰳽 ', 'SymbolUsageDef' })
+          table.insert(
+            res,
+            { symbol.definition .. ' defs', 'SymbolUsageContent' }
+          )
+          table.insert(res, round_end)
+        end
+
+        if symbol.implementation then
+          if #res > 0 then table.insert(res, { ' ', 'NonText' }) end
+          table.insert(res, round_start)
+          table.insert(res, { '󰡱 ', 'SymbolUsageImpl' })
+          table.insert(
+            res,
+            { symbol.implementation .. ' impls', 'SymbolUsageContent' }
+          )
+          table.insert(res, round_end)
+        end
+
+        return res
+      end
+
+      require('symbol-usage').setup({
+        text_format = text_format,
+      })
+    end,
+  },
   -- }}}
   ------------------------------------------------------------------------------
   -- Utilities {{{1
