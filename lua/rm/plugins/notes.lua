56e1a997f46f0c634dbf65f67fd4dcd207cb25d0
razak17 <moabdulrazak17@gmail.com>
2023-10-25 20:16:09 +0000

    perf(markdown): prevent headlines.nvim slowing down initial rendering

--- lua/rm/plugins/notes.lua
+++ lua/rm/plugins/notes.lua
@@ -113,5 +113,12 @@ return {
       },
       markdown = { headline_highlights = { 'Headline1' } },
     },
+    config = function(_, opts)
+      -- PERF: schedule to prevent headlines slowing down opening a file
+      vim.schedule(function()
+        require('headlines').setup(opts)
+        require('headlines').refresh()
+      end)
+    end,
   },
 }
