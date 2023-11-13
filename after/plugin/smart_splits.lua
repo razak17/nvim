9708f896705a2335cc6fa10d8b7dc7b6785ba754
razak17 <moabdulrazak17@gmail.com>
2023-11-13 19:04:37 +0000

    refactor(plugins): move smart_splits and smart_tilde module

---
+++ after/plugin/smart_splits.lua
@@ -0,0 +1,64 @@
+if not rvim then return end
+
+local fn = vim.fn
+
+--------------------------------------------------------------------------------
+-- Smart Splits
+--------------------------------------------------------------------------------
+--[[ move_or_create_win()
+-- Move to a window (one of hjkl) or create a split if a window does not exist in the direction
+-- Vimscript translation of:
+-- https://www.reddit.com/r/vim/comments/166a3ij/comment/jyivcnl/?utm_source=share&utm_medium=web2x&context=3
+-- Usage: vim.keymap("n", "<C-h>", function() move_or_create_win("h") end, {})
+--
+-- @arg key: One of h, j, k, l, a direction to move or create a split
+--]]
+-- ref: https://github.com/theopn/theovim/blob/main/lua/core.lua#L207
+local function move_or_create_win(key)
+  local exclusions = {
+    'alpha',
+    'neo-tree',
+    'lazy',
+    'NeogitStatus',
+    'oil',
+    'gitcommit',
+    'NeogitCommitMessage',
+  }
+  if rvim.find_string(exclusions, vim.bo.ft) then
+    vim.cmd('wincmd ' .. key)
+    return
+  end
+  local curr_win = fn.winnr()
+  vim.cmd('wincmd ' .. key) --> attempt to move
+
+  if curr_win == fn.winnr() then --> didn't move, so create a split
+    if key == 'h' or key == 'l' then
+      vim.cmd('wincmd v')
+    else
+      vim.cmd('wincmd s')
+    end
+    vim.cmd('wincmd ' .. key)
+  end
+end
+
+map('n', '<C-h>', function() move_or_create_win('h') end, {
+  desc = '[h]: Move to window on the left or create a split',
+})
+map(
+  'n',
+  '<C-j>',
+  function() move_or_create_win('j') end,
+  { desc = '[j]: Move to window below or create a vertical split' }
+)
+map(
+  'n',
+  '<C-k>',
+  function() move_or_create_win('k') end,
+  { desc = '[k]: Move to window above or create a vertical split' }
+)
+map(
+  'n',
+  '<C-l>',
+  function() move_or_create_win('l') end,
+  { desc = '[l]: Move to window on the right or create a split' }
+)
---
+++ after/plugin/smart_tilde.lua
@@ -0,0 +1,30 @@
+--------------------------------------------------------------------------------
+-- Smart Tilde
+--------------------------------------------------------------------------------
+local toggleSigns = {
+  ['|'] = '&',
+  [','] = ';',
+  ["'"] = '"',
+  ['^'] = '$',
+  ['/'] = '*',
+  ['+'] = '-',
+  ['('] = ')',
+  ['['] = ']',
+  ['{'] = '}',
+  ['<'] = '>',
+}
+
+map({ 'n', 'x' }, '~', function()
+  local col = vim.fn.col('.') -- fn.col correctly considers tab-indentation
+  local charUnderCursor = vim.api.nvim_get_current_line():sub(col, col)
+  local isLetter = charUnderCursor:find('^%a$')
+  local function normal(cmd) vim.cmd.normal({ cmd, bang = true }) end
+  if isLetter then
+    normal('~h')
+    return
+  end
+  for left, right in pairs(toggleSigns) do
+    if charUnderCursor == left then normal('r' .. right) end
+    if charUnderCursor == right then normal('r' .. left) end
+  end
+end, { desc = 'smart tilde' })
