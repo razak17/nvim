e62d9b2dbf35faf3b3a26d69a77b0e6d8c2bf05c
razak17 <moabdulrazak17@gmail.com>
2023-11-18 18:54:38 +0000

    feat(plugins): add readline

--- lua/rm/plugins/core.lua
+++ lua/rm/plugins/core.lua
@@ -57,6 +57,24 @@ return {
   },
   -- }}}
   ------------------------------------------------------------------------------
+  -- Editing {{{1
+  ------------------------------------------------------------------------------
+  {
+    'assistcontrol/readline.nvim',
+    keys = {
+      { '<M-f>', function() require('readline').forward_word() end, mode = '!' },
+      { '<M-b>', function() require('readline').backward_word() end, mode = '!' },
+      { '<C-a>', function() require('readline').beginning_of_line() end, mode = '!' },
+      { '<C-e>', function() require('readline').end_of_line() end, mode = '!' },
+      { '<M-d>', function() require('readline').kill_word() end, mode = '!' },
+      { '<M-BS>', function() require('readline').backward_kill_word() end, mode = '!' },
+      { '<C-w>', function() require('readline').unix_word_rubout() end, mode = '!' },
+      { '<C-k>', function() require('readline').kill_line() end, mode = '!' },
+      { '<C-u>', function() require('readline').backward_kill_line() end, mode = '!' },
+    },
+  },
+  -- }}}
+  ------------------------------------------------------------------------------
   -- Utilities {{{1
   ------------------------------------------------------------------------------
   { 'sQVe/sort.nvim', cmd = { 'Sort' } },
