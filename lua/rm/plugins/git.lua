29f74bbb496ba975c6165de9bc164005bd7397c3
razak17 <moabdulrazak17@gmail.com>
2023-11-25 11:03:30 +0000

    chore(git): update git header icons

--- lua/rm/plugins/git.lua
+++ lua/rm/plugins/git.lua
@@ -26,7 +26,7 @@ return {
       disable_insert_on_commit = false,
       disable_context_highlighting = true,
       signs = {
-        section = { '', '󰘕' }, -- "󰁙", "󰁊"
+        section = { '󰁙', '󰁊' }, -- "", "󰘕"
         item = { '▸', '▾' },
         hunk = { '樂', '' },
       },
