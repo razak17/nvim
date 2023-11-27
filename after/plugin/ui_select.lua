98684fef8cae11b0e95814df26a1a70c7777a388
razak17 <moabdulrazak17@gmail.com>
2023-11-27 21:44:21 +0000

    refactor(ui-select): consolidate ui toggles

--- after/plugin/ui_select.lua
+++ after/plugin/ui_select.lua
@@ -3,20 +3,80 @@ if not rvim or rvim.none or not rvim.plugins.enable then return end
 local file_select = require('rm.files_select')
 local git_select = require('rm.git_select')
 local lsp_select = require('rm.lsp_select')
-local toggles_select = require('rm.toggles_select')
 
 --------------------------------------------------------------------------------
 -- Toggles
 --------------------------------------------------------------------------------
+
+local function mappings_notify(msg, type)
+  vim.schedule(function() vim.notify(msg, type, { title = 'UI Toggles' }) end)
+end
+
 ---@param opt string
 local function toggle_opt(opt)
   local prev = vim.api.nvim_get_option_value(opt, {})
   local value
   if type(prev) == 'boolean' then value = not prev end
   vim.wo[opt] = value
-  toggles_select.mappings_notify(
-    string.format('%s %s', opt, rvim.bool2str(vim.wo[opt]))
+  mappings_notify(string.format('%s %s', opt, rvim.bool2str(vim.wo[opt])))
+end
+
+--- Toggle laststatus=3|2|0
+local function toggle_statusline()
+  local laststatus = vim.opt.laststatus:get()
+  local status
+  if laststatus == 0 then
+    vim.opt.laststatus = 2
+    status = 'local'
+  elseif laststatus == 2 then
+    vim.opt.laststatus = 3
+    status = 'global'
+  elseif laststatus == 3 then
+    vim.opt.laststatus = 0
+    status = 'off'
+  end
+  mappings_notify(string.format('statusline %s', status))
+end
+
+--- Toggle conceal=2|0
+local function toggle_conceal()
+  vim.opt_local.conceallevel = vim.opt_local.conceallevel:get() == 0 and 2 or 0
+  mappings_notify(
+    string.format(
+      'conceal %s',
+      rvim.bool2str(vim.opt_local.conceallevel:get() == 2)
+    )
   )
+end
+map('n', '<localleader>cl', toggle_conceal, { desc = 'toggle conceallevel' })
+
+--- Toggle conceal cursor=n|''
+local function toggle_conceal_cursor()
+  vim.opt_local.concealcursor = vim.opt_local.concealcursor:get() == 'n' and ''
+    or 'n'
+  mappings_notify(
+    string.format(
+      'conceal cursor %s',
+      rvim.bool2str(vim.opt_local.concealcursor:get() == '')
+    )
+  )
+end
+-- stylua: ignore
+map( 'n', '<localleader>cc', toggle_conceal_cursor, { desc = 'toggle concealcursor' })
+
+local function toggle_sunglasses()
+  local success, _ = pcall(require, 'sunglasses')
+  if not success then return end
+  local is_shaded
+  for _, winnr in ipairs(vim.api.nvim_list_wins()) do
+    is_shaded = require('sunglasses.window').get(winnr):is_shaded()
+    if is_shaded then
+      vim.cmd('SunglassesDisable')
+      return
+    end
+  end
+  vim.cmd('SunglassesEnable')
+  vim.cmd('SunglassesOff')
 end
 
 local toggle_options = {
@@ -24,7 +84,7 @@ local toggle_options = {
   ['Toggle Wrap'] = function() toggle_opt('wrap') end,
   ['Toggle Cursorline'] = function() toggle_opt('cursorline') end,
   ['Toggle Spell'] = function() toggle_opt('spell') end,
-  ['Toggle Statusline'] = toggles_select.toggle_statusline,
+  ['Toggle Statusline'] = toggle_statusline,
   ['Toggle Ccc'] = 'CccHighlighterToggle',
   ['Toggle Pick'] = 'CccPick',
   ['Toggle Cloak'] = 'CloakToggle',
@@ -32,7 +92,7 @@ local toggle_options = {
   ['Toggle Lengthmatters'] = 'LengthmattersToggle',
   ['Toggle Twilight'] = 'Twilight',
   ['Toggle ZenMode'] = 'ZenMode',
-  ['Toggle Sunglasses'] = toggles_select.toggle_sunglasses,
+  ['Toggle Sunglasses'] = toggle_sunglasses,
   ['Toggle Zoom'] = 'lua require("mini.misc").zoom()',
 }
 
@@ -48,13 +108,10 @@ if not rvim.plugins.enable then return end
 --------------------------------------------------------------------------------
 -- Files
 --------------------------------------------------------------------------------
+-- stylua: ignore
 local file_options = {
-  ['Open File From Current Dir'] = function()
-    file_select.open_file_cur_dir(false)
-  end,
-  ['Open File From Current Dir And Children'] = function()
-    file_select.open_file_cur_dir(true)
-  end,
+  ['Open File From Current Dir'] = function() file_select.open_file_cur_dir(false) end,
+  ['Open File From Current Dir And Children'] = function() file_select.open_file_cur_dir(true) end,
   ['Reload All Files From Disk'] = 'lua rvim.reload_all()',
   ['Copy File Path'] = file_select.copy_file_path,
   ['Copy Full File Path'] = 'let @+ = expand("%:p")',
