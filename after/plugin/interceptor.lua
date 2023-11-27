ba9be40a15fa991590ac4765ba9761e755be3a32
razak17 <moabdulrazak17@gmail.com>
2023-11-27 08:04:40 +0000

    chore(interceptor): open ico files with sxiv

--- after/plugin/interceptor.lua
+++ after/plugin/interceptor.lua
@@ -24,10 +24,12 @@ end
 ---@param buf integer The buffer handle for the opening buffer
 ---@param fpath string The file path given to the program
 ---@param fname string The file name used in notifications
+---@param prog string? The program to execute against the file path
 -- TODO: when disabled then enabled again, causes errors with deleting buffer
-local function open_in_prog(buf, fpath, fname)
+local function open_in_prog(buf, fpath, fname, prog)
   notify(string.format('Opening `%s`', fname), 'Open File in External Program')
-  rvim.open(fpath)
+  if prog == nil then rvim.open(fpath) end
+  if prog ~= nil then vim.system({ prog, fpath }, { detach = true }) end
   api.nvim_buf_delete(buf, { force = true })
 end
 
@@ -40,7 +42,7 @@ end, { desc = 'Toggles intercepting BufNew to open files in custom programs' })
 
 rvim.augroup('InterceptToggle', {
   event = { 'BufNew', 'BufReadPre' },
-  pattern = { '**.png', '*.pdf', '*.mp3', '*.mp4', '*.gif' },
+  pattern = { '*.png', '*.jpg', '*.ico', '*.pdf', '*.mp3', '*.mp4', '*.gif' },
   command = function(args)
     if not rvim.interceptor.enable then return end
     local path, bufnr = args.match, args.buf
@@ -52,6 +54,10 @@ rvim.augroup('InterceptToggle', {
       and extension ~= nil
       and not extension:match('^%s*$')
     then
+      if extension == 'ico' then
+        open_in_prog(bufnr, path, filename, 'sxiv')
+        return
+      end
       open_in_prog(bufnr, path, filename)
     end
   end,
