faf4e4cecb05f62d70e517811c22af18b2051fb2
razak17 <moabdulrazak17@gmail.com>
2023-12-08 22:21:14 +0000

    refactor(ui-select): move functions to own module

--- after/plugin/ui_select.lua
+++ after/plugin/ui_select.lua
@@ -5,84 +5,13 @@ end
 --------------------------------------------------------------------------------
 -- Toggles
 --------------------------------------------------------------------------------
-
-local function mappings_notify(msg, type)
-  vim.schedule(function() vim.notify(msg, type, { title = 'UI Toggles' }) end)
-end
-
----@param opt string
-local function toggle_opt(opt)
-  local prev = vim.api.nvim_get_option_value(opt, {})
-  local value
-  if type(prev) == 'boolean' then value = not prev end
-  vim.wo[opt] = value
-  mappings_notify(string.format('%s %s', opt, rvim.bool2str(vim.wo[opt])))
-end
-
---- Toggle laststatus=3|2|0
-local function toggle_statusline()
-  local laststatus = vim.opt.laststatus:get()
-  local status
-  if laststatus == 0 then
-    vim.opt.laststatus = 2
-    status = 'local'
-  elseif laststatus == 2 then
-    vim.opt.laststatus = 3
-    status = 'global'
-  elseif laststatus == 3 then
-    vim.opt.laststatus = 0
-    status = 'off'
-  end
-  mappings_notify(string.format('statusline %s', status))
-end
-
---- Toggle conceal=2|0
-local function toggle_conceal()
-  vim.opt_local.conceallevel = vim.opt_local.conceallevel:get() == 0 and 2 or 0
-  mappings_notify(
-    string.format(
-      'conceal %s',
-      rvim.bool2str(vim.opt_local.conceallevel:get() == 2)
-    )
-  )
-end
-map('n', '<localleader>cl', toggle_conceal, { desc = 'toggle conceallevel' })
-
---- Toggle conceal cursor=n|''
-local function toggle_conceal_cursor()
-  vim.opt_local.concealcursor = vim.opt_local.concealcursor:get() == 'n' and ''
-    or 'n'
-  mappings_notify(
-    string.format(
-      'conceal cursor %s',
-      rvim.bool2str(vim.opt_local.concealcursor:get() == '')
-    )
-  )
-end
--- stylua: ignore
-map( 'n', '<localleader>cc', toggle_conceal_cursor, { desc = 'toggle concealcursor' })
-
-local function toggle_sunglasses()
-  local success, _ = pcall(require, 'sunglasses')
-  if not success then return end
-  local is_shaded
-  for _, winnr in ipairs(vim.api.nvim_list_wins()) do
-    is_shaded = require('sunglasses.window').get(winnr):is_shaded()
-    if is_shaded then
-      vim.cmd('SunglassesDisable')
-      return
-    end
-  end
-  vim.cmd('SunglassesEnable')
-  vim.cmd('SunglassesOff')
-end
-
 local toggle_options = {
+  ['Toggle Wrap'] = 'lua require"rm.toggle_select".toggle_opt("wrap")',
+  ['Toggle Cursorline'] = 'lua require"rm.toggle_select".toggle_opt("cursorline")',
+  ['Toggle Spell'] = 'lua require"rm.toggle_select".toggle_opt("spell")',
+  ['Toggle Conceal'] = 'lua require"rm.toggle_select".toggle_conceal()',
+  ['Toggle Statusline'] = 'lua require"rm.toggle_select".toggle_statusline()',
   ['Toggle Aerial'] = 'AerialToggle',
-  ['Toggle Wrap'] = function() toggle_opt('wrap') end,
-  ['Toggle Cursorline'] = function() toggle_opt('cursorline') end,
-  ['Toggle Spell'] = function() toggle_opt('spell') end,
-  ['Toggle Statusline'] = toggle_statusline,
   ['Toggle Ccc'] = 'CccHighlighterToggle',
   ['Toggle Pick'] = 'CccPick',
   ['Toggle Cloak'] = 'CloakToggle',
@@ -90,7 +19,7 @@ local toggle_options = {
   ['Toggle Lengthmatters'] = 'LengthmattersToggle',
   ['Toggle Twilight'] = 'Twilight',
   ['Toggle ZenMode'] = 'ZenMode',
-  ['Toggle Sunglasses'] = toggle_sunglasses,
+  ['Toggle Sunglasses'] = 'lua require"rm.toggle_select".toggle_sunglasses()',
   ['Toggle Zoom'] = 'lua require("mini.misc").zoom()',
 }
 
@@ -106,7 +35,6 @@ if not rvim.plugins.enable then return end
 --------------------------------------------------------------------------------
 -- Files
 --------------------------------------------------------------------------------
--- stylua: ignore
 local file_options = {
   ['Open File From Current Dir'] = "lua require'rm.file_select'.open_file_cur_dir(false)",
   ['Open File From Current Dir And Children'] = "lua require'rm.file_select'.open_file_cur_dir(true)",
---
+++ lua/rm/toggle_select.lua
@@ -0,0 +1,75 @@
+local M = {}
+
+local function mappings_notify(msg, type)
+  vim.schedule(function() vim.notify(msg, type, { title = 'UI Toggles' }) end)
+end
+
+---@param opt string
+function M.toggle_opt(opt)
+  local prev = vim.api.nvim_get_option_value(opt, {})
+  local value
+  if type(prev) == 'boolean' then value = not prev end
+  vim.wo[opt] = value
+  mappings_notify(string.format('%s %s', opt, rvim.bool2str(vim.wo[opt])))
+end
+
+--- Toggle laststatus=3|2|0
+function M.toggle_statusline()
+  local laststatus = vim.opt.laststatus:get()
+  local status
+  if laststatus == 0 then
+    vim.opt.laststatus = 2
+    status = 'local'
+  elseif laststatus == 2 then
+    vim.opt.laststatus = 3
+    status = 'global'
+  elseif laststatus == 3 then
+    vim.opt.laststatus = 0
+    status = 'off'
+  end
+  mappings_notify(string.format('statusline %s', status))
+end
+
+--- Toggle conceal=2|0
+function M.toggle_conceal()
+  vim.opt_local.conceallevel = vim.opt_local.conceallevel:get() == 0 and 2 or 0
+  mappings_notify(
+    string.format(
+      'conceal %s',
+      rvim.bool2str(vim.opt_local.conceallevel:get() == 2)
+    )
+  )
+end
+-- stylua: ignore
+map('n', '<localleader>cl', ':lua require"rm.toggle_select".toggle_conceal()', { desc = 'toggle conceallevel' })
+
+--- Toggle conceal cursor=n|''
+function M.toggle_conceal_cursor()
+  vim.opt_local.concealcursor = vim.opt_local.concealcursor:get() == 'n' and ''
+    or 'n'
+  mappings_notify(
+    string.format(
+      'conceal cursor %s',
+      rvim.bool2str(vim.opt_local.concealcursor:get() == '')
+    )
+  )
+end
+-- stylua: ignore
+map( 'n', '<localleader>cc', ':lua require"rm.toggle_select".toggle_conceal_cursor()', { desc = 'toggle concealcursor' })
+
+function Mtoggle_sunglasses()
+  local success, _ = pcall(require, 'sunglasses')
+  if not success then return end
+  local is_shaded
+  for _, winnr in ipairs(vim.api.nvim_list_wins()) do
+    is_shaded = require('sunglasses.window').get(winnr):is_shaded()
+    if is_shaded then
+      vim.cmd('SunglassesDisable')
+      return
+    end
+  end
+  vim.cmd('SunglassesEnable')
+  vim.cmd('SunglassesOff')
+end
+
+return M
