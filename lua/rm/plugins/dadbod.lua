76d8e4f4934d5f924e6b1d416c53994606424882
razak17 <moabdulrazak17@gmail.com>
2023-12-08 21:53:50 +0000

    chore(dadbod): set dbout custom height

--- lua/rm/plugins/dadbod.lua
+++ lua/rm/plugins/dadbod.lua
@@ -16,19 +16,16 @@ return {
       desc = 'dadbod: add connection',
     },
   },
+  -- stylua: ignore
   cmd = {
-    'DBUI',
-    'DBUIAddConnection',
-    'DBUIClose',
-    'DBUIToggle',
-    'DBUIFindBuffer',
-    'DBUIRenameBuffer',
-    'DBUILastQueryInfo',
+    'DBUI', 'DBUIAddConnection', 'DBUIClose', 'DBUIToggle', 'DBUIFindBuffer',
+    'DBUIRenameBuffer', 'DBUILastQueryInfo',
   },
   config = function()
     vim.g.db_ui_notification_width = 1
     vim.g.db_ui_debug = 1
     vim.g.db_ui_save_location = join_paths(vim.fn.stdpath('data'), 'db_ui')
+    vim.g.db_ui_use_nerd_fonts = 1
 
     rvim.augroup('dad-bod', {
       event = { 'FileType' },
@@ -46,6 +43,10 @@ return {
           end
         )
       end,
+    }, {
+      event = { 'FileType' },
+      pattern = { 'dbout' },
+      command = function() vim.api.nvim_win_set_height(0, 40) end,
     })
   end,
 }
