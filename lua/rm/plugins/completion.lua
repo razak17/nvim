5691abab6644dc8cea516cf478c4c187451b91be
razak17 <moabdulrazak17@gmail.com>
2023-12-17 12:34:36 +0000

    feat(plugins): add cmp-fuzzy-path

--- lua/rm/plugins/completion.lua
+++ lua/rm/plugins/completion.lua
@@ -125,6 +125,7 @@ return {
             rvim.is_available('copilot-cmp') and require(
               'copilot_cmp.comparators'
             ).prioritize or nil,
+            require('cmp_fuzzy_path.compare'),
             cmp.config.compare.recently_used,
             cmp.config.compare.locality,
             cmp.config.compare.offset,
@@ -170,6 +171,7 @@ return {
               luasnip = '[SNIP]',
               emoji = '[EMOJI]',
               path = '[PATH]',
+              fuzzy_path = '[FPATH]',
               neorg = '[NEORG]',
               buffer = '[BUF]',
               spell = '[SPELL]',
@@ -269,6 +271,7 @@ return {
             max_item_count = 10,
             group_index = 1,
           },
+          -- { name = 'fuzzy_path', priority = 4, group_index = 1 },
           { name = 'path', priority = 4, group_index = 1 },
           { name = 'emoji', priority = 3, group_index = 1 },
           {
@@ -328,6 +331,7 @@ return {
           { name = 'cmdline', keyword_pattern = [=[[^[:blank:]\!]*]=] },
           { name = 'path' },
           { name = 'cmdline_history', priority = 10, max_item_count = 5 },
+          { name = 'fuzzy_path', option = { fd_timeout_msec = 1500 } },
         }),
       })
 
@@ -361,6 +365,17 @@ return {
       { 'hrsh7th/cmp-nvim-lsp', cond = rvim.lsp.enable },
       { 'hrsh7th/cmp-cmdline', config = function() vim.o.wildmode = '' end },
       { 'hrsh7th/cmp-nvim-lsp-document-symbol', cond = rvim.lsp.enable },
+      {
+        'tzachar/cmp-fuzzy-path',
+        event = { 'CmdlineEnter' },
+        dependencies = {
+          'tzachar/fuzzy.nvim',
+          {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+          },
+        },
+      },
       {
         'uga-rosa/cmp-dictionary',
         cond = not rvim.plugins.minimal,
