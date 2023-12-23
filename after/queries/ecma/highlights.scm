3a72e66cd6b20fc557e5d3930db3828f0f435fd1
razak17 <moabdulrazak17@gmail.com>
2023-12-23 21:58:35 +0000

    chore(scope): add ecma conceals

---
+++ after/queries/ecma/highlights.scm
@@ -0,0 +1,10 @@
+;; extends
+
+;; Keywords
+(("from"     @keyword) (#set! conceal "󰜱"))
+(("import"   @keyword) (#set! conceal "󰋺"))
+(("function" @keyword) (#set! conceal "󰊕"))
+(("for"      @keyword) (#set! conceal ""))
+(("return"   @keyword) (#set! conceal ""))
+(("class"    @keyword) (#set! conceal ""))
+(("return"   @keyword) (#set! conceal "󰜴"))
