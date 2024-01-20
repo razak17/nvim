c2ecd4b1d03ee2e0a518643171fb60ddc75f92aa
razak17 <moabdulrazak17@gmail.com>
2024-01-20 15:02:13 +0000

    chore(theme): enable all colorschemes

--- lua/rm/plugins/theme.lua
+++ lua/rm/plugins/theme.lua
@@ -2,12 +2,12 @@ return {
   ------------------------------------------------------------------------------
   -- Themes {{{1
   ------------------------------------------------------------------------------
-  { 'LunarVim/horizon.nvim', cond = false, lazy = false, priority = 1000 },
-  { 'dotsilas/darcubox-nvim', cond = false, lazy = false, priority = 1000 },
-  { 'Wansmer/serenity.nvim', cond = false, priority = 1000, opts = {} },
-  { 'judaew/ronny.nvim', cond = false, priority = 1000, opts = {} },
-  { 'oxfist/night-owl.nvim', cond = false, lazy = false, priority = 1000 },
-  { 'kvrohit/rasmus.nvim', cond = false, lazy = false, priority = 1000 },
+  { 'LunarVim/horizon.nvim', lazy = false, priority = 1000 },
+  { 'dotsilas/darcubox-nvim', lazy = false, priority = 1000 },
+  { 'Wansmer/serenity.nvim', priority = 1000, opts = {} },
+  { 'judaew/ronny.nvim', priority = 1000, opts = {} },
+  { 'oxfist/night-owl.nvim', lazy = false, priority = 1000 },
+  { 'kvrohit/rasmus.nvim', lazy = false, priority = 1000 },
   {
     'razak17/onedark.nvim',
     lazy = false,
@@ -15,7 +15,6 @@ return {
     opts = { variant = 'fill' },
   },
   {
-    'NTBBloodbath/doom-one.nvim',
     cond = false,
     lazy = false,
     config = function()
