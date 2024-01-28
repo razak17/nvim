cb1f0a0cf2a4c528fbd16215ddc01fcb81a20493
razak17 <moabdulrazak17@gmail.com>
2024-01-28 00:50:46 +0000

    feat(plugins): add CopilotChat

--- lua/rm/plugins/ai.lua
+++ lua/rm/plugins/ai.lua
@@ -88,36 +88,6 @@ return {
     },
   },
   {
-    'piersolenski/wtf.nvim',
-    cond = rvim.lsp.enable and rvim.ai.enable,
-    event = 'VeryLazy',
-    -- stylua: ignore
-    keys = {
-      { '<leader>ao', function() require('wtf').ai() end, desc = 'wtf: debug diagnostic with AI', },
-      { '<leader>ag', function() require('wtf').search() end, desc = 'wtf: google search diagnostic', },
-    },
-    opts = {
-      popup_type = 'horizontal', -- | 'popup' | 'horizontal' | 'vertical',
-      language = 'english',
-      search_engine = 'google', -- 'google' | 'duck_duck_go' | 'stack_overflow' | 'github',
-      winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
-    },
-    dependencies = { 'MunifTanjim/nui.nvim' },
-  },
-  {
-    'moozd/aidoc.nvim',
-    cond = not rvim.plugins.minimal,
-    keys = {
-      {
-        mode = 'x',
-        '<leader>do',
-        '<cmd>lua require("aidoc.api").generate({width = 65})<CR>',
-        desc = 'aidoc: generate',
-      },
-    },
-    opts = { keymap = '' },
-  },
-  {
     'zbirenbaum/copilot.lua',
     cond = rvim.ai.enable and not rvim.plugins.minimal,
     event = 'InsertEnter',
@@ -156,9 +126,48 @@ return {
     },
   },
   {
-    'David-Kunz/gen.nvim',
-    cond = not rvim.plugins.minimal and rvim.ai.enable,
-    cmd = { 'Gen' },
+    'jellydn/CopilotChat.nvim',
+    cmd = {
+      'CopilotChat',
+      'CopilotChatExplain',
+      'CopilotChatRefactor',
+      'CopilotChatReview',
+      'CopilotChatTests',
+    },
+    build = function()
+      vim.defer_fn(function()
+        vim.cmd('UpdateRemotePlugins')
+        vim.notify(
+          'CopilotChat - Updated remote plugins. Please restart Neovim.'
+        )
+      end, 3000)
+    end,
+    opts = {
+      mode = 'split',
+      prompts = {
+        Explain = 'What does this code do?',
+        Review = 'Review the following code and provide concise suggestions.',
+        Tests = 'Briefly explain how the selected code works, then generate unit tests.',
+        Refactor = 'Refactor the code to improve clarity and readability.',
+      },
+    },
+  },
+  {
+    'piersolenski/wtf.nvim',
+    cond = rvim.lsp.enable and rvim.ai.enable,
+    event = 'VeryLazy',
+    -- stylua: ignore
+    keys = {
+      { '<leader>ao', function() require('wtf').ai() end, desc = 'wtf: debug diagnostic with AI', },
+      { '<leader>ag', function() require('wtf').search() end, desc = 'wtf: google search diagnostic', },
+    },
+    opts = {
+      popup_type = 'horizontal', -- | 'popup' | 'horizontal' | 'vertical',
+      language = 'english',
+      search_engine = 'google', -- 'google' | 'duck_duck_go' | 'stack_overflow' | 'github',
+      winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+    },
+    dependencies = { 'MunifTanjim/nui.nvim' },
   },
   {
     'razak17/backseat.nvim',
@@ -169,5 +178,23 @@ return {
       winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
     },
     dependencies = { 'MunifTanjim/nui.nvim' },
+  },
+  {
+    'moozd/aidoc.nvim',
+    cond = not rvim.plugins.minimal,
+    keys = {
+      {
+        mode = 'x',
+        '<leader>do',
+        '<cmd>lua require("aidoc.api").generate({width = 65})<CR>',
+        desc = 'aidoc: generate',
+      },
+    },
+    opts = { keymap = '' },
+  },
+  {
+    'David-Kunz/gen.nvim',
+    cond = not rvim.plugins.minimal and rvim.ai.enable and false,
+    cmd = { 'Gen' },
   },
 }
