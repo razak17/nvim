5dca8ff8fc4ec5ab8a079d6b486a70a603ee64fb
razak17 <moabdulrazak17@gmail.com>
2024-01-30 12:42:34 +0000

    refactor(plugins): motions -> motion, trailblazer to marks module

--- lua/rm/plugins/marks.lua
+++ lua/rm/plugins/marks.lua
@@ -95,39 +95,4 @@ return {
       },
     },
   },
-  {
-    'razak17/accelerated-jk.nvim',
-    -- stylua: ignore
-    keys = {
-      { 'j', "<cmd>lua require'accelerated-jk'.command('gj')<CR>", mode = { 'x', 'n' }, },
-      { 'k', "<cmd>lua require'accelerated-jk'.command('gk')<CR>", mode = { 'x', 'n' }, },
-    },
-  },
-  {
-    'm4xshen/hardtime.nvim',
-    cond = false,
-    event = 'VeryLazy',
-    opts = {
-      disabled_filetypes = {
-        'qf',
-        'neo-tree',
-        'lazy',
-        'mason',
-        'NeogitStatus',
-        'lspinfo',
-        'null-ls-info',
-      },
-    },
-  },
-  {
-    'chrisgrieser/nvim-spider',
-    cond = not rvim.plugins.minimal and rvim.plugins.niceties,
-    -- stylua: ignore
-    keys = {
-      { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'x', 'n', 'o' }, },
-      { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'x', 'n', 'o' }, },
-      { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'x', 'n', 'o' }, },
-      { 'ge', "<cmd>lua require('spider').motion('ge')<CR>", mode = { 'x', 'n', 'o' }, },
-    },
-  },
 }
