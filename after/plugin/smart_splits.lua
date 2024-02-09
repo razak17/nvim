b25a5cf38dcd1b57d45ce1235e061fa131fb648c
razak17 <moabdulrazak17@gmail.com>
2024-02-09 10:02:30 +0000

    chore(smart_splits): disable for qf

--- after/plugin/smart_splits.lua
+++ after/plugin/smart_splits.lua
@@ -25,7 +25,8 @@ local function move_or_create_win(key)
     'oil',
     'gitcommit',
     'NeogitCommitMessage',
-    'DiffviewFileHistory'
+    'DiffviewFileHistory',
+    'qf'
   }
   if rvim.find_string(exclusions, vim.bo.ft) then
     vim.cmd('wincmd ' .. key)
