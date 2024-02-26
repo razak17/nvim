46900b21d29fc4479da5de5f1e6538860dd433f0
razak17 <moabdulrazak17@gmail.com>
2024-02-26 18:54:18 +0000

    feat(plugins): replace format-ts-errors with ts-error-translator

--- lua/rm/plugins/typescript-tools.lua
+++ lua/rm/plugins/typescript-tools.lua
@@ -1,94 +1,97 @@
 return {
-  'pmizio/typescript-tools.nvim',
-  ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
-  cond = rvim.lsp.enable
-    and not rvim.find_string(rvim.plugins.disabled, 'typescript-tools.nvim'),
-  -- stylua: ignore
-  keys = {
-    { '<localleader>li', '<Cmd>TSToolsAddMissingImports<CR>', desc = 'add missing imports' },
-    { '<localleader>lx', '<Cmd>TSToolsRemoveUnusedImports<CR>', desc = 'remove unused missing imports' },
-  },
-  opts = {
-    on_attach = function(client, bufnr)
-      require('twoslash-queries').attach(client, bufnr)
-    end,
-    settings = {
-      tsserver_file_preferences = {
-        includeInlayParameterNameHints = 'literal',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = false,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
+  {
+
+    'pmizio/typescript-tools.nvim',
+    ft = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
+    cond = rvim.lsp.enable
+      and not rvim.find_string(rvim.plugins.disabled, 'typescript-tools.nvim'),
+    -- stylua: ignore
+    keys = {
+      { '<localleader>li', '<Cmd>TSToolsAddMissingImports<CR>', desc = 'add missing imports' },
+      { '<localleader>lx', '<Cmd>TSToolsRemoveUnusedImports<CR>', desc = 'remove unused missing imports' },
+    },
+    opts = {
+      on_attach = function(client, bufnr)
+        require('twoslash-queries').attach(client, bufnr)
+      end,
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = 'literal',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+      handlers = {
+        ['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
+          if result.diagnostics == nil then return end
+
+          -- ignore some tsserver diagnostics
+          local idx = 1
+          while idx <= #result.diagnostics do
+            local entry = result.diagnostics[idx]
+
+            local translate = require('ts-error-translator').translate
+            entry.message = translate and translate(entry.code, entry.message)
+              or entry.message
+
+            -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            if entry.code == 80001 then
+              -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+              table.remove(result.diagnostics, idx)
+            else
+              idx = idx + 1
+            end
+          end
+
+          vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+        end,
       },
     },
-    handlers = {
-      ['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
-        if result.diagnostics == nil then return end
-
-        -- ignore some tsserver diagnostics
-        local idx = 1
-        while idx <= #result.diagnostics do
-          local entry = result.diagnostics[idx]
-
-          local formatter = require('format-ts-errors')[entry.code]
-          entry.message = formatter and formatter(entry.message)
-            or entry.message
-
-          -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-          if entry.code == 80001 then
-            -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-            table.remove(result.diagnostics, idx)
-          else
-            idx = idx + 1
-          end
-        end
-
-        vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-      end,
-    },
-  },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'neovim/nvim-lspconfig',
-    'davidosomething/format-ts-errors.nvim',
-    {
-      'razak17/twoslash-queries.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'neovim/nvim-lspconfig',
+      'dmmulroy/ts-error-translator.nvim',
+      {
+        'OlegGulevskyy/better-ts-errors.nvim',
+        opts = {
+          keymaps = {
+            toggle = '<localleader>lo',
+            go_to_definition = '<localleader>ld',
+          },
+        },
+        dependencies = { 'MunifTanjim/nui.nvim' },
+      },
+      {
+        'razak17/twoslash-queries.nvim',
         -- stylua: ignore
         keys = {
           { '<localleader>lI', '<Cmd>TwoslashQueriesInspect<CR>', desc = 'twoslash-queries: inspect', },
         },
-      opts = {},
-      config = function(_, opts)
-        require('twoslash-queries').setup(opts)
+        opts = {},
+        config = function(_, opts)
+          require('twoslash-queries').setup(opts)
 
-        rvim.highlight.plugin('twoslash-queries', {
-          theme = {
-            ['onedark'] = {
-              { TypeVirtualText = { link = 'DiagnosticVirtualTextInfo' } },
+          rvim.highlight.plugin('twoslash-queries', {
+            theme = {
+              ['onedark'] = {
+                { TypeVirtualText = { link = 'DiagnosticVirtualTextInfo' } },
+              },
             },
-          },
-        })
-      end,
-    },
-    {
-      'dmmulroy/tsc.nvim',
-      cond = rvim.lsp.enable,
-      cmd = 'TSC',
-      opts = {},
-      ft = { 'typescript', 'typescriptreact' },
-    },
-    {
-      'OlegGulevskyy/better-ts-errors.nvim',
-      opts = {
-        keymaps = {
-          toggle = '<localleader>lo',
-          go_to_definition = '<localleader>ld',
-        },
+          })
+        end,
       },
-      dependencies = { 'MunifTanjim/nui.nvim' },
+      {
+        'dmmulroy/tsc.nvim',
+        cond = rvim.lsp.enable,
+        cmd = 'TSC',
+        opts = {},
+        ft = { 'typescript', 'typescriptreact' },
+      },
     },
   },
 }
