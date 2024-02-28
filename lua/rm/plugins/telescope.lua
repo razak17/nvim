c6ac3525f714e1f685198c3f32b523693804bc0f
razak17 <moabdulrazak17@gmail.com>
2024-02-28 00:07:34 +0000

    chore(picker): use fzf_lua for word search

--- lua/rm/plugins/telescope.lua
+++ lua/rm/plugins/telescope.lua
@@ -293,7 +293,7 @@ return {
       { '<leader>fu', undo, desc = 'undo' },
       { '<leader>fm', harpoon, desc = 'harpoon' },
       { '<leader>ft', textcase, desc = 'textcase', mode = { 'n', 'v' } },
-      { '<leader>fw', b('grep_string'), desc = 'find word' },
+      -- { '<leader>fw', b('grep_string'), desc = 'find word' },
       { '<leader>fw', visual_grep_string, desc = 'find word', mode = 'x' },
       { '<leader>fvc', b('commands'), desc = 'commands' },
       { '<leader>fvh', b('highlights'), desc = 'highlights' },
