95bb427d30e861885ba3cd0d0e4522000226ba96
razak17 <moabdulrazak17@gmail.com>
2024-03-06 23:02:16 +0000

    chore(whatthejump): update mappings

--- lua/rm/plugins/marks.lua
+++ lua/rm/plugins/marks.lua
@@ -132,12 +132,12 @@ return {
     'lewis6991/whatthejump.nvim',
     event = { 'BufRead', 'BufNewFile' },
     config = function()
-      map('n', '<M-i>', function()
+      map('n', '<M-o>', function()
         require('whatthejump').show_jumps(false)
         return '<C-o>'
       end, { expr = true })
 
-      map('n', '<M-o>', function()
+      map('n', '<M-i>', function()
         require('whatthejump').show_jumps(true)
         return '<C-i>'
       end, { expr = true })
