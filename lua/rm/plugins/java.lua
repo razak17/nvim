e5073bb1acd09601ee7bfc27c0057bfa7c0cfd19
razak17 <moabdulrazak17@gmail.com>
2024-03-14 04:51:25 +0000

    feat(plugins): add nvim-java

---
+++ lua/rm/plugins/java.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-java/nvim-java",
+    -- lazy = false,
+    ft = { "java" },
+    dependencies = {
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          registries = {
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
+          },
+        },
+      },
+    },
+    config = function()
+      require("java").setup {}
+      require("lspconfig").jdtls.setup {
+        on_attach = require("plugins.configs.lspconfig").on_attach,
+        capabilities = require("plugins.configs.lspconfig").capabilities,
+        filetypes = { "java" },
+      }
+    end,
+  }
+}
