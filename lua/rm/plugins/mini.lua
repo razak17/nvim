987e62d1c277008e804adc45549230727f6540d9
razak17 <moabdulrazak17@gmail.com>
2024-04-11 05:05:15 +0000

    chore(mini): update mappings

--- lua/rm/plugins/mini.lua
+++ lua/rm/plugins/mini.lua
@@ -91,6 +91,19 @@ return {
   {
     'echasnovski/mini.move',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = '<M-h>',
+        right = '<M-l>',
+        down = '<M-j>',
+        up = '<M-k>',
+        -- Move current line in Normal mode
+        line_left = '<M-h>',
+        line_right = '<M-/>',
+        line_down = '<M-j>',
+        line_up = '<M-k>',
+      },
+    },
   },
 }
