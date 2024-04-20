3f04378704f3416f2d03ea24daa6ebda9176e842
razak17 <moabdulrazak17@gmail.com>
2024-04-20 11:06:49 +0000

    feat(plugins): add mini.pairs

--- lua/rm/plugins/mini.lua
+++ lua/rm/plugins/mini.lua
@@ -106,4 +106,33 @@ return {
       },
     },
   },
+  {
+    'echasnovski/mini.pairs',
+    cond = false,
+    event = 'VeryLazy',
+    opts = {
+      mappings = {
+        ['`'] = {
+          action = 'closeopen',
+          pair = '``',
+          neigh_pattern = '[^\\`].',
+          register = { cr = false },
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>up',
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          if vim.g.minipairs_disable then
+            vim.notify('Disabled auto pairs', { title = 'Option' })
+          else
+            vim.notify('Enabled auto pairs', { title = 'Option' })
+          end
+        end,
+        desc = 'Toggle Auto Pairs',
+      },
+    },
+  },
 }
