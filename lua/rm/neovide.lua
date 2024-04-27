bd7cf94fc0b577269560b815d7d36fd0e153bf98
razak17 <moabdulrazak17@gmail.com>
2024-04-27 00:48:41 +0000

    chore(neovide): add mapping for zoom

--- lua/rm/neovide.lua
+++ lua/rm/neovide.lua
@@ -5,3 +5,7 @@ vim.g.neovide_cursor_vfx_mode = 'railgun'
 vim.o.guifont = 'Operator Mono SSm Lig Book:h8' -- text below applies for VimScript
 vim.g.neovide_input_macos_alt_is_meta = true
 vim.opt.linespace = 0
+
+vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 0.9<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
