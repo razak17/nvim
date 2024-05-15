601e71e77d6f5c1037e9828c949a88ac9f3005e5
razak17 <moabdulrazak17@gmail.com>
2024-05-15 23:34:49 +0000

    refactor(plugins): create games module

--- lua/rm/plugins/core.lua
+++ lua/rm/plugins/core.lua
@@ -366,32 +366,6 @@ return {
     },
     dependencies = { 'rcarriga/nvim-notify' },
   },
-  -- Games
-  --------------------------------------------------------------------------------
-  { 'ThePrimeagen/vim-be-good', cmd = 'VimBeGood' },
-  { 'NStefan002/speedtyper.nvim', cmd = 'Speedtyper', opts = {} },
-  {
-    'Febri-i/snake.nvim',
-    opts = {},
-    cmd = { 'SnakeStart' },
-    dependencies = { 'Febri-i/fscreen.nvim' },
-  },
-  {
-    'NStefan002/2048.nvim',
-    cmd = 'Play2048',
-    opts = {},
-  },
-  {
-    'NStefan002/15puzzle.nvim',
-    cmd = 'Play15puzzle',
-    opts = {},
-  },
-  {
-    'willothy/strat-hero.nvim',
-    opts = {},
-    cmd = 'StratHero',
-    dependencies = { 'MunifTanjim/nui.nvim' },
-  },
   -- Share Code
   --------------------------------------------------------------------------------
   { 'TobinPalmer/rayso.nvim', cmd = { 'Rayso' }, opts = {} },
---
+++ lua/rm/plugins/games.lua
@@ -0,0 +1,28 @@
+return {
+  -- Games
+  --------------------------------------------------------------------------------
+  { 'ThePrimeagen/vim-be-good', cmd = 'VimBeGood' },
+  { 'NStefan002/speedtyper.nvim', cmd = 'Speedtyper', opts = {} },
+  {
+    'Febri-i/snake.nvim',
+    opts = {},
+    cmd = { 'SnakeStart' },
+    dependencies = { 'Febri-i/fscreen.nvim' },
+  },
+  {
+    'NStefan002/2048.nvim',
+    cmd = 'Play2048',
+    opts = {},
+  },
+  {
+    'NStefan002/15puzzle.nvim',
+    cmd = 'Play15puzzle',
+    opts = {},
+  },
+  {
+    'willothy/strat-hero.nvim',
+    opts = {},
+    cmd = 'StratHero',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+  },
+}
