961e3bf632b77a96bca28fb9a67ca4af32fae879
razak17 <moabdulrazak17@gmail.com>
2024-07-13 19:24:29 +0000

    feat(mini.ai): configure textobjects

--- lua/ar/plugins/mini.lua
+++ lua/ar/plugins/mini.lua
@@ -1,6 +1,80 @@
 local separators = ar.ui.icons.separators
 local minimal = ar.plugins.minimal
 
+-- https://www.lazyvim.org/plugins/coding#miniai
+-- Mini.ai indent text object
+-- For "a", it will include the non-whitespace line surrounding the indent block.
+-- "a" is line-wise, "i" is character-wise.
+local function ai_indent(ai_type)
+  local spaces = (' '):rep(vim.o.tabstop)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local indents = {} ---@type {line: number, indent: number, text: string}[]
+
+  for l, line in ipairs(lines) do
+    if not line:find('^%s*$') then
+      indents[#indents + 1] = {
+        line = l,
+        indent = #line:gsub('\t', spaces):match('^%s*'),
+        text = line,
+      }
+    end
+  end
+
+  local ret = {}
+
+  for i = 1, #indents do
+    if i == 1 or indents[i - 1].indent < indents[i].indent then
+      local from, to = i, i
+      for j = i + 1, #indents do
+        if indents[j].indent < indents[i].indent then break end
+        to = j
+      end
+      from = ai_type == 'a' and from > 1 and from - 1 or from
+      to = ai_type == 'a' and to < #indents and to + 1 or to
+      ret[#ret + 1] = {
+        indent = indents[i].indent,
+        from = {
+          line = indents[from].line,
+          col = ai_type == 'a' and 1 or indents[from].indent + 1,
+        },
+        to = { line = indents[to].line, col = #indents[to].text },
+      }
+    end
+  end
+
+  return ret
+end
+
+-- taken from MiniExtra.gen_ai_spec.buffer
+local function ai_buffer(ai_type)
+  local start_line, end_line = 1, vim.fn.line('$')
+  if ai_type == 'i' then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank =
+      vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
+
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return {
+    from = { line = start_line, col = 1 },
+    to = { line = end_line, col = to_col },
+  }
+end
+
+local function deepcopy_with_prefix(base, prefix, desc_modify)
+  local new_table = vim.deepcopy(base)
+  for _, v in pairs(new_table) do
+    v[1] = prefix .. v[1]
+    if desc_modify then v.desc = v.desc:gsub(' including.*', '') end
+  end
+  return new_table
+end
+
 return {
   'echasnovski/mini.hipatterns',
   {
@@ -71,9 +145,92 @@ return {
   },
   {
     'echasnovski/mini.ai',
-    event = { 'BufRead', 'BufNewFile' },
+    cond = not minimal,
+    event = { 'VeryLazy' },
     config = function()
+      local base = {
+        { '<space>', desc = 'Whitespace' },
+        { '"', desc = 'Balanced "' },
+        { "'", desc = "Balanced '" },
+        { '`', desc = 'Balanced `' },
+        { '(', desc = 'Balanced (' },
+        { ')', desc = 'Balanced ) including white-space' },
+        { '>', desc = 'Balanced > including white-space' },
+        { '<lt>', desc = 'Balanced <' },
+        { ']', desc = 'Balanced ] including white-space' },
+        { '[', desc = 'Balanced [' },
+        { '}', desc = 'Balanced } including white-space' },
+        { '{', desc = 'Balanced {' },
+        { '?', desc = 'User Prompt' },
+        { '_', desc = 'Underscore' },
+        { 'a', desc = 'Argument' },
+        { 'b', desc = 'Balanced ), ], }' },
+        { 'c', desc = 'Class' },
+        { 'd', desc = 'Digit(s)' },
+        { 'e', desc = 'Word in CamelCase & snake_case' },
+        { 'f', desc = 'Function' },
+        { 'g', desc = 'Entire file' },
+        { 'i', desc = 'Indent' },
+        { 'o', desc = 'Block, conditional, loop' },
+        { 'q', desc = 'Quote `, ", \'' },
+        { 't', desc = 'Tag' },
+        { 'u', desc = 'Use/call function & method' },
+        { 'U', desc = 'Use/call without dot in name' },
+      }
+
+      local i = deepcopy_with_prefix(base, 'i')
+      local a = deepcopy_with_prefix(base, 'a', true)
+      local ic = deepcopy_with_prefix(base, 'in')
+      local il = deepcopy_with_prefix(base, 'il')
+      local ac = deepcopy_with_prefix(base, 'an', true)
+      local al = deepcopy_with_prefix(base, 'al', true)
+
+      require('which-key').add({
+        mode = { 'o', 'x' },
+        { 'a', group = 'around' },
+        { 'i', group = 'inside' },
+        { 'in', group = 'Inside next textobject' },
+        { 'il', group = 'Inside last textobject' },
+        { 'an', group = 'Around next textobject' },
+        { 'al', group = 'Around last textobject' },
+        i,
+        a,
+        ic,
+        il,
+        ac,
+        al,
+      })
+
+      local ai = require('mini.ai')
+
       require('mini.ai').setup({
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+          }),
+          f = ai.gen_spec.treesitter({
+            a = '@function.outer',
+            i = '@function.inner',
+          }), -- function
+          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }), -- class
+          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+          d = { '%f[%d]%d+' }, -- digits
+          e = { -- Word with case
+            {
+              '%u[%l%d]+%f[^%l%d]',
+              '%f[%S][%l%d]+%f[^%l%d]',
+              '%f[%P][%l%d]+%f[^%l%d]',
+              '^[%l%d]+%f[^%l%d]',
+            },
+            '^().*()$',
+          },
+          i = ai_indent, -- indent
+          g = ai_buffer, -- buffer
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = '[%w_]' }), -- without dot in function name
+        },
         mappings = { around_last = '', inside_last = '' },
       })
     end,
