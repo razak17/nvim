4570ddd53bc9f459b60afa0375ca64c10e95049a
razak17 <moabdulrazak17@gmail.com>
2024-07-13 19:25:21 +0000

    chore(treesitter): remove select textobjects
    use mini.ai

--- lua/ar/plugins/treesitter.lua
+++ lua/ar/plugins/treesitter.lua
@@ -40,43 +40,6 @@ return {
           },
         },
         textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            include_surrounding_whitespace = true,
-            -- stylua: ignore
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-              ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-              ["L="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-              ["R="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
-
-              -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
-              ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
-              ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
-              ["L:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
-              ["R:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
-
-              ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-              ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
-
-              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-
-              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
-              ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
-              ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
-
-              ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
-              ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
-
-              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-            },
-          },
           move = {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
