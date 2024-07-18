2a2e088e04748e4dab1c7ea828513bc1ae08c567
razak17 <moabdulrazak17@gmail.com>
2024-07-18 21:22:31 +0000

    feat(dropbar): use opts

--- lua/ar/plugins/dropbar.lua
+++ lua/ar/plugins/dropbar.lua
@@ -5,7 +5,7 @@ local border = ui.current.border
 return {
   'Bekaboo/dropbar.nvim',
   event = { 'BufRead', 'BufNewFile' },
-  cond = false,
+  cond = not ar.plugins.minimal,
   keys = {
     {
       '<leader>wp',
@@ -13,41 +13,43 @@ return {
       desc = 'winbar: pick',
     },
   },
-  config = function()
-    require('dropbar').setup({
-      general = {
-        update_interval = 100,
-        enable = function(buf, win)
-          local b, w = vim.bo[buf], vim.wo[win]
-          local decor =
-            ui.decorations.get({ ft = b.ft, bt = b.bt, setting = 'winbar' })
-          return decor
-            and decor.ft ~= false
-            and decor.bt ~= false
-            and b.bt == ''
-            and not w.diff
-            and not api.nvim_win_get_config(win).zindex
-            and api.nvim_buf_get_name(buf) ~= ''
+  opts = {
+    general = {
+      update_interval = 100,
+      enable = function(buf, win)
+        local b, w = vim.bo[buf], vim.wo[win]
+        local decor =
+          ui.decorations.get({ ft = b.ft, bt = b.bt, setting = 'winbar' })
+        return decor
+          and decor.ft ~= false
+          and decor.bt ~= false
+          and b.bt == ''
+          and not w.diff
+          and not api.nvim_win_get_config(win).zindex
+          and api.nvim_buf_get_name(buf) ~= ''
+      end,
+    },
+    icons = {
+      ui = { bar = { separator = ' ' .. ui.icons.misc.chevron_right .. ' ' } },
+      kinds = {
+        symbols = vim.tbl_map(
+          function(value) return value .. ' ' end,
+          require('lspkind').symbol_map
+        ),
+      },
+    },
+    menu = {
+      win_configs = {
+        border = border,
+        col = function(menu)
+          return menu.prev_menu and menu.prev_menu._win_configs.width + 1 or 0
         end,
       },
-      icons = {
-        ui = { bar = { separator = ' ' .. ui.icons.misc.triangle .. ' ' } },
-        kinds = {
-          symbols = vim.tbl_map(
-            function(value) return value .. ' ' end,
-            require('lspkind').symbol_map
-          ),
-        },
-      },
-      menu = {
-        win_configs = {
-          border = border,
-          col = function(menu)
-            return menu.prev_menu and menu.prev_menu._win_configs.width + 1 or 0
-          end,
-        },
-      },
-    })
+    },
+  },
+  config = function(_, opts)
+    require('dropbar').setup(opts)
+
     highlight.plugin('dropbar', {
       theme = {
         ['onedark'] = {
