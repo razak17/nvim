f5b0a66197a4559af402957c104c19313a10b710
razak17 <moabdulrazak17@gmail.com>
2024-07-18 16:01:27 +0000

    fix(textobjects): fix config

--- lua/ar/plugins/treesitter.lua
+++ lua/ar/plugins/treesitter.lua
@@ -76,6 +76,30 @@ return {
     dependencies = {
       {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        event = 'VeryLazy',
+        config = function()
+          -- ref: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua?plain=1#L108
+          -- When in diff mode, we want to use the default
+          -- vim text objects c & C instead of the treesitter ones.
+          local move = require('nvim-treesitter.textobjects.move') ---@type table<string,fun(...)>
+          local configs = require('nvim-treesitter.configs')
+          for name, fn in pairs(move) do
+            if name:find('goto') == 1 then
+              move[name] = function(q, ...)
+                if vim.wo.diff then
+                  local config = configs.get_module('textobjects.move')[name] ---@type table<string,string>
+                  for key, query in pairs(config or {}) do
+                    if q == query and key:find('[%]%[][cC]') then
+                      vim.cmd('normal! ' .. key)
+                      return
+                    end
+                  end
+                end
+                return fn(q, ...)
+              end
+            end
+          end
+        end,
         init = function()
           -- PERF: no need to load the plugin, if we only need its queries for mini.ai
           local plugin =
