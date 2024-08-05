d01daccdf77ce1707454e1ca5919183a637296e9
razak17 <moabdulrazak17@gmail.com>
2024-08-05 18:51:09 +0000

    feat(reticle): add FileType event again?

--- lua/ar/plugins/ui.lua
+++ lua/ar/plugins/ui.lua
@@ -124,7 +124,7 @@ return {
     event = { 'BufRead', 'CursorMoved', 'CursorMovedI', 'WinEnter', 'WinLeave' },
     init = function()
       ar.augroup('reticle', {
-        event = { 'VimEnter', 'BufRead', 'WinEnter' },
+        event = { 'VimEnter', 'BufRead', 'WinEnter', 'FileType' },
         command = function(args)
           if not ui.show_cursorline(args.buf) then
             require('reticle').set_cursorline(false)
