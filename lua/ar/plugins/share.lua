1f2b31cc04bdf6af122951a3548ff5302b165949
razak17 <moabdulrazak17@gmail.com>
2024-08-07 23:51:24 +0000

    refactor(core): add share module

---
+++ lua/ar/plugins/share.lua
@@ -0,0 +1,8 @@
+return {
+  --------------------------------------------------------------------------------
+  -- Share Code
+  { 'TobinPalmer/rayso.nvim', cmd = { 'Rayso' }, opts = {} },
+  { 'ellisonleao/carbon-now.nvim', cmd = 'CarbonNow', opts = {} },
+  { 'Sanix-Darker/snips.nvim', cmd = { 'SnipsCreate' }, opts = {} },
+  { 'ethanholz/freeze.nvim', cmd = 'Freeze', opts = {} },
+}
