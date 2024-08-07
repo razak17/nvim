3d88492fb583e21be69e6acefd562bac7cc1f3b2
razak17 <moabdulrazak17@gmail.com>
2024-08-07 23:18:11 +0000

    feat(dropbar): update mapping, refactor key

--- lua/ar/plugins/dropbar.lua
+++ lua/ar/plugins/dropbar.lua
@@ -7,77 +7,77 @@ local decor = ui.decorations
 return {
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
-  'Bekaboo/dropbar.nvim',
-  lazy = false,
-  cond = not ar.plugins.minimal,
-  keys = {
-    {
+    'Bekaboo/dropbar.nvim',
+    lazy = false,
+    cond = not ar.plugins.minimal,
+    keys = {
+      {
         '<leader>wo',
-      function() require('dropbar.api').pick() end,
-      desc = 'winbar: pick',
-    },
-  },
-  opts = {
-    general = {
-      update_interval = 0,
-      attach_events = {
-        'OptionSet',
-        'BufWinEnter',
-        'BufWritePost',
-        'BufEnter',
+        function() require('dropbar.api').pick() end,
+        desc = 'winbar: pick',
       },
-      enable = function(buf, win)
-        local b, w = vim.bo[buf], vim.wo[win]
-        local decs = decor.get({ ft = b.ft, bt = b.bt, setting = 'winbar' })
+    },
+    opts = {
+      general = {
+        update_interval = 0,
+        attach_events = {
+          'OptionSet',
+          'BufWinEnter',
+          'BufWritePost',
+          'BufEnter',
+        },
+        enable = function(buf, win)
+          local b, w = vim.bo[buf], vim.wo[win]
+          local decs = decor.get({ ft = b.ft, bt = b.bt, setting = 'winbar' })
 
-        local show = false
-        if not decs or ar.falsy(decs) then
-          show = true
-        else
-          show = decs.ft == true or decs.bt == true
-        end
-        if not show then
-          vim.opt_local.winbar = ''
-          return false
-        end
-        return show
-          and b.bt == ''
-          and not w.diff
-          and not api.nvim_win_get_config(win).zindex
-          and api.nvim_buf_get_name(buf) ~= ''
-      end,
-    },
-    icons = {
-      ui = { bar = { separator = fmt(' %s ', ui.icons.misc.chevron_right) } },
-      kinds = {
-        symbols = vim.tbl_map(
-          function(value) return value .. ' ' end,
-          require('lspkind').symbol_map
-        ),
-      },
-    },
-    menu = {
-      win_configs = {
-        border = border,
-        col = function(menu)
-          return menu.prev_menu and menu.prev_menu._win_configs.width + 1 or 0
+          local show = false
+          if not decs or ar.falsy(decs) then
+            show = true
+          else
+            show = decs.ft == true or decs.bt == true
+          end
+          if not show then
+            vim.opt_local.winbar = ''
+            return false
+          end
+          return show
+            and b.bt == ''
+            and not w.diff
+            and not api.nvim_win_get_config(win).zindex
+            and api.nvim_buf_get_name(buf) ~= ''
         end,
       },
-    },
-  },
-  config = function(_, opts)
-    require('dropbar').setup(opts)
-
-    highlight.plugin('dropbar', {
-      theme = {
-        ['onedark'] = {
-          { DropBarIconUISeparator = { fg = { from = 'Label' } } },
-          { DropBarIconUIIndicator = { link = 'Label' } },
-          { DropBarMenuHoverEntry = { bg = 'NONE' } },
-          { DropBarMenuCurrentContext = { link = 'CursorLine' } },
+      icons = {
+        ui = { bar = { separator = fmt(' %s ', ui.icons.misc.chevron_right) } },
+        kinds = {
+          symbols = vim.tbl_map(
+            function(value) return value .. ' ' end,
+            require('lspkind').symbol_map
+          ),
         },
       },
-    })
-  end,
+      menu = {
+        win_configs = {
+          border = border,
+          col = function(menu)
+            return menu.prev_menu and menu.prev_menu._win_configs.width + 1 or 0
+          end,
+        },
+      },
+    },
+    config = function(_, opts)
+      require('dropbar').setup(opts)
+
+      highlight.plugin('dropbar', {
+        theme = {
+          ['onedark'] = {
+            { DropBarIconUISeparator = { fg = { from = 'Label' } } },
+            { DropBarIconUIIndicator = { link = 'Label' } },
+            { DropBarMenuHoverEntry = { bg = 'NONE' } },
+            { DropBarMenuCurrentContext = { link = 'CursorLine' } },
+          },
+        },
+      })
+    end,
   },
 }
