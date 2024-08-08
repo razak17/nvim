8ee81a1e819691cc428da83a15eb9921c913590f
razak17 <moabdulrazak17@gmail.com>
2024-08-08 00:59:29 +0000

    feat(lsp): change cursor diagnostics mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -313,7 +313,7 @@ local function setup_mappings(client, bufnr)
       capability = M.textDocument_definition,
       exclude = { 'tsserver' },
     },
-    { 'n', 'gl', diagnostic_float(), desc = 'cursor diagnostics' },
+    { 'n', 'gk', diagnostic_float(), desc = 'cursor diagnostics' },
     { 'n', 'gL', diagnostic_float(true), desc = 'line diagnostics' },
     {
       'n',
