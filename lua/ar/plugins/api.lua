d8c3a82aeeab54e1f39d9419ddf23f752834a54f
razak17 <moabdulrazak17@gmail.com>
2024-09-07 17:46:07 +0000

    refactor(plugins): add api module

---
+++ lua/ar/plugins/api.lua
@@ -0,0 +1,44 @@
+local minimal = ar.plugins.minimal
+
+return {
+  {
+    'rest-nvim/rest.nvim',
+    enabled = false,
+    cond = not minimal and false, -- archived
+    ft = { 'http', 'json' },
+    init = function()
+      require('which-key').add({
+        { '<leader>rr', group = 'Rest' },
+      })
+    end,
+    -- stylua: ignore
+    keys = {
+      { '<leader>rrs', '<Plug>RestNvim', desc = 'rest: run', buffer = 0 },
+      { '<leader>rrp', '<Plug>RestNvimPreview', desc = 'rest: preview', buffer = 0, },
+      { '<leader>rrl', '<Plug>RestNvimLast', desc = 'rest: run last', buffer = 0, },
+    },
+    opts = { skip_ssl_verification = true },
+    config = function(_, opts) require('rest-nvim').setup(opts) end,
+    dependencies = {
+      { 'vhyrro/luarocks.nvim', opts = {} },
+    },
+  },
+  {
+    'mistweaverco/kulala.nvim',
+    cond = not minimal,
+    ft = { 'http' },
+    init = function()
+      require('which-key').add({ { '<leader>rk', group = 'Kulala' } })
+    end,
+    -- stylua: ignore
+    keys = {
+      { '<leader>rke', ':lua require("kulala").set_selected_env()<CR>', desc = 'kulala: select env' },
+      { '<leader>rkk', ':lua require("kulala").run()<CR>', desc = 'kulala: run' },
+      { '<leader>rkn', ':lua require("kulala").jump_next()<CR>', desc = 'kulala: next' },
+      { '<leader>rkp', ':lua require("kulala").jump_prev()<CR>', desc = 'kulala: prev' },
+      { '<leader>rks', ':lua require("kulala").scratchpad()<CR>', desc = 'kulala: scratchpad' },
+      { '<leader>rkt', ':lua require("kulala").toggle_view()<CR>', desc = 'kulala: toggle view' },
+    },
+    opts = { default_env = 'local' },
+  },
+}
