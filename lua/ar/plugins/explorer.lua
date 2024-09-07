165cef09964e95d536674d1b6bf67fe5fdd2f47c
razak17 <moabdulrazak17@gmail.com>
2024-09-07 16:46:16 +0000

    refactor(neo-tree): use neo-tree events table

--- lua/ar/plugins/explorer.lua
+++ lua/ar/plugins/explorer.lua
@@ -51,223 +51,232 @@ return {
     keys = {
       { '<c-n>', '<cmd>Neotree toggle reveal<CR>', desc = 'toggle tree' },
     },
-    opts = {
-      close_if_last_window = true,
-      sources = { 'filesystem', 'git_status', 'document_symbols' },
-      enable_opened_markers = true,
-      source_selector = {
-        winbar = true,
-        separator_active = '',
-        tabs_layout = 'center',
-        sources = {
-          { source = 'filesystem' },
-          -- { source = 'git_status' },
-          { source = 'document_symbols' },
+    opts = function()
+      local events = require('neo-tree.events')
+
+      local function on_move(data) on_rename(data.source, data.destination) end
+
+      local function disable_autosave()
+        ar.autosave.current = ar.autosave.enable
+        if ar.autosave.current then ar.autosave.enable = false end
+      end
+
+      return {
+        close_if_last_window = true,
+        sources = { 'filesystem', 'git_status', 'document_symbols' },
+        enable_opened_markers = true,
+        source_selector = {
+          winbar = true,
+          separator_active = '',
+          tabs_layout = 'center',
+          sources = {
+            { source = 'filesystem' },
+            -- { source = 'git_status' },
+            { source = 'document_symbols' },
+          },
         },
-      },
-      enable_git_status = false,
-      enable_diagnostics = false,
-      git_status_async = false,
-      nesting_rules = {
-        ['dart'] = { 'freezed.dart', 'g.dart' },
-        ['go'] = {
-          pattern = '(.*)%.go$',
-          files = { '%1_test.go' },
+        enable_git_status = false,
+        enable_diagnostics = false,
+        git_status_async = false,
+        nesting_rules = {
+          ['dart'] = { 'freezed.dart', 'g.dart' },
+          ['go'] = {
+            pattern = '(.*)%.go$',
+            files = { '%1_test.go' },
+          },
+          ['docker'] = {
+            pattern = '^dockerfile$',
+            ignore_case = true,
+            files = { '.dockerignore', 'docker-compose.*', 'dockerfile*' },
+          },
         },
-        ['docker'] = {
-          pattern = '^dockerfile$',
-          ignore_case = true,
-          files = { '.dockerignore', 'docker-compose.*', 'dockerfile*' },
+        event_handlers = {
+          {
+            event = events.NEO_TREE_BUFFER_ENTER,
+            handler = function() highlight.set('Cursor', { blend = 100 }) end,
+          },
+          {
+            event = events.NEO_TREE_POPUP_BUFFER_ENTER,
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
+          },
+          {
+            event = events.NEO_TREE_BUFFER_LEAVE,
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
+          },
+          {
+            event = events.NEO_TREE_POPUP_BUFFER_LEAVE,
+            handler = function() highlight.set('Cursor', { blend = 100 }) end,
+          },
+          {
+            event = events.NEO_TREE_WINDOW_AFTER_CLOSE,
+            handler = function() highlight.set('Cursor', { blend = 0 }) end,
+          },
+          {
+            event = events.NEO_TREE_POPUP_INPUT_READY,
+            handler = function() vim.cmd('stopinsert') end,
+          },
+          {
+            event = events.BEFORE_FILE_RENAME,
+            handler = function() disable_autosave() end,
+          },
+          {
+            event = events.FILE_RENAMED,
+            handler = function() ar.autosave.enable = ar.autosave.current end,
+          },
+          {
+            event = 'before_file_delete',
+            handler = function() disable_autosave() end,
+          },
+          { event = events.FILE_MOVED, handler = on_move },
+          { event = events.FILE_RENAMED, handler = on_move },
         },
-      },
-      event_handlers = {
-        {
-          event = 'neo_tree_buffer_enter',
-          handler = function() highlight.set('Cursor', { blend = 100 }) end,
-        },
-        {
-          event = 'neo_tree_popup_buffer_enter',
-          handler = function() highlight.set('Cursor', { blend = 0 }) end,
-        },
-        {
-          event = 'neo_tree_buffer_leave',
-          handler = function() highlight.set('Cursor', { blend = 0 }) end,
-        },
-        {
-          event = 'neo_tree_popup_buffer_leave',
-          handler = function() highlight.set('Cursor', { blend = 100 }) end,
-        },
-        {
-          event = 'neo_tree_window_after_close',
-          handler = function() highlight.set('Cursor', { blend = 0 }) end,
-        },
-        {
-          event = 'neo_tree_popup_input_ready',
-          handler = function(input) vim.cmd('stopinsert') end,
-        },
-        {
-          event = 'before_file_rename',
-          handler = function()
-            ar.autosave.current = ar.autosave.enable
-            if ar.autosave.current then ar.autosave.enable = false end
-          end,
-        },
-        {
-          event = 'file_renamed',
-          handler = function() ar.autosave.enable = ar.autosave.current end,
-        },
-        {
-          event = 'before_file_delete',
-          handler = function()
-            ar.autosave.current = ar.autosave.enable
-            if ar.autosave.current then ar.autosave.enable = false end
-          end,
-        },
-      },
-      filesystem = {
-        hijack_netrw_behavior = 'disabled',
-        use_libuv_file_watcher = true,
-        follow_current_file = {
-          enabled = false,
-          leave_dirs_open = true,
-        },
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          never_show = { '.DS_Store' },
-        },
-        -- Adding custom commands for delete and delete_visual
-        -- https://github.com/nvim-neo-tree/neo-tree.nvim/issues/202#issuecomment-1428278234
-        commands = {
-          delete = function(state)
-            local inputs = require('neo-tree.ui.inputs')
-            local path = state.tree:get_node().path
-            local msg = 'Are you sure you want to trash ' .. path
-            inputs.confirm(msg, function(confirmed)
-              if confirmed then
-                ar.trash_file(fn.fnameescape(path))
+        filesystem = {
+          hijack_netrw_behavior = 'disabled',
+          use_libuv_file_watcher = true,
+          follow_current_file = {
+            enabled = false,
+            leave_dirs_open = true,
+          },
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+            never_show = { '.DS_Store' },
+          },
+          -- Adding custom commands for delete and delete_visual
+          -- https://github.com/nvim-neo-tree/neo-tree.nvim/issues/202#issuecomment-1428278234
+          commands = {
+            delete = function(state)
+              local inputs = require('neo-tree.ui.inputs')
+              local path = state.tree:get_node().path
+              local msg = 'Are you sure you want to trash ' .. path
+              inputs.confirm(msg, function(confirmed)
+                if confirmed then
+                  ar.trash_file(fn.fnameescape(path))
+                  require('neo-tree.sources.manager').refresh(state.name)
+                end
+              end)
+            end,
+            delete_visual = function(state, selected_nodes)
+              local inputs = require('neo-tree.ui.inputs')
+              local function get_table_len(tbl)
+                local len = 0
+                for _ in pairs(tbl) do
+                  len = len + 1
+                end
+                return len
+              end
+              local count = get_table_len(selected_nodes)
+              local msg = 'Are you sure you want to trash '
+                .. count
+                .. ' files?'
+              inputs.confirm(msg, function(confirmed)
+                if not confirmed then return end
+                for _, node in ipairs(selected_nodes) do
+                  ar.trash_file(fn.fnameescape(node.path))
+                end
                 require('neo-tree.sources.manager').refresh(state.name)
-              end
-            end)
-          end,
-          delete_visual = function(state, selected_nodes)
-            local inputs = require('neo-tree.ui.inputs')
-            local function get_table_len(tbl)
-              local len = 0
-              for _ in pairs(tbl) do
-                len = len + 1
-              end
-              return len
-            end
-            local count = get_table_len(selected_nodes)
-            local msg = 'Are you sure you want to trash ' .. count .. ' files?'
-            inputs.confirm(msg, function(confirmed)
-              if not confirmed then return end
-              for _, node in ipairs(selected_nodes) do
-                ar.trash_file(fn.fnameescape(node.path))
-              end
-              require('neo-tree.sources.manager').refresh(state.name)
-            end)
-          end,
+              end)
+            end,
+          },
+          window = {
+            mappings = {
+              ['<space>'] = 'none',
+              ['<C-p>'] = function(state)
+                local node = state.tree:get_node()
+                local cwd = node.path
+                if node.type == 'file' then cwd = node._parent_id end
+
+                find_or_search_in_dir(cwd, 'find')
+              end,
+              ['<C-g>'] = function(state)
+                local node = state.tree:get_node()
+                local cwd = node.path
+                if node.type == 'file' then cwd = node._parent_id end
+
+                find_or_search_in_dir(cwd, 'search')
+              end,
+              ['oi'] = function(state)
+                api.nvim_input(': ' .. state.tree:get_node().path .. '<Home>')
+              end,
+              ['<C-o>'] = function(state)
+                local node = state.tree:get_node()
+                if vim.list_contains(ar.get_media(), node.ext) then
+                  ar.open_media(node.path)
+                else
+                  vim.notify('Not a media file')
+                end
+              end,
+              ['oP'] = function(state) copy(state.tree:get_node().path) end,
+              ['oY'] = function(state) copy(state.tree:get_node().name) end,
+            },
+          },
+        },
+        default_component_configs = {
+          indent = {
+            with_markers = false,
+            with_expanders = true,
+            expander_collapsed = icons.misc.triangle_short_right,
+            expander_expanded = icons.misc.triangle_short_down,
+          },
+          icon = {
+            folder_closed = codicons.documents.folder,
+            folder_open = codicons.documents.open_folder,
+            folder_empty = codicons.documents.empty_folder,
+            folder_empty_open = codicons.documents.empty_folder,
+            default = codicons.documents.default_file,
+            highlight = 'DevIconDefault',
+          },
+          name = { highlight_opened_files = true },
+          document_symbols = {
+            follow_cursor = true,
+            kinds = vim.iter(lspkind.symbol_map):fold({}, function(acc, k, v)
+              acc[k] = { icon = v, hl = lsp_hls[k] }
+              return acc
+            end),
+          },
+          modified = { symbol = codicons.misc.circle .. ' ' },
+          git_status = {
+            symbols = {
+              added = codicons.git.added,
+              deleted = codicons.git.removed,
+              modified = codicons.git.mod,
+              renamed = codicons.git.renamed,
+              untracked = icons.git.untracked,
+              ignored = codicons.git.ignored,
+              unstaged = codicons.git.unstaged,
+              staged = codicons.git.staged,
+              conflict = icons.git.branch,
+            },
+          },
+          file_size = {
+            required_width = 50,
+          },
+          diagnostics = {
+            highlights = {
+              hint = 'DiagnosticHint',
+              info = 'DiagnosticInfo',
+              warn = 'DiagnosticWarn',
+              error = 'DiagnosticError',
+            },
+          },
         },
         window = {
+          position = 'right',
+          width = 35,
           mappings = {
-            ['<space>'] = 'none',
-            ['<C-p>'] = function(state)
-              local node = state.tree:get_node()
-              local cwd = node.path
-              if node.type == 'file' then cwd = node._parent_id end
-
-              find_or_search_in_dir(cwd, 'find')
-            end,
-            ['<C-g>'] = function(state)
-              local node = state.tree:get_node()
-              local cwd = node.path
-              if node.type == 'file' then cwd = node._parent_id end
-
-              find_or_search_in_dir(cwd, 'search')
-            end,
-            ['oi'] = function(state)
-              api.nvim_input(': ' .. state.tree:get_node().path .. '<Home>')
-            end,
-            ['<C-o>'] = function(state)
-              local node = state.tree:get_node()
-              if vim.list_contains(ar.get_media(), node.ext) then
-                ar.open_media(node.path)
-              else
-                vim.notify('Not a media file')
-              end
-            end,
-            ['oP'] = function(state) copy(state.tree:get_node().path) end,
-            ['oY'] = function(state) copy(state.tree:get_node().name) end,
+            ['<esc>'] = 'revert_preview',
+            ['<CR>'] = 'open_with_window_picker',
+            ['l'] = 'open',
+            ['o'] = 'toggle_node',
+            ['P'] = { 'toggle_preview', config = { use_float = false } },
+            ['v'] = 'vsplit_with_window_picker',
+            ['O'] = 'expand_all_nodes',
+            ['Z'] = 'close_all_nodes',
           },
         },
-      },
-      default_component_configs = {
-        indent = {
-          with_markers = false,
-          with_expanders = true,
-          expander_collapsed = icons.misc.triangle_short_right,
-          expander_expanded = icons.misc.triangle_short_down,
-        },
-        icon = {
-          folder_closed = codicons.documents.folder,
-          folder_open = codicons.documents.open_folder,
-          folder_empty = codicons.documents.empty_folder,
-          folder_empty_open = codicons.documents.empty_folder,
-          default = codicons.documents.default_file,
-          highlight = 'DevIconDefault',
-        },
-        name = { highlight_opened_files = true },
-        document_symbols = {
-          follow_cursor = true,
-          kinds = vim.iter(lspkind.symbol_map):fold({}, function(acc, k, v)
-            acc[k] = { icon = v, hl = lsp_hls[k] }
-            return acc
-          end),
-        },
-        modified = { symbol = codicons.misc.circle .. ' ' },
-        git_status = {
-          symbols = {
-            added = codicons.git.added,
-            deleted = codicons.git.removed,
-            modified = codicons.git.mod,
-            renamed = codicons.git.renamed,
-            untracked = icons.git.untracked,
-            ignored = codicons.git.ignored,
-            unstaged = codicons.git.unstaged,
-            staged = codicons.git.staged,
-            conflict = icons.git.branch,
-          },
-        },
-        file_size = {
-          required_width = 50,
-        },
-        diagnostics = {
-          highlights = {
-            hint = 'DiagnosticHint',
-            info = 'DiagnosticInfo',
-            warn = 'DiagnosticWarn',
-            error = 'DiagnosticError',
-          },
-        },
-      },
-      window = {
-        position = 'right',
-        width = 35,
-        mappings = {
-          ['<esc>'] = 'revert_preview',
-          ['<CR>'] = 'open_with_window_picker',
-          ['l'] = 'open',
-          ['o'] = 'toggle_node',
-          ['P'] = { 'toggle_preview', config = { use_float = false } },
-          ['v'] = 'vsplit_with_window_picker',
-          ['O'] = 'expand_all_nodes',
-          ['Z'] = 'close_all_nodes',
-        },
-      },
-    },
+      }
+    end,
     config = function(_, opts)
       highlight.plugin('NeoTree', {
         theme = {
@@ -283,15 +292,6 @@ return {
             },
           },
         },
-      })
-
-      local function on_move(data) on_rename(data.source, data.destination) end
-
-      local events = require('neo-tree.events')
-      opts.event_handlers = opts.event_handlers or {}
-      vim.list_extend(opts.event_handlers, {
-        { event = events.FILE_MOVED, handler = on_move },
-        { event = events.FILE_RENAMED, handler = on_move },
       })
       require('neo-tree').setup(opts)
     end,
