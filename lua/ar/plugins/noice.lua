ef92ce716c9746b3fb2a04f78ef08c59bf99d7fb
razak17 <moabdulrazak17@gmail.com>
2024-09-08 18:54:50 +0000

    feat(noice): skip validation messages from jdtls

--- lua/ar/plugins/noice.lua
+++ lua/ar/plugins/noice.lua
@@ -238,6 +238,23 @@ return {
         },
         opts = { skip = true },
       },
+      -- skip validation messages from jdtls
+      {
+        filter = {
+          event = 'lsp',
+          kind = 'progress',
+          cond = function(message)
+            local client = vim.tbl_get(message.opts, 'progress', 'client')
+            if client == 'jdtls' then
+              local content = vim.tbl_get(message.opts, 'progress', 'message')
+              return content == 'Validate documents'
+            end
+
+            return false
+          end,
+        },
+        opts = { skip = true },
+      },
     },
     commands = {
       history = { view = 'vsplit' },
