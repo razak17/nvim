a2dc5457bc779e344927be88392963996b095b5f
razak17 <moabdulrazak17@gmail.com>
2024-09-22 22:08:03 +0000

    feat(plugins): add keepcursor.nvim

--- lua/ar/plugins/ui.lua
+++ lua/ar/plugins/ui.lua
@@ -21,11 +21,35 @@ return {
   {
     'arnamak/stay-centered.nvim',
     event = 'BufReadPost',
-    -- stylua: ignore
-    keys = {
-      { '<leader>o;', '<Cmd>lua require("stay-centered").toggle()<CR>', desc = 'stay-centered: toggle' },
+    init = function()
+      ar.add_to_menu('toggle', {
+        ['Toggle Stay Centered'] = 'lua require("stay-centered").toggle()',
+      })
+    end,
+    opts = { enabled = true },
+  },
+  {
+    'rlychrisg/keepcursor.nvim',
+    cmd = {
+      'ToggleCursorTop',
+      'ToggleCursorBot',
+      'ToggleCursorMid',
+      'ToggleCursorRight',
+      'ToggleCursorLeft',
     },
-    opts = {},
+    init = function()
+      ar.add_to_menu('toggle', {
+        ['Toggle Cursor Top'] = 'ToggleCursorTop',
+        ['Toggle Cursor Bottom'] = 'ToggleCursorBot 2',
+        ['Toggle Cursor Middle'] = 'ToggleCursorMid',
+        ['Toggle Cursor Right'] = 'ToggleCursorRight 30',
+        ['Toggle Cursor Left'] = 'ToggleCursorLeft',
+      })
+    end,
+    opts = {
+      enabled_on_start_v = 'none',
+      enabled_on_start_h = 'none',
+    },
   },
   {
     'nmac427/guess-indent.nvim',
