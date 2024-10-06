c3573bf8f37c61eed4d3eaaf52da06d3a5508732
razak17 <moabdulrazak17@gmail.com>
2024-10-06 17:39:01 +0000

    refactor(plugins): add transparent module

---
+++ lua/ar/plugins/transparent.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    'tribela/transparent.nvim',
+    cond = ar.ui.transparent.enable,
+    event = 'VimEnter',
+    opts = {
+      extra_groups = {
+        'PopupNormal',
+        'PopupBorder',
+        'NormalFloat',
+        'FloatBorder',
+        'NeoTreeTabInactive',
+        'NoiceMini',
+        'NoicePopupBaseGroup',
+        'NoicePopupWarnBaseGroup',
+        'NoicePopupInfoBaseGroup',
+        'NoiceCmdlinePopup',
+        'NoiceCmdlinePopupBorder',
+        'NoiceFormatProgressDone',
+        'NoiceFormatProgressTodo',
+        'NoiceLspProgressClient',
+        'NoiceLspProgressSpinner',
+        'NoiceLspProgressTitle',
+        'NoiceFormatEvent',
+        'NoiceConfirm',
+        'NoiceConfirmBorder',
+        'MarkviewCode',
+        'MarkviewInlineCode',
+        'PickerBorder',
+        'PickerPromptNormal',
+        'PickerPromptBorder',
+        'PickerResultsNormal',
+        'PickerResultsBorder',
+        'PickerPreviewNormal',
+        'PickerPreviewBorder',
+        'PickerSelection',
+      },
+    },
+    config = function(_, opts)
+      require('transparent').setup(opts)
+
+      ar.highlight.plugin('transparent', {
+        theme = {
+          ['onedark'] = {
+            { MarkviewCode = { link = 'NormalFloat' } },
+            { MarkviewInlineCode = { link = 'NormalFloat' } },
+            { FloatBorder = { link = 'WinSeparator' } },
+            { PickerBorder = { link = 'WinSeparator' } },
+            { PickerPromptBorder = { link = 'WinSeparator' } },
+            { PickerResultsBorder = { link = 'WinSeparator' } },
+            { PickerPreviewBorder = { link = 'WinSeparator' } },
+            { PopupBorder = { link = 'FloatBorder' } },
+            {
+              NoicePopupBaseGroup = {
+                fg = { from = 'Directory', alter = -0.25 },
+              },
+            },
+            { NoicePopupWarnBaseGroup = { link = 'NoicePopupBaseGroup' } },
+            { NoicePopupInfoBaseGroup = { link = 'NoicePopupBaseGroup' } },
+          },
+        },
+      })
+    end,
+  },
+}
