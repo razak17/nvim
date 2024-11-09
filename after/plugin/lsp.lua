9fa60f9afa69152a3ab6e650dd318f63adadc082
razak17 <moabdulrazak17@gmail.com>
2024-11-09 18:44:50 +0000

    feat(lsp): add handlers for when noice is not enabled

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -683,6 +683,35 @@ diagnostic.config({
     end,
   },
 })
+
+if not ar.is_available('noice.nvim') then
+  lsp.handlers['textDocument/hover'] = function(...)
+    local hover_handler = lsp.with(lsp.handlers.hover, {
+      border = border,
+      max_width = max_width,
+      max_height = max_height,
+    })
+    vim.b.lsp_hover_buf, vim.b.lsp_hover_win = hover_handler(...)
+  end
+
+  lsp.handlers['textDocument/signatureHelp'] =
+    lsp.with(lsp.handlers.signature_help, {
+      border = border,
+      max_width = max_width,
+      max_height = max_height,
+    })
+
+  lsp.handlers['window/showMessage'] = function(_, result, ctx)
+    local client = lsp.get_client_by_id(ctx.client_id)
+    if not client then return end
+    local lvl = ({ 'ERROR', 'WARN', 'INFO', 'DEBUG' })[result.type]
+    vim.notify(result.message, lvl, {
+      title = 'LSP | ' .. client.name,
+      timeout = 8000,
+      keep = function() return lvl == 'ERROR' or lvl == 'WARN' end,
+    })
+  end
+end
 --------------------------------------------------------------------------------
 -- LSP Progress
 --------------------------------------------------------------------------------
