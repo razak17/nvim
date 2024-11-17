a93b9d732f356307a94289835db413b6e16ffe43
razak17 <moabdulrazak17@gmail.com>
2024-11-17 23:44:02 +0000

    feat(plugins): add lua-console

---
+++ lua/ar/plugins/scratchpad.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'yarospace/lua-console.nvim',
+    keys = '<leader>ok',
+    opts = {
+      buffer = {
+        prepend_result_with = '=> ',
+        save_path = vim.fn.stdpath('state') .. '/lua-console.lua',
+        load_on_start = true, -- load saved session on first entry
+        preserve_context = true, -- preserve context between executions
+      },
+      windo = {
+        border = 'double', -- single|double|rounded
+        height = 0.6, -- percentage of main window
+      },
+      mappings = {
+        toggle = '<leader>ok',
+        quit = 'q',
+        eval = '<CR>',
+        clear = 'C',
+        messages = 'M',
+        save = 'S',
+        load = 'L',
+        resize_up = '<C-Up>',
+        resize_down = '<C-Down>',
+        help = '?',
+      },
+    },
+  },
+}
