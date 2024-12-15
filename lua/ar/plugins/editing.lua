cdd85b4a613950f3d9934f20eba03422be3cb5d2
razak17 <moabdulrazak17@gmail.com>
2024-12-15 12:20:37 +0000

    feat(ultimate-autopair): customize config

--- lua/ar/plugins/editing.lua
+++ lua/ar/plugins/editing.lua
@@ -140,7 +140,44 @@ return {
     'altermo/ultimate-autopair.nvim',
     cond = not minimal,
     event = { 'InsertEnter', 'CmdlineEnter' },
-    opts = {},
+    opts = {
+
+      bs = {
+        space = 'balance',
+        cmap = false, -- keep my `<BS>` mapping for the cmdline
+      },
+      cr = { autoclose = true },
+      space = { enable = true },
+      space2 = { enable = true },
+      config_internal_pairs = {
+        { "'", "'", nft = { 'markdown' } }, -- since used as apostrophe
+        { '"', '"', nft = { 'vim' } }, -- vimscript uses quotes as comments
+      },
+      -- INFO custom keys need to be "appended" to the opts as a list
+      { '**', '**', ft = { 'markdown' } }, -- bold
+      { [[\"]], [[\"]], ft = { 'zsh', 'json', 'applescript' } }, -- escaped quote
+
+      { -- commit scope (= only first word) for commit messages
+        '(',
+        '): ',
+        ft = { 'gitcommit' },
+        cond = function(_) return not vim.api.nvim_get_current_line():find(' ') end,
+      },
+
+      -- for keymaps like `<C-a>`
+      { '<', '>', ft = { 'vim' } },
+      {
+        '<',
+        '>',
+        ft = { 'lua' },
+        cond = function(fn)
+          -- FIX https://github.com/altermo/ultimate-autopair.nvim/issues/88
+          local inLuaLua =
+            vim.endswith(vim.api.nvim_buf_get_name(0), '/ftplugin/lua.lua')
+          return not inLuaLua and fn.in_string()
+        end,
+      },
+    },
   },
   {
     'windwp/nvim-autopairs',
