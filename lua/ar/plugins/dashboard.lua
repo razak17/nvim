4a2ec6596c3018d2333ac1bbe4e1ca5afeaff36a
razak17 <moabdulrazak17@gmail.com>
2024-12-27 22:49:51 +0000

    refactor(startup): simplify setup

--- lua/ar/plugins/dashboard.lua
+++ lua/ar/plugins/dashboard.lua
@@ -179,7 +179,94 @@ return {
     'startup-nvim/startup.nvim',
     cond = minimal and niceties,
     lazy = false,
-    config = function() require('startup').setup({ theme = 'linguini' }) end,
+    opts = function()
+      local header = {
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                                                                        ]],
+        [[                                                                        ]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂         ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂         ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂         ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂         ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[          ▂▂▂▂▂    ▂▂▂▂▂▂    ▂▂▂▂▂    ▂▂▂▂▂    ▂▂▂▂▂▂    ▂▂▂▂▂          ]],
+        [[          ▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂          ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                                                                        ]],
+        [[                                                                        ]],
+        [[          ▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂          ]],
+        [[          ▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂          ]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂    ▂▂▂▂▂    ▂▂▂▂▂    ▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂]],
+        [[                        ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂                        ]],
+        [[                        ▂▂▂▂▂▂▂▂▂▂    ▂▂▂▂▂▂▂▂▂▂                        ]],
+        [[                                                                        ]],
+        [[                                                                        ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂▂▂▂▂              ▂▂▂▂▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+        [[                   ▂▂▂▂▂▂                      ▂▂▂▂▂▂                   ]],
+      }
+
+      return {
+        header = {
+          type = 'text',
+          align = 'center',
+          fold_section = false,
+          title = 'Header',
+          margin = 3,
+          content = header,
+          highlight = '@variable',
+          oldfiles_amount = 0,
+        },
+        clock = {
+          type = 'text',
+          content = function()
+            local time = '' .. os.date('%H:%M')
+            local date = '' .. os.date('%Y-%m-%d')
+            return { date, time }
+          end,
+          oldfiles_directory = false,
+          align = 'center',
+          fold_section = false,
+          title = '',
+          margin = 3,
+          highlight = '@variable',
+        },
+        statistics = {
+          type = 'text',
+          content = function()
+            local fmt = string.format
+            local v = vim.version()
+            local version = fmt(
+              'Neovim v%d.%d.%d %s',
+              v.major,
+              v.minor,
+              v.patch,
+              v.prerelease and '(nightly)' or ''
+            )
+            return { version }
+          end,
+          oldfiles_directory = false,
+          align = 'center',
+          fold_section = false,
+          title = '',
+          margin = 3,
+          highlight = '@comment',
+        },
+        parts = { 'header', 'clock', 'statistics' },
+      }
+    end,
+    config = function(_, opts) require('startup').setup(opts) end,
   },
   {
     'letieu/btw.nvim',
