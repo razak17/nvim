29a7cb603e79ef348bdf7e041e8b623d5b0adefb
razak17 <moabdulrazak17@gmail.com>
2024-12-30 22:54:45 +0000

    refactor(plugins): add multicursor module

--- lua/ar/plugins/editing.lua
+++ lua/ar/plugins/editing.lua
@@ -1,4 +1,3 @@
-local border = ar.ui.current.border
 local minimal, niceties = ar.plugins.minimal, ar.plugins.niceties
 
 return {
@@ -339,54 +338,6 @@ return {
     'subnut/nvim-ghost.nvim',
     cond = ar.plugins.overrides.ghost_text.enable,
     lazy = not ar.plugins.overrides.ghost_text.enable,
-  },
-  {
-    'jake-stewart/multicursor.nvim',
-    event = 'VeryLazy',
-    config = function()
-      local mc = require('multicursor-nvim')
-      mc.setup()
-      map({ 'n', 'v' }, '<M-e>', function() mc.matchAddCursor(1) end)
-      map({ 'n', 'v' }, '<M-f>', function() mc.matchSkipCursor(1) end)
-      map('n', '<esc>', function()
-        if mc.cursorsEnabled() then mc.clearCursors() end
-      end)
-      ar.highlight.plugin('multicursor', {
-        theme = {
-          ['onedark'] = {
-            {
-              MultiCursorCursor = {
-                bg = { from = 'Special', attr = 'fg' },
-                fg = { from = 'Comment' },
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    'smoka7/multicursors.nvim',
-    cond = not minimal and false,
-    opts = {
-      hint_config = { border = border },
-    },
-    cmd = {
-      'MCstart',
-      'MCvisual',
-      'MCclear',
-      'MCpattern',
-      'MCvisualPattern',
-      'MCunderCursor',
-    },
-    keys = {
-      {
-        '<M-e>',
-        '<cmd>MCstart<cr>',
-        mode = { 'v', 'n' },
-        desc = 'Create a selection for selected text or word under the cursor',
-      },
-    },
   },
   {
     'gabrielpoca/replacer.nvim',
---
+++ lua/ar/plugins/multicursor.lua
@@ -0,0 +1,54 @@
+local border = ar.ui.current.border
+local minimal = ar.plugins.minimal
+
+return {
+  {
+    'jake-stewart/multicursor.nvim',
+    cond = not minimal,
+    event = 'VeryLazy',
+    config = function()
+      local mc = require('multicursor-nvim')
+      mc.setup()
+      map({ 'n', 'v' }, '<M-e>', function() mc.matchAddCursor(1) end)
+      map({ 'n', 'v' }, '<M-f>', function() mc.matchSkipCursor(1) end)
+      map('n', '<esc>', function()
+        if mc.cursorsEnabled() then mc.clearCursors() end
+      end)
+      ar.highlight.plugin('multicursor', {
+        theme = {
+          ['onedark'] = {
+            {
+              MultiCursorCursor = {
+                bg = { from = 'Special', attr = 'fg' },
+                fg = { from = 'Comment' },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    'smoka7/multicursors.nvim',
+    cond = not minimal and false,
+    opts = {
+      hint_config = { border = border },
+    },
+    cmd = {
+      'MCstart',
+      'MCvisual',
+      'MCclear',
+      'MCpattern',
+      'MCvisualPattern',
+      'MCunderCursor',
+    },
+    keys = {
+      {
+        '<M-e>',
+        '<cmd>MCstart<cr>',
+        mode = { 'v', 'n' },
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
+}
