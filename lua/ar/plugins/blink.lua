d23b176fb10b93f28b3d018b3dd312df5f7c5839
razak17 <moabdulrazak17@gmail.com>
2025-01-01 09:15:07 +0000

    chore(blink): add signature border

--- lua/ar/plugins/blink.lua
+++ lua/ar/plugins/blink.lua
@@ -18,7 +18,7 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
       },
-      signature = { enabled = true },
+      signature = { window = { border = border } },
       completion = {
         accept = {
           -- experimental auto-brackets support
