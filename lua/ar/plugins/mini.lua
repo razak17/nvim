73fc95724da73b675239a991f93cff8cf034263b
razak17 <moabdulrazak17@gmail.com>
2025-01-05 12:18:15 +0000

    feat(mini.pick): send to qflist

--- lua/ar/plugins/mini.lua
+++ lua/ar/plugins/mini.lua
@@ -363,6 +363,41 @@ return {
         })
       end
 
+      local parsed_matches = function()
+        local list = {}
+        local matches = picker.get_picker_matches().all
+
+        for _, match in ipairs(matches) do
+          if type(match) == 'table' then
+            table.insert(list, match)
+          else
+            local path, lnum, col, search =
+              string.match(match, '(.-)%z(%d+)%z(%d+)%z%s*(.+)')
+            local text = path
+              and string.format('%s [%s:%s]  %s', path, lnum, col, search)
+            local filename = path or vim.trim(match):match('%s+(.+)')
+
+            table.insert(list, {
+              filename = filename or match,
+              lnum = lnum or 1,
+              col = col or 1,
+              text = text or match,
+            })
+          end
+        end
+
+        return list
+      end
+
+      opts.mappings = opts.mappings or {}
+      opts.mappings.send_to_qflist = {
+        char = '<C-q>',
+        func = function()
+          fn.setqflist(parsed_matches(), 'r')
+          if picker.is_picker_active() then picker.stop() end
+          vim.cmd('copen')
+        end,
+      }
       picker.setup(opts)
     end,
   },
