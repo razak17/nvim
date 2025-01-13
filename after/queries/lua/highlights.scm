55c8903015f64548b39e1e33beefc84197cfed2f
razak17 <moabdulrazak17@gmail.com>
2025-01-13 22:14:31 +0000

    chore(conceal): disable lua conceals

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,50 +1,50 @@
 ;; extends
 ;; source: https://github.com/ziontee113/yt-tutorials/tree/treesitter_conceal
 
-(("return"   @keyword) (#set! conceal "")) ;;󰜴
-
-(("local"    @keyword) (#set! conceal "~"))
-
-(("if"       @keyword) (#set! conceal "?"))
-(("else"     @keyword) (#set! conceal "!"))
-(("elseif"   @keyword) (#set! conceal "¿"))
-(("function" @keyword) (#set! conceal "󰘧"))
-(("function" @keyword) (#set! conceal "󰊕"))
-(("for"      @keyword) (#set! conceal "")) ;;
-
-(("and"      @keyword) (#set! conceal "󰣡")) ;;▼
-(("or"       @keyword) (#set! conceal "󰣥"))
-(("not"      @keyword) (#set! conceal "󰣤"))
-(("end"      @keyword) (#set! conceal "⏹")) ;;–
-(("then"     @keyword) (#set! conceal "󰁂"))
-(("do"       @keyword) (#set! conceal ""))
-
-; (("comment_start"    @comment) (#set! conceal ""))
-
-;; Function names
-((function_call name: (identifier) @TSNote (#eq? @TSNote "require"   )) (#set! conceal "󰇚"))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "print"     )) (#set! conceal ""))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "pairs"     )) (#set! conceal "P"))
-((function_call name: (identifier) @TSNote (#eq? @TSNote "ipairs"    )) (#set! conceal "I"))
-
-;; table.
-((dot_index_expression table: (identifier) @keyword  (#eq? @keyword  "math" )) (#set! conceal "󰿈"))
-
-;; break_statement
-(((break_statement) @keyword) (#set! conceal "󰑟"))
-
-;; vim.*
-; (((identifier) @field (#eq? @field "vim")) (#set! conceal ""))
-(((dot_index_expression) @field (#eq? @field "vim.cmd"     )) (#set! conceal "")) ;;
-(((dot_index_expression) @field (#eq? @field "vim.api"     )) (#set! conceal ""))
-(((dot_index_expression) @field (#eq? @field "vim.fn"      )) (#set! conceal "#"))
-(((dot_index_expression) @field (#eq? @field "vim.g"       )) (#set! conceal "G"))
-(((dot_index_expression) @field (#eq? @field "vim.schedule")) (#set! conceal "󱦟"))
-(((dot_index_expression) @field (#eq? @field "vim.opt"     )) (#set! conceal "S"))
-(((dot_index_expression) @field (#eq? @field "vim.env"     )) (#set! conceal "$"))
-(((dot_index_expression) @field (#eq? @field "vim.o"       )) (#set! conceal "O"))
-(((dot_index_expression) @field (#eq? @field "vim.bo"      )) (#set! conceal "B"))
-(((dot_index_expression) @field (#eq? @field "vim.b"       )) (#set! conceal "b"))
-(((dot_index_expression) @field (#eq? @field "vim.wo"      )) (#set! conceal "W"))
-
-(((dot_index_expression) @field (#eq? @field "vim.keymap.set")) (#set! conceal ""))
+; (("return"   @keyword) (#set! conceal "")) ;;󰜴
+;
+; (("local"    @keyword) (#set! conceal "~"))
+;
+; (("if"       @keyword) (#set! conceal "?"))
+; (("else"     @keyword) (#set! conceal "!"))
+; (("elseif"   @keyword) (#set! conceal "¿"))
+; (("function" @keyword) (#set! conceal "󰘧"))
+; (("function" @keyword) (#set! conceal "󰊕"))
+; (("for"      @keyword) (#set! conceal "")) ;;
+;
+; (("and"      @keyword) (#set! conceal "󰣡")) ;;▼
+; (("or"       @keyword) (#set! conceal "󰣥"))
+; (("not"      @keyword) (#set! conceal "󰣤"))
+; (("end"      @keyword) (#set! conceal "⏹")) ;;–
+; (("then"     @keyword) (#set! conceal "󰁂"))
+; (("do"       @keyword) (#set! conceal ""))
+;
+; ; (("comment_start"    @comment) (#set! conceal ""))
+;
+; ;; Function names
+; ((function_call name: (identifier) @TSNote (#eq? @TSNote "require"   )) (#set! conceal "󰇚"))
+; ((function_call name: (identifier) @TSNote (#eq? @TSNote "print"     )) (#set! conceal ""))
+; ((function_call name: (identifier) @TSNote (#eq? @TSNote "pairs"     )) (#set! conceal "P"))
+; ((function_call name: (identifier) @TSNote (#eq? @TSNote "ipairs"    )) (#set! conceal "I"))
+;
+; ;; table.
+; ((dot_index_expression table: (identifier) @keyword  (#eq? @keyword  "math" )) (#set! conceal "󰿈"))
+;
+; ;; break_statement
+; (((break_statement) @keyword) (#set! conceal "󰑟"))
+;
+; ;; vim.*
+; ; (((identifier) @field (#eq? @field "vim")) (#set! conceal ""))
+; (((dot_index_expression) @field (#eq? @field "vim.cmd"     )) (#set! conceal "")) ;;
+; (((dot_index_expression) @field (#eq? @field "vim.api"     )) (#set! conceal ""))
+; (((dot_index_expression) @field (#eq? @field "vim.fn"      )) (#set! conceal "#"))
+; (((dot_index_expression) @field (#eq? @field "vim.g"       )) (#set! conceal "G"))
+; (((dot_index_expression) @field (#eq? @field "vim.schedule")) (#set! conceal "󱦟"))
+; (((dot_index_expression) @field (#eq? @field "vim.opt"     )) (#set! conceal "S"))
+; (((dot_index_expression) @field (#eq? @field "vim.env"     )) (#set! conceal "$"))
+; (((dot_index_expression) @field (#eq? @field "vim.o"       )) (#set! conceal "O"))
+; (((dot_index_expression) @field (#eq? @field "vim.bo"      )) (#set! conceal "B"))
+; (((dot_index_expression) @field (#eq? @field "vim.b"       )) (#set! conceal "b"))
+; (((dot_index_expression) @field (#eq? @field "vim.wo"      )) (#set! conceal "W"))
+;
+; (((dot_index_expression) @field (#eq? @field "vim.keymap.set")) (#set! conceal ""))
