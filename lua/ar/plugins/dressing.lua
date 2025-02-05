24988d719ccaca2d5372062e03788551fe3bfba1
razak17 <moabdulrazak17@gmail.com>
2025-02-05 23:53:13 +0000

    feat(dressing): enable when using telescope as picker

--- lua/ar/plugins/dressing.lua
+++ lua/ar/plugins/dressing.lua
@@ -1,14 +1,22 @@
 local border = ar.ui.current.border
+local minimal = ar.plugins.minimal
 
 return {
   'stevearc/dressing.nvim',
   event = 'BufRead',
-  cond = not ar.plugins.minimal,
+  cond = function()
+    return not minimal and ar_config.picker.variant == 'telescope'
+  end,
   init = function()
     ---@diagnostic disable-next-line: duplicate-set-field
     vim.ui.select = function(...)
       require('lazy').load({ plugins = { 'dressing.nvim' } })
       return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require('lazy').load({ plugins = { 'dressing.nvim' } })
+      return vim.ui.input(...)
     end
   end,
   opts = {
