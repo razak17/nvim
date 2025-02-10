c3c1ce097acaed94d27af38e3ec76d967abee7a9
razak17 <moabdulrazak17@gmail.com>
2025-02-10 17:19:01 +0000

    feat(smart_close): extract to local plugin

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -9,59 +9,6 @@ local falsy = ar.falsy
 local decor = ar.ui.decorations
 local config_path = fn.stdpath('config')
 
-local smart_close_filetypes = ar.p_table({
-  ['qf'] = true,
-  ['log'] = true,
-  ['help'] = true,
-  ['query'] = true,
-  ['dap-float'] = true,
-  ['dbui'] = true,
-  ['lspinfo'] = true,
-  ['httpResult'] = true,
-  ['git.*'] = true,
-  ['Neogit.*'] = true,
-  ['neotest.*'] = true,
-  ['fugitive.*'] = true,
-  ['copilot.*'] = true,
-  ['tsplayground'] = true,
-  ['oil'] = true,
-  ['readup'] = true,
-  ['telescope-lazy'] = true,
-  ['grug-far'] = true,
-  ['toggleterm'] = true,
-  ['sticky'] = true,
-})
-
-local smart_close_buftypes = ar.p_table({
-  ['nofile'] = true,
-})
-
-function ar.smart_close()
-  if fn.winnr('$') ~= 1 then api.nvim_win_close(0, true) end
-end
-
-augroup('SmartClose', {
-  -- Auto open grep quickfix window
-  event = { 'QuickFixCmdPost' },
-  pattern = { '*grep*' },
-  command = 'cwindow',
-}, {
-  -- Close certain filetypes by pressing q.
-  event = { 'FileType' },
-  command = function(args)
-    local is_unmapped = fn.hasmapto('q', 'n') == 0
-
-    local buf = vim.bo[args.buf]
-    local is_eligible = is_unmapped
-      or vim.wo.previewwindow
-      or smart_close_filetypes[buf.ft]
-      or smart_close_buftypes[buf.bt]
-    if is_eligible then
-      map('n', 'q', ar.smart_close, { buffer = args.buf, nowait = true })
-    end
-  end,
-})
-
 augroup('CheckOutsideTime', {
   -- automatically check for changed files outside vim
   event = {
