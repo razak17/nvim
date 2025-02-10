1377a53f17b5de216de9f4a93dcd668394d6df4a
razak17 <moabdulrazak17@gmail.com>
2025-02-10 18:32:10 +0000

    refactor(gitlinker): use opts

--- lua/ar/plugins/git.lua
+++ lua/ar/plugins/git.lua
@@ -495,39 +495,39 @@ return {
   {
     'linrongbin16/gitlinker.nvim',
     cond = enabled,
-    config = function()
-      require('gitlinker').setup({
-        router = {
-          default_branch = {
-            ['^github%.com'] = 'https://github.com/'
-              .. '{_A.USER}/'
-              .. '{_A.REPO}/blob/'
-              .. '{_A.DEFAULT_BRANCH}/' -- always 'master'/'main' branch
-              .. '{_A.FILE}?plain=1' -- '?plain=1'
-              .. '#L{_A.LSTART}'
-              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-          },
-          current_branch = {
-            ['^github%.com'] = 'https://github.com/'
-              .. '{_A.USER}/'
-              .. '{_A.REPO}/blob/'
-              .. '{_A.CURRENT_BRANCH}/' -- always current branch
-              .. '{_A.FILE}?plain=1' -- '?plain=1'
-              .. '#L{_A.LSTART}'
-              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-          },
-          blame_default_branch = {
-            ['^github%.com'] = 'https://github.com/'
-              .. '{_A.USER}/'
-              .. '{_A.REPO}/blame/'
-              .. '{_A.DEFAULT_BRANCH}/'
-              .. '{_A.FILE}?plain=1' -- '?plain=1'
-              .. '#L{_A.LSTART}'
-              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-          },
+    cmd = { 'GitLink' },
+    opts = {
+
+      router = {
+        default_branch = {
+          ['^github%.com'] = 'https://github.com/'
+            .. '{_A.USER}/'
+            .. '{_A.REPO}/blob/'
+            .. '{_A.DEFAULT_BRANCH}/' -- always 'master'/'main' branch
+            .. '{_A.FILE}?plain=1' -- '?plain=1'
+            .. '#L{_A.LSTART}'
+            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
         },
-      })
-    end,
+        current_branch = {
+          ['^github%.com'] = 'https://github.com/'
+            .. '{_A.USER}/'
+            .. '{_A.REPO}/blob/'
+            .. '{_A.CURRENT_BRANCH}/' -- always current branch
+            .. '{_A.FILE}?plain=1' -- '?plain=1'
+            .. '#L{_A.LSTART}'
+            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+        },
+        blame_default_branch = {
+          ['^github%.com'] = 'https://github.com/'
+            .. '{_A.USER}/'
+            .. '{_A.REPO}/blame/'
+            .. '{_A.DEFAULT_BRANCH}/'
+            .. '{_A.FILE}?plain=1' -- '?plain=1'
+            .. '#L{_A.LSTART}'
+            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+        },
+      },
+    },
     keys = {
       {
         '<leader>gom',
