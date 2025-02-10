3348eecefae1b96836f6d0afc4a9b0c61a0101ee
razak17 <moabdulrazak17@gmail.com>
2025-02-10 18:32:44 +0000

    feat(treesitter): load treesitter early when opening a file from the cmdline

--- lua/ar/plugins/treesitter.lua
+++ lua/ar/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     event = 'BufReadPost',
     build = ':TSUpdate',
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     init = function(plugin)
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
