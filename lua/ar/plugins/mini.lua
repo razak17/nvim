adefe3812da501fa4f38b6054afb408e8b5c1799
razak17 <moabdulrazak17@gmail.com>
2025-02-11 19:42:15 +0000

    feat(mini.completion): disable for picker prompts

--- lua/ar/plugins/mini.lua
+++ lua/ar/plugins/mini.lua
@@ -325,8 +325,26 @@ return {
   {
     'echasnovski/mini.completion',
     cond = minimal,
-    event = { 'InsertEnter' },
+    event = { 'InsertEnter', 'BufEnter' },
     opts = {},
+    config = function(_, opts)
+      require('mini.completion').setup(opts)
+
+      ar.augroup('MiniCompletionDisable', {
+        event = { 'FileType' },
+        desc = 'Disable completion for certain files',
+        command = function()
+          local ignored_filetypes = {
+            'TelescopePrompt',
+            'minifiles',
+            'snacks_picker_input',
+          }
+          if vim.tbl_contains(ignored_filetypes, vim.bo.filetype) then
+            vim.b.minicompletion_disable = true
+          end
+        end,
+      })
+    end,
   },
   {
     'echasnovski/mini.pick',
