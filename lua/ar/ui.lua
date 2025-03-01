0d7fa00d56b36ba81ea04f2afeddb383ff36dd46
razak17 <moabdulrazak17@gmail.com>
2025-03-01 20:28:24 +0000

    feat(ui): show statusline in neogit popup

--- lua/ar/ui.lua
+++ lua/ar/ui.lua
@@ -278,7 +278,10 @@ local buftypes = {
 --- again but check if there is matching lua pattern. This is useful for filetypes for
 --- plugins like Neogit which have a filetype of Neogit<something>.
 local filetypes = ar.p_table({
-  ['^Neogit.*'] = presets.tool_panel:with({ cursorline = true, statusline = true }),
+  ['^Neogit.*'] = presets.tool_panel:with({
+    cursorline = true,
+    statusline = true,
+  }),
   ['^copilot.*'] = presets.tool_panel,
   ['aerial'] = presets.tool_panel,
   ['agitator'] = presets.tool_panel,
