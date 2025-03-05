ffb7ab52e167a642825e52bca95a2cf34a716586
razak17 <moabdulrazak17@gmail.com>
2025-03-05 00:53:02 +0000

    feat(mini.icons): overide some icons

--- lua/ar/plugins/mini.lua
+++ lua/ar/plugins/mini.lua
@@ -108,13 +108,45 @@ return {
   },
   {
     'echasnovski/mini.icons',
-    opts = {},
     init = function()
       package.preload['nvim-web-devicons'] = function()
         require('mini.icons').mock_nvim_web_devicons()
         return package.loaded['nvim-web-devicons']
       end
     end,
+    opts = {
+      file = {
+        ['init.lua'] = { glyph = '󰢱', hl = 'MiniIconsAzure' }, -- disable nvim glyph: https://github.com/echasnovski/mini.nvim/issues/1384
+        ['README.md'] = { glyph = '' },
+        ['.ignore'] = { glyph = '󰈉', hl = 'MiniIconsGrey' },
+        ['pre-commit'] = { glyph = '󰊢' },
+      },
+      extension = {
+        ['d.ts'] = { hl = 'MiniIconsRed' }, -- distinguish `.d.ts` from `.ts`
+        ['applescript'] = { glyph = '󰀵', hl = 'MiniIconsGrey' },
+        ['log'] = { glyph = '󱂅', hl = 'MiniIconsGrey' },
+        ['gitignore'] = { glyph = '' },
+      },
+      filetype = {
+        ['css'] = { glyph = '', hl = 'MiniIconsRed' },
+        ['typescript'] = { hl = 'MiniIconsCyan' },
+        ['vim'] = { glyph = '' }, -- used for `obsidian.vimrc`
+
+        -- plugin-filetypes
+        ['snacks_input'] = { glyph = '󰏫' },
+        ['snacks_notif'] = { glyph = '󰎟' },
+        ['noice'] = { glyph = '󰎟' },
+        ['mason'] = { glyph = '' },
+        ['ccc-ui'] = { glyph = '' },
+        ['scissors-snippet'] = { glyph = '󰩫' },
+        ['rip-substitute'] = { glyph = '' },
+      },
+      config = function(_, opts)
+        require('mini.icons').setup(opts)
+        -- plugin still needing the devicons mock: telescope
+        require('mini.icons').mock_nvim_web_devicons()
+      end,
+    },
   },
   {
     'echasnovski/mini.indentscope',
