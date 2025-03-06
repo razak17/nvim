35a86b63848f1052331221cdf80fbeb9b24ae5cf
razak17 <moabdulrazak17@gmail.com>
2025-03-06 23:08:27 +0000

    feat(treesj): update config

--- lua/ar/plugins/editing.lua
+++ lua/ar/plugins/editing.lua
@@ -280,15 +280,62 @@ return {
   {
     'Wansmer/treesj',
     cond = not minimal,
+    -- stylua: ignore
     keys = {
-      {
-        'gS',
-        '<cmd>TSJSplit<CR>',
-        desc = 'split to multiple lines',
-      },
+      { '<leader>oK', function() require('treesj').toggle() end, desc = 'split-join lines' },
+      { '<leader>oK', 'gw}', ft = 'markdown', desc = 'reflow rest of paragraph' },
+      { 'gS', '<cmd>TSJSplit<CR>', desc = 'split to multiple lines' },
       { 'gJ', '<cmd>TSJJoin<CR>', desc = 'join to single line' },
     },
-    opts = { use_default_keymaps = false, max_join_length = 150 },
+    opts = {
+      use_default_keymaps = false,
+      cursor_behavior = 'start',
+      max_join_length = math.huge,
+    },
+    config = function(_, opts)
+      local gww =
+        { both = { fallback = function() vim.cmd('normal! gww') end } }
+      local joinWithoutCurly = {
+        -- remove curly brackets in js when joining if statements https://github.com/Wansmer/treesj/issues/150
+        statement_block = {
+          join = {
+            format_tree = function(tsj)
+              if tsj:tsnode():parent():type() == 'if_statement' then
+                tsj:remove_child({ '{', '}' })
+                tsj:update_preset({ recursive = false }, 'join')
+              else
+                require('treesj.langs.javascript').statement_block.join.format_tree(
+                  tsj
+                )
+              end
+            end,
+          },
+        },
+        -- one-line-if-statement can be split into multi-line https://github.com/Wansmer/treesj/issues/150
+        expression_statement = {
+          join = { enable = false },
+          split = {
+            enable = function(tsn) return tsn:parent():type() == 'if_statement' end,
+            format_tree = function(tsj) tsj:wrap({ left = '{', right = '}' }) end,
+          },
+        },
+        return_statement = {
+          join = { enable = false },
+          split = {
+            enable = function(tsn) return tsn:parent():type() == 'if_statement' end,
+            format_tree = function(tsj) tsj:wrap({ left = '{', right = '}' }) end,
+          },
+        },
+      }
+      opts.langs = {
+        comment = { source = gww, element = gww }, -- comments in any language
+        lua = { comment = gww },
+        jsdoc = { source = gww, description = gww },
+        javascript = joinWithoutCurly,
+        typescript = joinWithoutCurly,
+      }
+      require('treesj').setup(opts)
+    end,
   },
   {
     'ThePrimeagen/refactoring.nvim',
