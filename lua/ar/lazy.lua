52902026ddb1a7572a81a7433398bceb6b7e34fb
razak17 <moabdulrazak17@gmail.com>
2025-03-07 20:31:58 +0000

    chore(lazy): colorscheme

--- lua/ar/lazy.lua
+++ lua/ar/lazy.lua
@@ -30,6 +30,7 @@ require('lazy').setup({
     patterns = { 'razak17' },
     fallback = true,
   },
+  install = { colorscheme = { 'onedark', 'habamax' } },
   -- ui = { border = ar.ui.current.border },
   checker = {
     enabled = true,
