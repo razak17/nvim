cd6d920af7ead664b6f7069aa2b1a629311ce244
razak17 <moabdulrazak17@gmail.com>
2025-03-16 15:18:04 +0000

    feat(session): use persisted again

--- lua/ar/plugins/session.lua
+++ lua/ar/plugins/session.lua
@@ -1,6 +1,7 @@
 return {
   {
     'folke/persistence.nvim',
+    cond = false,
     event = 'BufReadPre',
     opts = {},
     -- stylua: ignore
@@ -13,17 +14,17 @@ return {
   },
   {
     'olimorris/persisted.nvim',
-    cond = false,
-    init = function() ar.command('ListSessions', 'Telescope persisted') end,
-    cmd = { 'SessionLoad', 'SessionLoadLast', 'ListSessions', 'SessionStop' },
+    event = 'VeryLazy',
+    cmd = { 'SessionLoad', 'SessionLoadLast', 'SessionSelect', 'SessionStop' },
     -- stylua: ignore
     keys = {
       { '<leader>qs', ':SessionLoad<CR>', desc = 'restore session', silent = true },
       { '<leader>ql', ':SessionLoadLast<CR>', desc = 'restore last session', silent = true },
-      { '<leader>qL', ':ListSessions<CR>', desc = 'list session', silent = true },
+      { '<leader>qL', ':SessionSelect<CR>', desc = 'list session', silent = true },
       { '<leader>qd', ':SessionStop<CR>', desc = "don't save current session", silent = true },
     },
     opts = {
+      follow_cwd = true, -- Change the session file to match any change in the cwd?
       use_git_branch = true,
       save_dir = vim.fn.expand(vim.fn.stdpath('cache') .. '/sessions/'),
       on_autoload_no_session = function() vim.cmd.Alpha() end,
