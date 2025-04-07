66a5b315fd370e542e36e90bc5ec4e20b707399e
razak17 <moabdulrazak17@gmail.com>
2025-04-07 06:52:44 +0000

    refactor(devdocs): move disabled

--- lua/ar/plugins/docs.lua
+++ lua/ar/plugins/docs.lua
@@ -2,33 +2,9 @@ local minimal, niceties = ar.plugins.minimal, ar.plugins.niceties
 
 return {
   {
-    'luckasRanarison/nvim-devdocs',
-    cond = not minimal and false,
-    init = function()
-      vim.g.whichkey_add_spec({ '<localleader>v', group = 'Devdocs' })
-    end,
-    -- stylua: ignore
-    keys = {
-      { '<localleader>vf', '<cmd>DevdocsOpenFloat<CR>', desc = 'devdocs: open float', },
-      { '<localleader>vb', '<cmd>DevdocsOpen<CR>', desc = 'devdocs: open in buffer', },
-      { '<localleader>vo', '<cmd>DevdocsOpenFloat ', desc = 'devdocs: open documentation', },
-      { '<localleader>vi', '<cmd>DevdocsInstall ', desc = 'devdocs: install' },
-      { '<localleader>vu', '<cmd>DevdocsUninstall ', desc = 'devdocs: uninstall' },
-    },
-    opts = {
-      -- stylua: ignore
-      ensure_installed = {
-        'git', 'bash', 'lua-5.4', 'html', 'css', 'javascript', 'typescript',
-        'react', 'svelte', 'web_extensions', 'postgresql-15', 'python-3.11',
-        'go', 'docker', 'tailwindcss', 'astro',
-      },
-      wrap = true,
-    },
-  },
-  {
     'maskudo/devdocs.nvim',
     cond = not minimal,
-    lazy = false,
+    -- lazy = false,
     cmd = { 'DevDocs' },
     init = function()
       vim.g.whichkey_add_spec({ '<localleader>v', group = 'DevDocs' })
@@ -53,6 +29,33 @@ return {
         'typescript',
         -- 'rust',
       },
+    },
+  },
+  --------------------------------------------------------------------------------
+  -- Disabled
+  --------------------------------------------------------------------------------
+  {
+    'luckasRanarison/nvim-devdocs',
+    cond = not minimal and false,
+    init = function()
+      vim.g.whichkey_add_spec({ '<localleader>v', group = 'Devdocs' })
+    end,
+    -- stylua: ignore
+    keys = {
+      { '<localleader>vf', '<cmd>DevdocsOpenFloat<CR>', desc = 'devdocs: open float', },
+      { '<localleader>vb', '<cmd>DevdocsOpen<CR>', desc = 'devdocs: open in buffer', },
+      { '<localleader>vo', '<cmd>DevdocsOpenFloat ', desc = 'devdocs: open documentation', },
+      { '<localleader>vi', '<cmd>DevdocsInstall ', desc = 'devdocs: install' },
+      { '<localleader>vu', '<cmd>DevdocsUninstall ', desc = 'devdocs: uninstall' },
+    },
+    opts = {
+      -- stylua: ignore
+      ensure_installed = {
+        'git', 'bash', 'lua-5.4', 'html', 'css', 'javascript', 'typescript',
+        'react', 'svelte', 'web_extensions', 'postgresql-15', 'python-3.11',
+        'go', 'docker', 'tailwindcss', 'astro',
+      },
+      wrap = true,
     },
   },
 }
