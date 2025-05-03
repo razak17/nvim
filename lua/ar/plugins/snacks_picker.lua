460ebabcf439f27824da27d06f947ddf9e6a8f4c
razak17 <moabdulrazak17@gmail.com>
2025-05-03 10:41:31 +0000

    fix(snacks): fix git commit keymap

--- lua/ar/plugins/snacks_picker.lua
+++ lua/ar/plugins/snacks_picker.lua
@@ -190,9 +190,9 @@ return {
           -- stylua: ignore start
           { '<M-space>', buffers, desc = 'snacks: buffers' },
           { '<leader>fc', p('files', { cwd = fn.stdpath('config') }), desc = 'find config file' },
-          { '<leader>ff', p('files'), desc = 'Find Files' },
+          { '<leader>ff', p('files'), desc = 'find files' },
           { '<leader>fgb', p('git_branches'), desc = 'find git branches' },
-          { '<leader>fgc', p('git_commits'), desc = 'find git commits' },
+          { '<leader>fgc', p('git_log'), desc = 'find git commits' },
           { '<leader>fgd', p('git_diff'), desc = 'git diff (hunks)' },
           { '<leader>fgf', p('git_log_file'), desc = 'git log file' },
           { '<leader>fgF', p('git_grep'), desc = 'git grep' },
