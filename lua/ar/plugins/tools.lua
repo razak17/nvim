185c0291df33fbaa977f2690aecdc2f7b0a05706
razak17 <moabdulrazak17@gmail.com>
2025-05-21 19:23:40 +0000

    fix(conform): use command for getting formatter names

--- lua/ar/plugins/tools.lua
+++ lua/ar/plugins/tools.lua
@@ -150,7 +150,7 @@ return {
         if conform_ok then
           local formatters = vim
             .iter(pairs(conform.list_all_formatters()))
-            :map(function(_, f) return f.name end)
+            :map(function(_, f) return f.command end)
             :totable()
           vim.list_extend(packages, formatters)
         end
