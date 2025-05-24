9fbba9293bf27795feed6679c96572fd3e95debc
razak17 <moabdulrazak17@gmail.com>
2025-05-24 10:40:23 +0000

    feat(codecompanion): update action palette layout

--- lua/ar/plugins/ai.lua
+++ lua/ar/plugins/ai.lua
@@ -157,6 +157,16 @@ return {
           inline = {},
         },
         display = {
+          action_palette = {
+            width = 95,
+            height = 10,
+            prompt = 'Prompt ', -- Prompt used for interactive LLM calls
+            provider = 'default', -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+            opts = {
+              show_default_actions = true, -- Show the default actions in the action palette?
+              show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+            },
+          },
           chat = {
             window = {
               layout = 'vertical', -- float|vertical|horizontal|buffer
