7745f1b1f752d7f19d96f93a6ae85baaf583f437
razak17 <moabdulrazak17@gmail.com>
2025-06-06 22:11:15 +0000

    chore(grug-far): update config

--- lua/ar/plugins/replace.lua
+++ lua/ar/plugins/replace.lua
@@ -7,23 +7,6 @@ return {
     'MagicDuck/grug-far.nvim',
     cond = not minimal,
     cmd = { 'GrugFar' },
-    keys = {
-      {
-        mode = { 'n', 'v' },
-        '<leader>sr',
-        function()
-          local grug = require('grug-far')
-          local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
-          grug.grug_far({
-            transient = true,
-            prefills = {
-              filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
-            },
-          })
-        end,
-        desc = 'grug_far: search and replace',
-      },
-    },
     opts = {
       startInInsertMode = false,
       transient = false,
