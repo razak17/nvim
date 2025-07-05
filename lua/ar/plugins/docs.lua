9404412e39cef4618b32b246520f174d3d5529a2
razak17 <moabdulrazak17@gmail.com>
2025-07-05 18:18:32 +0000

    chore(docs): remove nvim-devdocs

--- lua/ar/plugins/docs.lua
+++ lua/ar/plugins/docs.lua
@@ -36,31 +36,4 @@ return {
       },
     },
   },
-  --------------------------------------------------------------------------------
-  -- Disabled
-  --------------------------------------------------------------------------------
-  {
-    'luckasRanarison/nvim-devdocs',
-    cond = not minimal and false,
-    init = function()
-      vim.g.whichkey_add_spec({ '<localleader>v', group = 'Devdocs' })
-    end,
-    -- stylua: ignore
-    keys = {
-      { '<localleader>vf', '<cmd>DevdocsOpenFloat<CR>', desc = 'devdocs: open float', },
-      { '<localleader>vb', '<cmd>DevdocsOpen<CR>', desc = 'devdocs: open in buffer', },
-      { '<localleader>vo', '<cmd>DevdocsOpenFloat ', desc = 'devdocs: open documentation', },
-      { '<localleader>vi', '<cmd>DevdocsInstall ', desc = 'devdocs: install' },
-      { '<localleader>vu', '<cmd>DevdocsUninstall ', desc = 'devdocs: uninstall' },
-    },
-    opts = {
-      -- stylua: ignore
-      ensure_installed = {
-        'git', 'bash', 'lua-5.4', 'html', 'css', 'javascript', 'typescript',
-        'react', 'svelte', 'web_extensions', 'postgresql-15', 'python-3.11',
-        'go', 'docker', 'tailwindcss', 'astro',
-      },
-      wrap = true,
-    },
-  },
 }
