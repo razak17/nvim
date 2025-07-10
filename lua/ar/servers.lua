407d069d7f6f0dd31c76e684a21402f58563d565
razak17 <moabdulrazak17@gmail.com>
2025-07-10 10:36:01 +0000

    feat(servers): extend existing json schemas

--- lua/ar/servers.lua
+++ lua/ar/servers.lua
@@ -173,9 +173,15 @@ local servers = {
   -- jdtls = {},
   jedi_language_server = {},
   jsonls = {
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(
+        new_config.settings.json.schemas,
+        require('schemastore').json.schemas()
+      )
+    end,
     settings = {
       json = {
-        schemas = require('schemastore').json.schemas(),
         validate = { enable = true },
       },
     },
