30fff36cb7cd14bc1ec0b3d0af69650619260450
razak17 <moabdulrazak17@gmail.com>
2025-08-03 21:22:13 +0000

    feat(plugins): add unnest.nvim
    replaces flatten.nvim

--- lua/ar/plugins/core.lua
+++ lua/ar/plugins/core.lua
@@ -275,37 +275,5 @@ return {
       },
     },
   },
-  {
-    'willothy/flatten.nvim',
-    lazy = false,
-    cond = function()
-      local condition = not minimal and ar_config.flatten.enable
-      return ar.get_plugin_cond('flatten.nvim', condition)
-    end,
-    priority = 1001,
-    opts = {
-      window = { open = 'tab' },
-      block_for = {
-        gitcommit = true,
-        gitrebase = true,
-      },
-      post_open = function(bufnr, winnr, _, is_blocking)
-        vim.w[winnr].is_remote = true
-        if is_blocking then
-          vim.bo.bufhidden = 'wipe'
-          vim.api.nvim_create_autocmd('BufHidden', {
-            desc = 'Close window when buffer is hidden',
-            callback = function()
-              if vim.api.nvim_win_is_valid(winnr) then
-                vim.api.nvim_win_close(winnr, true)
-              end
-            end,
-            buffer = bufnr,
-            once = true,
-          })
-        end
-      end,
-    },
-  },
   -- }}}
 }
---
+++ lua/ar/plugins/pipe.lua
@@ -0,0 +1,42 @@
+local minimal = ar.plugins.minimal
+
+return {
+  {
+    'brianhuster/unnest.nvim',
+    lazy = false,
+    cond = function() return ar.get_plugin_cond('unnest.nvim', not minimal) end,
+  },
+  {
+    'willothy/flatten.nvim',
+    lazy = false,
+    enabled = false,
+    cond = function()
+      local condition = not minimal and ar_config.flatten.enable
+      return ar.get_plugin_cond('flatten.nvim', condition)
+    end,
+    priority = 1001,
+    opts = {
+      window = { open = 'tab' },
+      block_for = {
+        gitcommit = true,
+        gitrebase = true,
+      },
+      post_open = function(bufnr, winnr, _, is_blocking)
+        vim.w[winnr].is_remote = true
+        if is_blocking then
+          vim.bo.bufhidden = 'wipe'
+          vim.api.nvim_create_autocmd('BufHidden', {
+            desc = 'Close window when buffer is hidden',
+            callback = function()
+              if vim.api.nvim_win_is_valid(winnr) then
+                vim.api.nvim_win_close(winnr, true)
+              end
+            end,
+            buffer = bufnr,
+            once = true,
+          })
+        end
+      end,
+    },
+  },
+}
