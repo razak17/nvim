960624b9a7cbe1283fa7d211ab0d79abcda72c3a
razak17 <moabdulrazak17@gmail.com>
2025-08-19 22:47:07 +0000

    refactor(ai): mcphub module

--- lua/ar/plugins/ai.lua
+++ lua/ar/plugins/ai.lua
@@ -32,28 +32,6 @@ return {
     end,
   },
   {
-    'ravitemer/mcphub.nvim',
-    cond = function() return ar.get_plugin_cond('mcphub.nvim', ar.ai.enable) end,
-    cmd = { 'MCPHub' },
-    keys = { { '<leader>am', '<Cmd>MCPHub<CR>', desc = 'mcphub: toggle' } },
-    build = 'bundled_build.lua',
-    opts = {
-      port = 37373,
-      config = vim.fn.expand('~/.config/mcphub/servers.json'),
-      use_bundled_binary = true, -- Use local `mcp-hub` binary (set this to true when using build = "bundled_build.lua"
-      extensions = {
-        avante = { make_slash_commands = true },
-      },
-      ui = { window = { border = 'single' } },
-      log = {
-        level = vim.log.levels.INFO,
-        to_file = true,
-        file_path = vim.fn.stdpath('state') .. '/mcphub.log',
-        prefix = 'MCPHub',
-      },
-    },
-  },
-  {
     'milanglacier/minuet-ai.nvim',
     cond = function()
       local is_minuet = models.gemini and ai_cmp == 'minuet'
