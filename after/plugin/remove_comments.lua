c3dd2c9a8a62872f577e3e24ffc0163d5861df90
razak17 <moabdulrazak17@gmail.com>
2025-09-12 14:49:15 +0000

    feat(remove_comments): enhance comment removal for JSX/TSX files

--- after/plugin/remove_comments.lua
+++ after/plugin/remove_comments.lua
@@ -9,6 +9,8 @@ local queries = {
   config = {
     javascript = [[ (comment) @comment ]],
     typescript = [[ (comment) @comment ]],
+    javascriptreact = [[ (comment) @comment ]],
+    typescriptreact = [[ (comment) @comment ]],
     java = [[
       (line_comment) @comment
       (block_comment) @comment
@@ -48,12 +50,39 @@ local remove_comments = function()
 
   local tree = parser:parse()[1]
   local root = tree:root()
-  local query_str = queries[ft]
+  local query_str = queries.config[ft]
   local query = ts.query.parse(lang, query_str or [[ (comment) @comment ]])
 
   local ranges = {}
   for _, node in query:iter_captures(root, bufnr, 0, -1) do
-    table.insert(ranges, { node:range() })
+    local start_row, start_col, end_row, end_col = node:range()
+    local range = { start_row, start_col, end_row, end_col }
+
+    -- For JSX/TSX files, check if comment is wrapped in curly brackets
+    if ft == 'javascriptreact' or ft == 'typescriptreact' then
+      local lines = api.nvim_buf_get_lines(bufnr, start_row, end_row + 1, false)
+      if #lines > 0 then
+        local first_line = lines[1]
+        local last_line = lines[#lines]
+
+        -- Check if comment starts with {/* and ends with */}
+        local before_comment = first_line:sub(1, start_col)
+        local after_comment = last_line:sub(end_col + 1)
+
+        -- Extend range to include surrounding { and } if they exist
+        if before_comment:match('%{%s*$') and after_comment:match('^%s*%}') then
+          -- Find the opening brace
+          local brace_start = before_comment:reverse():find('%{')
+          if brace_start then range[2] = start_col - brace_start end
+
+          -- Find the closing brace
+          local brace_end = after_comment:find('%}')
+          if brace_end then range[4] = end_col + brace_end end
+        end
+      end
+    end
+
+    table.insert(ranges, range)
   end
 
   table.sort(ranges, function(a, b)
