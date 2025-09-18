e8d1188de860efc5a4630fa9ebc0ebf0a43678e5
razak17 <moabdulrazak17@gmail.com>
2025-09-18 14:21:49 +0000

    feat(lsp): add ts_ls code actions and commands

--- plugin/filetypes.lua
+++ plugin/filetypes.lua
@@ -101,10 +101,100 @@ settings({
       })
     end,
   },
-  [{ 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' }] = {
+  [{
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  }] = {
     bo = { textwidth = 100 },
     opt = { spell = true },
-    function() map('i', 't', require('ar.async_func').add, { buffer = true }) end,
+    function()
+      map('i', 't', require('ar.async_func').add, { buffer = true })
+
+      if not ar.lsp.enable then return end
+
+      local override = ar_config.lsp.override
+      if not ar.falsy(override) and vim.tbl_contains(override, 'ts_ls') then
+        goto continue
+      end
+      if not ar_config.lsp.lang.typescript['ts_ls'] then return end
+
+      ::continue::
+      local sources = {
+        add_missing_imports = 'source.addMissingImports.ts',
+        organize_imports = 'source.organizeImports',
+        remove_unused = 'source.removeUnused',
+        fix_all = 'source.fixAll.ts',
+      }
+
+      local function action(source)
+        return function()
+          vim.lsp.buf.code_action({
+            apply = true,
+            context = {
+              only = { source },
+              diagnostics = {},
+            },
+          })
+        end
+      end
+
+      local function execute(opts)
+        local params = {
+          command = opts.command,
+          arguments = opts.arguments,
+        }
+        return vim.lsp.buf_request(
+          0,
+          'workspace/executeCommand',
+          params,
+          opts.handler
+        )
+      end
+
+      ---@param name string
+      ---@param cb function
+      ---@param desc string
+      local function ts_cmd(name, cb, desc)
+        api.nvim_buf_create_user_command(0, name, cb, { desc = desc })
+      end
+
+      ts_cmd(
+        'AddMissingImports',
+        action(sources.add_missing_imports),
+        'Add missing imports'
+      )
+      ts_cmd(
+        'OrganizeImports',
+        action(sources.organize_imports),
+        'Organize imports'
+      )
+      ts_cmd('RemoveUnused', action(sources.remove_unused), 'Remove unused')
+      ts_cmd('FixAll', action(sources.fix_all), 'Fix All')
+      ts_cmd('GotoSourceDefinition', function()
+        local params = vim.lsp.util.make_position_params()
+        execute({
+          command = 'typescript.goToSourceDefinition',
+          arguments = { params.textDocument.uri, params.position },
+        })
+      end, 'Goto Source Definition')
+      ts_cmd(
+        'FileReferences',
+        execute({
+          command = 'typescript.findAllFileReferences',
+          arguments = { vim.uri_from_bufnr(0) },
+        }),
+        'File References'
+      )
+      ts_cmd(
+        'SelectWorkspaceVersion',
+        execute({ command = 'typescript.selectTypeScriptVersion' }),
+        'Select TS workspace version'
+      )
+    end,
   },
   [{ 'javascriptreact', 'typescriptreact' }] = {
     bo = { textwidth = 100 },
