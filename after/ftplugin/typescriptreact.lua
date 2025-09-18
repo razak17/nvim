0098344244b3166e9255496adf7d8d4e3bf8097a
razak17 <moabdulrazak17@gmail.com>
2025-09-18 20:06:09 +0000

    feat(typescript): improve 'use client' toggle function

--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -4,10 +4,15 @@ local api = vim.api
 
 local function toggle_use_client()
   local first_line = api.nvim_buf_get_lines(0, 0, 1, false)[1]
-  if string.match(first_line, 'use client') then
+  if first_line and string.match(first_line, 'use client') then
+    -- Remove the 'use client' line
     api.nvim_buf_set_lines(0, 0, 1, false, {})
+  elseif first_line and first_line ~= '' then
+    -- Insert 'use client' at the top, keep the existing first line
+    api.nvim_buf_set_lines(0, 0, 0, false, { '"use client"' })
   else
-    api.nvim_buf_set_lines(0, 0, 1, false, { '"use client";' })
+    -- Buffer is empty, just add 'use client'
+    api.nvim_buf_set_lines(0, 0, 1, false, { '"use client"' })
   end
 end
 
