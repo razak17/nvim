f96a169d431e9a33f12c65deaf9fbc07c3fb7814
razak17 <moabdulrazak17@gmail.com>
2025-10-20 15:03:58 +0000

    feat(sidekick): to send the (selected) results to an AI tool
    move to own module

---
+++ lua/ar/plugins/sidekick.lua
@@ -0,0 +1,124 @@
+local minimal = ar.plugins.minimal
+local tiny_diags_disabled_by_nes = false
+
+return {
+  {
+    'folke/sidekick.nvim',
+    cond = function()
+      local condition = not minimal and ar.ai.enable and false
+      return ar.get_plugin_cond('sidekick.nvim', condition)
+    end,
+    lazy = true,
+    opts = {
+      nes = {
+        enabled = true,
+      },
+      mux = {
+        backend = 'tmux',
+        enabled = true,
+      },
+      cli = {
+        win = {
+          keys = {
+            stopinsert = { '<esc>', 'stopinsert', mode = 't' }, -- enter normal mode
+            win_p = { '<M-Left>', 'blur' },
+          },
+        },
+      },
+    },
+    init = function()
+      vim.g.whichkey_add_spec({
+        '<leader>as',
+        group = 'Sidekick',
+        mode = { 'n', 'x' },
+      })
+    end,
+    keys = {
+      {
+        '<leader>aso',
+        function() require('sidekick.cli').toggle() end,
+        desc = 'sidekick: toggle cli',
+      },
+      {
+        '<leader>ass',
+        function() require('sidekick.cli').select() end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = 'sidekick: select cli',
+      },
+      {
+        '<leader>ast',
+        function() require('sidekick.cli').send({ msg = '{this}' }) end,
+        mode = { 'x', 'n' },
+        desc = 'sidekick: send this',
+      },
+      {
+        '<leader>asv',
+        function() require('sidekick.cli').send({ msg = '{selection}' }) end,
+        mode = { 'x' },
+        desc = 'sidekick: send visual selection',
+      },
+      {
+        '<leader>asp',
+        function() require('sidekick.cli').prompt() end,
+        mode = { 'n', 'x' },
+        desc = 'sidekick: select prompt',
+      },
+      {
+        '<leader>af',
+        function() require('sidekick.cli').send({ msg = '{file}' }) end,
+        desc = 'sidekick: send file',
+      },
+      {
+        '<c-.>',
+        function() require('sidekick.cli').toggle() end,
+        mode = { 'n', 't', 'i', 'x' },
+        desc = 'sidekick: toggle',
+      },
+    },
+    config = function(_, opts)
+      require('sidekick').setup(opts)
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SidekickNesHide',
+        callback = function()
+          if tiny_diags_disabled_by_nes then
+            tiny_diags_disabled_by_nes = false
+            require('tiny-inline-diagnostic').enable()
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'SidekickNesShow',
+        callback = function()
+          tiny_diags_disabled_by_nes = true
+          require('tiny-inline-diagnostic').disable()
+        end,
+      })
+    end,
+  },
+  {
+    'folke/snacks.nvim',
+    optional = true,
+    opts = {
+      picker = {
+        actions = {
+          sidekick_send = function(...)
+            return require('sidekick.cli.picker.snacks').send(...)
+          end,
+        },
+        win = {
+          input = {
+            keys = {
+              ['<a-a>'] = {
+                'sidekick_send',
+                mode = { 'n', 'i' },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
