134dd432a506620087a9456dc2e1b03fcee7e76f
razak17 <moabdulrazak17@gmail.com>
2025-11-02 13:29:48 +0000

    refactor(smart_splits): simplify again
    test

--- after/plugin/smart_splits.lua
+++ after/plugin/smart_splits.lua
@@ -2,7 +2,7 @@ local enabled = ar_config.plugin.custom.smart_splits.enable
 
 if not ar or ar.none or not enabled then return end
 
-local api, fn = vim.api, vim.fn
+local fn = vim.fn
 
 local config = {
   max_columns = 3,
@@ -49,28 +49,7 @@ local config = {
 -- Smart Splits
 --------------------------------------------------------------------------------
 -- Move to a window (one of hjkl) or create a split if a window does not exist in the direction
--- @see: https://github.com/theopn/theovim/blob/main/lua/config/keymap.lua#L100
-local function count_windows_in_direction(direction)
-  local current_tabpage = api.nvim_get_current_tabpage()
-  local windows = api.nvim_tabpage_list_wins(current_tabpage)
-  local current_win = api.nvim_get_current_win()
-  local current_pos = api.nvim_win_get_position(current_win)
-
-  local count = 0
-  for _, win in ipairs(windows) do
-    if win ~= current_win then
-      local pos = api.nvim_win_get_position(win)
-      if direction == 'horizontal' then
-        if pos[1] == current_pos[1] then count = count + 1 end
-      elseif direction == 'vertical' then
-        if pos[2] == current_pos[2] then count = count + 1 end
-      end
-    end
-  end
-
-  return count + 1
-end
-
+-- @see: https://github.com/theopn/dotfiles/blob/0901eb3ac67700225b44d1025513e843621a991f/nvim/.config/nvim/lua/theovim/keymaps.lua#L123
 local function move_or_create_win(key)
   local is_excluded_bt = vim.tbl_contains(config.excluded.buftypes, vim.bo.bt)
   local is_excluded_ft = vim.tbl_contains(config.excluded.filetypes, vim.bo.ft)
@@ -81,27 +60,14 @@ local function move_or_create_win(key)
   local curr_win = fn.winnr()
   vim.cmd('wincmd ' .. key)
 
-  if curr_win == fn.winnr() then
-    local should_create_split = true
-
+  if curr_win == fn.winnr() then --> didn't move, so create a split
     if key == 'h' or key == 'l' then
-      local current_columns = count_windows_in_direction('horizontal')
-      if current_columns >= config.max_columns then
-        should_create_split = false
-      end
+      vim.cmd('wincmd v')
     else
-      local current_rows = count_windows_in_direction('vertical')
-      if current_rows >= config.max_rows then should_create_split = false end
+      vim.cmd('wincmd s')
     end
 
-    if should_create_split then
-      if key == 'h' or key == 'l' then
-        vim.cmd('wincmd v')
-      else
-        vim.cmd('wincmd s')
-      end
-      vim.cmd('wincmd ' .. key)
-    end
+    vim.cmd('wincmd ' .. key) --> move again
   end
 end
 
