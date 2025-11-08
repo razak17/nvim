fe6924f560eef64a808faecbff7dce3a4b77151f
razak17 <moabdulrazak17@gmail.com>
2025-11-08 14:36:44 +0000

    feat(theme): remove MsgSeparator override for onedark

--- lua/ar/plugins/theme.lua
+++ lua/ar/plugins/theme.lua
@@ -47,7 +47,6 @@ return {
     opts = { variant = variant },
     init = function()
       local overrides = {
-        { MsgSeparator = { link = 'VertSplit' } },
         { Dim = { inherit = 'VertSplit' } },
         { NeorgContext = { inherit = 'Normal' } },
         -- { ['@variable'] = { fg = { from = '@none' } } },
