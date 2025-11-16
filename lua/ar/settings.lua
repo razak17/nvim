365771e56e07748f725ea55e534bc22032b884ff
razak17 <moabdulrazak17@gmail.com>
2025-11-16 13:09:11 +0000

    feat(settings): vim.o.winborder

--- lua/ar/settings.lua
+++ lua/ar/settings.lua
@@ -165,7 +165,7 @@ o.showbreak = [[↪ ]] -- Options include -> '…', '↳ ', '→', '↴'
 if not ar_config.plugin.custom.auto_cursorline.enable then
   o.cursorline = true
 end
--- o.winborder = 'single' -- Single border for windows
+o.winborder = 'single' -- Single border for windows
 --------------------------------------------------------------------------------
 -- List chars {{{1
 --------------------------------------------------------------------------------
