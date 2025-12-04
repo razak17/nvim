7dcf0076c614e10f7659ca5e82988c4dcbabdbcf
razak17 <moabdulrazak17@gmail.com>
2025-12-04 15:07:38 +0000

    feat(mappings): improve search navigation in command mode

--- plugin/mappings.lua
+++ plugin/mappings.lua
@@ -309,17 +309,16 @@ cnoremap('<C-d>', '<Del>')
 -- smooth searching, allow tabbing between search results similar to using <c-g>
 -- or <c-t> the main difference being tab is easier to hit and remapping those keys
 -- to these would swallow up a tab mapping
+-- UPDATE: Pick a key you don’t type normally (commonly Ctrl-Z) as wildcharm.
+-- Return that key from your expr mapping when cmdtype is ':'.
+vim.opt.wildcharm = 26 -- Ctrl-Z
 local function search(direction_key, default)
   local c_type = fn.getcmdtype()
   return (c_type == '/' or c_type == '?') and fmt('<CR>%s<C-r>/', direction_key)
     or default
 end
-cnoremap('<Tab>', function() return search('/', '<Tab>') end, { expr = true })
-cnoremap(
-  '<S-Tab>',
-  function() return search('?', '<S-Tab>') end,
-  { expr = true }
-)
+cnoremap('<Tab>', function() return search('/', '<C-z>') end, { expr = true })
+cnoremap('<S-Tab>', function() return search('?', '<C-z>') end, { expr = true })
 -- insert path of current file into a command
 cnoremap(';p', "<C-r>=fnameescape(expand('%:p:h'))<cr>/")
 cnoremap(';;', "<C-r>=fnameescape(expand('%:h'))<cr>/")
