4e546cb9d5151f1a4dddb2130992d2902b077a7d
razak17 <moabdulrazak17@gmail.com>
2025-12-08 20:19:07 +0000

    feat(snacks-picker): don't use preselect for my_select layout

--- lua/ar/plugins/snacks-picker.lua
+++ lua/ar/plugins/snacks-picker.lua
@@ -381,8 +381,25 @@ return {
               },
             },
             my_select = {
-              preset = 'select',
-              layout = { border = border_style },
+              layout = {
+                backdrop = false,
+                width = 0.5,
+                min_width = 80,
+                height = 0.4,
+                min_height = 3,
+                box = 'vertical',
+                border = border_style,
+                title = '{title}',
+                title_pos = 'center',
+                { win = 'input', height = 1, border = 'bottom' },
+                { win = 'list', border = 'none' },
+                {
+                  win = 'preview',
+                  title = '{preview}',
+                  height = 0.4,
+                  border = 'top',
+                },
+              },
             },
             right_sidebar = {
               preset = 'sidebar',
