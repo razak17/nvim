cece1245a6c4f37e5dac377cc80cef94640f80d9
razak17 <moabdulrazak17@gmail.com>
2026-01-22 13:41:46 +0000

    refactor(word-web-search): simplify mappings

--- after/plugin/word-web-search.lua
+++ after/plugin/word-web-search.lua
@@ -2,36 +2,35 @@ local enabled = ar.config.plugin.custom.word_web_search.enable
 
 if not ar or ar.none or not enabled then return end
 
-function ar.mappings.ddg(path)
+function ar.ddg_search(path)
   ar.web_search(path, 'https://html.duckduckgo.com/html?q=')
 end
-function ar.mappings.gh(path)
-  ar.web_search(path, 'https://github.com/search?q=')
-end
+
+function ar.gh(path) ar.web_search(path, 'https://github.com/search?q=') end
 
 -- Search DuckDuckGo
 map(
   'n',
   '<localleader>?',
-  [[:lua ar.mappings.ddg(vim.fn.expand("<cword>"))<CR>]],
+  [[:lua ar.ddg_search(vim.fn.expand("<cword>"))<CR>]],
   { desc = 'search word' }
 )
 map(
   'x',
   '<localleader>?',
-  [["gy:lua ar.mappings.ddg(vim.api.nvim_eval("@g"))<CR>gv]],
+  [["gy:lua ar.ddg_search(vim.api.nvim_eval("@g"))<CR>gv]],
   { desc = 'search word' }
 )
 -- Search Github
 map(
   'n',
   '<localleader>!',
-  [[:lua ar.mappings.gh(vim.fn.expand("<cword>"))<CR>]],
+  [[:lua ar.gh_search(vim.fn.expand("<cword>"))<CR>]],
   { desc = 'gh search word' }
 )
 map(
   'x',
   '<localleader>!',
-  [["gy:lua ar.mappings.gh(vim.api.nvim_eval("@g"))<CR>gv]],
+  [["gy:lua ar.gh_search(vim.api.nvim_eval("@g"))<CR>gv]],
   { desc = 'gh search word' }
 )
