6d050e8463e97ea9ddfc7245da4b7357c6705bb4
razak17 <moabdulrazak17@gmail.com>
2026-01-24 18:22:34 +0000

    feat(plugins): add repeatable-move.nvim

--- lua/ar/plugins/motion.lua
+++ lua/ar/plugins/motion.lua
@@ -18,6 +18,22 @@ return {
     },
   },
   {
+    'kiyoon/repeatable-move.nvim',
+    cond = function()
+      return ar.get_plugin_cond('repeatable-move.nvim', ar.treesitter.enable)
+    end,
+    -- stylua: ignore
+    init = function()
+      local ok, ts_repeat_move = pcall(require, 'nvim-treesitter-textobjects.repeatable_move')
+      if ok then
+        map({ 'n', 'x', 'o' }, ';n', ts_repeat_move.repeat_last_move_next, { desc = 'repeatable: forward' })
+        map({ 'n', 'x', 'o' }, ';p', ts_repeat_move.repeat_last_move_previous, { desc = 'repeatable: backward' })
+      end
+    end,
+    keys = { ';', ']', '[' },
+    dependencies = 'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  {
     'aaronik/treewalker.nvim',
     event = 'VeryLazy',
     cmd = { 'Treewalker' },
