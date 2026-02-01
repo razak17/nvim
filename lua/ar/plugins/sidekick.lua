a9651ed00029fc3b084972935b4e46ba8b7000f8
razak17 <moabdulrazak17@gmail.com>
2026-02-01 19:58:23 +0000

    feat(sidekick): jump to nes keymap

--- lua/ar/plugins/sidekick.lua
+++ lua/ar/plugins/sidekick.lua
@@ -35,6 +35,17 @@ return {
     end,
     keys = {
       {
+        '<tab>',
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require('sidekick').nes_jump_or_apply() then
+            return '<Tab>' -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = 'sidekick: goto/apply next edit suggestion',
+      },
+      {
         '<leader>aso',
         function() require('sidekick.cli').toggle() end,
         desc = 'sidekick: toggle cli',
