dbcb1cdbd8b8c87ad9607cdbb8ff1514c66200eb
razak17 <moabdulrazak17@gmail.com>
2026-04-11 00:06:37 +0000

    feat(undo): builtin undotree keymap

--- plugin/pack.lua
+++ plugin/pack.lua
@@ -6,3 +6,11 @@ if not ar or ar.none or not enabled then return end
 
 vim.cmd('packadd nvim.undotree')
 vim.cmd('packadd nvim.difftool')
+
+local function undo()
+  require('undotree').open({
+    command = math.floor(vim.api.nvim_win_get_width(0) / 3) .. 'vnew',
+  })
+end
+
+map('n', '<leader><leader>U', undo, { desc = 'undotree toggle' })
