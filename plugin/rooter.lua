8558c2142b908c40e3a4614786e771b6678c8ba5
razak17 <moabdulrazak17@gmail.com>
2026-04-17 19:22:29 +0000

    feat(plugin): simplified rooter

--- plugin/rooter.lua
+++ plugin/rooter.lua
@@ -4,58 +4,21 @@ local enabled = ar.config.plugin.core.rooter.enable
 
 if ar.none or not enabled then return end
 
-local fn, fs, api = vim.fn, vim.fs, vim.api
+-- https://github.com/swaits/tiny-rooter.nvim/blob/main/lua/tiny-rooter/init.lua
 
-----------------------------------------------------------------------------------------------------
---  Project root finder
-----------------------------------------------------------------------------------------------------
--- https://github.com/akinsho/dotfiles/blob/main/.config/nvim/plugin/rooter.lua
-
-local root_names = { '.git', 'Makefile', 'go.mod', 'go.sum' }
-
--- Cache to use for speed up (at cost of possibly outdated results)
-local root_cache = {}
-
----@param buf number
----@return string?
----@return string?
-local function get_lsp_root(buf)
-  local clients = vim.lsp.get_clients({ bufnr = buf })
-  if not next(clients) then return end
-
-  for _, client in pairs(clients) do
-    local filetypes = client.config.filetypes
-    if filetypes and vim.tbl_contains(filetypes, vim.bo[buf].ft) then
-      return client.config.root_dir, client.name
-    end
-  end
-end
+-- stylua: ignore
+local markers = { '.git', 'Makefile', 'go.mod', 'go.sum', '.jj', 'Cargo.toml', 'pyproject.toml', 'package.json' }
 
 ---@param args AutocmdArgs
 local function set_root(args)
-  local path = api.nvim_buf_get_name(args.buf)
-  if path == '' then return end
-  path = fs.dirname(path)
+  local root = vim.fs.root(args.buf, markers)
+  if not root then return end
+  if root == vim.fn.getcwd() then return end
+  vim.cmd.lcd(root)
 
-  -- Try cache and resort to searching upward for root directory
-  local root = root_cache[path]
-  if not root then
-    -- Currently this prefers marker files over the lsp root but swapping the order will change that
-    local root_file = fs.find(root_names, {
-      path = path,
-      upward = true,
-    })[1]
-
-    root = fs.dirname(root_file) or get_lsp_root(args.buf)
-  end
-  if not root or not path then return end
-  root_cache[path] = root
-  if root == fn.getcwd() then return end
-
-  fn.chdir(root)
-  -- reset worktree and gitdir (if set, re: baredot.nvim)
+  -- reset worktree and gitdir (if set, re: baredot)
   vim.env.GIT_WORK_TREE = nil
   vim.env.GIT_DIR = nil
 end
 
-ar.augroup('FindProjectRoot', { event = 'BufEnter', command = set_root })
+ar.augroup('FindProjectRoot', { event = { 'BufEnter' }, command = set_root })
