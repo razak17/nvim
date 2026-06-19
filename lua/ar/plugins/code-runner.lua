26c454e421dcfca1a9d6166786ba376170680fbb
razak17 <moabdulrazak17@gmail.com>
2026-06-19 16:58:06 +0000

    refactor(code-runner): snip runner section

--- lua/ar/plugins/code-runner.lua
+++ lua/ar/plugins/code-runner.lua
@@ -4,6 +4,7 @@ local preview_cmd = '/usr/bin/zathura'
 local coding = ar.plugins.coding
 
 return {
+  --------------------------------------------------------------------------------
   -- Code Runner
   --------------------------------------------------------------------------------
   {
@@ -36,155 +37,6 @@ return {
     },
   },
   {
-    'jellydn/quick-code-runner.nvim',
-    cond = function()
-      return ar.get_plugin_cond('quick-code-runner.nvim', coding)
-    end,
-    opts = { debug = true },
-    init = function()
-      ar.add_to_select('command_palette', { ['Code Pad'] = 'QuickCodePad' })
-    end,
-    cmd = { 'QuickCodeRunner', 'QuickCodePad' },
-    keys = {
-      {
-        mode = 'v',
-        '<leader>rs',
-        ':QuickCodeRunner<CR>',
-        desc = 'quick-runner: run selection',
-      },
-      -- { '<leader>rP', ':QuickCodePad<CR>', desc = 'quick-runner: open pad' },
-    },
-  },
-  {
-    {
-      'razak17/lab.nvim',
-      event = { 'InsertEnter' },
-      cond = function()
-        local condition = coding
-          and ar.completion.enable
-          and ar.config.completion.variant == 'cmp'
-        return ar.get_plugin_cond('lab.nvim', condition)
-      end,
-      keys = {
-        { '<leader>rl', ':Lab code run<CR>', desc = 'lab: run' },
-        { '<leader>rq', ':Lab code stop<CR>', desc = 'lab: stop' },
-        -- { '<leader>rp', ':Lab code panel<CR>', desc = 'lab: panel' },
-      },
-      build = 'cd js && npm ci',
-      config = function()
-        highlight.plugin('lab', {
-          theme = {
-            ['onedark'] = {
-              { LabCodeRun = { link = 'DiagnosticVirtualTextInfo' } },
-            },
-          },
-        })
-        require('lab').setup({
-          quick_data = {
-            enabled = ar.completion.enable,
-          },
-        })
-      end,
-    },
-    {
-      'hrsh7th/nvim-cmp',
-      optional = true,
-      opts = function(_, opts)
-        opts = vim.g.cmp_add_source(opts, {
-          source = {
-            name = 'lab.quick_data',
-            priority = 6,
-            max_item_count = 10,
-            group_index = 1,
-          },
-          menu = { ['lab.quick_data'] = '[LAB]' },
-          format = {
-            ['lab.quick_data'] = {
-              icon = ui.icons.misc.beaker,
-              hl = 'CmpItemKindLab',
-            },
-          },
-        })
-      end,
-    },
-  },
-  {
-    -- 'google/executor.nvim',
-    'razak17/executor.nvim',
-    cond = function() return ar.get_plugin_cond('executor.nvim', coding) end,
-    init = function()
-      vim.g.whichkey_add_spec({ '<leader><leader>x', group = 'Executor' })
-    end,
-    -- stylua: ignore
-    keys = {
-      { '<leader><leader>xc', '<cmd>ExecutorRun<CR>', desc = 'executor: start' },
-      { '<leader><leader>xs', '<cmd>ExecutorSetCommand<CR>', desc = 'executor: set command', },
-      { '<leader><leader>xd', '<cmd>ExecutorToggleDetail<CR>', desc = 'executor: toggle detail', },
-      { '<leader><leader>xr', '<cmd>ExecutorReset<CR>', desc = 'executor: reset status', },
-      { '<leader><leader>xp', '<cmd>ExecutorShowPresets<CR>', desc = 'executor: show presets', },
-    },
-    opts = {
-      input = {
-        border = {
-          style = vim.o.winborder,
-          padding = {
-            top = 0,
-            bottom = 0,
-            left = 1,
-            right = 1,
-          },
-        },
-      },
-      notifications = {
-        task_started = true,
-        task_completed = true,
-      },
-      preset_commands = {
-        ['custom-website'] = { 'npm run dev' },
-      },
-    },
-  },
-  {
-    'michaelb/sniprun',
-    build = 'sh install.sh',
-    cmd = { 'SnipRun', 'SnipInfo' },
-    cond = function() return ar.get_plugin_cond('sniprun', coding) end,
-    init = function()
-      vim.g.whichkey_add_spec({ '<leader>rs', group = 'SnipRun' })
-    end,
-    keys = {
-      { mode = 'v', '<leader>rr', ':SnipRun<CR>', desc = 'sniprun: run code' },
-      { '<leader>rsr', ':SnipRun<CR>', desc = 'sniprun: run code' },
-      { '<leader>rsi', ':SnipInfo<CR>', desc = 'sniprun: info' },
-      { '<leader>rsc', ':SnipReset<CR>', desc = 'sniprun: reset' },
-      { '<leader>rsq', ':SnipClose<CR>', desc = 'sniprun: close' },
-    },
-    opts = {},
-    config = function(_, opts)
-      highlight.plugin('sniprun', {
-        theme = {
-          ['onedark'] = {
-            { SniprunVirtualTextOk = { link = 'DiagnosticVirtualTextInfo' } },
-            { SniprunFloatingWinOk = { link = 'DiagnosticVirtualTextInfo' } },
-            { SniprunVirtualTextErr = { link = 'DiffDelete' } },
-            { SniprunFloatingWinErr = { link = 'DiffDelete' } },
-          },
-        },
-      })
-      require('sniprun').setup(opts)
-    end,
-  },
-  {
-    'ej-shafran/compile-mode.nvim',
-    cond = function() return ar.get_plugin_cond('compile-mode.nvim', coding) end,
-    cmd = { 'Compile', 'Recompile' },
-    opts = { default_command = '' },
-    dependencies = {
-      { 'm00qek/baleia.nvim', tag = 'v1.3.0' },
-    },
-  },
-  -- https://github.com/CRAG666/dotfiles/blob/main/config/nvim/lua/plugins/dev/code_runner.lua
-  {
     'CRAG666/code_runner.nvim',
     cond = function() return ar.get_plugin_cond('code_runner.nvim', coding) end,
     -- stylua: ignore
@@ -192,6 +44,7 @@ return {
       { '<leader>rc', function() require('code_runner').run_code() end, desc = 'code runner: run code' },
     },
     opts = {
+      -- https://github.com/CRAG666/dotfiles/blob/main/config/nvim/lua/plugins/dev/code_runner.lua
       mode = 'float',
       filetype = {
         v = 'v run',
@@ -277,6 +130,7 @@ return {
   },
   {
     'valonmulolli/zignite.nvim',
+    cond = function() return ar.get_plugin_cond('zignite.nvim', coding) end,
     build = 'cd zig && zig build',
     -- stylua: ignore
     keys = {
@@ -305,5 +159,107 @@ return {
       },
     },
     config = function(_, opts) require('zignite.config').setup(opts) end,
+  },
+  --------------------------------------------------------------------------------
+  -- Snip Runner
+  --------------------------------------------------------------------------------
+  {
+    'jellydn/quick-code-runner.nvim',
+    cond = function()
+      return ar.get_plugin_cond('quick-code-runner.nvim', coding)
+    end,
+    opts = { debug = true },
+    init = function()
+      ar.add_to_select('command_palette', { ['Code Pad'] = 'QuickCodePad' })
+    end,
+    cmd = { 'QuickCodeRunner', 'QuickCodePad' },
+    keys = {
+      {
+        mode = 'v',
+        '<leader>rs',
+        ':QuickCodeRunner<CR>',
+        desc = 'quick-runner: run selection',
+      },
+    },
+  },
+  {
+    {
+      'razak17/lab.nvim',
+      event = { 'InsertEnter' },
+      cmd = { 'Lab' },
+      cond = function()
+        local condition = coding
+          and ar.completion.enable
+          and ar.config.completion.variant == 'cmp'
+        return ar.get_plugin_cond('lab.nvim', condition)
+      end,
+      keys = {
+        { '<leader>rl', ':Lab code run<CR>', desc = 'lab: run' },
+        { '<leader>rq', ':Lab code stop<CR>', desc = 'lab: stop' },
+        -- { '<leader>rp', ':Lab code panel<CR>', desc = 'lab: panel' },
+      },
+      build = 'cd js && npm ci',
+      config = function()
+        highlight.plugin('lab', {
+          theme = {
+            ['onedark'] = {
+              { LabCodeRun = { link = 'DiagnosticVirtualTextInfo' } },
+            },
+          },
+        })
+        require('lab').setup({
+          quick_data = {
+            enabled = ar.completion.enable,
+          },
+        })
+      end,
+    },
+    {
+      'hrsh7th/nvim-cmp',
+      optional = true,
+      opts = function(_, opts)
+        opts = vim.g.cmp_add_source(opts, {
+          source = {
+            name = 'lab.quick_data',
+            priority = 6,
+            max_item_count = 10,
+            group_index = 1,
+          },
+          menu = { ['lab.quick_data'] = '[LAB]' },
+          format = {
+            ['lab.quick_data'] = {
+              icon = ui.icons.misc.beaker,
+              hl = 'CmpItemKindLab',
+            },
+          },
+        })
+      end,
+    },
+  },
+  {
+    'michaelb/sniprun',
+    build = 'sh install.sh',
+    cmd = { 'SnipRun', 'SnipInfo' },
+    cond = function() return ar.get_plugin_cond('sniprun', coding) end,
+    init = function()
+      vim.g.whichkey_add_spec({ '<leader>rs', group = 'SnipRun' })
+    end,
+    keys = {
+      { mode = 'v', '<leader>rr', ':SnipRun<CR>', desc = 'sniprun: run code' },
+      { '<leader>rsr', ':SnipRun<CR>', desc = 'sniprun: run code' },
+      { '<leader>rsi', ':SnipInfo<CR>', desc = 'sniprun: info' },
+      { '<leader>rsc', ':SnipReset<CR>', desc = 'sniprun: reset' },
+      { '<leader>rsq', ':SnipClose<CR>', desc = 'sniprun: close' },
+    },
+    opts = {},
+    config = function(_, opts)
+      highlight.plugin('sniprun', {
+        { SniprunVirtualTextOk = { link = 'DiagnosticVirtualTextInfo' } },
+        { SniprunFloatingWinOk = { link = 'DiagnosticVirtualTextInfo' } },
+        { SniprunVirtualTextErr = { link = 'DiffDelete' } },
+        { SniprunFloatingWinErr = { link = 'DiffDelete' } },
+      })
+      require('sniprun').setup(opts)
+    end,
   },
 }
