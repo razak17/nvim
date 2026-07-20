ac1882dcfb3dee6157f37e97e3bffe953cbec11e
razak17 <moabdulrazak17@gmail.com>
2026-07-20 00:57:30 +0000

    fix(settings): preserve wildignore

--- lua/ar/settings.lua
+++ lua/ar/settings.lua
@@ -142,7 +142,8 @@ local wildignore = {
   '*/.next/*',
   '*/node_modules/*',
 }
-opt.wildignore = vim.tbl_extend('force', wildignore, ui.colorscheme.disabled)
+opt.wildignore =
+  vim.list_extend(vim.deepcopy(wildignore), ui.colorscheme.disabled)
 opt.wildoptions = { 'pum', 'fuzzy' }
 -- NOTE: causes codicons to be rendered funny in cmp window
 o.pumblend = 0 -- Make popup window translucent,
