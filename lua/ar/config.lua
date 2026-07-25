95e2751156edffa4d26fd7c20508609b17de9476
razak17 <moabdulrazak17@gmail.com>
2026-07-25 15:24:27 +0000

    fix(config): fix typo

--- lua/ar/config.lua
+++ lua/ar/config.lua
@@ -33,7 +33,7 @@ local env = vim.env
 ---@alias ArWhichCmdline 'builtin' | 'extui' | 'tiny-cmdline' | 'noice' | 'telescope-cmdline'
 ---@alias ArCompletion 'native' | 'blink' | 'cmp' | 'mini.completion'
 ---@alias ArCompletionIcons 'lspkind' | 'mini.icons'
----@alias ArWhichIntro 'builtin' | 'legacy' | | 'alpha' | 'snacks' | 'startup' | 'btw'
+---@alias ArWhichIntro 'builtin' | 'legacy' | 'alpha' | 'snacks' | 'startup' | 'btw'
 ---@alias ArWhichExplorer 'builtin' | 'neo-tree' | 'snacks' | 'mini.files' | 'oil' | 'fyler'
 ---@alias ArWhichGx 'custom' | 'gx.nvim'
 ---@alias ArWhichIcon 'nvim-web-devicons' | 'mini.icons'
