e65348c05a9037c27bb637b9f23e45f1ef532627
razak17 <moabdulrazak17@gmail.com>
2026-07-29 20:45:05 +0000

    feat(help): add option to open help in floating buffer

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -8,14 +8,29 @@ opt.textwidth = 78
 
 local fn, api = vim.fn, vim.api
 
+local config = {
+  float = { enable = false },
+  vertical = { size = 90 },
+}
+
 local opts = { buffer = 0 }
 -- if this a vim help file create mappings to make navigation easier otherwise enable preferred editing settings
 if vim.startswith(fn.expand('%'), vim.env.VIMRUNTIME) or vim.bo.readonly then
   opt.spell = false
-  api.nvim_create_autocmd(
-    'BufWinEnter',
-    { buffer = 0, command = 'wincmd L | vertical resize 80' }
-  )
+  api.nvim_create_autocmd('BufWinEnter', {
+    buffer = 0,
+    callback = function(args)
+      if config.float.enable then
+        local source_win = api.nvim_get_current_win()
+        ar.open_buf_centered_popup(args.buf)
+        api.nvim_win_close(source_win, true)
+        return
+      end
+      vim.cmd(
+        string.format('wincmd L | vertical resize %d', config.vertical.size)
+      )
+    end,
+  })
   -- https://vim.fandom.com/wiki/Learn_to_use_help
   map('n', '<CR>', '<C-]>', opts)
   map('n', '<BS>', '<C-T>', opts)
