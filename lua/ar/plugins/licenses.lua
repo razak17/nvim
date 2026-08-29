33214615bc430e458185a2415a1843fc799e0178
Abdul Razak <moabdulrazak17@gmail.com>
2026-08-29 02:18:49 +0000

    feat(software-licenses): lazy load on keymap

--- lua/ar/plugins/licenses.lua
+++ lua/ar/plugins/licenses.lua
@@ -6,54 +6,36 @@ then
 end
 
 return {
+  'razak17/software-licenses.nvim',
   {
     'folke/snacks.nvim',
     optional = true,
-    dependencies = { -- this will only be evaluated if snacks is enabled
-      'razak17/software-licenses.nvim',
-      cond = ar.get_plugin_cond('software-licenses.nvim'),
-      keys = function()
-        if ar.config.picker.variant == 'snacks' then
-          return {
-            {
-              '<leader>fL',
-              '<Cmd>SoftwareLicenses snacks<CR>',
-              desc = 'software licenses',
-            },
-          }
-        end
-      end,
-    },
+    keys = function()
+      if ar.config.picker.variant == 'snacks' then
+        return {
+          {
+            '<leader>fL',
+            function() require('software_licenses').command('snacks') end,
+            desc = 'software licenses',
+          },
+        }
+      end
+    end,
   },
   {
     'nvim-telescope/telescope.nvim',
     optional = true,
-    dependencies = {
-      {
-        'razak17/software-licenses.nvim',
-        cond = ar.get_plugin_cond('software-licenses.nvim'),
-        keys = function()
-          if ar.config.picker.variant == 'telescope' then
-            local function licenses()
-              require('telescope').extensions.software_licenses.find(
-                ar.telescope.horizontal()
-              )
-            end
-            return {
-              { '<leader>fL', licenses, desc = 'software licenses' },
-            }
-          end
-        end,
-        specs = {
-          'nvim-telescope/telescope.nvim',
-          optional = true,
-          opts = function(_, opts)
-            if ar.config.picker.variant == 'telescope' then
-              vim.g.telescope_add_extension({ 'software_licenses' }, opts)
-            end
-          end,
-        },
-      },
-    },
+    keys = function()
+      if ar.config.picker.variant == 'telescope' then
+        local function licenses()
+          require('telescope').extensions.software_licenses.find(
+            ar.telescope.horizontal()
+          )
+        end
+        return {
+          { '<leader>fL', licenses, desc = 'software licenses' },
+        }
+      end
+    end,
   },
 }
